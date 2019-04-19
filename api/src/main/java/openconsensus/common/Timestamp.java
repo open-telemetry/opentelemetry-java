f00c726bd3033affe8bbfee140cb040b2314e751
Bogdan Drutu <bogdandrutu@gmail.com>
2019-04-19 15:08:52 -0700

    Remove Duration and all unused methods from Timestamp.

--- api/src/main/java/openconsensus/common/Timestamp.java
+++ api/src/main/java/openconsensus/common/Timestamp.java
@@ -16,12 +16,6 @@
 
 package openconsensus.common;
 
-import static openconsensus.common.TimeUtils.MAX_NANOS;
-import static openconsensus.common.TimeUtils.MAX_SECONDS;
-import static openconsensus.common.TimeUtils.MILLIS_PER_SECOND;
-import static openconsensus.common.TimeUtils.NANOS_PER_MILLI;
-import static openconsensus.common.TimeUtils.NANOS_PER_SECOND;
-
 import com.google.auto.value.AutoValue;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -35,7 +29,11 @@ import javax.annotation.concurrent.Immutable;
  */
 @Immutable
 @AutoValue
-public abstract class Timestamp implements Comparable<Timestamp> {
+public abstract class Timestamp {
+  private static final long MAX_SECONDS = 315576000000L;
+  private static final int MAX_NANOS = 999999999;
+  private static final long MILLIS_PER_SECOND = 1000L;
+  private static final long NANOS_PER_MILLI = 1000 * 1000;
 
   Timestamp() {}
 
@@ -101,89 +99,6 @@ public abstract class Timestamp implements Comparable<Timestamp> {
    * @since 0.1.0
    */
   public abstract int getNanos();
-
-  /**
-   * Returns a {@code Timestamp} calculated as this {@code Timestamp} plus some number of
-   * nanoseconds.
-   *
-   * @param nanosToAdd the nanos to add, positive or negative.
-   * @return the calculated {@code Timestamp}. For invalid inputs, a {@code Timestamp} of zero is
-   *     returned.
-   * @throws ArithmeticException if numeric overflow occurs.
-   * @since 0.1.0
-   */
-  public Timestamp addNanos(long nanosToAdd) {
-    return plus(0, nanosToAdd);
-  }
-
-  /**
-   * Returns a {@code Timestamp} calculated as this {@code Timestamp} plus some {@code Duration}.
-   *
-   * @param duration the {@code Duration} to add.
-   * @return a {@code Timestamp} with the specified {@code Duration} added.
-   * @since 0.1.0
-   */
-  public Timestamp addDuration(Duration duration) {
-    return plus(duration.getSeconds(), duration.getNanos());
-  }
-
-  /**
-   * Returns a {@link Duration} calculated as: {@code this - timestamp}.
-   *
-   * @param timestamp the {@code Timestamp} to subtract.
-   * @return the calculated {@code Duration}. For invalid inputs, a {@code Duration} of zero is
-   *     returned.
-   * @since 0.1.0
-   */
-  public Duration subtractTimestamp(Timestamp timestamp) {
-    long durationSeconds = getSeconds() - timestamp.getSeconds();
-    int durationNanos = getNanos() - timestamp.getNanos();
-    if (durationSeconds < 0 && durationNanos > 0) {
-      durationSeconds += 1;
-      durationNanos = (int) (durationNanos - NANOS_PER_SECOND);
-    } else if (durationSeconds > 0 && durationNanos < 0) {
-      durationSeconds -= 1;
-      durationNanos = (int) (durationNanos + NANOS_PER_SECOND);
-    }
-    return Duration.create(durationSeconds, durationNanos);
-  }
-
-  /**
-   * Compares this {@code Timestamp} to the specified {@code Timestamp}.
-   *
-   * @param otherTimestamp the other {@code Timestamp} to compare to, not {@code null}.
-   * @return the comparator value: zero if equal, negative if this timestamp happens before
-   *     otherTimestamp, positive if after.
-   * @throws NullPointerException if otherTimestamp is {@code null}.
-   */
-  @Override
-  public int compareTo(Timestamp otherTimestamp) {
-    int cmp = TimeUtils.compareLongs(getSeconds(), otherTimestamp.getSeconds());
-    if (cmp != 0) {
-      return cmp;
-    }
-    return TimeUtils.compareLongs(getNanos(), otherTimestamp.getNanos());
-  }
-
-  // Returns a Timestamp with the specified duration added.
-  private Timestamp plus(long secondsToAdd, long nanosToAdd) {
-    if ((secondsToAdd | nanosToAdd) == 0) {
-      return this;
-    }
-    long epochSec = TimeUtils.checkedAdd(getSeconds(), secondsToAdd);
-    epochSec = TimeUtils.checkedAdd(epochSec, nanosToAdd / NANOS_PER_SECOND);
-    nanosToAdd = nanosToAdd % NANOS_PER_SECOND;
-    long nanoAdjustment = getNanos() + nanosToAdd; // safe int + NANOS_PER_SECOND
-    return ofEpochSecond(epochSec, nanoAdjustment);
-  }
-
-  // Returns a Timestamp calculated using seconds from the epoch and nanosecond fraction of
-  // second (arbitrary number of nanoseconds).
-  private static Timestamp ofEpochSecond(long epochSecond, long nanoAdjustment) {
-    long secs = TimeUtils.checkedAdd(epochSecond, floorDiv(nanoAdjustment, NANOS_PER_SECOND));
-    int nos = (int) floorMod(nanoAdjustment, NANOS_PER_SECOND);
-    return create(secs, nos);
-  }
 
   // Returns the result of dividing x by y rounded using floor.
   private static long floorDiv(long x, long y) {
