127e206d5edfcc19720c11cd9078df724324bbe5
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2019-04-24 18:32:17 +0200

    Clean some inline bits.

--- api/src/main/java/openconsensus/context/propagation/HttpTextFormat.java
+++ api/src/main/java/openconsensus/context/propagation/HttpTextFormat.java
@@ -21,10 +21,10 @@ import javax.annotation.Nullable;
 
 /**
  * Injects and extracts a value as text into carriers that travel in-band across process boundaries.
- * Encoding is expected to conform to the HTTP/1.1 Header Field semantics. Values are often encoded
+ * Encoding is expected to conform to the HTTP Header Field semantics. Values are often encoded
  * as RPC/HTTP request headers.
  *
- * <p>When using http, the carrier of propagated data on both the client (injector) and server
+ * <p>The carrier of propagated data on both the client (injector) and server
  * (extractor) side is usually an http request. Propagation is usually implemented via library-
  * specific request interceptors, where the client-side injects values and the server-side extracts
  * them.
