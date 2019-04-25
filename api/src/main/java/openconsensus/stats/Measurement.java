bc7d35d05c154eb2cfeda8cea026c2c93518476a
Bogdan Drutu <bogdandrutu@gmail.com>
2019-04-25 16:34:08 -0700

    Make Measurement an opaque interface in the API.

--- api/src/main/java/openconsensus/stats/Measurement.java
+++ api/src/main/java/openconsensus/stats/Measurement.java
@@ -16,105 +16,12 @@
 
 package openconsensus.stats;
 
-import com.google.auto.value.AutoValue;
 import javax.annotation.concurrent.Immutable;
 
 /**
- * Immutable representation of a Measurement.
+ * Immutable representation of a {@code Measurement}.
  *
  * @since 0.1.0
  */
 @Immutable
-public abstract class Measurement {
-
-  /**
-   * Extracts the measured {@link Measure}.
-   *
-   * @return the {@link Measure} if this measurement.
-   * @since 0.1.0
-   */
-  public abstract Measure getMeasure();
-
-  /**
-   * Returns the double value for the {@link Measurement}.
-   *
-   * <p>This method should only be called with {@link MeasurementDouble}.
-   *
-   * @return the double value.
-   * @throws UnsupportedOperationException if the {@code Measure} type is not {@link
-   *     Measure.Type#DOUBLE}.
-   * @since 0.1.0
-   */
-  public double getDoubleValue() {
-    throw new UnsupportedOperationException("This type can only return double data");
-  }
-
-  /**
-   * Returns the long value for the {@link Measurement}.
-   *
-   * <p>This method should only be called with {@link MeasurementLong}.
-   *
-   * @return the long value.
-   * @throws UnsupportedOperationException if the {@code Measure} type is not {@link
-   *     Measure.Type#LONG}.
-   * @since 0.1.0
-   */
-  public long getLongValue() {
-    throw new UnsupportedOperationException("This type can only return long data");
-  }
-
-  // Prevents this class from being subclassed anywhere else.
-  private Measurement() {}
-
-  /**
-   * {@code double} typed {@link Measurement}.
-   *
-   * @since 0.1.0
-   */
-  @Immutable
-  @AutoValue
-  public abstract static class MeasurementDouble extends Measurement {
-    MeasurementDouble() {}
-
-    /**
-     * Constructs a new {@link MeasurementDouble}.
-     *
-     * @since 0.1.0
-     */
-    public static MeasurementDouble create(Measure measure, double value) {
-      return new AutoValue_Measurement_MeasurementDouble(measure, value);
-    }
-
-    @Override
-    public abstract Measure getMeasure();
-
-    @Override
-    public abstract double getDoubleValue();
-  }
-
-  /**
-   * {@code long} typed {@link Measurement}.
-   *
-   * @since 0.1.0
-   */
-  @Immutable
-  @AutoValue
-  public abstract static class MeasurementLong extends Measurement {
-    MeasurementLong() {}
-
-    /**
-     * Constructs a new {@link MeasurementLong}.
-     *
-     * @since 0.1.0
-     */
-    public static MeasurementLong create(Measure measure, long value) {
-      return new AutoValue_Measurement_MeasurementLong(measure, value);
-    }
-
-    @Override
-    public abstract Measure getMeasure();
-
-    @Override
-    public abstract long getLongValue();
-  }
-}
+public interface Measurement {}
