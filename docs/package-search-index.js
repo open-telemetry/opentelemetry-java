038e36f1f425a0852637d1c7fcedeafd7d4351e8
Bogdan Drutu <bogdandrutu@gmail.com>
2019-05-03 08:04:42 -0700

    Update javadoc. (#240)

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"io.opentelemetry.context"},{"l":"io.opentelemetry.context.propagation"},{"l":"io.opentelemetry.metrics"},{"l":"io.opentelemetry.resource"},{"l":"io.opentelemetry.stats"},{"l":"io.opentelemetry.tags"},{"l":"io.opentelemetry.tags.unsafe"},{"l":"io.opentelemetry.trace"},{"l":"io.opentelemetry.trace.samplers"},{"l":"io.opentelemetry.trace.unsafe"}]
