b7e4bc86ec42a3069413dd76b01e3162a816c538
Pavol Loffay <ploffay@redhat.com>
2019-05-24 16:50:38 +0200

    Move SDK stats to metrics (#346)
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- sdk/src/main/java/io/opentelemetry/sdk/metrics/stats/View.java
+++ sdk/src/main/java/io/opentelemetry/sdk/metrics/stats/View.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.opentelemetry.sdk.stats;
+package io.opentelemetry.sdk.metrics.stats;
 
 import com.google.auto.value.AutoValue;
 import io.opentelemetry.internal.StringUtils;
--- sdk/src/main/java/io/opentelemetry/sdk/metrics/stats/ViewManager.java
+++ sdk/src/main/java/io/opentelemetry/sdk/metrics/stats/ViewManager.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.opentelemetry.sdk.stats;
+package io.opentelemetry.sdk.metrics.stats;
 
 import java.util.List;
 
