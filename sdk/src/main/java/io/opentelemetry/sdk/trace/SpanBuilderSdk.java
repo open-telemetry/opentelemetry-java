dee22be301cde30cba4cc1001e8bdba942c9a339
Pavol Loffay <ploffay@redhat.com>
2019-06-06 14:14:31 +0200

    Add span builder to SDK
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

---
+++ sdk/src/main/java/io/opentelemetry/sdk/trace/SpanBuilderSdk.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.sdk.trace;
+
+import io.opentelemetry.internal.Utils;
+import io.opentelemetry.trace.Link;
+import io.opentelemetry.trace.Sampler;
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.Span.Builder;
+import io.opentelemetry.trace.Span.Kind;
+import io.opentelemetry.trace.SpanContext;
+import java.util.ArrayList;
+import java.util.List;
+
+/** {@link SpanBuilderSdk} is SDK implementation of {@link Span.Builder}. */
+class SpanBuilderSdk implements Span.Builder {
+
+  private SpanContext parent;
+  private Kind spanKind;
+  private List<Link> links;
+  private boolean recordEvents;
+  private Sampler sampler;
+  private final String spanName;
+  private boolean noParent;
+
+  SpanBuilderSdk(String spanName) {
+    this.spanName = spanName;
+  }
+
+  @Override
+  public Builder setParent(Span parent) {
+    Utils.checkNotNull(parent, "parent");
+    this.parent = parent.getContext();
+    this.noParent = false;
+    return this;
+  }
+
+  @Override
+  public Builder setParent(SpanContext remoteParent) {
+    this.parent = Utils.checkNotNull(remoteParent, "remoteParent");
+    this.noParent = false;
+    return this;
+  }
+
+  @Override
+  public Builder setNoParent() {
+    this.noParent = true;
+    this.parent = null;
+    return this;
+  }
+
+  @Override
+  public Builder setSpanKind(Kind spanKind) {
+    this.spanKind = Utils.checkNotNull(spanKind, "spanKind");
+    return this;
+  }
+
+  @Override
+  public Builder setSampler(Sampler sampler) {
+    this.sampler = Utils.checkNotNull(sampler, "sampler");
+    return this;
+  }
+
+  @Override
+  public Builder addLink(Link link) {
+    Utils.checkNotNull(link, "link");
+    if (links == null) {
+      links = new ArrayList<>();
+    }
+    links.add(link);
+    return this;
+  }
+
+  @Override
+  public Builder addLinks(List<Link> links) {
+    Utils.checkNotNull(links, "links");
+    if (this.links == null) {
+      this.links = new ArrayList<>();
+    }
+    this.links.addAll(links);
+    return this;
+  }
+
+  @Override
+  public Builder setRecordEvents(boolean recordEvents) {
+    this.recordEvents = recordEvents;
+    return this;
+  }
+
+  @Override
+  public SpanSdk startSpan() {
+    // TODO get parent span from the context if noParent=false and parent/remoteParents are null
+    return null;
+  }
+}
---
+++ sdk/src/test/java/io/opentelemetry/sdk/trace/SpanBuilderSdkTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.sdk.trace;
+
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.SpanContext;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Unit tests for {@link SpanBuilderSdk}. */
+@RunWith(JUnit4.class)
+public class SpanBuilderSdkTest {
+  private static final String SPAN_NAME = "span_name";
+  private final TracerSdk tracer = new TracerSdk();
+
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void setSpanKind_null() {
+    thrown.expect(NullPointerException.class);
+    tracer.spanBuilder(SPAN_NAME).setSpanKind(null);
+  }
+
+  @Test
+  public void setSampler_null() {
+    thrown.expect(NullPointerException.class);
+    tracer.spanBuilder(SPAN_NAME).setSampler(null);
+  }
+
+  @Test
+  public void setParent_null() {
+    thrown.expect(NullPointerException.class);
+    tracer.spanBuilder(SPAN_NAME).setParent((Span) null);
+  }
+
+  @Test
+  public void setRemoteParent_null() {
+    thrown.expect(NullPointerException.class);
+    tracer.spanBuilder(SPAN_NAME).setParent((SpanContext) null);
+  }
+
+  @Test
+  public void addLink_null() {
+    thrown.expect(NullPointerException.class);
+    tracer.spanBuilder(SPAN_NAME).addLink(null);
+  }
+
+  @Test
+  public void addLinks_null() {
+    thrown.expect(NullPointerException.class);
+    tracer.spanBuilder(SPAN_NAME).addLinks(null);
+  }
+}
