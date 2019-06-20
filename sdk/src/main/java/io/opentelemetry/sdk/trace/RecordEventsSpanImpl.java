7d42e11df2536471ec0f8a4a72429ea64b3b472c
Yang Song <songy23@users.noreply.github.com>
2019-06-19 18:16:09 -0700

    Add RecordEventsSpanImpl to SDK. (#362)
    * Add RecordEventsSpanImpl to SDK.
    
    * Add toSpanProto.
    
    * StartEndHandler -> SpanProcessor. Remove sampleToLocalSpanStore. Fix events.
    
    * Remove ConcurrentIntrusiveList.
    
    * Replace TraceEvents<E> with EvictQueue + total count.
    
    * Move proto utils to another class.
    
    * Remove TraceProtoUtils from this PR.
    
    * Remove duplication in tests.
    
    * Remove hasRemoteParent to match proto changes.
    
    * Fix merge error.
    
    * Remove toSpanData. Reduce visibility.
    
    * Use Link interface as types.

---
+++ sdk/src/main/java/io/opentelemetry/sdk/trace/RecordEventsSpanImpl.java
@@ -0,0 +1,456 @@
+/*
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.sdk.trace;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.EvictingQueue;
+import io.opentelemetry.resources.Resource;
+import io.opentelemetry.sdk.internal.Clock;
+import io.opentelemetry.sdk.internal.TimestampConverter;
+import io.opentelemetry.sdk.trace.config.TraceConfig;
+import io.opentelemetry.trace.AttributeValue;
+import io.opentelemetry.trace.Event;
+import io.opentelemetry.trace.Link;
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.SpanContext;
+import io.opentelemetry.trace.SpanData;
+import io.opentelemetry.trace.SpanId;
+import io.opentelemetry.trace.Status;
+import io.opentelemetry.trace.Tracer;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
+
+/** Implementation for the {@link Span} class that records trace events. */
+@ThreadSafe
+final class RecordEventsSpanImpl implements SpanSdk {
+  private static final Logger logger = Logger.getLogger(Tracer.class.getName());
+
+  // Contains the identifiers associated with this Span.
+  private final SpanContext context;
+  // The parent SpanId of this span. Null if this is a root span.
+  @Nullable private final SpanId parentSpanId;
+  // Active trace configs when the Span was created.
+  private final TraceConfig traceConfig;
+  // Handler called when the span starts and ends.
+  private final SpanProcessor spanProcessor;
+  // The displayed name of the span.
+  @GuardedBy("this")
+  private String name;
+  // The kind of the span.
+  private final Kind kind;
+  // The clock used to get the time.
+  private final Clock clock;
+  // The time converter used to convert nano time to Timestamp. This is needed because Java has
+  // millisecond granularity for Timestamp and tracing events are recorded more often.
+  private final TimestampConverter timestampConverter;
+  // The resource associated with this span.
+  private final Resource resource;
+  // The start time of the span.
+  private final long startNanoTime;
+  // Set of recorded attributes. DO NOT CALL any other method that changes the ordering of events.
+  @GuardedBy("this")
+  @Nullable
+  private AttributesWithCapacity attributes;
+  // List of recorded events.
+  @GuardedBy("this")
+  @Nullable
+  private EvictingQueue<TimedEvent> events;
+  // Number of events recorded.
+  @GuardedBy("this")
+  private int totalRecordedEvents = 0;
+  // List of recorded links to parent and child spans.
+  @GuardedBy("this")
+  @Nullable
+  private EvictingQueue<Link> links;
+  // Number of link recorded.
+  @GuardedBy("this")
+  private int totalRecordedLinks = 0;
+  // The number of children.
+  @GuardedBy("this")
+  private int numberOfChildren;
+  // The status of the span.
+  @GuardedBy("this")
+  @Nullable
+  private Status status;
+  // The end time of the span.
+  @GuardedBy("this")
+  private long endNanoTime;
+  // True if the span is ended.
+  @GuardedBy("this")
+  private boolean hasBeenEnded;
+
+  /**
+   * Creates and starts a span with the given configuration.
+   *
+   * @param context supplies the trace_id and span_id for the newly started span.
+   * @param name the displayed name for the new span.
+   * @param parentSpanId the span_id of the parent span, or null if the new span is a root span.
+   * @param traceConfig trace parameters like sampler and probability.
+   * @param spanProcessor handler called when the span starts and ends.
+   * @param timestampConverter null if the span is a root span or the parent is not sampled. If the
+   *     parent is sampled, we should use the same converter to ensure ordering between tracing
+   *     events.
+   * @param clock the clock used to get the time.
+   * @return a new and started span.
+   */
+  @VisibleForTesting
+  static RecordEventsSpanImpl startSpan(
+      SpanContext context,
+      String name,
+      Kind kind,
+      @Nullable SpanId parentSpanId,
+      TraceConfig traceConfig,
+      SpanProcessor spanProcessor,
+      @Nullable TimestampConverter timestampConverter,
+      Clock clock,
+      Resource resource) {
+    RecordEventsSpanImpl span =
+        new RecordEventsSpanImpl(
+            context,
+            name,
+            kind,
+            parentSpanId,
+            traceConfig,
+            spanProcessor,
+            timestampConverter,
+            clock,
+            resource);
+    // Call onStart here instead of calling in the constructor to make sure the span is completely
+    // initialized.
+    spanProcessor.onStartSync(span);
+    return span;
+  }
+
+  /**
+   * Returns the name of the {@code Span}.
+   *
+   * @return the name of the {@code Span}.
+   */
+  @Override
+  public String getName() {
+    synchronized (this) {
+      return name;
+    }
+  }
+
+  @Override
+  public io.opentelemetry.proto.trace.v1.Span toSpanProto() {
+    throw new UnsupportedOperationException("to be implemented");
+  }
+
+  /**
+   * Returns the status of the {@code Span}. If not set defaults to {@link Status#OK}.
+   *
+   * @return the status of the {@code Span}.
+   */
+  @VisibleForTesting
+  Status getStatus() {
+    synchronized (this) {
+      return getStatusWithDefault();
+    }
+  }
+
+  /**
+   * Returns the end nano time (see {@link System#nanoTime()}). If the current {@code Span} is not
+   * ended then returns {@link Clock#nowNanos()}.
+   *
+   * @return the end nano time.
+   */
+  long getEndNanoTime() {
+    synchronized (this) {
+      return hasBeenEnded ? endNanoTime : clock.nowNanos();
+    }
+  }
+
+  /**
+   * Returns the latency of the {@code Span} in nanos. If still active then returns now() - start
+   * time.
+   *
+   * @return the latency of the {@code Span} in nanos.
+   */
+  long getLatencyNs() {
+    synchronized (this) {
+      return hasBeenEnded ? endNanoTime - startNanoTime : clock.nowNanos() - startNanoTime;
+    }
+  }
+
+  /**
+   * Returns the kind of this {@code Span}.
+   *
+   * @return the kind of this {@code Span}.
+   */
+  @VisibleForTesting
+  Kind getKind() {
+    return kind;
+  }
+
+  /**
+   * Returns the {@code TimestampConverter} used by this {@code Span}.
+   *
+   * @return the {@code TimestampConverter} used by this {@code Span}.
+   */
+  @Nullable
+  TimestampConverter getTimestampConverter() {
+    return timestampConverter;
+  }
+
+  @Override
+  public void setAttribute(String key, String value) {
+    setAttribute(key, AttributeValue.stringAttributeValue(value));
+  }
+
+  @Override
+  public void setAttribute(String key, long value) {
+    setAttribute(key, AttributeValue.longAttributeValue(value));
+  }
+
+  @Override
+  public void setAttribute(String key, double value) {
+    setAttribute(key, AttributeValue.doubleAttributeValue(value));
+  }
+
+  @Override
+  public void setAttribute(String key, boolean value) {
+    setAttribute(key, AttributeValue.booleanAttributeValue(value));
+  }
+
+  @Override
+  public void setAttribute(String key, AttributeValue value) {
+    Preconditions.checkNotNull(key, "key");
+    Preconditions.checkNotNull(value, "value");
+    synchronized (this) {
+      if (hasBeenEnded) {
+        logger.log(Level.FINE, "Calling setAttribute() on an ended Span.");
+        return;
+      }
+      getInitializedAttributes().putAttribute(key, value);
+    }
+  }
+
+  @Override
+  public void addEvent(String name) {
+    addTimedEvent(TimedEvent.create(clock.nowNanos(), name));
+  }
+
+  @Override
+  public void addEvent(String name, Map<String, AttributeValue> attributes) {
+    addTimedEvent(TimedEvent.create(clock.nowNanos(), name, attributes));
+  }
+
+  @Override
+  public void addEvent(Event event) {
+    addTimedEvent(TimedEvent.create(clock.nowNanos(), event));
+  }
+
+  private void addTimedEvent(TimedEvent timedEvent) {
+    synchronized (this) {
+      if (hasBeenEnded) {
+        logger.log(Level.FINE, "Calling addEvent() on an ended Span.");
+        return;
+      }
+      getInitializedEvents().add(timedEvent);
+      totalRecordedEvents++;
+    }
+  }
+
+  @Override
+  public void addLink(Link link) {
+    Preconditions.checkNotNull(link, "link");
+    addLinkInternal(SpanData.Link.create(link.getContext(), link.getAttributes()));
+  }
+
+  @Override
+  public void addLink(SpanContext spanContext) {
+    addLinkInternal(SpanData.Link.create(spanContext));
+  }
+
+  @Override
+  public void addLink(SpanContext spanContext, Map<String, AttributeValue> attributes) {
+    addLinkInternal(SpanData.Link.create(spanContext, attributes));
+  }
+
+  private void addLinkInternal(Link link) {
+    synchronized (this) {
+      if (hasBeenEnded) {
+        logger.log(Level.FINE, "Calling addLink() on an ended Span.");
+        return;
+      }
+      getInitializedLinks().add(link);
+      totalRecordedLinks++;
+    }
+  }
+
+  @Override
+  public void setStatus(Status status) {
+    Preconditions.checkNotNull(status, "status");
+    synchronized (this) {
+      if (hasBeenEnded) {
+        logger.log(Level.FINE, "Calling setStatus() on an ended Span.");
+        return;
+      }
+      this.status = status;
+    }
+  }
+
+  @Override
+  public void updateName(String name) {
+    Preconditions.checkNotNull(name, "name");
+    synchronized (this) {
+      this.name = name;
+    }
+  }
+
+  @Override
+  public void end() {
+    synchronized (this) {
+      if (hasBeenEnded) {
+        logger.log(Level.FINE, "Calling end() on an ended Span.");
+        return;
+      }
+      endNanoTime = clock.nowNanos();
+      hasBeenEnded = true;
+    }
+    spanProcessor.onEndSync(this);
+  }
+
+  @Override
+  public SpanContext getContext() {
+    return context;
+  }
+
+  @Override
+  public boolean isRecordingEvents() {
+    return true;
+  }
+
+  void addChild() {
+    synchronized (this) {
+      if (hasBeenEnded) {
+        logger.log(Level.FINE, "Calling end() on an ended Span.");
+        return;
+      }
+      numberOfChildren++;
+    }
+  }
+
+  @GuardedBy("this")
+  private AttributesWithCapacity getInitializedAttributes() {
+    if (attributes == null) {
+      attributes = new AttributesWithCapacity(traceConfig.getMaxNumberOfAttributes());
+    }
+    return attributes;
+  }
+
+  @GuardedBy("this")
+  private EvictingQueue<TimedEvent> getInitializedEvents() {
+    if (events == null) {
+      events = EvictingQueue.<TimedEvent>create((int) traceConfig.getMaxNumberOfEvents());
+    }
+    return events;
+  }
+
+  @GuardedBy("this")
+  private EvictingQueue<Link> getInitializedLinks() {
+    if (links == null) {
+      links = EvictingQueue.<Link>create((int) traceConfig.getMaxNumberOfLinks());
+    }
+    return links;
+  }
+
+  @GuardedBy("this")
+  private Status getStatusWithDefault() {
+    return status == null ? Status.OK : status;
+  }
+
+  // A map implementation with a fixed capacity that drops events when the map gets full. Eviction
+  // is based on the access order.
+  static final class AttributesWithCapacity extends LinkedHashMap<String, AttributeValue> {
+    private final long capacity;
+    private int totalRecordedAttributes = 0;
+    // Here because -Werror complains about this: [serial] serializable class AttributesWithCapacity
+    // has no definition of serialVersionUID. This class shouldn't be serialized.
+    private static final long serialVersionUID = 42L;
+
+    private AttributesWithCapacity(long capacity) {
+      // Capacity of the map is capacity + 1 to avoid resizing because removeEldestEntry is invoked
+      // by put and putAll after inserting a new entry into the map. The loadFactor is set to 1
+      // to avoid resizing because. The accessOrder is set to true.
+      super((int) capacity + 1, 1, /*accessOrder=*/ true);
+      this.capacity = capacity;
+    }
+
+    // Users must call this method instead of put to keep count of the total number of entries
+    // inserted.
+    private void putAttribute(String key, AttributeValue value) {
+      totalRecordedAttributes += 1;
+      put(key, value);
+    }
+
+    int getNumberOfDroppedAttributes() {
+      return totalRecordedAttributes - size();
+    }
+
+    // It is called after each put or putAll call in order to determine if the eldest inserted
+    // entry should be removed or not.
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<String, AttributeValue> eldest) {
+      return size() > this.capacity;
+    }
+  }
+
+  private RecordEventsSpanImpl(
+      SpanContext context,
+      String name,
+      Kind kind,
+      @Nullable SpanId parentSpanId,
+      TraceConfig traceConfig,
+      SpanProcessor spanProcessor,
+      @Nullable TimestampConverter timestampConverter,
+      Clock clock,
+      Resource resource) {
+    this.context = context;
+    this.parentSpanId = parentSpanId;
+    this.name = name;
+    this.kind = kind;
+    this.traceConfig = traceConfig;
+    this.spanProcessor = spanProcessor;
+    this.clock = clock;
+    this.resource = resource;
+    this.hasBeenEnded = false;
+    this.numberOfChildren = 0;
+    this.timestampConverter =
+        timestampConverter != null ? timestampConverter : TimestampConverter.now(clock);
+    startNanoTime = clock.nowNanos();
+  }
+
+  @SuppressWarnings("NoFinalizer")
+  @Override
+  protected void finalize() throws Throwable {
+    synchronized (this) {
+      if (!hasBeenEnded) {
+        logger.log(Level.SEVERE, "Span " + name + " is GC'ed without being ended.");
+      }
+    }
+    super.finalize();
+  }
+}
---
+++ sdk/src/test/java/io/opentelemetry/sdk/trace/RecordEventsSpanImplTest.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.sdk.trace;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.protobuf.Timestamp;
+import io.opentelemetry.resources.Resource;
+import io.opentelemetry.sdk.internal.TestClock;
+import io.opentelemetry.sdk.internal.TimestampConverter;
+import io.opentelemetry.sdk.trace.config.TraceConfig;
+import io.opentelemetry.trace.AttributeValue;
+import io.opentelemetry.trace.Event;
+import io.opentelemetry.trace.Link;
+import io.opentelemetry.trace.Span.Kind;
+import io.opentelemetry.trace.SpanContext;
+import io.opentelemetry.trace.SpanData;
+import io.opentelemetry.trace.SpanId;
+import io.opentelemetry.trace.Status;
+import io.opentelemetry.trace.TraceId;
+import io.opentelemetry.trace.TraceOptions;
+import io.opentelemetry.trace.Tracestate;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+/** Unit tests for {@link RecordEventsSpanImpl}. */
+@RunWith(JUnit4.class)
+public class RecordEventsSpanImplTest {
+  private static final String SPAN_NAME = "MySpanName";
+  private final TraceId traceId = TestUtils.generateRandomTraceId();
+  private final SpanId spanId = TestUtils.generateRandomSpanId();
+  private final SpanId parentSpanId = TestUtils.generateRandomSpanId();
+  private final SpanContext spanContext =
+      SpanContext.create(traceId, spanId, TraceOptions.getDefault(), Tracestate.getDefault());
+  private final Timestamp timestamp = Timestamp.newBuilder().setSeconds(1000).build();
+  private final TestClock testClock = TestClock.create(timestamp);
+  private final TimestampConverter timestampConverter = TimestampConverter.now(testClock);
+  private final Resource resource = Resource.getEmpty();
+  private final Map<String, AttributeValue> attributes = new HashMap<String, AttributeValue>();
+  private final Map<String, AttributeValue> expectedAttributes =
+      new HashMap<String, AttributeValue>();
+  @Mock private SpanProcessor spanProcessor;
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    attributes.put(
+        "MyStringAttributeKey", AttributeValue.stringAttributeValue("MyStringAttributeValue"));
+    attributes.put("MyLongAttributeKey", AttributeValue.longAttributeValue(123L));
+    attributes.put("MyBooleanAttributeKey", AttributeValue.booleanAttributeValue(false));
+    expectedAttributes.putAll(attributes);
+    expectedAttributes.put(
+        "MySingleStringAttributeKey",
+        AttributeValue.stringAttributeValue("MySingleStringAttributeValue"));
+  }
+
+  @Test
+  public void noEventsRecordedAfterEnd() {
+    RecordEventsSpanImpl span =
+        RecordEventsSpanImpl.startSpan(
+            spanContext,
+            SPAN_NAME,
+            Kind.INTERNAL,
+            parentSpanId,
+            TraceConfig.DEFAULT,
+            spanProcessor,
+            timestampConverter,
+            testClock,
+            resource);
+    span.end();
+    // Check that adding trace events after Span#end() does not throw any thrown and are not
+    // recorded.
+    span.setAttribute(
+        "MySingleStringAttributeKey",
+        AttributeValue.stringAttributeValue("MySingleStringAttributeValue"));
+    span.addEvent("event");
+    span.addLink(
+        SpanData.Link.create(
+            SpanContext.create(
+                TraceId.getInvalid(),
+                SpanId.getInvalid(),
+                TraceOptions.getDefault(),
+                Tracestate.getDefault())));
+  }
+
+  @Test
+  public void toSpanData_EndedSpan() {
+    RecordEventsSpanImpl span =
+        RecordEventsSpanImpl.startSpan(
+            spanContext,
+            SPAN_NAME,
+            Kind.INTERNAL,
+            parentSpanId,
+            TraceConfig.DEFAULT,
+            spanProcessor,
+            timestampConverter,
+            testClock,
+            resource);
+    Mockito.verify(spanProcessor, Mockito.times(1)).onStartSync(span);
+    span.setAttribute(
+        "MySingleStringAttributeKey",
+        AttributeValue.stringAttributeValue("MySingleStringAttributeValue"));
+    for (Map.Entry<String, AttributeValue> attribute : attributes.entrySet()) {
+      span.setAttribute(attribute.getKey(), attribute.getValue());
+    }
+    testClock.advanceMillis(1000);
+    Event event = new SimpleEvent("event2", Collections.<String, AttributeValue>emptyMap());
+    Event expectdEvent =
+        SpanData.Event.create("event2", Collections.<String, AttributeValue>emptyMap());
+    span.addEvent(event);
+    Link link = SpanData.Link.create(spanContext);
+    span.addLink(link);
+    testClock.advanceMillis(1000);
+    span.setStatus(Status.CANCELLED);
+    span.end();
+    Mockito.verify(spanProcessor, Mockito.times(1)).onEndSync(span);
+  }
+
+  @Test
+  public void status_ViaSetStatus() {
+    RecordEventsSpanImpl span =
+        RecordEventsSpanImpl.startSpan(
+            spanContext,
+            SPAN_NAME,
+            Kind.INTERNAL,
+            parentSpanId,
+            TraceConfig.DEFAULT,
+            spanProcessor,
+            timestampConverter,
+            testClock,
+            resource);
+    Mockito.verify(spanProcessor, Mockito.times(1)).onStartSync(span);
+    testClock.advanceMillis(10);
+    assertThat(span.getStatus()).isEqualTo(Status.OK);
+    span.setStatus(Status.CANCELLED);
+    assertThat(span.getStatus()).isEqualTo(Status.CANCELLED);
+    span.end();
+    assertThat(span.getStatus()).isEqualTo(Status.CANCELLED);
+  }
+
+  @Test
+  public void getSpanKind() {
+    RecordEventsSpanImpl span =
+        RecordEventsSpanImpl.startSpan(
+            spanContext,
+            SPAN_NAME,
+            Kind.SERVER,
+            parentSpanId,
+            TraceConfig.DEFAULT,
+            spanProcessor,
+            timestampConverter,
+            testClock,
+            resource);
+    assertThat(span.getKind()).isEqualTo(Kind.SERVER);
+  }
+
+  private static final class SimpleEvent implements Event {
+
+    private final String name;
+    private final Map<String, AttributeValue> attributes;
+
+    private SimpleEvent(String name, Map<String, AttributeValue> attributes) {
+      this.name = name;
+      this.attributes = attributes;
+    }
+
+    @Override
+    public String getName() {
+      return name;
+    }
+
+    @Override
+    public Map<String, AttributeValue> getAttributes() {
+      return attributes;
+    }
+  }
+}
