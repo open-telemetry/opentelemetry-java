c5dd0b9e11ef1fdc1f597c9f6e2c136549158245
Bogdan Drutu <bogdandrutu@gmail.com>
2019-06-20 18:08:23 -0700

    Add a MultiSpanExporter that can be used to export to multiple expoters. (#423)

---
+++ sdk/src/main/java/io/opentelemetry/sdk/trace/export/MultiSpanExporter.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.sdk.trace.export;
+
+import io.opentelemetry.proto.trace.v1.Span;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Implementation of the {@code SpanExporter} that simply forwards all received spans to a list of
+ * {@code SpanExporter}.
+ *
+ * <p>Can be used to export to multiple backends using the same {@code SpanProcessor} like a {@code
+ * SimpleSampledSpansProcessor} or a {@code BatchSampledSpansProcessor}.
+ */
+public final class MultiSpanExporter implements SpanExporter {
+  private final List<SpanExporter> spanExporters;
+
+  static SpanExporter create(List<SpanExporter> spanExporters) {
+    return new MultiSpanExporter(Collections.unmodifiableList(new ArrayList<>(spanExporters)));
+  }
+
+  @Override
+  public void export(List<Span> spans) {
+    for (SpanExporter spanExporter : spanExporters) {
+      spanExporter.export(spans);
+    }
+  }
+
+  @Override
+  public void shutdown() {
+    for (SpanExporter spanExporter : spanExporters) {
+      spanExporter.shutdown();
+    }
+  }
+
+  private MultiSpanExporter(List<SpanExporter> spanExporters) {
+    this.spanExporters = spanExporters;
+  }
+}
