78308e046d72b047c21fdb822f5d83c299f0736b
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2019-09-03 17:52:35 -0500

    added metrics to http server handler

--- contrib/http_base/src/main/java/io/opentelemetry/contrib/http/core/HttpServerHandler.java
+++ contrib/http_base/src/main/java/io/opentelemetry/contrib/http/core/HttpServerHandler.java
@@ -26,12 +26,19 @@ import io.opentelemetry.distributedcontext.EntryKey;
 import io.opentelemetry.distributedcontext.EntryMetadata;
 import io.opentelemetry.distributedcontext.EntryMetadata.EntryTtl;
 import io.opentelemetry.distributedcontext.EntryValue;
+import io.opentelemetry.metrics.MeasureBatchRecorder;
+import io.opentelemetry.metrics.MeasureDouble;
+import io.opentelemetry.metrics.MeasureLong;
 import io.opentelemetry.metrics.Meter;
+import io.opentelemetry.trace.AttributeValue;
 import io.opentelemetry.trace.Span;
 import io.opentelemetry.trace.Span.Kind;
 import io.opentelemetry.trace.SpanContext;
 import io.opentelemetry.trace.Tracer;
 import io.opentelemetry.trace.Tracestate;
+import io.opentelemetry.trace.util.Links;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 import javax.annotation.Nullable;
 
@@ -48,10 +55,13 @@ public class HttpServerHandler<Q, P, C> extends AbstractHttpHandler<Q, P> {
 
   private final Tracer tracer;
   private final DistributedContextManager contextManager;
+  private final Meter meter;
   private final HttpTextFormat.Getter<C> getter;
   private final boolean publicEndpoint;
   private final HttpTextFormat<SpanContext> spanContextHttpTextFormat;
   private final HttpTextFormat<DistributedContext> distributedContextHttpTextFormat;
+  private final MeasureDouble measureDuration;
+  private final MeasureLong measureMessageSize;
 
   /**
    * Constructs a handler object.
@@ -82,7 +92,7 @@ public class HttpServerHandler<Q, P, C> extends AbstractHttpHandler<Q, P> {
       DistributedContextManager contextManager,
       Meter meter,
       Boolean publicEndpoint) {
-    super(extractor, statusConverter, meter);
+    super(extractor, statusConverter);
     checkNotNull(getter, "getter is required");
     if (tracer == null) {
       this.tracer = OpenTelemetry.getTracer();
@@ -94,6 +104,11 @@ public class HttpServerHandler<Q, P, C> extends AbstractHttpHandler<Q, P> {
     } else {
       this.contextManager = contextManager;
     }
+    if (meter == null) {
+      this.meter = OpenTelemetry.getMeter();
+    } else {
+      this.meter = meter;
+    }
     this.getter = getter;
     if (publicEndpoint == null) {
       this.publicEndpoint = false;
@@ -103,6 +118,15 @@ public class HttpServerHandler<Q, P, C> extends AbstractHttpHandler<Q, P> {
     this.spanContextHttpTextFormat = this.tracer.getHttpTextFormat();
     //    this.distributedContextHttpTextFormat = this.contextManager.getHttpTextFormat();
     this.distributedContextHttpTextFormat = new TemporaryDistributedContextTextFormat();
+    //    this.measureDuration =
+    // this.meter.measureDoubleBuilder(HttpTraceConstants.MEASURE_DURATION)
+    //        .setUnit("s")
+    //        .build();
+    this.measureDuration = new TemporaryMeasureDouble();
+    //    this.measureMessageSize = this.meter.measureLongBuilder(HttpTraceConstants.MEASURE_SIZE)
+    //        .setUnit("B")
+    //        .build();
+    this.measureMessageSize = new TemporaryMeasureLong();
   }
 
   /**
@@ -151,10 +175,9 @@ public class HttpServerHandler<Q, P, C> extends AbstractHttpHandler<Q, P> {
 
     Span span = spanBuilder.setSpanKind(Kind.SERVER).startSpan();
     if (publicEndpoint && spanContext != null) {
-      //      Map<String, AttributeValue> attributes = new HashMap<>();
-      //      attributes.put(HttpTraceConstants.LINK_TYPE,
-      // HttpTraceConstants.LINK_ATTR_ORIGINATING);
-      //      span.addLink(Links.create(spanContext, attributes));
+      Map<String, AttributeValue> attributes = new HashMap<>();
+      attributes.put(HttpTraceConstants.LINK_TYPE, HttpTraceConstants.LINK_ATTR_ORIGINATING);
+      span.addLink(Links.create(spanContext, attributes));
     }
 
     if (span.isRecordingEvents()) {
@@ -193,6 +216,21 @@ public class HttpServerHandler<Q, P, C> extends AbstractHttpHandler<Q, P> {
     int httpCode = extractor.getStatusCode(response);
     recordMeasurements(context, httpCode);
     spanEnd(context.span, httpCode, error);
+  }
+
+  private void recordMeasurements(HttpRequestContext context, int httpCode) {
+    //    MeasureBatchRecorder recorder = meter.newMeasureBatchRecorder();
+    try {
+      meter.newMeasureBatchRecorder();
+      meter.counterLongBuilder(HttpTraceConstants.MEASURE_COUNT + httpCode).build();
+    } catch (UnsupportedOperationException ignore) {
+      // NoOp
+    }
+    MeasureBatchRecorder recorder = new TemporaryMeasureBatchRecorder();
+    recorder.setDistributedContext(context.distContext);
+    recorder.put(measureDuration, (System.nanoTime() - context.requestStartTime) / 1000000000.0);
+    recorder.put(measureMessageSize, context.receiveMessageSize.longValue());
+    recorder.record();
   }
 
   @Override
