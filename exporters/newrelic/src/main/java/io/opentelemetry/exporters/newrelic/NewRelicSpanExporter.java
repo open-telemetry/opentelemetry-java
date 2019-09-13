dc4ee3203433e50d443ce6cb6d4f3f5649a78b48
jwatson <jwatson@newrelic.com>
2019-09-13 13:02:31 -0700

    Most basic possible span export with a very simple test.

--- exporters/newrelic/src/main/java/io/opentelemetry/exporters/newrelic/NewRelicSpanExporter.java
+++ exporters/newrelic/src/main/java/io/opentelemetry/exporters/newrelic/NewRelicSpanExporter.java
@@ -1,14 +1,49 @@
 package io.opentelemetry.exporters.newrelic;
 
+import com.newrelic.telemetry.Attributes;
+import com.newrelic.telemetry.exceptions.ResponseException;
+import com.newrelic.telemetry.exceptions.RetryWithBackoffException;
+import com.newrelic.telemetry.exceptions.RetryWithRequestedWaitException;
+import com.newrelic.telemetry.spans.SpanBatch;
+import com.newrelic.telemetry.spans.SpanBatchSender;
 import io.opentelemetry.proto.trace.v1.Span;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 public class NewRelicSpanExporter implements SpanExporter {
 
+  private final SpanBatchSender spanBatchSender;
+  private final Attributes commonAttributes;
+
+  public NewRelicSpanExporter(SpanBatchSender spanBatchSender, Attributes commonAttributes) {
+    this.spanBatchSender = spanBatchSender;
+    this.commonAttributes = commonAttributes;
+  }
+
   @Override
-  public ResultCode export(List<Span> spans) {
-    throw new UnsupportedOperationException("implement me");
+  public ResultCode export(List<Span> openTracingSpans) {
+    Collection<com.newrelic.telemetry.spans.Span> newRelicSpans = new HashSet<>();
+    for (Span openTracingSpan : openTracingSpans) {
+      newRelicSpans.add(makeNewRelicSpan(openTracingSpan));
+    }
+    SpanBatch spanBatch = new SpanBatch(newRelicSpans, new Attributes());
+    try {
+      spanBatchSender.sendBatch(spanBatch);
+      return ResultCode.SUCCESS;
+    } catch (RetryWithRequestedWaitException | RetryWithBackoffException e) {
+      return ResultCode.FAILED_RETRYABLE;
+    } catch (ResponseException e) {
+      return ResultCode.FAILED_NOT_RETRYABLE;
+    }
+  }
+
+  private com.newrelic.telemetry.spans.Span makeNewRelicSpan(Span span) {
+    return com.newrelic.telemetry.spans.Span.builder(span.getSpanId().toStringUtf8())
+        .timestamp(1000L * span.getStartTime().getSeconds())
+        .attributes(commonAttributes)
+        .build();
   }
 
   @Override
--- exporters/newrelic/src/test/java/io/opentelemetry/exporters/newrelic/NewRelicSpanExporterTest.java
+++ exporters/newrelic/src/test/java/io/opentelemetry/exporters/newrelic/NewRelicSpanExporterTest.java
@@ -1,13 +1,44 @@
 package io.opentelemetry.exporters.newrelic;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Timestamp;
+import com.newrelic.telemetry.Attributes;
+import com.newrelic.telemetry.spans.SpanBatch;
+import com.newrelic.telemetry.spans.SpanBatchSender;
+import io.opentelemetry.proto.trace.v1.Span;
+import io.opentelemetry.sdk.trace.export.SpanExporter.ResultCode;
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class NewRelicSpanExporterTest {
 
+  @Mock private SpanBatchSender spanBatchSender;
+
   @Test
-  void testPlaceholder() throws Exception {
-    fail("fixMe");
+  void testSpanSending() throws Exception {
+    com.newrelic.telemetry.spans.Span span1 =
+        com.newrelic.telemetry.spans.Span.builder("spanIdNumber1")
+            .timestamp(1000000).build();
+    SpanBatch expected = new SpanBatch(Collections.singleton(span1), new Attributes());
+
+    NewRelicSpanExporter testClass = new NewRelicSpanExporter(spanBatchSender, new Attributes());
+
+    Span inputSpan = Span.newBuilder().setSpanId(ByteString.copyFromUtf8("spanIdNumber1"))
+        .setStartTime(Timestamp.newBuilder().setSeconds(1000).build())
+        .build();
+    ResultCode result =
+        testClass.export(
+            Collections.singletonList(
+                inputSpan));
+
+    assertEquals(ResultCode.SUCCESS, result);
+    verify(spanBatchSender).sendBatch(expected);
   }
 }
