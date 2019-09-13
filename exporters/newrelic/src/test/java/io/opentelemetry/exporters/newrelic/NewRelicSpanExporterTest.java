d900630649903c923aad23ccb43bd1f8ef6578cf
jwatson <jwatson@newrelic.com>
2019-09-13 13:04:55 -0700

    add licenses so the checkstyle passes

--- exporters/newrelic/src/test/java/io/opentelemetry/exporters/newrelic/NewRelicSpanExporterTest.java
+++ exporters/newrelic/src/test/java/io/opentelemetry/exporters/newrelic/NewRelicSpanExporterTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.opentelemetry.exporters.newrelic;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -24,19 +40,17 @@ class NewRelicSpanExporterTest {
   @Test
   void testSpanSending() throws Exception {
     com.newrelic.telemetry.spans.Span span1 =
-        com.newrelic.telemetry.spans.Span.builder("spanIdNumber1")
-            .timestamp(1000000).build();
+        com.newrelic.telemetry.spans.Span.builder("spanIdNumber1").timestamp(1000000).build();
     SpanBatch expected = new SpanBatch(Collections.singleton(span1), new Attributes());
 
     NewRelicSpanExporter testClass = new NewRelicSpanExporter(spanBatchSender, new Attributes());
 
-    Span inputSpan = Span.newBuilder().setSpanId(ByteString.copyFromUtf8("spanIdNumber1"))
-        .setStartTime(Timestamp.newBuilder().setSeconds(1000).build())
-        .build();
-    ResultCode result =
-        testClass.export(
-            Collections.singletonList(
-                inputSpan));
+    Span inputSpan =
+        Span.newBuilder()
+            .setSpanId(ByteString.copyFromUtf8("spanIdNumber1"))
+            .setStartTime(Timestamp.newBuilder().setSeconds(1000).build())
+            .build();
+    ResultCode result = testClass.export(Collections.singletonList(inputSpan));
 
     assertEquals(ResultCode.SUCCESS, result);
     verify(spanBatchSender).sendBatch(expected);
