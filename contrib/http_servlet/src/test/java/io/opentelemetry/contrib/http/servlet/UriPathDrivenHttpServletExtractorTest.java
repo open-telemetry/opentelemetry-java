30626636535df0fc0908048707a1f507147fc6d9
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2019-09-22 14:40:54 -0500

    initial dev of path matcher

--- contrib/http_servlet/src/test/java/io/opentelemetry/contrib/http/servlet/UriPathDrivenHttpServletExtractorTest.java
+++ contrib/http_servlet/src/test/java/io/opentelemetry/contrib/http/servlet/UriPathDrivenHttpServletExtractorTest.java
@@ -24,6 +24,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.junit.Test;
 
+/** Unit tests for {@link UriPathDrivenHttpServletExtractor}. */
 public class UriPathDrivenHttpServletExtractorTest {
 
   @Test
