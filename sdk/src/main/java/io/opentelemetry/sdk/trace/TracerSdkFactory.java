e7c456f68cba3bfb4042c17c4a815bc023413a01
Bogdan Drutu <bogdandrutu@gmail.com>
2019-10-27 08:36:07 -0700

    Extract the shared state between Tracers in TracerSharedState.
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- sdk/src/main/java/io/opentelemetry/sdk/trace/TracerSdkFactory.java
+++ sdk/src/main/java/io/opentelemetry/sdk/trace/TracerSdkFactory.java
@@ -16,10 +16,13 @@
 
 package io.opentelemetry.sdk.trace;
 
+import io.opentelemetry.sdk.trace.config.TraceConfig;
 import io.opentelemetry.trace.Tracer;
 import io.opentelemetry.trace.TracerFactory;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * {@code Tracer} provider implementation for {@link TracerFactory}.
@@ -28,22 +31,72 @@ import java.util.concurrent.ConcurrentHashMap;
  * io.opentelemetry.OpenTelemetry}.
  */
 public class TracerSdkFactory implements TracerFactory {
-  private final Map<String, Tracer> tracersByKey = new ConcurrentHashMap<>();
+  private static final Logger logger = Logger.getLogger(TracerFactory.class.getName());
+  private final Map<String, TracerSdk> tracersByKey = new ConcurrentHashMap<>();
+  private final TracerSharedState sharedState = new TracerSharedState();
 
   @Override
-  public Tracer get(String instrumentationName) {
+  public TracerSdk get(String instrumentationName) {
     return get(instrumentationName, null);
   }
 
   @Override
-  public Tracer get(String instrumentationName, String instrumentationVersion) {
+  public TracerSdk get(String instrumentationName, String instrumentationVersion) {
     String key = instrumentationName + "/" + instrumentationVersion;
-    Tracer tracer = tracersByKey.get(key);
+    TracerSdk tracer = tracersByKey.get(key);
     if (tracer == null) {
       // todo: pass in the name & version here to the implementation to be used for purposes.
-      tracer = new TracerSdk();
+      tracer = new TracerSdk(sharedState);
       tracersByKey.put(key, tracer);
     }
     return tracer;
+  }
+
+  /**
+   * Returns the active {@code TraceConfig}.
+   *
+   * @return the active {@code TraceConfig}.
+   */
+  public TraceConfig getActiveTraceConfig() {
+    return sharedState.getActiveTraceConfig();
+  }
+
+  /**
+   * Updates the active {@link TraceConfig}.
+   *
+   * @param traceConfig the new active {@code TraceConfig}.
+   */
+  public void updateActiveTraceConfig(TraceConfig traceConfig) {
+    sharedState.updateActiveTraceConfig(traceConfig);
+  }
+
+  /**
+   * Adds a new {@code SpanProcessor} to this {@code Tracer}.
+   *
+   * <p>Any registered processor cause overhead, consider to use an async/batch processor especially
+   * for span exporting, and export to multiple backends using the {@link
+   * io.opentelemetry.sdk.trace.export.MultiSpanExporter}.
+   *
+   * @param spanProcessor the new {@code SpanProcessor} to be added.
+   */
+  public void addSpanProcessor(SpanProcessor spanProcessor) {
+    sharedState.addSpanProcessor(spanProcessor);
+  }
+
+  /**
+   * Attempts to stop all the activity for this {@link Tracer}. Calls {@link
+   * SpanProcessor#shutdown()} for all registered {@link SpanProcessor}s.
+   *
+   * <p>This operation may block until all the Spans are processed. Must be called before turning
+   * off the main application to ensure all data are processed and exported.
+   *
+   * <p>After this is called all the newly created {@code Span}s will be no-op.
+   */
+  public void shutdown() {
+    if (sharedState.isStopped()) {
+      logger.log(Level.WARNING, "Calling shutdown() multiple times.");
+      return;
+    }
+    sharedState.stop();
   }
 }
