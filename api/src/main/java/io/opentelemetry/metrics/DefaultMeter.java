5f0ef531d8f316343715adf2c8f57a657b220a7e
Bogdan Drutu <bogdandrutu@gmail.com>
2019-11-27 09:17:04 -0800

    Update api/src/main/java/io/opentelemetry/metrics/DefaultMeter.java
    Co-Authored-By: Armin Ruech <armin.ruech@gmail.com>

--- api/src/main/java/io/opentelemetry/metrics/DefaultMeter.java
+++ api/src/main/java/io/opentelemetry/metrics/DefaultMeter.java
@@ -142,7 +142,7 @@ public final class DefaultMeter implements Meter {
   @Override
   public LabelSet createLabelSet(String k1, String v1, String k2, String v2, String k3, String v3) {
     Utils.checkNotNull(k1, "k1");
-    Utils.checkNotNull(v1, "k1");
+    Utils.checkNotNull(v1, "v1");
     Utils.checkNotNull(k2, "k2");
     Utils.checkNotNull(v2, "v2");
     Utils.checkNotNull(k3, "k3");
