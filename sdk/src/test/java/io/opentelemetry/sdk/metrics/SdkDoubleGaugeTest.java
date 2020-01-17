edb947682afe639e0087373673881b2344caf9be
Giovanni Liva <giovanni.liva.91@gmail.com>
2020-01-17 22:57:09 +0100

    Fix javadoc

--- sdk/src/test/java/io/opentelemetry/sdk/metrics/SdkDoubleGaugeTest.java
+++ sdk/src/test/java/io/opentelemetry/sdk/metrics/SdkDoubleGaugeTest.java
@@ -29,7 +29,7 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Unit tests for {@link SdkLongCounter}. */
+/** Unit tests for {@link SdkLongGauge}. */
 @RunWith(JUnit4.class)
 public class SdkDoubleGaugeTest {
 
