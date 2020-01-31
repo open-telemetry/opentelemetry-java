d9ae7ef0b061bf0b171aaee97d8fae5103e3c9a0
Bogdan Drutu <bogdandrutu@gmail.com>
2020-01-31 08:34:26 -0800

    Clean comments

--- sdk/src/test/java/io/opentelemetry/sdk/trace/spi/TraceProviderSdkTest.java
+++ sdk/src/test/java/io/opentelemetry/sdk/trace/spi/TraceProviderSdkTest.java
@@ -24,7 +24,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Unit tests for {@link io.opentelemetry.sdk.trace.spi.TraceProviderSdk}. */
+/** Unit tests for {@link TraceProviderSdk}. */
 @RunWith(JUnit4.class)
 public class TraceProviderSdkTest {
 
