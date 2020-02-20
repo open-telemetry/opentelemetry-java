c0814b145bbac048353c41a55716655ec82546e2
Giovanni Liva <giovanni.liva@dynatrace.com>
2020-02-20 08:09:02 +0100

    gradlew goJF

--- api/src/main/java/io/opentelemetry/trace/TracerProvider.java
+++ api/src/main/java/io/opentelemetry/trace/TracerProvider.java
@@ -19,9 +19,9 @@ package io.opentelemetry.trace;
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
- * A registry for creating named {@link Tracer}s.
- * Although the class is provided at runtime via {@link io.opentelemetry.trace.spi.TraceProvider},
- * the name <i>Provider</i> is for consistency with other languages.
+ * A registry for creating named {@link Tracer}s. Although the class is provided at runtime via
+ * {@link io.opentelemetry.trace.spi.TraceProvider}, the name <i>Provider</i> is for consistency
+ * with other languages.
  *
  * @see io.opentelemetry.OpenTelemetry
  * @see io.opentelemetry.trace.Tracer
