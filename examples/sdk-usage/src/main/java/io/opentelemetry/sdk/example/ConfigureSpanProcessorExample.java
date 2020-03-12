4bfd290b1b11c0b15a54e7d5df743d90d7796b02
Giovanni Liva <giovanni.liva@dynatrace.com>
2020-03-12 16:43:56 +0100

    Format and Copyright

--- examples/sdk-usage/src/main/java/io/opentelemetry/sdk/example/ConfigureSpanProcessorExample.java
+++ examples/sdk-usage/src/main/java/io/opentelemetry/sdk/example/ConfigureSpanProcessorExample.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.opentelemetry.sdk.example;
 
 import io.opentelemetry.exporters.inmemory.InMemorySpanExporter;
@@ -13,81 +29,78 @@ import io.opentelemetry.sdk.trace.export.SimpleSpansProcessor;
 
 import java.util.Arrays;
 
-/**
- * This example shows how to different Span Processors.
- */
+/** This example shows how to instantiate different Span Processors. */
 class ConfigureSpanProcessorExample {
 
-	static InMemorySpanExporter exporter = InMemorySpanExporter.create();
+  static InMemorySpanExporter exporter = InMemorySpanExporter.create();
 
-	public static void main(String[] args) throws Exception {
-		// Get the Tracer Provider
-		TracerSdkFactory tracerProvider = OpenTelemetrySdk.getTracerFactory();
+  public static void main(String[] args) throws Exception {
+    // Get the Tracer Provider
+    TracerSdkFactory tracerProvider = OpenTelemetrySdk.getTracerFactory();
 
-		// Configure the simple spans processor.
-		tracerProvider.addSpanProcessor(SimpleSpansProcessor.newBuilder(exporter).build());
+    // Configure the simple spans processor.
+    tracerProvider.addSpanProcessor(SimpleSpansProcessor.newBuilder(exporter).build());
 
-		// Now we can acquire a tracer
-		TracerSdk tracer = tracerProvider.get("ConfigureSpanProcessorExample");
-		// Print to the console the list of span processors enabled.
-		DemoUtils.printProcessorList(tracer);
+    // Now we can acquire a tracer
+    TracerSdk tracer = tracerProvider.get("ConfigureSpanProcessorExample");
+    // Print to the console the list of span processors enabled.
+    DemoUtils.printProcessorList(tracer);
 
-		// Processors that are plugged after the tracer creation are still propagated to the tracer.
-		tracerProvider.addSpanProcessor(SimpleSpansProcessor.newBuilder(exporter).build());
-		// Print to the console the list of span processors enabled.
-		DemoUtils.printProcessorList(tracer);
+    // Processors that are plugged after the tracer creation are still propagated to the tracer.
+    tracerProvider.addSpanProcessor(SimpleSpansProcessor.newBuilder(exporter).build());
+    // Print to the console the list of span processors enabled.
+    DemoUtils.printProcessorList(tracer);
 
-		// We can also implement our own SpanProcessor. It is necessary to only implement the relative interface
-		// which requires three methods that are called during the lifespan of Spans.
-		class MySpanProcessor implements SpanProcessor {
+    // We can also implement our own SpanProcessor. It is necessary to only implement the relative
+    // interface
+    // which requires three methods that are called during the lifespan of Spans.
+    class MySpanProcessor implements SpanProcessor {
 
-			@Override
-			public void onStart(ReadableSpan span) {
-				// This method is called when a span is created;
-				System.out.printf("Span %s - Started\n", span.getName());
-			}
+      @Override
+      public void onStart(ReadableSpan span) {
+        // This method is called when a span is created;
+        System.out.printf("Span %s - Started\n", span.getName());
+      }
 
-			@Override
-			public void onEnd(ReadableSpan span) {
-				// This method is called when a span is terminated;
-				System.out.printf("Span %s - Terminated\n", span.getName());
-			}
+      @Override
+      public void onEnd(ReadableSpan span) {
+        // This method is called when a span is terminated;
+        System.out.printf("Span %s - Terminated\n", span.getName());
+      }
 
-			@Override
-			public void shutdown() {
-				// This method is called when the OpenTelemetry library is shutting down;
-				System.out.printf("Goodbye by %s\n",  this.getClass().getSimpleName());
-			}
+      @Override
+      public void shutdown() {
+        // This method is called when the OpenTelemetry library is shutting down;
+        System.out.printf("Goodbye by %s\n", this.getClass().getSimpleName());
+      }
 
-			@Override
-			public String toString() {
-				return "MySpanProcessor{}";
-			}
-		}
+      @Override
+      public String toString() {
+        return "MySpanProcessor{}";
+      }
+    }
 
-		// We can also configure multiple span processors at the same time using the MultiSpanProcessor class.
-		// MultiSpanProcessor can be nested.
-		tracerProvider.addSpanProcessor(
-				MultiSpanProcessor.create(Arrays.asList(
-						new MySpanProcessor(),
-						MultiSpanProcessor.create(
-								Arrays.asList(
-										SimpleSpansProcessor.newBuilder(exporter).build(),
-										BatchSpansProcessor.newBuilder(exporter).build()
-								)
-						)
-				))
-		);
-		// Print to the console the list of span processors enabled.
-		DemoUtils.printProcessorList(tracer);
+    // We can also configure multiple span processors at the same time using the MultiSpanProcessor
+    // class.
+    // MultiSpanProcessor can be nested.
+    tracerProvider.addSpanProcessor(
+        MultiSpanProcessor.create(
+            Arrays.asList(
+                new MySpanProcessor(),
+                MultiSpanProcessor.create(
+                    Arrays.asList(
+                        SimpleSpansProcessor.newBuilder(exporter).build(),
+                        BatchSpansProcessor.newBuilder(exporter).build())))));
+    // Print to the console the list of span processors enabled.
+    DemoUtils.printProcessorList(tracer);
 
-		// We generate some Spans so we can test MySpanProcessor implementation
-		tracer.spanBuilder("Span #1").startSpan().end();
-		tracer.spanBuilder("Span #2").startSpan().end();
-		tracer.spanBuilder("Span #3").startSpan().end();
+    // We generate some Spans so we can test MySpanProcessor implementation
+    tracer.spanBuilder("Span #1").startSpan().end();
+    tracer.spanBuilder("Span #2").startSpan().end();
+    tracer.spanBuilder("Span #3").startSpan().end();
 
-		// We shutdown the OpenTelemetry library
-		// This automatically shuts down
-		OpenTelemetrySdk.getTracerFactory().shutdown();
-	}
+    // We shutdown the OpenTelemetry library
+    // This automatically shuts down
+    OpenTelemetrySdk.getTracerFactory().shutdown();
+  }
 }
--- examples/sdk-usage/src/main/java/io/opentelemetry/sdk/example/ConfigureTraceExample.java
+++ examples/sdk-usage/src/main/java/io/opentelemetry/sdk/example/ConfigureTraceExample.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.opentelemetry.sdk.example;
 
 import io.opentelemetry.sdk.OpenTelemetrySdk;
@@ -21,124 +37,130 @@ import java.util.Map;
 
 class ConfigureTraceExample {
 
-	// Class that showcases how to implement your own SpanProcessor
-	private static class MyProcessor implements SpanProcessor {
+  // Class that showcases how to implement your own SpanProcessor
+  private static class MyProcessor implements SpanProcessor {
 
-		@Override
-		public void onStart(ReadableSpan span) {
-			//We just do something when a span ends.
-		}
+    @Override
+    public void onStart(ReadableSpan span) {
+      // We just do something when a span ends.
+    }
 
-		@Override
-		public void onEnd(ReadableSpan span) {
-			Map<String, AttributeValue> attrs = span.toSpanData().getAttributes();
-			System.out.printf("Span %s has %d attributes: \n", span.getName(), attrs.size());
-			for(String key : attrs.keySet()){
-				System.out.printf("\t %s : %s\n", key, attrs.get(key).getStringValue());
-			}
-			System.out.println("-----------------------");
-		}
+    @Override
+    public void onEnd(ReadableSpan span) {
+      Map<String, AttributeValue> attrs = span.toSpanData().getAttributes();
+      System.out.printf("Span %s has %d attributes: \n", span.getName(), attrs.size());
+      for (String key : attrs.keySet()) {
+        System.out.printf("\t %s : %s\n", key, attrs.get(key).getStringValue());
+      }
+      System.out.println("-----------------------");
+    }
 
-		@Override
-		public void shutdown() {
+    @Override
+    public void shutdown() {}
+  }
 
-		}
-	}
+  public static void main(String[] args) {
+    // Configure a tracer for these examples
+    TracerSdkFactory tracerProvider = OpenTelemetrySdk.getTracerFactory();
+    TracerSdk tracer = tracerProvider.get("example");
+    tracerProvider.addSpanProcessor(new MyProcessor());
 
-	public static void main(String[] args) {
-		// Configure a tracer for these examples
-		TracerSdkFactory tracerProvider = OpenTelemetrySdk.getTracerFactory();
-		TracerSdk tracer = tracerProvider.get("example");
-		tracerProvider.addSpanProcessor(new MyProcessor());
+    // TraceConfig handles the global tracing configuration
+    TraceConfig config = TraceConfig.getDefault();
+    printTraceConfig();
 
-		//TraceConfig handles the global tracing configuration
-		TraceConfig config = TraceConfig.getDefault();
-		printTraceConfig();
+    // We can have 32 Attributes by default. Let's add some to a span and verify they are stored
+    // correctly.
+    Span multiAttrSpan = tracer.spanBuilder("Example Span Attributes").startSpan();
+    multiAttrSpan.setAttribute("Attribute 1", "first attribute value");
+    multiAttrSpan.setAttribute("Attribute 2", "second attribute value");
+    multiAttrSpan.end();
 
-		// We can have 32 Attributes by default. Let's add some to a span and verify they are stored correctly.
-		Span multiAttrSpan = tracer.spanBuilder("Example Span Attributes").startSpan();
-		multiAttrSpan.setAttribute("Attribute 1", "first attribute value");
-		multiAttrSpan.setAttribute("Attribute 2", "second attribute value");
-		multiAttrSpan.end();
+    // The configuration can be changed in the trace provider
+    // For example, we can change the maximum number of Attributes per span to 1
+    TraceConfig newConf = config.toBuilder().setMaxNumberOfAttributes(1).build();
+    tracerProvider.updateActiveTraceConfig(newConf);
+    printTraceConfig();
 
-		// The configuration can be changed in the trace provider
-		// For example, we can change the maximum number of Attributes per span to 1
-		TraceConfig newConf = config.toBuilder().setMaxNumberOfAttributes(1).build();
-		tracerProvider.updateActiveTraceConfig(newConf);
-		printTraceConfig();
+    // Let check that now only one attribute is stored in the Span
+    Span singleAttrSpan = tracer.spanBuilder("Example Span Attributes").startSpan();
+    singleAttrSpan.setAttribute("Attribute 1", "first attribute value");
+    singleAttrSpan.setAttribute("Attribute 2", "second attribute value");
+    singleAttrSpan.end();
 
-		// Let check that now only one attribute is stored in the Span
-		Span singleAttrSpan = tracer.spanBuilder("Example Span Attributes").startSpan();
-		singleAttrSpan.setAttribute("Attribute 1", "first attribute value");
-		singleAttrSpan.setAttribute("Attribute 2", "second attribute value");
-		singleAttrSpan.end();
+    // We can also change how the Spans are sampled. For example, we can turn off completely the
+    // spans.
+    TraceConfig alwaysOff =
+        TraceConfig.getDefault().toBuilder().setSampler(Samplers.alwaysOff()).build();
+    tracerProvider.updateActiveTraceConfig(alwaysOff);
+    printTraceConfig();
+    tracer.spanBuilder("Not forwarded to any processors").startSpan().end();
+    tracer.spanBuilder("Not forwarded to any processors").startSpan().end();
+    tracer.spanBuilder("Not forwarded to any processors").startSpan().end();
+    tracer.spanBuilder("Not forwarded to any processors").startSpan().end();
+    tracer.spanBuilder("Not forwarded to any processors").startSpan().end();
 
-		// We can also change how the Spans are sampled. For example, we can turn off completely the spans.
-		TraceConfig alwaysOff = TraceConfig.getDefault().toBuilder().setSampler(
-				Samplers.alwaysOff()
-		).build();
-		tracerProvider.updateActiveTraceConfig(alwaysOff);
-		printTraceConfig();
-		tracer.spanBuilder("Not forwarded to any processors").startSpan().end();
-		tracer.spanBuilder("Not forwarded to any processors").startSpan().end();
-		tracer.spanBuilder("Not forwarded to any processors").startSpan().end();
-		tracer.spanBuilder("Not forwarded to any processors").startSpan().end();
-		tracer.spanBuilder("Not forwarded to any processors").startSpan().end();
+    // We can also implement our own sampler. We need to implement the
+    // io.opentelemetry.sdk.trace.Sampler interface.
+    class MySampler implements Sampler {
 
-		// We can also implement our own sampler. We need to implement the io.opentelemetry.sdk.trace.Sampler interface.
-		class MySampler implements Sampler {
+      @Override
+      public Decision shouldSample(
+          SpanContext parentContext,
+          TraceId traceId,
+          SpanId spanId,
+          String name,
+          List<Link> parentLinks) {
+        // We sample only if the name contains "SAMPLE"
+        return new Decision() {
 
-			@Override
-			public Decision shouldSample(SpanContext parentContext, TraceId traceId, SpanId spanId, String name, List<Link> parentLinks) {
-				// We sample only if the name contains "SAMPLE"
-				return new Decision() {
+          @Override
+          public boolean isSampled() {
+            return name.contains("SAMPLE");
+          }
 
-					@Override
-					public boolean isSampled() {
-						return name.contains("SAMPLE");
-					}
+          @Override
+          public Map<String, AttributeValue> attributes() {
+            return Collections.emptyMap();
+          }
+        };
+      }
 
-					@Override
-					public Map<String, AttributeValue> attributes() {
-						return Collections.emptyMap();
-					}
-				};
-			}
+      @Override
+      public String getDescription() {
+        return "My Sampler Implementation!";
+      }
+    }
 
-			@Override
-			public String getDescription() {
-				return "My Sampler Implementation!";
-			}
-		}
+    // Add MySampler to the Trace Configuration
+    TraceConfig mySampler =
+        TraceConfig.getDefault().toBuilder().setSampler(new MySampler()).build();
+    tracerProvider.updateActiveTraceConfig(mySampler);
+    printTraceConfig();
+    tracer.spanBuilder("#1 - SamPleD").startSpan().end();
+    tracer.spanBuilder("#2 - SAMPLED").startSpan().end();
+    tracer.spanBuilder("#3 - Smth").startSpan().end();
+    tracer
+        .spanBuilder("#4 - SAMPLED this trace will be showed in the console output")
+        .startSpan()
+        .end();
+    tracer.spanBuilder("#5").startSpan().end();
+  }
 
-		// Add MySampler to the Trace Configuration
-		TraceConfig mySampler = TraceConfig.getDefault().toBuilder().setSampler(
-				new MySampler()
-		).build();
-		tracerProvider.updateActiveTraceConfig(mySampler);
-		printTraceConfig();
-		tracer.spanBuilder("#1 - SamPleD").startSpan().end();
-		tracer.spanBuilder("#2 - SAMPLED").startSpan().end();
-		tracer.spanBuilder("#3 - Smth").startSpan().end();
-		tracer.spanBuilder("#4 - SAMPLED this trace will be showed in the console output").startSpan().end();
-		tracer.spanBuilder("#5").startSpan().end();
-	}
-
-	private static void printTraceConfig() {
-		TraceConfig config = OpenTelemetrySdk.getTracerFactory().getActiveTraceConfig();
-		System.out.print("Max number of attributes: ");
-		System.out.println(config.getMaxNumberOfAttributes());
-		System.out.print("Max number of attributes per event: ");
-		System.out.println(config.getMaxNumberOfAttributesPerEvent());
-		System.out.print("Max number of attributes per link: ");
-		System.out.println(config.getMaxNumberOfAttributesPerLink());
-		System.out.print("Max number of events: ");
-		System.out.println(config.getMaxNumberOfEvents());
-		System.out.print("Max number of links: ");
-		System.out.println(config.getMaxNumberOfLinks());
-		System.out.print("Sampler: ");
-		System.out.println(config.getSampler().getDescription());
-		System.out.println("==================================");
-	}
-
+  private static void printTraceConfig() {
+    TraceConfig config = OpenTelemetrySdk.getTracerFactory().getActiveTraceConfig();
+    System.out.print("Max number of attributes: ");
+    System.out.println(config.getMaxNumberOfAttributes());
+    System.out.print("Max number of attributes per event: ");
+    System.out.println(config.getMaxNumberOfAttributesPerEvent());
+    System.out.print("Max number of attributes per link: ");
+    System.out.println(config.getMaxNumberOfAttributesPerLink());
+    System.out.print("Max number of events: ");
+    System.out.println(config.getMaxNumberOfEvents());
+    System.out.print("Max number of links: ");
+    System.out.println(config.getMaxNumberOfLinks());
+    System.out.print("Sampler: ");
+    System.out.println(config.getSampler().getDescription());
+    System.out.println("==================================");
+  }
 }
