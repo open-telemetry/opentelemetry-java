fa6d91741c99f5534786d5dd4bc563c7cc23030e
Uladzislau Kiva <knijaz@gmail.com>
2020-03-23 20:40:21 +0300

    style: apply google code style

--- exporters/jaeger/src/test/java/io/opentelemetry/exporters/jaeger/JaegerIntegrationTest.java
+++ exporters/jaeger/src/test/java/io/opentelemetry/exporters/jaeger/JaegerIntegrationTest.java
@@ -47,7 +47,8 @@ public class JaegerIntegrationTest {
   private static final String JAEGER_VERSION = "1.17";
   private static final String SERVICE_NAME = "E2E-test";
   private static final String JAEGER_URL = "http://localhost";
-  private final Tracer tracer = OpenTelemetry.getTracerProvider().get(getClass().getCanonicalName());
+  private final Tracer tracer =
+      OpenTelemetry.getTracerProvider().get(getClass().getCanonicalName());
   private JaegerGrpcSpanExporter jaegerExporter;
 
   @SuppressWarnings("rawtypes")
