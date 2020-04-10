b76fca7f0ae1bb23413eab7d4f3ae87702512946
jwatson <jwatson@newrelic.com>
2020-04-10 14:14:57 -0700

    A simple stress-test for the TracerSdk.

--- sdk/src/test/java/io/opentelemetry/sdk/trace/TracerSdkTest.java
+++ sdk/src/test/java/io/opentelemetry/sdk/trace/TracerSdkTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019, OpenTelemetry Authors
+ * Copyright 2020, OpenTelemetry Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,11 +19,14 @@ package io.opentelemetry.sdk.trace;
 import static com.google.common.truth.Truth.assertThat;
 
 import io.grpc.Context;
+import io.opentelemetry.common.AttributeValue;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.sdk.common.InstrumentationLibraryInfo;
+import io.opentelemetry.sdk.trace.StressTestRunner.OperationUpdater;
 import io.opentelemetry.trace.DefaultSpan;
 import io.opentelemetry.trace.Span;
 import io.opentelemetry.trace.TracingContextUtils;
+import java.util.concurrent.atomic.AtomicLong;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,6 +40,7 @@ import org.mockito.MockitoAnnotations;
 // try-with-resources.
 @SuppressWarnings("MustBeClosedChecker")
 public class TracerSdkTest {
+
   private static final String SPAN_NAME = "span_name";
   private static final String INSTRUMENTATION_LIBRARY_NAME =
       "io.opentelemetry.sdk.trace.TracerSdkTest";
@@ -81,7 +85,7 @@ public class TracerSdkTest {
   @Test
   public void withSpan_NullSpan() {
     assertThat(tracer.getCurrentSpan()).isInstanceOf(DefaultSpan.class);
-    try (Scope ws = tracer.withSpan(null)) {
+    try (Scope ignored = tracer.withSpan(null)) {
       assertThat(tracer.getCurrentSpan()).isInstanceOf(DefaultSpan.class);
     }
     assertThat(tracer.getCurrentSpan()).isInstanceOf(DefaultSpan.class);
@@ -90,7 +94,7 @@ public class TracerSdkTest {
   @Test
   public void getCurrentSpan_WithSpan() {
     assertThat(tracer.getCurrentSpan()).isInstanceOf(DefaultSpan.class);
-    try (Scope ws = tracer.withSpan(span)) {
+    try (Scope ignored = tracer.withSpan(span)) {
       assertThat(tracer.getCurrentSpan()).isSameInstanceAs(span);
     }
     assertThat(tracer.getCurrentSpan()).isInstanceOf(DefaultSpan.class);
@@ -105,5 +109,79 @@ public class TracerSdkTest {
   public void propagatesInstrumentationLibraryInfoToSpan() {
     ReadableSpan readableSpan = (ReadableSpan) tracer.spanBuilder("spanName").startSpan();
     assertThat(readableSpan.getInstrumentationLibraryInfo()).isEqualTo(instrumentationLibraryInfo);
+  }
+
+  @Test
+  public void stressTest() {
+    CountingSpanProcessor spanProcessor = new CountingSpanProcessor();
+    TracerSdkProvider tracerSdkProvider = TracerSdkProvider.builder().build();
+    tracerSdkProvider.addSpanProcessor(spanProcessor);
+    TracerSdk tracer =
+        tracerSdkProvider.get(INSTRUMENTATION_LIBRARY_NAME, INSTRUMENTATION_LIBRARY_VERSION);
+
+    StressTestRunner.Builder stressTestBuilder =
+        StressTestRunner.builder().setTracer(tracer).setSpanProcessor(spanProcessor);
+
+    for (int i = 0; i < 4; i++) {
+      stressTestBuilder.addOperation(
+          StressTestRunner.Operation.create(2_000, 1, new SimpleSpanOperation(tracer)));
+    }
+
+    stressTestBuilder.build().run();
+    assertThat(spanProcessor.numberOfSpansFinished.get()).isEqualTo(8_000);
+    assertThat(spanProcessor.numberOfSpansStarted.get()).isEqualTo(8_000);
+  }
+
+  private static class CountingSpanProcessor implements SpanProcessor {
+    private final AtomicLong numberOfSpansStarted = new AtomicLong();
+    private final AtomicLong numberOfSpansFinished = new AtomicLong();
+
+    @Override
+    public void onStart(ReadableSpan span) {
+      numberOfSpansStarted.incrementAndGet();
+    }
+
+    @Override
+    public boolean isStartRequired() {
+      return true;
+    }
+
+    @Override
+    public void onEnd(ReadableSpan span) {
+      numberOfSpansFinished.incrementAndGet();
+    }
+
+    @Override
+    public boolean isEndRequired() {
+      return true;
+    }
+
+    @Override
+    public void shutdown() {
+      // no-op
+    }
+
+    @Override
+    public void forceFlush() {
+      // no-op
+    }
+  }
+
+  private static class SimpleSpanOperation implements OperationUpdater {
+    private final TracerSdk tracer;
+
+    public SimpleSpanOperation(TracerSdk tracer) {
+      this.tracer = tracer;
+    }
+
+    @Override
+    public void update() {
+      Span span = tracer.spanBuilder("testSpan").startSpan();
+      try (Scope ignored = tracer.withSpan(span)) {
+        span.setAttribute("testAttribute", AttributeValue.stringAttributeValue("testValue"));
+      } finally {
+        span.end();
+      }
+    }
   }
 }
