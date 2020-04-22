9f957214a403f907151919ad48dacf601f65d6bf
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2020-04-22 23:16:32 +0300

    Add Guava copyright to the code copy-pasted from Guava

--- contrib/trace_propagators/src/main/java/io/opentelemetry/contrib/trace/propagation/StringUtils.java
+++ contrib/trace_propagators/src/main/java/io/opentelemetry/contrib/trace/propagation/StringUtils.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2020, OpenTelemetry Authors
+ * Copyright (C) 2010 The Guava Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
