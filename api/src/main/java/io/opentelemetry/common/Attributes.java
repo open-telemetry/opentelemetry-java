785b571756fb81dbf6216ef8b11443b47d167761
jwatson <jwatson@newrelic.com>
2020-05-27 12:51:53 -0700

    Add an iterator/iterable and some simple tests

--- api/src/main/java/io/opentelemetry/common/Attributes.java
+++ api/src/main/java/io/opentelemetry/common/Attributes.java
@@ -16,14 +16,18 @@
 
 package io.opentelemetry.common;
 
+import static java.util.Collections.emptyIterator;
 import static java.util.Collections.emptySet;
 import static java.util.Collections.singleton;
 
 import com.google.auto.value.AutoValue;
 import com.google.auto.value.extension.memoized.Memoized;
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -33,7 +37,7 @@ import javax.annotation.concurrent.Immutable;
 
 /** javadoc me. */
 @Immutable
-public abstract class Attributes {
+public abstract class Attributes implements Iterable<Entry<String, AttributeValue>> {
   private static final Attributes EMPTY = new EmptyAttributes();
 
   /** javadoc me. */
@@ -50,6 +54,11 @@ public abstract class Attributes {
       data.add(entry.getKey());
       data.add(entry.getValue());
     }
+    return sortAndFilter(data);
+  }
+
+  /** TODO: Implement sorting and filtering out duplicate keys and null keys. */
+  private static Attributes sortAndFilter(List<Object> data) {
     return new AutoValue_Attributes_ArrayBackedAttributes(data);
   }
 
@@ -66,7 +75,7 @@ public abstract class Attributes {
   /** javadoc me. */
   public static Attributes of(
       String key1, AttributeValue value1, String key2, AttributeValue value2) {
-    return new AutoValue_Attributes_ArrayBackedAttributes(
+    return sortAndFilter(
         Arrays.asList(
             key1, value1,
             key2, value2));
@@ -80,7 +89,7 @@ public abstract class Attributes {
       AttributeValue value2,
       String key3,
       AttributeValue value3) {
-    return new AutoValue_Attributes_ArrayBackedAttributes(
+    return sortAndFilter(
         Arrays.asList(
             key1, value1,
             key2, value2,
@@ -97,7 +106,7 @@ public abstract class Attributes {
       AttributeValue value3,
       String key4,
       AttributeValue value4) {
-    return new AutoValue_Attributes_ArrayBackedAttributes(
+    return sortAndFilter(
         Arrays.asList(
             key1, value1,
             key2, value2,
@@ -117,7 +126,7 @@ public abstract class Attributes {
       AttributeValue value4,
       String key5,
       AttributeValue value5) {
-    return new AutoValue_Attributes_ArrayBackedAttributes(
+    return sortAndFilter(
         Arrays.asList(
             key1, value1,
             key2, value2,
@@ -160,6 +169,35 @@ public abstract class Attributes {
       }
       return null;
     }
+
+    @Override
+    public Iterator<Entry<String, AttributeValue>> iterator() {
+      return new ArrayIterator();
+    }
+
+    private class ArrayIterator implements Iterator<Entry<String, AttributeValue>> {
+      private int currentIndex = 0;
+
+      @Override
+      public boolean hasNext() {
+        return data().size() > currentIndex;
+      }
+
+      @Override
+      public Entry<String, AttributeValue> next() {
+        if (!hasNext()) {
+          throw new IllegalStateException("no more");
+        }
+        List<Object> data = data();
+        return new SimpleImmutableEntry<>(
+            (String) data.get(currentIndex++), (AttributeValue) data.get(currentIndex++));
+      }
+
+      @Override
+      public void remove() {
+        throw new UnsupportedOperationException("remove");
+      }
+    }
   }
 
   /** javadoc me. */
@@ -168,7 +206,7 @@ public abstract class Attributes {
 
     /** javadoc me. */
     public Attributes build() {
-      return new AutoValue_Attributes_ArrayBackedAttributes(Arrays.asList(data.toArray()));
+      return sortAndFilter(Arrays.asList(data.toArray()));
     }
 
     /** javadoc me. */
@@ -240,26 +278,38 @@ public abstract class Attributes {
     public AttributeValue getValue(String key) {
       return null;
     }
+
+    @Override
+    public Iterator<Entry<String, AttributeValue>> iterator() {
+      return emptyIterator();
+    }
   }
 
   private static class SingleAttributes extends Attributes {
-    private final Set<String> keys;
+    private final String key;
     private final AttributeValue value;
 
     private SingleAttributes(String key, AttributeValue value) {
-      this.keys = singleton(key);
+      this.key = key;
       this.value = value;
     }
 
     @Override
     public Set<String> keys() {
-      return keys;
+      return singleton(key);
     }
 
     @Nullable
     @Override
     public AttributeValue getValue(String key) {
-      return keys.contains(key) ? value : null;
+      return this.key.equals(key) ? value : null;
+    }
+
+    @Override
+    public Iterator<Entry<String, AttributeValue>> iterator() {
+      return Collections.<Entry<String, AttributeValue>>singleton(
+              new SimpleImmutableEntry<>(key, value))
+          .iterator();
     }
 
     @Override
@@ -273,7 +323,7 @@ public abstract class Attributes {
 
       SingleAttributes that = (SingleAttributes) o;
 
-      if (keys != null ? !keys.equals(that.keys) : that.keys != null) {
+      if (key != null ? !key.equals(that.key) : that.key != null) {
         return false;
       }
       return value != null ? value.equals(that.value) : that.value == null;
@@ -281,14 +331,14 @@ public abstract class Attributes {
 
     @Override
     public int hashCode() {
-      int result = keys != null ? keys.hashCode() : 0;
+      int result = key != null ? key.hashCode() : 0;
       result = 31 * result + (value != null ? value.hashCode() : 0);
       return result;
     }
 
     @Override
     public String toString() {
-      return "SingleAttributes{" + "keys=" + keys + ", value=" + value + '}';
+      return "SingleAttributes{" + "key=" + key + ", value=" + value + '}';
     }
   }
 }
---
+++ api/src/test/java/io/opentelemetry/common/AttributesTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.common;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.HashSet;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.junit.Test;
+
+public class AttributesTest {
+
+  @Test
+  public void testIteration() {
+    Set<String> keysSeen = new HashSet<>();
+
+    Attributes attributes =
+        Attributes.of(
+            "key1", AttributeValue.stringAttributeValue("value1"),
+            "key2", AttributeValue.longAttributeValue(333));
+
+    for (Entry<String, AttributeValue> attribute : attributes) {
+      keysSeen.add(attribute.getKey());
+    }
+
+    assertThat(keysSeen).containsExactly("key1", "key2");
+  }
+
+  @Test
+  public void testIteration_singleAttribute() {
+    Set<String> keysSeen = new HashSet<>();
+
+    Attributes attributes = Attributes.of("key", AttributeValue.stringAttributeValue("value"));
+
+    for (Entry<String, AttributeValue> attribute : attributes) {
+      keysSeen.add(attribute.getKey());
+    }
+
+    assertThat(keysSeen).containsExactly("key");
+  }
+
+  @Test
+  public void testIteration_empty() {
+    AtomicBoolean sawSomething = new AtomicBoolean(false);
+    Attributes emptyAttributes = Attributes.empty();
+    for (Entry<String, AttributeValue> ignored : emptyAttributes) {
+      sawSomething.set(true);
+    }
+    assertThat(sawSomething.get()).isFalse();
+  }
+}
