ffdc6f0c682102cd2bce9e835f0e78d257040367
Paulo Janotti <pjanotti@splunk.com>
2020-05-29 08:33:36 -0700

    Revert "Generate SpanId only for sampled spans"
    This reverts commit 5b51eaed887db21e4b6c68987b5859794b9e4cdc.

--- sdk/src/main/java/io/opentelemetry/sdk/trace/SpanBuilderSdk.java
+++ sdk/src/main/java/io/opentelemetry/sdk/trace/SpanBuilderSdk.java
@@ -196,6 +196,7 @@ final class SpanBuilderSdk implements Span.Builder {
   public Span startSpan() {
     SpanContext parentContext = parent(parentType, parent, remoteParent);
     TraceId traceId;
+    SpanId spanId = idsGenerator.generateSpanId();
     TraceState traceState = TraceState.getDefault();
     if (parentContext == null || !parentContext.isValid()) {
       // New root span.
@@ -224,13 +225,12 @@ final class SpanBuilderSdk implements Span.Builder {
             .shouldSample(
                 parentContext, traceId, spanName, spanKind, immutableAttributes, immutableLinks);
 
-    SpanId spanId = SpanId.getInvalid();
-    TraceFlags traceFlags = TRACE_OPTIONS_NOT_SAMPLED;
-    if (samplingDecision.isSampled()) {
-      spanId = idsGenerator.generateSpanId();
-      traceFlags = TRACE_OPTIONS_SAMPLED;
-    }
-    SpanContext spanContext = SpanContext.create(traceId, spanId, traceFlags, traceState);
+    SpanContext spanContext =
+        SpanContext.create(
+            traceId,
+            spanId,
+            samplingDecision.isSampled() ? TRACE_OPTIONS_SAMPLED : TRACE_OPTIONS_NOT_SAMPLED,
+            traceState);
 
     if (!samplingDecision.isSampled()) {
       return DefaultSpan.create(spanContext);
