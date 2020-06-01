322b661e3734ac4dafd89ecd20572a19bbc72cef
Rashmi <RashmiRam@users.noreply.github.com>
2020-06-02 00:21:13 +0530

    Support env vars for TraceConfig initialization (#1255)

---
+++ sdk/src/main/java/io/opentelemetry/sdk/trace/config/package-info.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Classes that holds global trace parameters
+ *
+ * <h2>Contents</h2>
+ *
+ * <ul>
+ *   <li>{@link io.opentelemetry.sdk.trace.config.TraceConfig}
+ * </ul>
+ *
+ * <h2>Default values for {@link io.opentelemetry.sdk.trace.config.TraceConfig}</h2>
+ *
+ * <ul>
+ *   <li>{@code SAMPLER: Samplers.alwaysOn()}
+ *   <li>{@code SPAN_MAX_NUM_ATTRIBUTES: 32}
+ *   <li>{@code SPAN_MAX_NUM_EVENTS: 128}
+ *   <li>{@code SPAN_MAX_NUM_LINKS: 32}
+ *   <li>{@code SPAN_MAX_NUM_ATTRIBUTES_PER_EVENT: 32}
+ *   <li>{@code SPAN_MAX_NUM_ATTRIBUTES_PER_LINK: 32}
+ * </ul>
+ *
+ * <p>Configuration options for {@link io.opentelemetry.sdk.trace.config.TraceConfig} can be read
+ * from system properties, environment variables, or {@link java.util.Properties} objects.
+ *
+ * <p>For system Properties and {@link java.util.Properties} objects, {@link
+ * io.opentelemetry.sdk.trace.config.TraceConfig} will look for the following names:
+ *
+ * <ul>
+ *   <li>{@code otel.config.sampler.probability}: to set the global default sampler which is used
+ *       when constructing a new {@code Span}.
+ *   <li>{@code otel.config.max.attrs}: to set the global default max number of attributes per
+ *       {@link io.opentelemetry.trace.Span}.
+ *   <li>{@code otel.config.max.events}: to set the global default max number of {@link
+ *       io.opentelemetry.trace.Event}s per {@link io.opentelemetry.trace.Span}.
+ *   <li>{@code otel.config.max.links}: to set the global default max number of {@link
+ *       io.opentelemetry.trace.Link} entries per {@link io.opentelemetry.trace.Span}.
+ *   <li>{@code otel.config.max.event.attrs}: to set the global default max number of attributes per
+ *       {@link io.opentelemetry.trace.Event}.
+ *   <li>{@code otel.config.max.link.attrs}: to set the global default max number of attributes per
+ *       {@link io.opentelemetry.trace.Link}.
+ * </ul>
+ *
+ * <p>For environment variable, {@link io.opentelemetry.sdk.trace.config.TraceConfig} will look for
+ * the following names:
+ *
+ * <ul>
+ *   <li>{@code OTEL_CONFIG_SAMPLER_PROBABILITY}: to set the global default sampler which is used
+ *       when constructing a new {@code Span}.
+ *   <li>{@code OTEL_CONFIG_MAX_ATTRS}: to set the global default max number of attributes per
+ *       {@link io.opentelemetry.trace.Span}.
+ *   <li>{@code OTEL_CONFIG_MAX_EVENTS}: to set the global default max number of {@link
+ *       io.opentelemetry.trace.Event}s per {@link io.opentelemetry.trace.Span}.
+ *   <li>{@code OTEL_CONFIG_MAX_LINKS}: to set the global default max number of {@link
+ *       io.opentelemetry.trace.Link} entries per {@link io.opentelemetry.trace.Span}.
+ *   <li>{@code OTEL_CONFIG_MAX_EVENT_ATTRS}: to set the global default max number of attributes per
+ *       {@link io.opentelemetry.trace.Event}.
+ *   <li>{@code OTEL_CONFIG_MAX_LINK_ATTRS}: to set the global default max number of attributes per
+ *       {@link io.opentelemetry.trace.Link}.
+ * </ul>
+ */
+package io.opentelemetry.sdk.trace.config;
---
+++ sdk/src/test/java/io/opentelemetry/sdk/common/export/ConfigBuilderTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2020, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.sdk.common.export;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import io.opentelemetry.sdk.common.export.ConfigBuilder.NamingConvention;
+import java.util.Collections;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link io.opentelemetry.sdk.common.export.ConfigBuilder}. */
+@RunWith(JUnit4.class)
+public class ConfigBuilderTest {
+
+  @Test
+  public void normalize() {
+    Map<String, String> dotValues =
+        NamingConvention.DOT.normalize(Collections.singletonMap("Test.Config.Key", "value"));
+    assertThat(dotValues).containsEntry("test.config.key", "value");
+
+    Map<String, String> envValue =
+        NamingConvention.ENV_VAR.normalize(Collections.singletonMap("TEST_CONFIG_KEY", "value"));
+    assertThat(envValue).containsEntry("test.config.key", "value");
+  }
+
+  @Test
+  public void booleanProperty() {
+    Boolean booleanProperty =
+        ConfigBuilder.getBooleanProperty("boolean", Collections.singletonMap("boolean", "true"));
+    assertThat(booleanProperty).isTrue();
+  }
+
+  @Test
+  public void longProperty() {
+    Long longProperty =
+        ConfigBuilder.getLongProperty("long", Collections.singletonMap("long", "42343"));
+    assertThat(longProperty).isEqualTo(42343);
+  }
+
+  @Test
+  public void intProperty() {
+    Integer intProperty =
+        ConfigBuilder.getIntProperty("int", Collections.singletonMap("int", "43543"));
+    assertThat(intProperty).isEqualTo(43543);
+  }
+
+  @Test
+  public void doubleProperty() {
+    Double doubleProperty =
+        ConfigBuilder.getDoubleProperty("double", Collections.singletonMap("double", "5.6"));
+    assertThat(doubleProperty).isEqualTo(5.6);
+  }
+
+  @Test
+  public void invalidBooleanProperty() {
+    Boolean booleanProperty =
+        ConfigBuilder.getBooleanProperty("boolean", Collections.singletonMap("boolean", "23435"));
+    assertThat(booleanProperty).isFalse();
+  }
+
+  @Test
+  public void invalidLongProperty() {
+    Long longProperty =
+        ConfigBuilder.getLongProperty("long", Collections.singletonMap("long", "45.6"));
+    assertThat(longProperty).isNull();
+  }
+
+  @Test
+  public void invalidIntProperty() {
+    Integer intProperty =
+        ConfigBuilder.getIntProperty("int", Collections.singletonMap("int", "false"));
+    assertThat(intProperty).isNull();
+  }
+
+  @Test
+  public void invalidDoubleProperty() {
+    Double doubleProperty =
+        ConfigBuilder.getDoubleProperty("double", Collections.singletonMap("double", "something"));
+    assertThat(doubleProperty).isNull();
+  }
+
+  @Test
+  public void nullValue_BooleanProperty() {
+    Boolean booleanProperty =
+        ConfigBuilder.getBooleanProperty("boolean", Collections.<String, String>emptyMap());
+    assertThat(booleanProperty).isNull();
+  }
+
+  @Test
+  public void nullValue_LongProperty() {
+    Long longProperty =
+        ConfigBuilder.getLongProperty("long", Collections.<String, String>emptyMap());
+    assertThat(longProperty).isNull();
+  }
+
+  @Test
+  public void nullValue_IntProperty() {
+    Integer intProperty =
+        ConfigBuilder.getIntProperty("int", Collections.<String, String>emptyMap());
+    assertThat(intProperty).isNull();
+  }
+
+  @Test
+  public void nullValue_DoubleProperty() {
+    Double doubleProperty =
+        ConfigBuilder.getDoubleProperty("double", Collections.<String, String>emptyMap());
+    assertThat(doubleProperty).isNull();
+  }
+}
