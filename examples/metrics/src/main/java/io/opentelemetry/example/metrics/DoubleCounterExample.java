d00c5b775d00fc91fe18fa289cb217cf5dcd49f4
Uladzislau Kiva <knijaz@gmail.com>
2020-06-04 18:01:21 +0300

    feat: apply review suggestions

--- examples/metrics/src/main/java/io/opentelemetry/example/metrics/DoubleCounterExample.java
+++ examples/metrics/src/main/java/io/opentelemetry/example/metrics/DoubleCounterExample.java
@@ -3,17 +3,18 @@ package io.opentelemetry.example.metrics;
 import io.opentelemetry.OpenTelemetry;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.metrics.DoubleCounter;
-import io.opentelemetry.metrics.LongCounter;
 import io.opentelemetry.metrics.Meter;
 import io.opentelemetry.trace.Span;
 import io.opentelemetry.trace.Span.Kind;
 import io.opentelemetry.trace.Status;
 import io.opentelemetry.trace.Tracer;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.filechooser.FileSystemView;
 
 /**
- * Example of using {@link DoubleCounter} and {@link DoubleCounter.BoundDoubleCounter} to count disk space used by files with a specific extension.
+ * Example of using {@link DoubleCounter} and {@link DoubleCounter.BoundDoubleCounter} to count disk space used by files with a specific extensions.
  */
 public class DoubleCounterExample {
 
@@ -21,7 +22,8 @@ public class DoubleCounterExample {
       OpenTelemetry.getTracer("io.opentelemetry.example.metrics");
   private static final Meter sampleMeter = OpenTelemetry.getMeterProvider()
       .get("io.opentelemetry.example.metrics", "0.5");
-  private static final File directoryToCountIn = FileSystemView.getFileSystemView().getHomeDirectory();
+  private static final File directoryToCountIn = FileSystemView.getFileSystemView()
+      .getHomeDirectory();
   private static final DoubleCounter methodCallCounter = sampleMeter
       .doubleCounterBuilder("calculated_used_space")
       .setDescription("Counts disk space used by file extension.")
@@ -34,7 +36,11 @@ public class DoubleCounterExample {
         .startSpan();
     DoubleCounterExample example = new DoubleCounterExample();
     try (Scope scope = tracer.withSpan(span)) {
-      example.calculateSpaceUsedByFilesWithExtension("dll", directoryToCountIn);
+      List<String> extensionsToFind = new ArrayList<>();
+      extensionsToFind.add("dll");
+      extensionsToFind.add("png");
+      extensionsToFind.add("exe");
+      example.calculateSpaceUsedByFilesWithExtension(extensionsToFind, directoryToCountIn);
     } catch (Exception e) {
       Status status = Status.UNKNOWN.withDescription("Error while calculating used space");
       span.setStatus(status);
@@ -43,13 +49,15 @@ public class DoubleCounterExample {
     }
   }
 
-  public void calculateSpaceUsedByFilesWithExtension(String extension, File directory) {
+  public void calculateSpaceUsedByFilesWithExtension(List<String> extensions, File directory) {
     File[] files = directory.listFiles();
     if (files != null) {
       for (File file : files) {
-        if (file.getName().endsWith("." + extension)) {
-          //we can add values to counter without addition label key-values pairs
-          methodCallCounter.add((double) file.length() / 1_000_000);
+        for (String extension : extensions) {
+          if (file.getName().endsWith("." + extension)) {
+            //we can add values to counter without addition label key-values pairs
+            methodCallCounter.add((double) file.length() / 1_000_000, "file_extension", extension);
+          }
         }
       }
     }
