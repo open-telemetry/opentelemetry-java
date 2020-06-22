24b4be90c31f8213a50da3aa7a7af94a50891e8f
John Watson <jkwatson@gmail.com>
2020-06-22 14:37:21 -0700

    Convert CorrelationContext entry keys and values to simple Strings (#1363)
    * convert CC entry keys and values to simple Strings
    
    * fix broken javadoc

--- api/src/test/java/io/opentelemetry/correlationcontext/EntryTest.java
+++ api/src/test/java/io/opentelemetry/correlationcontext/EntryTest.java
@@ -20,18 +20,22 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.testing.EqualsTester;
 import io.opentelemetry.correlationcontext.EntryMetadata.EntryTtl;
+import java.util.Arrays;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /** Tests for {@link Entry}. */
 @RunWith(JUnit4.class)
 public final class EntryTest {
+  @Rule public final ExpectedException thrown = ExpectedException.none();
 
-  private static final EntryKey KEY = EntryKey.create("KEY");
-  private static final EntryKey KEY_2 = EntryKey.create("KEY2");
-  private static final EntryValue VALUE = EntryValue.create("VALUE");
-  private static final EntryValue VALUE_2 = EntryValue.create("VALUE2");
+  private static final String KEY = "KEY";
+  private static final String KEY_2 = "KEY2";
+  private static final String VALUE = "VALUE";
+  private static final String VALUE_2 = "VALUE2";
   private static final EntryMetadata METADATA_UNLIMITED_PROPAGATION =
       EntryMetadata.create(EntryTtl.UNLIMITED_PROPAGATION);
   private static final EntryMetadata METADATA_NO_PROPAGATION =
@@ -58,5 +62,69 @@ public final class EntryTest {
         .addEqualityGroup(Entry.create(KEY_2, VALUE, METADATA_UNLIMITED_PROPAGATION))
         .addEqualityGroup(Entry.create(KEY, VALUE, METADATA_NO_PROPAGATION))
         .testEquals();
+  }
+
+  @Test
+  public void testKeyMaxLength() {
+    assertThat(Entry.MAX_KEY_LENGTH).isEqualTo(255);
+  }
+
+  @Test
+  public void create_AllowEntryKeyNameWithMaxLength() {
+    char[] chars = new char[Entry.MAX_KEY_LENGTH];
+    Arrays.fill(chars, 'k');
+    String key = new String(chars);
+    assertThat(Entry.create(key, "value", Entry.METADATA_UNLIMITED_PROPAGATION)).isNotNull();
+  }
+
+  @Test
+  public void create_DisallowEntryKeyNameOverMaxLength() {
+    char[] chars = new char[Entry.MAX_KEY_LENGTH + 1];
+    Arrays.fill(chars, 'k');
+    String key = new String(chars);
+    thrown.expect(IllegalArgumentException.class);
+    Entry.create(key, "value", Entry.METADATA_UNLIMITED_PROPAGATION);
+  }
+
+  @Test
+  public void create_DisallowKeyUnprintableChars() {
+    thrown.expect(IllegalArgumentException.class);
+    Entry.create("\2ab\3cd", "value", Entry.METADATA_UNLIMITED_PROPAGATION);
+  }
+
+  @Test
+  public void createString_DisallowKeyEmpty() {
+    thrown.expect(IllegalArgumentException.class);
+    Entry.create("", "value", Entry.METADATA_UNLIMITED_PROPAGATION);
+  }
+
+  @Test
+  public void testValueMaxLength() {
+    assertThat(Entry.MAX_VALUE_LENGTH).isEqualTo(255);
+  }
+
+  @Test
+  public void create_AllowEntryValueWithMaxLength() {
+    char[] chars = new char[Entry.MAX_VALUE_LENGTH];
+    Arrays.fill(chars, 'v');
+    String value = new String(chars);
+    assertThat(Entry.create("key", value, Entry.METADATA_UNLIMITED_PROPAGATION).getValue())
+        .isEqualTo(value);
+  }
+
+  @Test
+  public void create_DisallowEntryValueOverMaxLength() {
+    char[] chars = new char[Entry.MAX_VALUE_LENGTH + 1];
+    Arrays.fill(chars, 'v');
+    String value = new String(chars);
+    thrown.expect(IllegalArgumentException.class);
+    Entry.create("key", value, Entry.METADATA_UNLIMITED_PROPAGATION);
+  }
+
+  @Test
+  public void disallowEntryValueWithUnprintableChars() {
+    String value = "\2ab\3cd";
+    thrown.expect(IllegalArgumentException.class);
+    Entry.create("key", value, Entry.METADATA_UNLIMITED_PROPAGATION);
   }
 }
