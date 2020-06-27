59e7c9e5945c3d4572291fcad29f23404aee8d99
Sergei Malafeev <sergei@malafeev.org>
2020-06-27 14:52:22 +0800

    remove @Nonnull
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>

--- exporters/otlp/src/main/java/io/opentelemetry/exporters/otlp/OtlpGrpcSpanExporter.java
+++ exporters/otlp/src/main/java/io/opentelemetry/exporters/otlp/OtlpGrpcSpanExporter.java
@@ -35,7 +35,6 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 
@@ -227,7 +226,7 @@ public final class OtlpGrpcSpanExporter implements SpanExporter {
      * @param value header value
      * @return this builder's instance
      */
-    public Builder addHeader(@Nonnull String key, @Nonnull String value) {
+    public Builder addHeader(String key, String value) {
       if (metadata == null) {
         metadata = new Metadata();
       }
