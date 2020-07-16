e9e4dfd3019049ad02df8b880ed88f4d532aaaa4
Terry Wang <wtyanan@google.com>
2020-07-16 05:07:55 +0000

    Added two abstract getter method for obtaining page information

--- sdk_extensions/zpages/src/main/java/io/opentelemetry/sdk/extensions/zpages/ZPageHandler.java
+++ sdk_extensions/zpages/src/main/java/io/opentelemetry/sdk/extensions/zpages/ZPageHandler.java
@@ -33,6 +33,20 @@ public abstract class ZPageHandler {
   public abstract String getUrlPath();
 
   /**
+   * Returns the name of the zPage.
+   *
+   * @return the name of the zPage.
+   */
+  public abstract String getPageName();
+
+  /**
+   * Returns the description of the zPage.
+   *
+   * @return the description of the zPage.
+   */
+  public abstract String getPageDescription();
+
+  /**
    * Emits the generated HTML page to the {@code outputStream}.
    *
    * @param queryMap the map of the URL query parameters.
