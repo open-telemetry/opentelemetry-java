84abdf2f6bee13a9525adf424424c268539c244e
Anuraag Agrawal <anuraaga@gmail.com>
2020-07-20 12:21:53 +0900

    javadoc

--- sdk/src/jmh/java/io/opentelemetry/sdk/trace/PrintThrowableBenchmark.java
+++ sdk/src/jmh/java/io/opentelemetry/sdk/trace/PrintThrowableBenchmark.java
@@ -59,6 +59,7 @@ public class PrintThrowableBenchmark {
     }
   }
 
+  /** Measures performance of {@link StringBuilder} + Guava {@link CharStreams} */
   @Benchmark
   public String normalPrintWriter() {
     StringBuilder sb = new StringBuilder();
@@ -67,6 +68,7 @@ public class PrintThrowableBenchmark {
     return sb.toString();
   }
 
+  /** Measures performance of JDK {@link StringWriter} */
   @Benchmark
   public String stringWriter() {
     StringWriter sw = new StringWriter();
@@ -75,6 +77,7 @@ public class PrintThrowableBenchmark {
     return sw.toString();
   }
 
+  /** Measures performance of our custom {@link StringBuilderPrintWriter */
   @Benchmark
   public String customPrintWriter() {
     StringBuilder sb = new StringBuilder();
