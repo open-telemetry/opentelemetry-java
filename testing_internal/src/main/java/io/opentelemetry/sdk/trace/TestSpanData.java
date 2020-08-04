f9cc04fbce6bbdf4dbf8df59cb86da74d74373a6
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-08-04 14:24:34 +0900

    Don't depend on testing-internal automatically since some modules should include the SDK.

--- testing_internal/src/main/java/io/opentelemetry/sdk/trace/TestSpanData.java
+++ testing_internal/src/main/java/io/opentelemetry/sdk/trace/TestSpanData.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.opentelemetry.sdk.trace;
 
 import io.opentelemetry.common.Attributes;
@@ -9,8 +25,10 @@ import io.opentelemetry.trace.TraceFlags;
 import io.opentelemetry.trace.TraceState;
 import java.util.Collections;
 
+/** Factories for {@link SpanData} for use in tests. */
 public final class TestSpanData {
 
+  /** Returns a {@link SpanData.Builder} with optional fields filled to empty values. */
   // Intentionally calling AutoValue constructor for testing.
   @SuppressWarnings("AutoValueSubclassLeaked")
   public static SpanData.Builder newBuilder() {
