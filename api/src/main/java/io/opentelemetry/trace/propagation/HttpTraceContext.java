5498ab2b82fd7c54bb08acbfdd1968ae742870cc
Sergei Malafeev <sergei@malafeev.org>
2020-09-25 08:05:29 +0800

    validate version from traceparent according to spec (#1688)
    * #1674 validate version from traceparent according to spec
    
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>
    
    * add test
    
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>
    
    * add test
    
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>
    
    * rename TraceVersion to WireFormatUtils
    
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>
    
    * rename test method
    
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>
    
    * use precalculated versions
    
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>
    
    * get rid of unneded class
    
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>
    
    * get rid of empty lines
    
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>
    
    * fix style
    
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>

--- api/src/main/java/io/opentelemetry/trace/propagation/HttpTraceContext.java
+++ api/src/main/java/io/opentelemetry/trace/propagation/HttpTraceContext.java
@@ -31,7 +31,9 @@ import io.opentelemetry.trace.TraceState;
 import io.opentelemetry.trace.TracingContextUtils;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import javax.annotation.concurrent.Immutable;
@@ -69,8 +71,20 @@ public class HttpTraceContext implements TextMapPropagator {
   private static final char TRACESTATE_ENTRY_DELIMITER = ',';
   private static final Pattern TRACESTATE_ENTRY_DELIMITER_SPLIT_PATTERN =
       Pattern.compile("[ \t]*" + TRACESTATE_ENTRY_DELIMITER + "[ \t]*");
-
+  private static final Set<String> VALID_VERSIONS;
   private static final HttpTraceContext INSTANCE = new HttpTraceContext();
+
+  static {
+    // A valid version is 1 byte representing an 8-bit unsigned integer, version ff is invalid.
+    VALID_VERSIONS = new HashSet<>();
+    for (int i = 0; i < 255; i++) {
+      String version = Long.toHexString(i);
+      if (version.length() < 2) {
+        version = '0' + version;
+      }
+      VALID_VERSIONS.add(version);
+    }
+  }
 
   private HttpTraceContext() {
     // singleton
@@ -193,6 +207,11 @@ public class HttpTraceContext implements TextMapPropagator {
     }
 
     try {
+      String version = traceparent.substring(0, 2);
+      if (!VALID_VERSIONS.contains(version)) {
+        return SpanContext.getInvalid();
+      }
+
       String traceId =
           traceparent.substring(TRACE_ID_OFFSET, TRACE_ID_OFFSET + TraceId.getHexLength());
       String spanId = traceparent.substring(SPAN_ID_OFFSET, SPAN_ID_OFFSET + SpanId.getHexLength());
