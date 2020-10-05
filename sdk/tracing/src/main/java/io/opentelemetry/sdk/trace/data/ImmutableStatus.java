9853e5e8fc2c026e84885d456ece0eb444fbcc82
Bogdan Drutu <bogdandrutu@gmail.com>
2020-10-05 11:14:04 -0700

    Avoid creating new status objects when null description
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- sdk/tracing/src/main/java/io/opentelemetry/sdk/trace/data/ImmutableStatus.java
+++ sdk/tracing/src/main/java/io/opentelemetry/sdk/trace/data/ImmutableStatus.java
@@ -6,8 +6,10 @@
 package io.opentelemetry.sdk.trace.data;
 
 import com.google.auto.value.AutoValue;
+import io.opentelemetry.sdk.trace.data.SpanData.Status;
 import io.opentelemetry.trace.Span;
 import io.opentelemetry.trace.StatusCanonicalCode;
+import java.util.EnumMap;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 
@@ -25,13 +27,33 @@ public abstract class ImmutableStatus implements SpanData.Status {
    * The operation has been validated by an Application developers or Operator to have completed
    * successfully.
    */
-  public static final SpanData.Status OK = create(StatusCanonicalCode.OK, null);
+  public static final SpanData.Status OK = createInternal(StatusCanonicalCode.OK, null);
 
   /** The default status. */
-  public static final SpanData.Status UNSET = create(StatusCanonicalCode.UNSET, null);
+  public static final SpanData.Status UNSET = createInternal(StatusCanonicalCode.UNSET, null);
 
   /** The operation contains an error. */
-  public static final SpanData.Status ERROR = create(StatusCanonicalCode.ERROR, null);
+  public static final SpanData.Status ERROR = createInternal(StatusCanonicalCode.ERROR, null);
+
+  // Visible for test
+  static final EnumMap<StatusCanonicalCode, Status> codeToStatus =
+      new EnumMap<>(StatusCanonicalCode.class);
+
+  static {
+    codeToStatus.put(StatusCanonicalCode.UNSET, UNSET);
+    codeToStatus.put(StatusCanonicalCode.OK, OK);
+    codeToStatus.put(StatusCanonicalCode.ERROR, ERROR);
+
+    // Ensure all values are in the map, even if we don't have constants defined.
+    // This can happen if the API version is newer than the SDK and new values were added there.
+    StatusCanonicalCode[] codes = StatusCanonicalCode.values();
+    for (StatusCanonicalCode code : codes) {
+      Status status = codeToStatus.get(code);
+      if (status == null) {
+        codeToStatus.put(code, createInternal(code, null));
+      }
+    }
+  }
 
   /**
    * Creates a derived instance of {@code Status} with the given description.
@@ -41,6 +63,14 @@ public abstract class ImmutableStatus implements SpanData.Status {
    * @since 0.1.0
    */
   public static SpanData.Status create(
+      StatusCanonicalCode canonicalCode, @Nullable String description) {
+    if (description == null) {
+      return codeToStatus.get(canonicalCode);
+    }
+    return createInternal(canonicalCode, description);
+  }
+
+  private static SpanData.Status createInternal(
       StatusCanonicalCode canonicalCode, @Nullable String description) {
     return new AutoValue_ImmutableStatus(canonicalCode, description);
   }
