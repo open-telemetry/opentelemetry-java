6859fca1d56d4ae630566d2f3d980e1a3c9eb87f
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-10-05 12:04:47 +0900

    Use spotless for formatting / license check

--- extensions/trace_propagators/src/main/java/io/opentelemetry/extensions/trace/propagation/StringUtils.java
+++ extensions/trace_propagators/src/main/java/io/opentelemetry/extensions/trace/propagation/StringUtils.java
@@ -1,4 +1,9 @@
 /*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// Includes work from:
+/*
  * Copyright 2010, Guava Authors
  * SPDX-License-Identifier: Apache-2.0
  */
