5a0a62a8300fb64260e8a940018170d8bad12770
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-10-09 11:43:12 +0900

    Add a few missing package-info.java

---
+++ api/src/main/java/io/opentelemetry/metrics/spi/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * The SPI (service provider interface) for initializing the OpenTelemetry metrics API.
+ */
+@ParametersAreNonnullByDefault
+package io.opentelemetry.metrics.spi;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ api/src/main/java/io/opentelemetry/trace/propagation/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Default OpenTelemetry remote context propagators.
+ */
+@ParametersAreNonnullByDefault
+package io.opentelemetry.trace.propagation;
+
+import javax.annotation.ParametersAreNonnullByDefault;
