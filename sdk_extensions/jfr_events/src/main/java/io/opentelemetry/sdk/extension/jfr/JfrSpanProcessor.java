52e1a4250bca9c3c4bc3b4e30557d16f10a6f167
sfriberg <sfriberg@kth.se>
2020-10-22 13:54:20 -0500

    spotless

--- sdk_extensions/jfr_events/src/main/java/io/opentelemetry/sdk/extension/jfr/JfrSpanProcessor.java
+++ sdk_extensions/jfr_events/src/main/java/io/opentelemetry/sdk/extension/jfr/JfrSpanProcessor.java
@@ -1,25 +1,15 @@
 /*
- * Copyright 2020, OpenTelemetry Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
  */
+
 package io.opentelemetry.sdk.extension.jfr;
+
+import static java.util.Objects.nonNull;
 
 import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.trace.ReadWriteSpan;
-import static java.util.Objects.nonNull;
-
 import io.opentelemetry.sdk.trace.ReadableSpan;
 import io.opentelemetry.sdk.trace.SpanProcessor;
 import io.opentelemetry.trace.SpanContext;
@@ -27,53 +17,51 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * Span processor to create new JFR events for the Span as they are started, and
- * commit on end.
+ * Span processor to create new JFR events for the Span as they are started, and commit on end.
  *
- * <p>
- * NOTE: JfrSpanProcessor must be running synchronously to ensure that duration
- * is correctly captured.
+ * <p>NOTE: JfrSpanProcessor must be running synchronously to ensure that duration is correctly
+ * captured.
  */
 public class JfrSpanProcessor implements SpanProcessor {
 
-    private final Map<SpanContext, SpanEvent> spanEvents = new ConcurrentHashMap<>();
+  private final Map<SpanContext, SpanEvent> spanEvents = new ConcurrentHashMap<>();
 
-    @Override
-    public void onStart(ReadWriteSpan span, Context parentContext) {
-        if (span.getSpanContext().isValid()) {
-            SpanEvent event = new SpanEvent(span.toSpanData());
-            event.begin();
-            spanEvents.put(span.getSpanContext(), event);
-        }
+  @Override
+  public void onStart(ReadWriteSpan span, Context parentContext) {
+    if (span.getSpanContext().isValid()) {
+      SpanEvent event = new SpanEvent(span.toSpanData());
+      event.begin();
+      spanEvents.put(span.getSpanContext(), event);
     }
+  }
 
-    @Override
-    public boolean isStartRequired() {
-        return true;
-    }
+  @Override
+  public boolean isStartRequired() {
+    return true;
+  }
 
-    @Override
-    public void onEnd(ReadableSpan rs) {
-        SpanEvent event = spanEvents.remove(rs.getSpanContext());
-        if (nonNull(event) && event.shouldCommit()) {
-            event.commit();
-        }
+  @Override
+  public void onEnd(ReadableSpan rs) {
+    SpanEvent event = spanEvents.remove(rs.getSpanContext());
+    if (nonNull(event) && event.shouldCommit()) {
+      event.commit();
     }
+  }
 
-    @Override
-    public boolean isEndRequired() {
-        return true;
-    }
+  @Override
+  public boolean isEndRequired() {
+    return true;
+  }
 
-    @Override
-    public CompletableResultCode shutdown() {
-        spanEvents.forEach((id, event) -> event.commit());
-        spanEvents.clear();
-        return CompletableResultCode.ofSuccess();
-    }
+  @Override
+  public CompletableResultCode shutdown() {
+    spanEvents.forEach((id, event) -> event.commit());
+    spanEvents.clear();
+    return CompletableResultCode.ofSuccess();
+  }
 
-    @Override
-    public CompletableResultCode forceFlush() {
-        return CompletableResultCode.ofSuccess();
-    }
+  @Override
+  public CompletableResultCode forceFlush() {
+    return CompletableResultCode.ofSuccess();
+  }
 }
--- sdk_extensions/jfr_events/src/test/java/io/opentelemetry/sdk/extension/jfr/JfrSpanProcessorTest.java
+++ sdk_extensions/jfr_events/src/test/java/io/opentelemetry/sdk/extension/jfr/JfrSpanProcessorTest.java
@@ -1,28 +1,17 @@
 /*
- * Copyright 2020, OpenTelemetry Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package io.opentelemetry.sdk.extension.jfr;
 
-import io.opentelemetry.OpenTelemetry;
 import static org.junit.Assert.assertEquals;
 
+import io.opentelemetry.OpenTelemetry;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.trace.Tracer;
 import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.Tracer;
 import io.opentelemetry.trace.TracingContextUtils;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -40,8 +29,8 @@ public class JfrSpanProcessorTest {
 
   /** Simple test to validate JFR events for Span and Scope. */
   public JfrSpanProcessorTest() {
-      tracer = OpenTelemetry.getGlobalTracer("JfrSpanProcessorTest");
-      OpenTelemetrySdk.getTracerManagement().addSpanProcessor(new JfrSpanProcessor());
+    tracer = OpenTelemetry.getGlobalTracer("JfrSpanProcessorTest");
+    OpenTelemetrySdk.getTracerManagement().addSpanProcessor(new JfrSpanProcessor());
   }
 
   /**
