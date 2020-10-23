14b41342700bfaf436f20f082272f2f583b69583
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2020-10-23 02:50:51 +0200

    Fix the build.
    Ew, Java and your improper type inference...

--- integration_tests/tracecontext/src/main/java/io/opentelemetry/Application.java
+++ integration_tests/tracecontext/src/main/java/io/opentelemetry/Application.java
@@ -8,6 +8,8 @@ package io.opentelemetry;
 import com.google.gson.Gson;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.propagation.DefaultContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator.Getter;
+import io.opentelemetry.context.propagation.TextMapPropagator.Setter;
 import io.opentelemetry.trace.Span;
 import io.opentelemetry.trace.propagation.HttpTraceContext;
 import java.util.ArrayList;
@@ -17,14 +19,16 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.RequestBody;
-import okhttp3.Response;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 import spark.Spark;
 
 public class Application {
-
   private static final Logger logger = Logger.getLogger(Application.class.getName());
 
   private Application() {}
@@ -39,87 +43,98 @@ public class Application {
     Spark.port(5000);
     Spark.post(
         "verify-tracecontext",
-        (request, response) -> {
-          final Gson gson = new Gson();
+        new Route() {
+          @Override
+          public Object handle(Request request, Response response) {
+            final Gson gson = new Gson();
 
-          final io.opentelemetry.Request[] requests =
-              gson.fromJson(request.body(), io.opentelemetry.Request[].class);
+            final io.opentelemetry.Request[] requests =
+                gson.fromJson(request.body(), io.opentelemetry.Request[].class);
 
-          Context context =
+            Context context =
+                OpenTelemetry.getGlobalPropagators()
+                    .getTextMapPropagator()
+                    .extract(
+                        Context.current(),
+                        request.raw(),
+                        new Getter<HttpServletRequest>() {
+                          @Override
+                          public Collection<String> keys(HttpServletRequest carrier) {
+                            return Collections.list(carrier.getHeaderNames());
+                          }
+
+                          @Override
+                          public String get(HttpServletRequest carrier, String key) {
+                            Enumeration<String> headers = carrier.getHeaders(key);
+                            if (headers == null || !headers.hasMoreElements()) {
+                              return null;
+                            }
+                            List<String> values = new ArrayList<>();
+                            while (headers.hasMoreElements()) {
+                              String nextElement = headers.nextElement();
+                              if (!nextElement.trim().isEmpty()) {
+                                values.add(nextElement);
+                              }
+                            }
+                            if (values.isEmpty()) {
+                              return null;
+                            }
+                            if (values.size() == 1) {
+                              return values.get(0);
+                            }
+                            StringBuilder builder = new StringBuilder(values.get(0));
+                            for (int i = 1; i < values.size(); i++) {
+                              builder.append(",").append(values.get(i));
+                            }
+
+                            return builder.toString();
+                          }
+                        });
+
+            for (io.opentelemetry.Request req : requests) {
+              Span span =
+                  OpenTelemetry.getGlobalTracer("validation-server")
+                      .spanBuilder("Entering Validation Server")
+                      .setParent(context)
+                      .startSpan();
+
+              Context withSpanContext = context.with(span);
+
+              // Make a new request using the builder
+              okhttp3.Request.Builder reqBuilder = new okhttp3.Request.Builder();
+
+              // Inject the current context into the new request.
               OpenTelemetry.getGlobalPropagators()
                   .getTextMapPropagator()
-                  .extract(
-                      Context.current(),
-                      request.raw(),
-                      new Getter<HttpServletRequest>() {
+                  .inject(
+                      withSpanContext,
+                      reqBuilder,
+                      new Setter<okhttp3.Request.Builder>() {
                         @Override
-                        public Collection<String> keys(HttpServletRequest carrier) {
-                          return Collections.list(carrier.getHeaderNames());
-                        }
-
-                        @Override
-                        public String get(HttpServletRequest carrier, String key) {
-                          Enumeration<String> headers = carrier.getHeaders(key);
-                          if (headers == null || !headers.hasMoreElements()) {
-                            return null;
-                          }
-                          List<String> values = new ArrayList<>();
-                          while (headers.hasMoreElements()) {
-                            String nextElement = headers.nextElement();
-                            if (!nextElement.trim().isEmpty()) {
-                              values.add(nextElement);
-                            }
-                          }
-                          if (values.isEmpty()) {
-                            return null;
-                          }
-                          if (values.size() == 1) {
-                            return values.get(0);
-                          }
-                          StringBuilder builder = new StringBuilder(values.get(0));
-                          for (int i = 1; i < values.size(); i++) {
-                            builder.append(",").append(values.get(i));
-                          }
-
-                          return builder.toString();
+                        public void set(okhttp3.Request.Builder carrier, String key, String value) {
+                          carrier.addHeader(key, value);
                         }
                       });
 
-          for (io.opentelemetry.Request req : requests) {
-            Span span =
-                OpenTelemetry.getGlobalTracer("validation-server")
-                    .spanBuilder("Entering Validation Server")
-                    .setParent(context)
-                    .startSpan();
+              // Add the post body and build the request
+              String argumentsJson = gson.toJson(req.getArguments());
+              RequestBody argumentsBody =
+                  RequestBody.create(
+                      MediaType.parse("application/json; charset=utf-8"), argumentsJson);
+              okhttp3.Request newRequest = reqBuilder.url(req.getUrl()).post(argumentsBody).build();
 
-            Context withSpanContext = context.with(span);
-
-            // Make a new request using the builder
-            okhttp3.Request.Builder reqBuilder = new okhttp3.Request.Builder();
-
-            // Inject the current context into the new request.
-            OpenTelemetry.getGlobalPropagators()
-                .getTextMapPropagator()
-                .inject(withSpanContext, reqBuilder, okhttp3.Request.Builder::addHeader);
-
-            // Add the post body and build the request
-            String argumentsJson = gson.toJson(req.getArguments());
-            RequestBody argumentsBody =
-                RequestBody.create(
-                    MediaType.parse("application/json; charset=utf-8"), argumentsJson);
-            okhttp3.Request newRequest = reqBuilder.url(req.getUrl()).post(argumentsBody).build();
-
-            // Execute the request
-            OkHttpClient client = new OkHttpClient();
-            try (Response res = client.newCall(newRequest).execute()) {
-              logger.info("response: " + res.code());
-            } catch (Exception e) {
-              logger.log(Level.SEVERE, "failed to send", e);
+              // Execute the request
+              OkHttpClient client = new OkHttpClient();
+              try (okhttp3.Response res = client.newCall(newRequest).execute()) {
+                logger.info("response: " + res.code());
+              } catch (Exception e) {
+                logger.log(Level.SEVERE, "failed to send", e);
+              }
+              span.end();
             }
-            span.end();
-          }
 
-          return "Done";
+            return "Done";
+          }
         });
 
     Spark.awaitInitialization();
