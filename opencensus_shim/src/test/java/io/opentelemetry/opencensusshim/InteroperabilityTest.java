22fa46a1182eccead6a7471a8f25f8d0a88eb37b
zoercai <zoc@google.com>
2020-10-27 18:39:21 +1100

    Add interoperability test: all opencensus spans

--- opencensus_shim/src/test/java/io/opentelemetry/opencensusshim/InteroperabilityTest.java
+++ opencensus_shim/src/test/java/io/opentelemetry/opencensusshim/InteroperabilityTest.java
@@ -38,19 +38,19 @@ public class InteroperabilityTest {
   @Before
   public void init() {
     MockitoAnnotations.initMocks(this);
+    when(spanExporter.export(any())).thenReturn(CompletableResultCode.ofSuccess());
+    SpanProcessor spanProcessor = SimpleSpanProcessor.builder(spanExporter).build();
+    OpenTelemetrySdk.getTracerManagement().addSpanProcessor(spanProcessor);
   }
 
   @Test
   public void testParentChildRelationshipsAreExportedCorrectly() {
-    when(spanExporter.export(any())).thenReturn(CompletableResultCode.ofSuccess());
-    SpanProcessor spanProcessor = SimpleSpanProcessor.builder(spanExporter).build();
-    OpenTelemetrySdk.getTracerManagement().addSpanProcessor(spanProcessor);
-
     Tracer tracer = OpenTelemetry.getTracer("io.opentelemetry.test.scoped.span.1");
     Span span = tracer.spanBuilder("OpenTelemetrySpan").startSpan();
     try (Scope scope = TracingContextUtils.currentContextWith(span)) {
       span.addEvent("OpenTelemetry: Event 1");
-      createOpenCensusScopedSpans();
+      createOpenCensusScopedSpanWithChildSpan(
+          /* withInnerOpenTelemetrySpan= */ true, /* withInnerOpenCensusSpan= */ false);
       span.addEvent("OpenTelemetry: Event 2");
     } finally {
       span.end();
@@ -70,10 +70,10 @@ public class InteroperabilityTest {
 
     assertEquals(1, export2.size());
     SpanData spanData2 = export2.iterator().next();
-    assertEquals("OpenCensusSpan", spanData2.getName());
+    assertEquals("OpenCensusSpan1", spanData2.getName());
     assertEquals(2, spanData2.getTotalRecordedEvents());
-    assertEquals("OpenCensus: Event 1", spanData2.getEvents().get(0).getName());
-    assertEquals("OpenCensus: Event 2", spanData2.getEvents().get(1).getName());
+    assertEquals("OpenCensus1: Event 1", spanData2.getEvents().get(0).getName());
+    assertEquals("OpenCensus1: Event 2", spanData2.getEvents().get(1).getName());
 
     assertEquals(1, export3.size());
     SpanData spanData3 = export3.iterator().next();
@@ -87,7 +87,8 @@ public class InteroperabilityTest {
     assertEquals(NULL_SPAN_ID, spanData3.getParentSpanId());
   }
 
-  private static void createOpenCensusScopedSpans() {
+  @Test
+  public void testParentChildRelationshipsAreExportedCorrectlyForOpenCensusOnly() {
     io.opencensus.trace.Tracer tracer = Tracing.getTracer();
     try (io.opencensus.common.Scope scope =
         tracer
@@ -97,13 +98,81 @@ public class InteroperabilityTest {
             .startScopedSpan()) {
       io.opencensus.trace.Span span = tracer.getCurrentSpan();
       span.addAnnotation("OpenCensus: Event 1");
-      createOpenTelemetryScopedSpans();
+      createOpenCensusScopedSpanWithChildSpan(
+          /* withInnerOpenTelemetrySpan= */ false, /* withInnerOpenCensusSpan= */ true);
       span.addAnnotation("OpenCensus: Event 2");
+    }
+    Tracing.getExportComponent().shutdown();
+
+    verify(spanExporter, times(3)).export(spanDataCaptor.capture());
+    Collection<SpanData> export1 = spanDataCaptor.getAllValues().get(0);
+    Collection<SpanData> export2 = spanDataCaptor.getAllValues().get(1);
+    Collection<SpanData> export3 = spanDataCaptor.getAllValues().get(2);
+
+    assertEquals(1, export1.size());
+    SpanData spanData1 = export1.iterator().next();
+    assertEquals("OpenCensusSpan2", spanData1.getName());
+    assertEquals(2, spanData1.getTotalRecordedEvents());
+    assertEquals("OpenCensus2: Event 1", spanData1.getEvents().get(0).getName());
+    assertEquals("OpenCensus2: Event 2", spanData1.getEvents().get(1).getName());
+
+    assertEquals(1, export2.size());
+    SpanData spanData2 = export2.iterator().next();
+    assertEquals("OpenCensusSpan1", spanData2.getName());
+    assertEquals(2, spanData2.getTotalRecordedEvents());
+    assertEquals("OpenCensus1: Event 1", spanData2.getEvents().get(0).getName());
+    assertEquals("OpenCensus1: Event 2", spanData2.getEvents().get(1).getName());
+
+    assertEquals(1, export3.size());
+    SpanData spanData3 = export3.iterator().next();
+    assertEquals("OpenCensusSpan", spanData3.getName());
+    assertEquals(2, spanData3.getTotalRecordedEvents());
+    assertEquals("OpenCensus: Event 1", spanData3.getEvents().get(0).getName());
+    assertEquals("OpenCensus: Event 2", spanData3.getEvents().get(1).getName());
+
+    assertEquals(spanData2.getSpanId(), spanData1.getParentSpanId());
+    assertEquals(spanData3.getSpanId(), spanData2.getParentSpanId());
+    assertEquals(NULL_SPAN_ID, spanData3.getParentSpanId());
+  }
+
+  private static void createOpenCensusScopedSpanWithChildSpan(
+      boolean withInnerOpenTelemetrySpan, boolean withInnerOpenCensusSpan) {
+    io.opencensus.trace.Tracer tracer = Tracing.getTracer();
+    try (io.opencensus.common.Scope scope =
+        tracer
+            .spanBuilder("OpenCensusSpan1")
+            .setRecordEvents(true)
+            .setSampler(Samplers.alwaysSample())
+            .startScopedSpan()) {
+      io.opencensus.trace.Span span = tracer.getCurrentSpan();
+      span.addAnnotation("OpenCensus1: Event 1");
+      if (withInnerOpenTelemetrySpan) {
+        createOpenTelemetryScopedSpan();
+      }
+      if (withInnerOpenCensusSpan) {
+        createOpenCensusScopedSpan();
+      }
+      span.addAnnotation("OpenCensus1: Event 2");
     }
     Tracing.getExportComponent().shutdown();
   }
 
-  private static void createOpenTelemetryScopedSpans() {
+  private static void createOpenCensusScopedSpan() {
+    io.opencensus.trace.Tracer tracer = Tracing.getTracer();
+    try (io.opencensus.common.Scope scope =
+        tracer
+            .spanBuilder("OpenCensusSpan2")
+            .setRecordEvents(true)
+            .setSampler(Samplers.alwaysSample())
+            .startScopedSpan()) {
+      io.opencensus.trace.Span span = tracer.getCurrentSpan();
+      span.addAnnotation("OpenCensus2: Event 1");
+      span.addAnnotation("OpenCensus2: Event 2");
+    }
+    Tracing.getExportComponent().shutdown();
+  }
+
+  private static void createOpenTelemetryScopedSpan() {
     Tracer tracer = OpenTelemetry.getTracer("io.opentelemetry.test.scoped.span.2");
     Span span = tracer.spanBuilder("OpenTelemetrySpan2").startSpan();
     try (Scope scope = TracingContextUtils.currentContextWith(span)) {
