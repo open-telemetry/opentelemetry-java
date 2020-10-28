b7d00fa47fcad9a1f11b69c47d2767f2c70c6fd1
zoercai <zoc@google.com>
2020-10-28 17:34:26 +1100

    Fix license

--- opencensus_shim/src/test/java/io/opentelemetry/opencensusshim/InteroperabilityTest.java
+++ opencensus_shim/src/test/java/io/opentelemetry/opencensusshim/InteroperabilityTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.opencensusshim;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
