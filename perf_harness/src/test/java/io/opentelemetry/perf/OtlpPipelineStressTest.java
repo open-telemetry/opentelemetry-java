1eca9d37727ee8f1c84509037d03367679eb9ec5
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-10-30 00:06:43 +0900

    Add workflow to copy docker hub images to bintray and use in integration tests. (#1915)
    * Add workflow to copy docker images to bintray.
    
    * Fix registry
    
    * Update images
    
    * Spotless

--- perf_harness/src/test/java/io/opentelemetry/perf/OtlpPipelineStressTest.java
+++ perf_harness/src/test/java/io/opentelemetry/perf/OtlpPipelineStressTest.java
@@ -59,7 +59,9 @@ public class OtlpPipelineStressTest {
 
   @Container
   public static GenericContainer<?> collectorContainer =
-      new GenericContainer<>(DockerImageName.parse("otel/opentelemetry-collector-dev:latest"))
+      new GenericContainer<>(
+              DockerImageName.parse(
+                  "open-telemetry-docker-dev.bintray.io/java-test-containers:otel-collector-dev"))
           .withNetwork(network)
           .withNetworkAliases("otel-collector")
           .withExposedPorts(OTLP_RECEIVER_PORT)
@@ -83,7 +85,9 @@ public class OtlpPipelineStressTest {
 
   @Container
   public static GenericContainer<?> toxiproxyContainer =
-      new GenericContainer<>(DockerImageName.parse("shopify/toxiproxy:latest"))
+      new GenericContainer<>(
+              DockerImageName.parse(
+                  "open-telemetry-docker-dev.bintray.io/java-test-containers:toxiproxy"))
           .withNetwork(network)
           .withNetworkAliases("toxiproxy")
           .withExposedPorts(TOXIPROXY_CONTROL_PORT, COLLECTOR_PROXY_PORT)
