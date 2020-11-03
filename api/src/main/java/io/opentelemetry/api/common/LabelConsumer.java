cf6d3b608b6db34ed9480233c5df84e26a1a0ed2
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-11-03 16:14:40 +0900

    more

--- api/src/main/java/io/opentelemetry/api/common/LabelConsumer.java
+++ api/src/main/java/io/opentelemetry/api/common/LabelConsumer.java
@@ -7,9 +7,6 @@ package io.opentelemetry.api.common;
 
 /**
  * Convenience interface for consuming {@link Labels}.
- *
- * <p>This interface should be considered to be a FunctionalInterface in the java 8+ meaning of that
- * term.
  */
 @FunctionalInterface
 public interface LabelConsumer {
