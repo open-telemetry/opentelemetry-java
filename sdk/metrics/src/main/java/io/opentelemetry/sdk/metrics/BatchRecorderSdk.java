743ec823e5faae9f20f9deb1f2bbeb286ea3c281
jkwatson <jkwatson@gmail.com>
2020-11-04 13:21:58 -0800

    Make the BatchRecorderSdk actually batch the recordings Rather than just forwarding them immediately.

--- sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/BatchRecorderSdk.java
+++ sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/BatchRecorderSdk.java
@@ -10,18 +10,24 @@ import io.opentelemetry.api.metrics.BatchRecorder;
 import io.opentelemetry.api.metrics.DoubleCounter;
 import io.opentelemetry.api.metrics.DoubleUpDownCounter;
 import io.opentelemetry.api.metrics.DoubleValueRecorder;
+import io.opentelemetry.api.metrics.Instrument;
 import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.metrics.LongUpDownCounter;
 import io.opentelemetry.api.metrics.LongValueRecorder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Minimal implementation of the {@link BatchRecorder} that simply redirects the calls to the
  * instruments.
- *
- * <p>TODO: Add an async queue processing to process batch records.
  */
 final class BatchRecorderSdk implements BatchRecorder {
   private final Labels labelSet;
+
+  private volatile Map<Instrument, List<Number>> recordings = new HashMap<>();
+  private volatile Map<Instrument, List<Number>> previousRecordings = new HashMap<>();
 
   BatchRecorderSdk(String... keyValuePairs) {
     this.labelSet = Labels.of(keyValuePairs);
@@ -29,42 +35,80 @@ final class BatchRecorderSdk implements BatchRecorder {
 
   @Override
   public BatchRecorder put(LongValueRecorder valueRecorder, long value) {
-    valueRecorder.record(value, labelSet);
+    recordings.computeIfAbsent(valueRecorder, instrument -> new ArrayList<>()).add(value);
     return this;
   }
 
   @Override
   public BatchRecorder put(DoubleValueRecorder valueRecorder, double value) {
-    valueRecorder.record(value, labelSet);
+    recordings.computeIfAbsent(valueRecorder, instrument -> new ArrayList<>()).add(value);
     return this;
   }
 
   @Override
   public BatchRecorder put(LongCounter counter, long value) {
-    counter.add(value, labelSet);
+    recordings.computeIfAbsent(counter, instrument -> new ArrayList<>()).add(value);
     return this;
   }
 
   @Override
   public BatchRecorder put(DoubleCounter counter, double value) {
-    counter.add(value, labelSet);
+    recordings.computeIfAbsent(counter, instrument -> new ArrayList<>()).add(value);
     return this;
   }
 
   @Override
   public BatchRecorder put(LongUpDownCounter upDownCounter, long value) {
-    upDownCounter.add(value, labelSet);
+    recordings.computeIfAbsent(upDownCounter, instrument -> new ArrayList<>()).add(value);
     return this;
   }
 
   @Override
   public BatchRecorder put(DoubleUpDownCounter upDownCounter, double value) {
-    upDownCounter.add(value, labelSet);
+    recordings.computeIfAbsent(upDownCounter, instrument -> new ArrayList<>()).add(value);
     return this;
   }
 
   @Override
   public void record() {
-    // No-op in this minimal implementation.
+    previousRecordings.clear();
+
+    Map<Instrument, List<Number>> temp = recordings;
+    recordings = previousRecordings;
+    previousRecordings = temp;
+
+    previousRecordings.forEach(
+        (instrument, numbers) -> {
+          if (instrument instanceof DoubleUpDownCounter) {
+            for (Number number : numbers) {
+              ((DoubleUpDownCounter) instrument).add(number.doubleValue(), labelSet);
+            }
+          }
+          if (instrument instanceof DoubleCounter) {
+            for (Number number : numbers) {
+              ((DoubleCounter) instrument).add(number.doubleValue(), labelSet);
+            }
+          }
+          if (instrument instanceof DoubleValueRecorder) {
+            for (Number number : numbers) {
+              ((DoubleValueRecorder) instrument).record(number.doubleValue(), labelSet);
+            }
+          }
+          if (instrument instanceof LongUpDownCounter) {
+            for (Number number : numbers) {
+              ((LongUpDownCounter) instrument).add(number.longValue(), labelSet);
+            }
+          }
+          if (instrument instanceof LongCounter) {
+            for (Number number : numbers) {
+              ((LongCounter) instrument).add(number.longValue(), labelSet);
+            }
+          }
+          if (instrument instanceof LongValueRecorder) {
+            for (Number number : numbers) {
+              ((LongValueRecorder) instrument).record(number.longValue(), labelSet);
+            }
+          }
+        });
   }
 }
--- sdk/metrics/src/test/java/io/opentelemetry/sdk/metrics/BatchRecorderSdkTest.java
+++ sdk/metrics/src/test/java/io/opentelemetry/sdk/metrics/BatchRecorderSdkTest.java
@@ -6,17 +6,21 @@
 package io.opentelemetry.sdk.metrics;
 
 import static io.opentelemetry.api.common.AttributeKey.stringKey;
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.common.Labels;
+import io.opentelemetry.api.metrics.BatchRecorder;
 import io.opentelemetry.sdk.common.InstrumentationLibraryInfo;
 import io.opentelemetry.sdk.internal.TestClock;
 import io.opentelemetry.sdk.metrics.data.MetricData;
 import io.opentelemetry.sdk.metrics.data.MetricData.DoublePoint;
 import io.opentelemetry.sdk.metrics.data.MetricData.LongPoint;
 import io.opentelemetry.sdk.resources.Resource;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import org.junit.jupiter.api.Test;
 
@@ -54,16 +58,55 @@ class BatchRecorderSdkTest {
         testSdk.longValueRecorderBuilder("testLongValueRecorder").build();
     Labels labelSet = Labels.of("key", "value");
 
-    testSdk
-        .newBatchRecorder("key", "value")
+    BatchRecorder batchRecorder = testSdk.newBatchRecorder("key", "value");
+
+    batchRecorder
         .put(longCounter, 12)
         .put(doubleUpDownCounter, -12.1d)
         .put(longUpDownCounter, -12)
         .put(doubleCounter, 12.1d)
+        .put(doubleCounter, 12.1d)
         .put(longValueRecorder, 13)
-        .put(doubleValueRecorder, 13.1d)
-        .record();
+        .put(doubleValueRecorder, 13.1d);
 
+    // until record() is called, nothing should be recorded.
+    Collection<MetricData> preRecord = testSdk.collectAll();
+    preRecord.forEach(metricData -> assertThat(metricData.getPoints()).isEmpty());
+
+    batchRecorder.record();
+
+    assertBatchRecordings(
+        doubleCounter,
+        longCounter,
+        doubleUpDownCounter,
+        longUpDownCounter,
+        doubleValueRecorder,
+        longValueRecorder,
+        labelSet,
+        /* shouldHaveDeltas=*/ true);
+
+    // a second record, with no recordings added should not change any of the values.
+    batchRecorder.record();
+    assertBatchRecordings(
+        doubleCounter,
+        longCounter,
+        doubleUpDownCounter,
+        longUpDownCounter,
+        doubleValueRecorder,
+        longValueRecorder,
+        labelSet,
+        /* shouldHaveDeltas=*/ false);
+  }
+
+  private void assertBatchRecordings(
+      DoubleCounterSdk doubleCounter,
+      LongCounterSdk longCounter,
+      DoubleUpDownCounterSdk doubleUpDownCounter,
+      LongUpDownCounterSdk longUpDownCounter,
+      DoubleValueRecorderSdk doubleValueRecorder,
+      LongValueRecorderSdk longValueRecorder,
+      Labels labelSet,
+      boolean shouldHaveDeltas) {
     assertThat(doubleCounter.collectAll())
         .containsExactly(
             MetricData.create(
@@ -74,7 +117,7 @@ class BatchRecorderSdkTest {
                 "1",
                 MetricData.Type.MONOTONIC_DOUBLE,
                 Collections.singletonList(
-                    DoublePoint.create(testClock.now(), testClock.now(), labelSet, 12.1d))));
+                    DoublePoint.create(testClock.now(), testClock.now(), labelSet, 24.2d))));
     assertThat(longCounter.collectAll())
         .containsExactly(
             MetricData.create(
@@ -108,5 +151,48 @@ class BatchRecorderSdkTest {
                 MetricData.Type.NON_MONOTONIC_LONG,
                 Collections.singletonList(
                     LongPoint.create(testClock.now(), testClock.now(), labelSet, -12))));
+
+    assertThat(doubleValueRecorder.collectAll())
+        .containsExactly(
+            MetricData.create(
+                RESOURCE,
+                INSTRUMENTATION_LIBRARY_INFO,
+                "testDoubleValueRecorder",
+                "",
+                "1",
+                MetricData.Type.SUMMARY,
+                shouldHaveDeltas
+                    ? Collections.singletonList(
+                        MetricData.SummaryPoint.create(
+                            testClock.now(),
+                            testClock.now(),
+                            labelSet,
+                            1,
+                            13.1d,
+                            Arrays.asList(
+                                MetricData.ValueAtPercentile.create(0.0, 13.1),
+                                MetricData.ValueAtPercentile.create(100.0, 13.1))))
+                    : emptyList()));
+    assertThat(longValueRecorder.collectAll())
+        .containsExactly(
+            MetricData.create(
+                RESOURCE,
+                INSTRUMENTATION_LIBRARY_INFO,
+                "testLongValueRecorder",
+                "",
+                "1",
+                MetricData.Type.SUMMARY,
+                shouldHaveDeltas
+                    ? Collections.singletonList(
+                        MetricData.SummaryPoint.create(
+                            testClock.now(),
+                            testClock.now(),
+                            labelSet,
+                            1,
+                            13,
+                            Arrays.asList(
+                                MetricData.ValueAtPercentile.create(0.0, 13),
+                                MetricData.ValueAtPercentile.create(100.0, 13))))
+                    : emptyList()));
   }
 }
