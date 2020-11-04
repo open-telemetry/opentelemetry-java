3014bb7fa05f6314e594a187cd98d48524ae17f6
Bogdan Drutu <bogdandrutu@gmail.com>
2020-11-03 18:23:31 -0800

    Rename package name for extensions to singular extension (#1995)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

---
+++ extensions/trace-propagators/src/main/java/io/opentelemetry/extension/trace/propagation/B3PropagatorExtractor.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.extension.trace.propagation;
+
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import java.util.Optional;
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+interface B3PropagatorExtractor {
+
+  <C> Optional<Context> extract(Context context, C carrier, TextMapPropagator.Getter<C> getter);
+}
---
+++ extensions/trace-propagators/src/main/java/io/opentelemetry/extension/trace/propagation/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Implementations of {@link io.opentelemetry.context.propagation.TextMapPropagator} for various
+ * formats that are commonly used in distributed tracing.
+ */
+@ParametersAreNonnullByDefault
+package io.opentelemetry.extension.trace.propagation;
+
+import javax.annotation.ParametersAreNonnullByDefault;
