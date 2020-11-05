f84bda9aa993a49b7b2035dab8cbade3f729d0f4
jkwatson <jkwatson@gmail.com>
2020-11-05 10:56:34 -0800

    rework the batch recorder to use a queue of recordings

--- sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/BatchRecorderSdk.java
+++ sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/BatchRecorderSdk.java
@@ -15,9 +15,9 @@ import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.metrics.LongUpDownCounter;
 import io.opentelemetry.api.metrics.LongValueRecorder;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TransferQueue;
 
 /**
  * Minimal implementation of the {@link BatchRecorder} that simply redirects the calls to the
@@ -26,8 +26,9 @@ import java.util.Map;
 final class BatchRecorderSdk implements BatchRecorder {
   private final Labels labelSet;
 
-  private volatile Map<Instrument, List<Number>> recordings = new HashMap<>();
-  private volatile Map<Instrument, List<Number>> previousRecordings = new HashMap<>();
+  // todo: this queue is unbounded; should we make it bounded and drop recordings after it gets
+  // full?
+  private final TransferQueue<Recording> pendingRecordings = new LinkedTransferQueue<>();
 
   BatchRecorderSdk(String... keyValuePairs) {
     this.labelSet = Labels.of(keyValuePairs);
@@ -35,80 +36,72 @@ final class BatchRecorderSdk implements BatchRecorder {
 
   @Override
   public BatchRecorder put(LongValueRecorder valueRecorder, long value) {
-    recordings.computeIfAbsent(valueRecorder, instrument -> new ArrayList<>()).add(value);
+    pendingRecordings.offer(new Recording(valueRecorder, value));
     return this;
   }
 
   @Override
   public BatchRecorder put(DoubleValueRecorder valueRecorder, double value) {
-    recordings.computeIfAbsent(valueRecorder, instrument -> new ArrayList<>()).add(value);
+    pendingRecordings.offer(new Recording(valueRecorder, value));
     return this;
   }
 
   @Override
   public BatchRecorder put(LongCounter counter, long value) {
-    recordings.computeIfAbsent(counter, instrument -> new ArrayList<>()).add(value);
+    pendingRecordings.offer(new Recording(counter, value));
     return this;
   }
 
   @Override
   public BatchRecorder put(DoubleCounter counter, double value) {
-    recordings.computeIfAbsent(counter, instrument -> new ArrayList<>()).add(value);
+    pendingRecordings.offer(new Recording(counter, value));
     return this;
   }
 
   @Override
   public BatchRecorder put(LongUpDownCounter upDownCounter, long value) {
-    recordings.computeIfAbsent(upDownCounter, instrument -> new ArrayList<>()).add(value);
+    pendingRecordings.offer(new Recording(upDownCounter, value));
     return this;
   }
 
   @Override
   public BatchRecorder put(DoubleUpDownCounter upDownCounter, double value) {
-    recordings.computeIfAbsent(upDownCounter, instrument -> new ArrayList<>()).add(value);
+    pendingRecordings.offer(new Recording(upDownCounter, value));
     return this;
   }
 
   @Override
   public void record() {
-    previousRecordings.clear();
+    List<Recording> recordings = new ArrayList<>();
+    pendingRecordings.drainTo(recordings);
 
-    Map<Instrument, List<Number>> temp = recordings;
-    recordings = previousRecordings;
-    previousRecordings = temp;
-
-    previousRecordings.forEach(
-        (instrument, numbers) -> {
+    recordings.forEach(
+        (recording) -> {
+          Instrument instrument = recording.instrument;
+          Number value = recording.value;
           if (instrument instanceof DoubleUpDownCounter) {
-            for (Number number : numbers) {
-              ((DoubleUpDownCounter) instrument).add(number.doubleValue(), labelSet);
-            }
-          }
-          if (instrument instanceof DoubleCounter) {
-            for (Number number : numbers) {
-              ((DoubleCounter) instrument).add(number.doubleValue(), labelSet);
-            }
-          }
-          if (instrument instanceof DoubleValueRecorder) {
-            for (Number number : numbers) {
-              ((DoubleValueRecorder) instrument).record(number.doubleValue(), labelSet);
-            }
-          }
-          if (instrument instanceof LongUpDownCounter) {
-            for (Number number : numbers) {
-              ((LongUpDownCounter) instrument).add(number.longValue(), labelSet);
-            }
-          }
-          if (instrument instanceof LongCounter) {
-            for (Number number : numbers) {
-              ((LongCounter) instrument).add(number.longValue(), labelSet);
-            }
-          }
-          if (instrument instanceof LongValueRecorder) {
-            for (Number number : numbers) {
-              ((LongValueRecorder) instrument).record(number.longValue(), labelSet);
-            }
+            ((DoubleUpDownCounter) instrument).add(value.doubleValue(), labelSet);
+          } else if (instrument instanceof DoubleCounter) {
+            ((DoubleCounter) instrument).add(value.doubleValue(), labelSet);
+          } else if (instrument instanceof DoubleValueRecorder) {
+            ((DoubleValueRecorder) instrument).record(value.doubleValue(), labelSet);
+          } else if (instrument instanceof LongUpDownCounter) {
+            ((LongUpDownCounter) instrument).add(value.longValue(), labelSet);
+          } else if (instrument instanceof LongCounter) {
+            ((LongCounter) instrument).add(value.longValue(), labelSet);
+          } else if (instrument instanceof LongValueRecorder) {
+            ((LongValueRecorder) instrument).record(value.longValue(), labelSet);
           }
         });
+  }
+
+  private static class Recording {
+    private final Instrument instrument;
+    private final Number value;
+
+    private Recording(Instrument instrument, Number value) {
+      this.instrument = instrument;
+      this.value = value;
+    }
   }
 }
