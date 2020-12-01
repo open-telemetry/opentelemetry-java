036f7f14772eb4a0f2cceac84a3c4a3a70b927b2
John Watson <jkwatson@gmail.com>
2020-12-01 08:21:36 -0800

    Convert TraceState to be an interface (#2138)
    * convert TraceState to an interface
    
    * don't expose the impl
    
    * Remove the un-needed TraceState Entry objects.
    
    * Add additional javadoc documenting requirements for interface implementors.
    
    * make the implementation non-public
    
    * remove un-needed test docs
    
    * Update api/src/main/java/io/opentelemetry/api/trace/TraceState.java
    
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>
    
    * Update api/src/main/java/io/opentelemetry/api/trace/TraceState.java
    
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>
    
    * formatting fix
    
    * Convert the TraceStateBuilder into an interface.
    
    * Introduce an intermediate class to get around classloading cycles.
    
    * extract a method to remove an entry from the builder
    
    * add a comment about the possible inefficiency in the implementation
    
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>

---
+++ api/src/main/java/io/opentelemetry/api/trace/ArrayBasedTraceState.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.api.trace;
+
+import com.google.auto.value.AutoValue;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiConsumer;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
+@AutoValue
+abstract class ArrayBasedTraceState implements TraceState {
+
+  @Override
+  @Nullable
+  public String get(String key) {
+    List<String> entries = getEntries();
+    for (int i = 0; i < entries.size(); i += 2) {
+      if (entries.get(i).equals(key)) {
+        return entries.get(i + 1);
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public int size() {
+    return getEntries().size() / 2;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return getEntries().isEmpty();
+  }
+
+  @Override
+  public void forEach(BiConsumer<String, String> consumer) {
+    List<String> entries = getEntries();
+    for (int i = 0; i < entries.size(); i += 2) {
+      consumer.accept(entries.get(i), entries.get(i + 1));
+    }
+  }
+
+  abstract List<String> getEntries();
+
+  @Override
+  public TraceStateBuilder toBuilder() {
+    return new ArrayBasedTraceStateBuilder(this);
+  }
+
+  static ArrayBasedTraceState create(List<String> entries) {
+    return new AutoValue_ArrayBasedTraceState(Collections.unmodifiableList(entries));
+  }
+
+  ArrayBasedTraceState() {}
+}
--- api/src/main/java/io/opentelemetry/api/trace/TraceState.java
+++ api/src/main/java/io/opentelemetry/api/trace/TraceState.java
@@ -5,9 +5,6 @@
 
 package io.opentelemetry.api.trace;
 
-import com.google.auto.value.AutoValue;
-import java.util.Collections;
-import java.util.List;
 import java.util.function.BiConsumer;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
@@ -24,19 +21,32 @@ import javax.annotation.concurrent.Immutable;
  *
  * <p>Value is opaque string up to 256 characters printable ASCII RFC0020 characters (i.e., the
  * range 0x20 to 0x7E) except comma , and =.
+ *
+ * <p>Implementations of this interface *must* be immutable and have well-defined value-based
+ * equals/hashCode implementations. If an implementation does not strictly conform to these
+ * requirements, behavior of the OpenTelemetry APIs and default SDK cannot be guaranteed.
+ *
+ * <p>Implementations of this interface that do not conform to the W3C specification risk
+ * incompatibility with W3C-compatible implementations.
+ *
+ * <p>For these reasons, it is strongly suggested that you use the implementation that is provided
+ * here via the {@link TraceState#builder}.
  */
 @Immutable
-@AutoValue
-public abstract class TraceState {
-  private static final TraceState DEFAULT = TraceState.builder().build();
+public interface TraceState {
 
   /**
    * Returns the default {@code TraceState} with no entries.
    *
    * @return the default {@code TraceState}.
    */
-  public static TraceState getDefault() {
-    return DEFAULT;
+  static TraceState getDefault() {
+    return DefaultTraceState.get();
+  }
+
+  /** Returns an empty {@code TraceStateBuilder}. */
+  static TraceStateBuilder builder() {
+    return new ArrayBasedTraceStateBuilder();
   }
 
   /**
@@ -48,93 +58,21 @@ public abstract class TraceState {
    *     for the key.
    */
   @Nullable
-  public String get(String key) {
-    for (Entry entry : getEntries()) {
-      if (entry.getKey().equals(key)) {
-        return entry.getValue();
-      }
-    }
-    return null;
-  }
+  String get(String key);
 
   /** Returns the number of entries in this {@link TraceState}. */
-  public int size() {
-    return getEntries().size();
-  }
+  int size();
 
   /** Returns whether this {@link TraceState} is empty, containing no entries. */
-  public boolean isEmpty() {
-    return getEntries().isEmpty();
-  }
+  boolean isEmpty();
 
   /** Iterates over all the key-value entries contained in this {@link TraceState}. */
-  public void forEach(BiConsumer<String, String> consumer) {
-    for (Entry entry : getEntries()) {
-      consumer.accept(entry.getKey(), entry.getValue());
-    }
-  }
-
-  /**
-   * Returns a {@link List} view of the mappings contained in this {@code TraceState}.
-   *
-   * @return a {@link List} view of the mappings contained in this {@code TraceState}.
-   */
-  abstract List<Entry> getEntries();
-
-  /**
-   * Returns a {@code Builder} based on an empty {@code TraceState}.
-   *
-   * @return a {@code Builder} based on an empty {@code TraceState}.
-   */
-  public static TraceStateBuilder builder() {
-    return new TraceStateBuilder();
-  }
+  void forEach(BiConsumer<String, String> consumer);
 
   /**
    * Returns a {@code Builder} based on this {@code TraceState}.
    *
    * @return a {@code Builder} based on this {@code TraceState}.
    */
-  public TraceStateBuilder toBuilder() {
-    return new TraceStateBuilder(this);
-  }
-
-  static TraceState create(List<Entry> entries) {
-    return new AutoValue_TraceState(Collections.unmodifiableList(entries));
-  }
-
-  TraceState() {}
-
-  /** Immutable key-value pair for {@code TraceState}. */
-  @Immutable
-  @AutoValue
-  abstract static class Entry {
-    /**
-     * Creates a new {@code Entry} for the {@code TraceState}.
-     *
-     * @param key the Entry's key.
-     * @param value the Entry's value.
-     * @return the new {@code Entry}.
-     */
-    // Visible for testing
-    static Entry create(String key, String value) {
-      return new AutoValue_TraceState_Entry(key, value);
-    }
-
-    /**
-     * Returns the key {@code String}.
-     *
-     * @return the key {@code String}.
-     */
-    abstract String getKey();
-
-    /**
-     * Returns the value {@code String}.
-     *
-     * @return the value {@code String}.
-     */
-    abstract String getValue();
-
-    Entry() {}
-  }
+  TraceStateBuilder toBuilder();
 }
