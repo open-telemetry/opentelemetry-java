d853475da9eaff849b35ebbf1a99bfce2110ac46
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-02 13:19:32 +0900

    Spot

--- extensions/trace-utils/src/test/java/io/opentelemetry/extension/trace/CurrentSpanUtilsTest.java
+++ extensions/trace-utils/src/test/java/io/opentelemetry/extension/trace/CurrentSpanUtilsTest.java
@@ -18,7 +18,7 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-@SuppressWarnings("deprecation")  // CurrentSpanUtils is going to be removed.
+@SuppressWarnings("deprecation") // CurrentSpanUtils is going to be removed.
 class CurrentSpanUtilsTest {
   @Spy private Span span;
 
