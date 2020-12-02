8df5ac1ba161adb7e1e8c9547f85477995b568b1
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-02 17:18:10 +0900

    Fix / more tests

--- api/src/test/java/io/opentelemetry/api/common/AttributeKeyTest.java
+++ api/src/test/java/io/opentelemetry/api/common/AttributeKeyTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.api.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
