ce37ed20a5731c6f51af09ed7594dc17b90dcd67
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-04 12:53:10 +0900

    Revert accidental

--- context/src/test/java/io/opentelemetry/context/ContextTest.java
+++ context/src/test/java/io/opentelemetry/context/ContextTest.java
@@ -174,10 +174,6 @@ class ContextTest {
 
     assertThat(context4.get(ANIMAL)).isEqualTo("dog");
     assertThat(context4.get(BAG)).isNull();
-
-    String newDogStr = new String("dog");
-    assertThat(newDogStr).isEqualTo("dog");
-    assertThat(newDogStr)
   }
 
   @Test
