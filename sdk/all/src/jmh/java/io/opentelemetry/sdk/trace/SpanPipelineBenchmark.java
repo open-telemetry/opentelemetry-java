fc157c9fcc543b0b7c4ba7843ef7c28c894bdd13
Prashant Srivastava <srprash@amazon.com>
2020-12-07 13:01:00 -0800

    Refactored benchmark to use OTLP exporter and collector

--- sdk/all/src/jmh/java/io/opentelemetry/sdk/trace/SpanPipelineBenchmark.java
+++ sdk/all/src/jmh/java/io/opentelemetry/sdk/trace/SpanPipelineBenchmark.java
@@ -5,24 +5,12 @@
 
 package io.opentelemetry.sdk.trace;
 
-import static io.opentelemetry.api.common.AttributeKey.booleanKey;
-import static io.opentelemetry.api.common.AttributeKey.doubleKey;
-import static io.opentelemetry.api.common.AttributeKey.longKey;
-import static io.opentelemetry.api.common.AttributeKey.stringKey;
-
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.Span.Kind;
-import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.common.CompletableResultCode;
-import io.opentelemetry.sdk.trace.data.SpanData;
+import io.opentelemetry.exporter.otlp.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.trace.config.TraceConfig;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
-import io.opentelemetry.sdk.trace.export.SpanExporter;
-import java.util.Collection;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
@@ -34,67 +22,66 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
 
 @State(Scope.Benchmark)
 public class SpanPipelineBenchmark {
-
-  private static final AttributeKey<String> OPERATION_KEY = stringKey("operation");
-  private static final AttributeKey<Long> LONG_ATTRIBUTE_KEY = longKey("longAttribute");
-  private static final AttributeKey<String> STRING_ATTRIBUTE_KEY = stringKey("stringAttribute");
-  private static final AttributeKey<Double> DOUBLE_ATTRIBUTE_KEY = doubleKey("doubleAttribute");
-  private static final AttributeKey<Boolean> BOOLEAN_ATTRIBUTE_KEY = booleanKey("booleanAttribute");
-  private final Tracer tracer = OpenTelemetry.getGlobalTracerProvider().get("benchmarkTracer");
+  private static SpanBuilderSdk spanBuilderSdk;
+  private static final DockerImageName OTLP_COLLECTOR_IMAGE =
+      DockerImageName.parse("otel/opentelemetry-collector-dev:latest");
+  private static final int EXPOSED_PORT = 5678;
+  private static final int HEALTH_CHECK_PORT = 13133;
 
   @Setup(Level.Trial)
   public final void setup() {
-    SpanExporter exporter = new NoOpSpanExporter();
-    OpenTelemetrySdk.getGlobalTracerManagement()
-        .addSpanProcessor(SimpleSpanProcessor.builder(exporter).build());
+    // Configuring the collector test-container
+    GenericContainer<?> collector =
+        new GenericContainer<>(OTLP_COLLECTOR_IMAGE)
+            .withExposedPorts(EXPOSED_PORT, HEALTH_CHECK_PORT)
+            .waitingFor(Wait.forHttp("/").forPort(HEALTH_CHECK_PORT))
+            .withCopyFileToContainer(
+                MountableFile.forClasspathResource("/otel.yaml"), "/etc/otel.yaml")
+            .withCommand("--config /etc/otel.yaml");
+
+    collector.start();
+
+    String address = collector.getHost() + ":" + collector.getMappedPort(EXPOSED_PORT);
+
+    TracerSdkProvider tracerProvider = TracerSdkProvider.builder().build();
+
+    SimpleSpanProcessor spanProcessor =
+        SimpleSpanProcessor.builder(
+                OtlpGrpcSpanExporter.builder().setEndpoint(address).setDeadlineMs(50000).build())
+            .build();
+
+    tracerProvider.addSpanProcessor(spanProcessor);
+
+    TraceConfig alwaysOn =
+        tracerProvider.getActiveTraceConfig().toBuilder().setSampler(Sampler.alwaysOn()).build();
+    tracerProvider.updateActiveTraceConfig(alwaysOn);
+
+    Tracer tracerSdk = tracerProvider.get("PipelineBenchmarkTracer");
+    spanBuilderSdk = (SpanBuilderSdk) tracerSdk.spanBuilder("PipelineBenchmarkSpan");
   }
 
   @Benchmark
-  @Threads(value = 5)
+  @Threads(value = 1)
   @Fork(1)
   @Warmup(iterations = 5, time = 1)
-  @Measurement(iterations = 5, time = 1)
-  @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public void runThePipeline_05Threads() {
+  @Measurement(iterations = 15, time = 1)
+  @OutputTimeUnit(TimeUnit.SECONDS)
+  public void runThePipeline_01Threads() {
     doWork();
   }
 
-  private void doWork() {
-    Span span =
-        tracer
-            .spanBuilder("benchmarkSpan")
-            .setSpanKind(Kind.CLIENT)
-            .setAttribute("key", "value")
-            .startSpan();
-    span.addEvent("started", Attributes.of(OPERATION_KEY, "some_work"));
-    span.setAttribute(LONG_ATTRIBUTE_KEY, 33L);
-    span.setAttribute(STRING_ATTRIBUTE_KEY, "test_value");
-    span.setAttribute(DOUBLE_ATTRIBUTE_KEY, 4844.44d);
-    span.setAttribute(BOOLEAN_ATTRIBUTE_KEY, false);
-    span.setStatus(StatusCode.OK);
-
-    span.addEvent("testEvent");
+  private static void doWork() {
+    Span span = spanBuilderSdk.startSpan();
+    for (int i = 0; i < 10; i++) {
+      span.setAttribute("benchmarkAttribute_" + i, "benchmarkAttrValue_" + i);
+    }
     span.end();
-  }
-
-  private static class NoOpSpanExporter implements SpanExporter {
-    @Override
-    public CompletableResultCode export(Collection<SpanData> spans) {
-      return CompletableResultCode.ofSuccess();
-    }
-
-    @Override
-    public CompletableResultCode flush() {
-      return CompletableResultCode.ofSuccess();
-    }
-
-    @Override
-    public CompletableResultCode shutdown() {
-      // no-op
-      return CompletableResultCode.ofSuccess();
-    }
   }
 }
