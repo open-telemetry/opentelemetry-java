69208f8f792faea63a8df11a58b2738b63918e70
Bogdan Drutu <bogdandrutu@gmail.com>
2020-12-07 13:39:33 -0800

    Remove version of checkArgument not used (#2213)
    This PR fixes an inconsistency that MaxLengthOfAttributeValues was allowed to be set to 0, then thrown internal exception.
    We should check for arguments only at public APIs.
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- api/internal/src/test/java/io/opentelemetry/api/internal/UtilsTest.java
+++ api/internal/src/test/java/io/opentelemetry/api/internal/UtilsTest.java
@@ -11,10 +11,6 @@ import org.junit.jupiter.api.Test;
 
 class UtilsTest {
   private static final String TEST_MESSAGE = "test message";
-  private static final String TEST_MESSAGE_TEMPLATE = "I ate %s eggs.";
-  private static final int TEST_MESSAGE_VALUE = 2;
-  private static final String FORMATTED_SIMPLE_TEST_MESSAGE = "I ate 2 eggs.";
-  private static final String FORMATTED_COMPLEX_TEST_MESSAGE = "I ate 2 eggs. [2]";
 
   @Test
   void checkArgument() {
@@ -23,23 +19,5 @@ class UtilsTest {
         IllegalArgumentException.class,
         () -> Utils.checkArgument(false, TEST_MESSAGE),
         TEST_MESSAGE);
-  }
-
-  @Test
-  void checkArgument_WithSimpleFormat() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () -> Utils.checkArgument(false, TEST_MESSAGE_TEMPLATE, TEST_MESSAGE_VALUE),
-        FORMATTED_SIMPLE_TEST_MESSAGE);
-  }
-
-  @Test
-  void checkArgument_WithComplexFormat() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () ->
-            Utils.checkArgument(
-                false, TEST_MESSAGE_TEMPLATE, TEST_MESSAGE_VALUE, TEST_MESSAGE_VALUE),
-        FORMATTED_COMPLEX_TEST_MESSAGE);
   }
 }
