24b5ef03f17183f2e10a206e5d7b5736f897c2a7
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-08 15:31:58 +0900

    Drift

--- sdk/all/src/main/java/io/opentelemetry/sdk/OpenTelemetrySdk.java
+++ sdk/all/src/main/java/io/opentelemetry/sdk/OpenTelemetrySdk.java
@@ -274,12 +274,8 @@ public final class OpenTelemetrySdk extends DefaultOpenTelemetry {
    * This interface allows the SDK to unobfuscate an obfuscated static global provider.
    *
    * <p>Static global providers are obfuscated when they are returned from the API to prevent users
-   * from casting them to their SDK specific implementation.
-   *
-   * <p>This is important for auto-instrumentation, because if users take the static global providers
-   * that are returned from the API, and cast them to their SDK specific implementations, then those
-   * casts will fail under auto-instrumentation, because auto-instrumentation takes over the static
-   * global providers returned by the API and points them to it's embedded SDK.
+   * from casting them to their SDK specific implementation. For example, we do not want users to
+   * use patterns like {@code (TracerSdkProvider) OpenTelemetry.getGlobalTracerProvider()}.
    */
   @ThreadSafe
   // Visible for testing
