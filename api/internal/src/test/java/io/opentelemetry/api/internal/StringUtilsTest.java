08ea07f7dca3ad6645226b50331fb951ab5d893c
Bogdan Drutu <bogdandrutu@gmail.com>
2020-12-07 16:09:40 -0800

    Add unit tests for isValidMetricName
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- api/internal/src/test/java/io/opentelemetry/api/internal/StringUtilsTest.java
+++ api/internal/src/test/java/io/opentelemetry/api/internal/StringUtilsTest.java
@@ -16,4 +16,18 @@ class StringUtilsTest {
     assertThat(StringUtils.isPrintableString("abcd")).isTrue();
     assertThat(StringUtils.isPrintableString("\2ab\3cd")).isFalse();
   }
+
+  @Test
+  void isValidMetricName() {
+    assertThat(StringUtils.isValidMetricName("")).isFalse();
+    assertThat(
+            StringUtils.isValidMetricName(
+                String.valueOf(new char[StringUtils.METRIC_NAME_MAX_LENGTH + 1])))
+        .isFalse();
+    assertThat(StringUtils.isValidMetricName("abcd")).isTrue();
+    assertThat(StringUtils.isValidMetricName("ab.cd")).isTrue();
+    assertThat(StringUtils.isValidMetricName("ab12cd")).isTrue();
+    assertThat(StringUtils.isValidMetricName("1abcd")).isFalse();
+    assertThat(StringUtils.isValidMetricName("ab*cd")).isFalse();
+  }
 }
