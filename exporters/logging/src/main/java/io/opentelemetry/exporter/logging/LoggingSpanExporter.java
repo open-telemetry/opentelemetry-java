ed45fd09dd023b9c0b8fe50b2e273c11541d34b4
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-14 15:32:18 +0900

    Manually structure output of log exporter instead of delegating to unstable toString

--- exporters/logging/src/main/java/io/opentelemetry/exporter/logging/LoggingSpanExporter.java
+++ exporters/logging/src/main/java/io/opentelemetry/exporter/logging/LoggingSpanExporter.java
@@ -15,12 +15,23 @@ import java.util.logging.Logger;
 
 /** A Span Exporter that logs every span at INFO level using java.util.logging. */
 public class LoggingSpanExporter implements SpanExporter {
-  private static final Logger logger = Logger.getLogger(LoggingSpanExporter.class.getName());
+  // Visible for testing
+  static final Logger logger = Logger.getLogger(LoggingSpanExporter.class.getName());
 
   @Override
   public CompletableResultCode export(Collection<SpanData> spans) {
+    // We always have 32 + 16 + name + several whitespace, 60 seems like an OK initial guess.
+    StringBuilder sb = new StringBuilder(60);
     for (SpanData span : spans) {
-      logger.log(Level.INFO, "span: {0}", span);
+      sb.setLength(0);
+      sb.append(span.getName())
+          .append(" ")
+          .append(span.getTraceId())
+          .append(" ")
+          .append(span.getSpanId())
+          .append(" ")
+          .append(span.getAttributes());
+      logger.log(Level.INFO, sb.toString());
     }
     return CompletableResultCode.ofSuccess();
   }
--- exporters/logging/src/test/java/io/opentelemetry/exporter/logging/LoggingSpanExporterTest.java
+++ exporters/logging/src/test/java/io/opentelemetry/exporter/logging/LoggingSpanExporterTest.java
@@ -6,6 +6,8 @@
 package io.opentelemetry.exporter.logging;
 
 import static io.opentelemetry.api.common.AttributeKey.booleanKey;
+import static io.opentelemetry.api.common.AttributeKey.longKey;
+import static io.opentelemetry.api.common.AttributeKey.stringKey;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,9 +20,15 @@ import io.opentelemetry.sdk.testing.trace.TestSpanData;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import io.opentelemetry.sdk.trace.data.SpanData.Event;
 import io.opentelemetry.sdk.trace.data.SpanData.Status;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 import java.util.logging.StreamHandler;
@@ -30,6 +38,39 @@ import org.junit.jupiter.api.Test;
 
 /** Tests for the {@link LoggingSpanExporter}. */
 class LoggingSpanExporterTest {
+
+  private static final SpanData SPAN1 =
+      TestSpanData.builder()
+          .setHasEnded(true)
+          .setTraceId(TraceId.fromLongs(1234L, 6789L))
+          .setSpanId(SpanId.fromLong(9876L))
+          .setStartEpochNanos(100)
+          .setEndEpochNanos(100 + 1000)
+          .setStatus(Status.ok())
+          .setName("testSpan1")
+          .setKind(Kind.INTERNAL)
+          .setAttributes(Attributes.of(stringKey("animal"), "cat", longKey("lives"), 9L))
+          .setEvents(
+              Collections.singletonList(
+                  Event.create(
+                      100 + 500,
+                      "somethingHappenedHere",
+                      Attributes.of(booleanKey("important"), true))))
+          .setTotalRecordedEvents(1)
+          .setTotalRecordedLinks(0)
+          .build();
+
+  private static final SpanData SPAN2 =
+      TestSpanData.builder()
+          .setHasEnded(false)
+          .setTraceId(TraceId.fromLongs(20L, 30L))
+          .setSpanId(SpanId.fromLong(15L))
+          .setStartEpochNanos(500)
+          .setEndEpochNanos(500 + 1001)
+          .setStatus(Status.error())
+          .setName("testSpan2")
+          .setKind(Kind.CLIENT)
+          .build();
 
   LoggingSpanExporter exporter;
 
@@ -41,6 +82,43 @@ class LoggingSpanExporterTest {
   @AfterEach
   void tearDown() {
     exporter.shutdown();
+  }
+
+  @Test
+  void log() {
+    Logger logger = LoggingSpanExporter.logger;
+    List<LogRecord> logged = new ArrayList<>();
+    Handler handler =
+        new Handler() {
+          @Override
+          public void publish(LogRecord record) {
+            logged.add(record);
+          }
+
+          @Override
+          public void flush() {}
+
+          @Override
+          public void close() {}
+        };
+    logger.addHandler(handler);
+    logger.setUseParentHandlers(false);
+    try {
+      exporter.export(Arrays.asList(SPAN1, SPAN2));
+
+      assertThat(logged)
+          .hasSize(2)
+          .allSatisfy(log -> assertThat(log.getLevel()).isEqualTo(Level.INFO));
+      assertThat(logged.get(0).getMessage())
+          .isEqualTo(
+              "testSpan1 00000000000004d20000000000001a85 0000000000002694 "
+                  + "{animal=\"cat\", lives=9}");
+      assertThat(logged.get(1).getMessage())
+          .isEqualTo("testSpan2 0000000000000014000000000000001e 000000000000000f {}");
+    } finally {
+      logger.removeHandler(handler);
+      logger.setUseParentHandlers(true);
+    }
   }
 
   @Test
