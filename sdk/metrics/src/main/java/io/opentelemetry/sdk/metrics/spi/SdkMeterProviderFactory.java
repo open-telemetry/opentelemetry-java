5107d9865f223ae09671bcb3f6fccdc92d136b54
Bogdan Drutu <bogdandrutu@gmail.com>
2020-12-16 13:10:19 -0800

    Consistent naming for SPI classes
    Follow the java convention to name Factories, ${CLASS_NAME}Factory.
    
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/spi/SdkMeterProviderFactory.java
+++ sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/spi/SdkMeterProviderFactory.java
@@ -5,17 +5,11 @@
 
 package io.opentelemetry.sdk.metrics.spi;
 
-import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.spi.metrics.MeterProviderFactory;
 
-/**
- * {@code MeterProvider} provider implementation for {@link MeterProviderFactory}.
- *
- * <p>This class is not intended to be used in application code and it is used only by {@link
- * OpenTelemetry}.
- */
-public final class MeterProviderFactorySdk implements MeterProviderFactory {
+/** SDK implementation of the {@link MeterProviderFactory} for SPI. */
+public final class SdkMeterProviderFactory implements MeterProviderFactory {
 
   @Override
   public SdkMeterProvider create() {
