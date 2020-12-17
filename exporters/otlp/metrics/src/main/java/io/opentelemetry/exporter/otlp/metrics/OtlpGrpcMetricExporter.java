b0652dcae2fdce8666a82103747a1384b4dc6cc0
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-17 13:49:42 +0900

    Split trace/metrics OTLP exporters. (#2315)
    * Split trace/metrics OTLP exporters.
    
    * git add
    
    * alpha version
    
    * Commmit
    
    * Restore otlp parent artifact

---
+++ exporters/otlp/metrics/src/main/java/io/opentelemetry/exporter/otlp/metrics/OtlpGrpcMetricExporter.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.exporter.otlp.metrics;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.MoreExecutors;
+import io.grpc.ManagedChannel;
+import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest;
+import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceResponse;
+import io.opentelemetry.proto.collector.metrics.v1.MetricsServiceGrpc;
+import io.opentelemetry.proto.collector.metrics.v1.MetricsServiceGrpc.MetricsServiceFutureStub;
+import io.opentelemetry.sdk.common.CompletableResultCode;
+import io.opentelemetry.sdk.extension.otproto.MetricAdapter;
+import io.opentelemetry.sdk.metrics.data.MetricData;
+import io.opentelemetry.sdk.metrics.export.MetricExporter;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * Exports metrics using OTLP via gRPC, using OpenTelemetry's protobuf model.
+ *
+ * <p>Configuration options for {@link OtlpGrpcMetricExporter} can be read from system properties,
+ * environment variables, or {@link java.util.Properties} objects.
+ *
+ * <p>For system properties and {@link java.util.Properties} objects, {@link OtlpGrpcMetricExporter}
+ * will look for the following names:
+ *
+ * <ul>
+ *   <li>{@code otel.exporter.otlp.metric.timeout}: to set the max waiting time allowed to send each
+ *       span batch.
+ *   <li>{@code otel.exporter.otlp.metric.endpoint}: to set the endpoint to connect to.
+ *   <li>{@code otel.exporter.otlp.metric.insecure}: whether to enable client transport security for
+ *       the connection.
+ *   <li>{@code otel.exporter.otlp.metric.headers}: the headers associated with the requests.
+ * </ul>
+ *
+ * <p>For environment variables, {@link OtlpGrpcMetricExporter} will look for the following names:
+ *
+ * <ul>
+ *   <li>{@code OTEL_EXPORTER_OTLP_METRIC_TIMEOUT}: to set the max waiting time allowed to send each
+ *       span batch.
+ *   <li>{@code OTEL_EXPORTER_OTLP_METRIC_ENDPOINT}: to set the endpoint to connect to.
+ *   <li>{@code OTEL_EXPORTER_OTLP_METRIC_INSECURE}: whether to enable client transport security for
+ *       the connection.
+ *   <li>{@code OTEL_EXPORTER_OTLP_METRIC_HEADERS}: the headers associated with the requests.
+ * </ul>
+ *
+ * <p>In both cases, if a property is missing, the name without "span" is used to resolve the value.
+ */
+@ThreadSafe
+public final class OtlpGrpcMetricExporter implements MetricExporter {
+  public static final String DEFAULT_ENDPOINT = "localhost:4317";
+  public static final long DEFAULT_DEADLINE_MS = TimeUnit.SECONDS.toMillis(10);
+
+  private static final Logger logger = Logger.getLogger(OtlpGrpcMetricExporter.class.getName());
+
+  private final MetricsServiceFutureStub metricsService;
+  private final ManagedChannel managedChannel;
+  private final long deadlineMs;
+
+  /**
+   * Creates a new OTLP gRPC Metric Reporter with the given name, using the given channel.
+   *
+   * @param channel the channel to use when communicating with the OpenTelemetry Collector.
+   * @param deadlineMs max waiting time for the collector to process each metric batch. When set to
+   *     0 or to a negative value, the exporter will wait indefinitely.
+   */
+  OtlpGrpcMetricExporter(ManagedChannel channel, long deadlineMs) {
+    this.managedChannel = channel;
+    this.deadlineMs = deadlineMs;
+    metricsService = MetricsServiceGrpc.newFutureStub(channel);
+  }
+
+  /**
+   * Submits all the given metrics in a single batch to the OpenTelemetry collector.
+   *
+   * @param metrics the list of Metrics to be exported.
+   * @return the result of the operation
+   */
+  @Override
+  public CompletableResultCode export(Collection<MetricData> metrics) {
+    ExportMetricsServiceRequest exportMetricsServiceRequest =
+        ExportMetricsServiceRequest.newBuilder()
+            .addAllResourceMetrics(MetricAdapter.toProtoResourceMetrics(metrics))
+            .build();
+
+    final CompletableResultCode result = new CompletableResultCode();
+    MetricsServiceFutureStub exporter;
+    if (deadlineMs > 0) {
+      exporter = metricsService.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS);
+    } else {
+      exporter = metricsService;
+    }
+
+    Futures.addCallback(
+        exporter.export(exportMetricsServiceRequest),
+        new FutureCallback<ExportMetricsServiceResponse>() {
+          @Override
+          public void onSuccess(@Nullable ExportMetricsServiceResponse response) {
+            result.succeed();
+          }
+
+          @Override
+          public void onFailure(Throwable t) {
+            logger.log(Level.WARNING, "Failed to export metrics", t);
+            result.fail();
+          }
+        },
+        MoreExecutors.directExecutor());
+    return result;
+  }
+
+  /**
+   * The OTLP exporter does not batch metrics, so this method will immediately return with success.
+   *
+   * @return always Success
+   */
+  @Override
+  public CompletableResultCode flush() {
+    return CompletableResultCode.ofSuccess();
+  }
+
+  /**
+   * Returns a new builder instance for this exporter.
+   *
+   * @return a new builder instance for this exporter.
+   */
+  public static OtlpGrpcMetricExporterBuilder builder() {
+    return new OtlpGrpcMetricExporterBuilder();
+  }
+
+  /**
+   * Returns a new {@link OtlpGrpcMetricExporter} reading the configuration values from the
+   * environment and from system properties. System properties override values defined in the
+   * environment. If a configuration value is missing, it uses the default value.
+   *
+   * @return a new {@link OtlpGrpcMetricExporter} instance.
+   */
+  public static OtlpGrpcMetricExporter getDefault() {
+    return builder().readEnvironmentVariables().readSystemProperties().build();
+  }
+
+  /**
+   * Initiates an orderly shutdown in which preexisting calls continue but new calls are immediately
+   * cancelled. The channel is forcefully closed after a timeout.
+   */
+  @Override
+  public CompletableResultCode shutdown() {
+    try {
+      managedChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      logger.log(Level.WARNING, "Failed to shutdown the gRPC channel", e);
+    }
+    return CompletableResultCode.ofSuccess();
+  }
+}
---
+++ exporters/otlp/trace/src/main/java/io/opentelemetry/exporter/otlp/trace/OtlpGrpcSpanExporter.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.exporter.otlp.trace;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.MoreExecutors;
+import io.grpc.ConnectivityState;
+import io.grpc.ManagedChannel;
+import io.opentelemetry.api.common.Labels;
+import io.opentelemetry.api.metrics.GlobalMetricsProvider;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
+import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceResponse;
+import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc;
+import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc.TraceServiceFutureStub;
+import io.opentelemetry.sdk.common.CompletableResultCode;
+import io.opentelemetry.sdk.extension.otproto.SpanAdapter;
+import io.opentelemetry.sdk.trace.data.SpanData;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * Exports spans using OTLP via gRPC, using OpenTelemetry's protobuf model.
+ *
+ * <p>Configuration options for {@link OtlpGrpcSpanExporter} can be read from system properties,
+ * environment variables, or {@link java.util.Properties} objects.
+ *
+ * <p>For system properties and {@link java.util.Properties} objects, {@link OtlpGrpcSpanExporter}
+ * will look for the following names:
+ *
+ * <ul>
+ *   <li>{@code otel.exporter.otlp.span.timeout}: to set the max waiting time allowed to send each
+ *       span batch.
+ *   <li>{@code otel.exporter.otlp.span.endpoint}: to set the endpoint to connect to.
+ *   <li>{@code otel.exporter.otlp.span.insecure}: whether to enable client transport security for
+ *       the connection.
+ *   <li>{@code otel.exporter.otlp.span.headers}: the headers associated with the requests.
+ * </ul>
+ *
+ * <p>For environment variables, {@link OtlpGrpcSpanExporter} will look for the following names:
+ *
+ * <ul>
+ *   <li>{@code OTEL_EXPORTER_OTLP_SPAN_TIMEOUT}: to set the max waiting time allowed to send each
+ *       span batch.
+ *   <li>{@code OTEL_EXPORTER_OTLP_SPAN_ENDPOINT}: to set the endpoint to connect to.
+ *   <li>{@code OTEL_EXPORTER_OTLP_SPAN_INSECURE}: whether to enable client transport security for
+ *       the connection.
+ *   <li>{@code OTEL_EXPORTER_OTLP_SPAN_HEADERS}: the headers associated with the requests.
+ * </ul>
+ *
+ * <p>In both cases, if a property is missing, the name without "span" is used to resolve the value.
+ */
+@ThreadSafe
+public final class OtlpGrpcSpanExporter implements SpanExporter {
+
+  public static final String DEFAULT_ENDPOINT = "localhost:4317";
+  public static final long DEFAULT_DEADLINE_MS = TimeUnit.SECONDS.toMillis(10);
+
+  private static final Logger logger = Logger.getLogger(OtlpGrpcSpanExporter.class.getName());
+  private static final String EXPORTER_NAME = OtlpGrpcSpanExporter.class.getSimpleName();
+
+  private static final Labels EXPORTER_NAME_LABELS = Labels.of("exporter", EXPORTER_NAME);
+  private static final Labels EXPORT_SUCCESS_LABELS =
+      Labels.of("exporter", EXPORTER_NAME, "success", "true");
+  private static final Labels EXPORT_FAILURE_LABELS =
+      Labels.of("exporter", EXPORTER_NAME, "success", "false");
+
+  private final TraceServiceFutureStub traceService;
+
+  private final ManagedChannel managedChannel;
+  private final long deadlineMs;
+  private final LongCounter.BoundLongCounter spansSeen;
+  private final LongCounter.BoundLongCounter spansExportedSuccess;
+  private final LongCounter.BoundLongCounter spansExportedFailure;
+
+  /**
+   * Creates a new OTLP gRPC Span Reporter with the given name, using the given channel.
+   *
+   * @param channel the channel to use when communicating with the OpenTelemetry Collector.
+   * @param deadlineMs max waiting time for the collector to process each span batch. When set to 0
+   *     or to a negative value, the exporter will wait indefinitely.
+   */
+  OtlpGrpcSpanExporter(ManagedChannel channel, long deadlineMs) {
+    Meter meter = GlobalMetricsProvider.getMeter("io.opentelemetry.exporters.otlp");
+    this.spansSeen =
+        meter.longCounterBuilder("spansSeenByExporter").build().bind(EXPORTER_NAME_LABELS);
+    LongCounter spansExportedCounter = meter.longCounterBuilder("spansExportedByExporter").build();
+    this.spansExportedSuccess = spansExportedCounter.bind(EXPORT_SUCCESS_LABELS);
+    this.spansExportedFailure = spansExportedCounter.bind(EXPORT_FAILURE_LABELS);
+    this.managedChannel = channel;
+    this.deadlineMs = deadlineMs;
+
+    this.traceService = TraceServiceGrpc.newFutureStub(channel);
+  }
+
+  /**
+   * Submits all the given spans in a single batch to the OpenTelemetry collector.
+   *
+   * @param spans the list of sampled Spans to be exported.
+   * @return the result of the operation
+   */
+  @Override
+  public CompletableResultCode export(Collection<SpanData> spans) {
+    spansSeen.add(spans.size());
+    ExportTraceServiceRequest exportTraceServiceRequest =
+        ExportTraceServiceRequest.newBuilder()
+            .addAllResourceSpans(SpanAdapter.toProtoResourceSpans(spans))
+            .build();
+
+    final CompletableResultCode result = new CompletableResultCode();
+
+    TraceServiceFutureStub exporter;
+    if (deadlineMs > 0) {
+      exporter = traceService.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS);
+    } else {
+      exporter = traceService;
+    }
+
+    Futures.addCallback(
+        exporter.export(exportTraceServiceRequest),
+        new FutureCallback<ExportTraceServiceResponse>() {
+          @Override
+          public void onSuccess(@Nullable ExportTraceServiceResponse response) {
+            spansExportedSuccess.add(spans.size());
+            result.succeed();
+          }
+
+          @Override
+          public void onFailure(Throwable t) {
+            spansExportedFailure.add(spans.size());
+            logger.log(Level.WARNING, "Failed to export spans. Error message: " + t.getMessage());
+            logger.log(Level.FINEST, "Failed to export spans. Details follow: " + t);
+            result.fail();
+          }
+        },
+        MoreExecutors.directExecutor());
+    return result;
+  }
+
+  /**
+   * The OTLP exporter does not batch spans, so this method will immediately return with success.
+   *
+   * @return always Success
+   */
+  @Override
+  public CompletableResultCode flush() {
+    return CompletableResultCode.ofSuccess();
+  }
+
+  /**
+   * Returns a new builder instance for this exporter.
+   *
+   * @return a new builder instance for this exporter.
+   */
+  public static OtlpGrpcSpanExporterBuilder builder() {
+    return new OtlpGrpcSpanExporterBuilder();
+  }
+
+  /**
+   * Returns a new {@link OtlpGrpcSpanExporter} reading the configuration values from the
+   * environment and from system properties. System properties override values defined in the
+   * environment. If a configuration value is missing, it uses the default value.
+   *
+   * @return a new {@link OtlpGrpcSpanExporter} instance.
+   */
+  public static OtlpGrpcSpanExporter getDefault() {
+    return builder().readEnvironmentVariables().readSystemProperties().build();
+  }
+
+  /**
+   * Initiates an orderly shutdown in which preexisting calls continue but new calls are immediately
+   * cancelled.
+   */
+  @Override
+  public CompletableResultCode shutdown() {
+    final CompletableResultCode result = new CompletableResultCode();
+    managedChannel.notifyWhenStateChanged(ConnectivityState.SHUTDOWN, result::succeed);
+    managedChannel.shutdown();
+    this.spansSeen.unbind();
+    this.spansExportedSuccess.unbind();
+    this.spansExportedFailure.unbind();
+    return result;
+  }
+
+  // Visible for testing
+  long getDeadlineMs() {
+    return deadlineMs;
+  }
+}
