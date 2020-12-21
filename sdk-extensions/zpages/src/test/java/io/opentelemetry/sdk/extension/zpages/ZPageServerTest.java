290b0db4000d8cbe1c7e2b9f75faa131e0a431c3
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-21 16:02:11 +0900

    Return SpanProcessor from zpages instead of automatically mutating global

--- sdk-extensions/zpages/src/test/java/io/opentelemetry/sdk/extension/zpages/ZPageServerTest.java
+++ sdk-extensions/zpages/src/test/java/io/opentelemetry/sdk/extension/zpages/ZPageServerTest.java
@@ -7,27 +7,12 @@ package io.opentelemetry.sdk.extension.zpages;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.sun.net.httpserver.HttpServer;
-import java.io.IOException;
-import java.net.InetSocketAddress;
 import org.junit.jupiter.api.Test;
 
-/** Unit tests for {@link ZPageServer}. */
-public final class ZPageServerTest {
+class ZPageServerTest {
+
   @Test
-  void tracezSpanProcessorOnlyAddedOnce() throws IOException {
-    // tracezSpanProcessor is not added yet
-    assertThat(ZPageServer.getIsTracezSpanProcesserAdded()).isFalse();
-    HttpServer server = null;
-    try {
-      server = HttpServer.create(new InetSocketAddress(0), 5);
-      ZPageServer.registerAllPagesToHttpServer(server);
-      // tracezSpanProcessor is added
-      assertThat(ZPageServer.getIsTracezSpanProcesserAdded()).isTrue();
-    } finally {
-      if (server != null) {
-        server.stop(0);
-      }
-    }
+  void spanProcessor() {
+    assertThat(ZPageServer.getSpanProcessor()).isInstanceOf(TracezSpanProcessor.class);
   }
 }
