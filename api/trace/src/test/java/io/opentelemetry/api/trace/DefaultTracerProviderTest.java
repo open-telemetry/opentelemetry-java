25f1f54ddad6886a046d7d78ea2b6b4c71f9094b
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-23 01:05:22 +0900

    Increase API test coverage a bit. (#2389)
    * Increase API test coverage a bit.
    
    * suppress

---
+++ api/trace/src/test/java/io/opentelemetry/api/trace/DefaultTracerProviderTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.api.trace;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class DefaultTracerProviderTest {
+
+  @Test
+  void returnsDefaultTracer() {
+    assertThat(TracerProvider.getDefault().get("test")).isInstanceOf(DefaultTracer.class);
+    assertThat(TracerProvider.getDefault().get("test", "1.0")).isInstanceOf(DefaultTracer.class);
+  }
+}
