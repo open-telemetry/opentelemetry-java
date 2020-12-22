dcc1506c09c604eda14cf6d84f2bacdf68b46919
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2020-12-22 09:17:05 -0800

    Update api/context/src/test/java/io/opentelemetry/context/propagation/MultiTextMapPropagatorTest.java
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>

--- api/context/src/test/java/io/opentelemetry/context/propagation/MultiTextMapPropagatorTest.java
+++ api/context/src/test/java/io/opentelemetry/context/propagation/MultiTextMapPropagatorTest.java
@@ -24,6 +24,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
+@SuppressWarnings("deprecation")
 class MultiTextMapPropagatorTest {
 
   @Mock private TextMapPropagator propagator1;
