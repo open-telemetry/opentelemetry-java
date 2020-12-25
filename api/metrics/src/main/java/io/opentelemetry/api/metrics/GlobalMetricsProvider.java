67f9258e5af8fcbebc566be9dc3997162b8b69ae
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-25 14:09:53 +0900

    Add SdkMeterProviderBuilder.buildAndRegisterGlobal

--- api/metrics/src/main/java/io/opentelemetry/api/metrics/GlobalMetricsProvider.java
+++ api/metrics/src/main/java/io/opentelemetry/api/metrics/GlobalMetricsProvider.java
@@ -40,6 +40,16 @@ public class GlobalMetricsProvider {
   }
 
   /**
+   * Sets the {@link MeterProvider} that should be the global instance. Future calls to {@link
+   * #get()} will return the provided {@link MeterProvider} instance. This should be called once as
+   * early as possible in your application initialization logic, often in a {@code static} block in
+   * your main class.
+   */
+  public static void set(MeterProvider meterProvider) {
+    globalMeterProvider.set(meterProvider);
+  }
+
+  /**
    * Gets or creates a named meter instance from the globally registered {@link MeterProvider}.
    *
    * <p>This is a shortcut method for {@code getGlobalMeterProvider().get(instrumentationName)}
