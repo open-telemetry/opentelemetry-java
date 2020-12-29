a72eee183bc45cfcf4fb01f4755c11ac1ec6c990
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-12-29 13:33:45 +0900

    WIP

--- sdk-extensions/autoconfigure/src/testFullConfig/java/io/opentelemetry/sdk/autoconfigure/FullConfigTest.java
+++ sdk-extensions/autoconfigure/src/testFullConfig/java/io/opentelemetry/sdk/autoconfigure/FullConfigTest.java
@@ -10,19 +10,39 @@ import static org.awaitility.Awaitility.await;
 
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.RequestHeaders;
 import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.ServiceRequestContext;
 import com.linecorp.armeria.server.grpc.GrpcService;
 import com.linecorp.armeria.testing.junit5.server.ServerExtension;
 import io.grpc.stub.StreamObserver;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.baggage.Baggage;
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
+import io.opentelemetry.api.trace.SpanId;
+import io.opentelemetry.api.trace.TraceFlags;
+import io.opentelemetry.api.trace.TraceId;
+import io.opentelemetry.api.trace.TraceState;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.Context;
 import io.opentelemetry.exporter.jaeger.proto.api_v2.Collector;
 import io.opentelemetry.exporter.jaeger.proto.api_v2.CollectorServiceGrpc;
+import io.opentelemetry.extension.trace.propagation.AwsXrayPropagator;
+import io.opentelemetry.extension.trace.propagation.B3Propagator;
+import io.opentelemetry.extension.trace.propagation.JaegerPropagator;
+import io.opentelemetry.extension.trace.propagation.OtTracerPropagator;
 import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest;
 import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceResponse;
 import io.opentelemetry.proto.collector.metrics.v1.MetricsServiceGrpc;
 import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
 import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceResponse;
 import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import org.junit.jupiter.api.Test;
@@ -52,6 +72,14 @@ public class FullConfigTest {
                         public void export(
                             ExportTraceServiceRequest request,
                             StreamObserver<ExportTraceServiceResponse> responseObserver) {
+                          try {
+                            RequestHeaders headers =
+                                ServiceRequestContext.current().request().headers();
+                            assertThat(headers.get("cat")).isEqualTo("meow");
+                            assertThat(headers.get("dog")).isEqualTo("bark");
+                          } catch (AssertionError e) {
+                            responseObserver.onError(e);
+                          }
                           otlpTraceRequests.add(request);
                           responseObserver.onNext(ExportTraceServiceResponse.getDefaultInstance());
                           responseObserver.onCompleted();
@@ -64,6 +92,14 @@ public class FullConfigTest {
                         public void export(
                             ExportMetricsServiceRequest request,
                             StreamObserver<ExportMetricsServiceResponse> responseObserver) {
+                          try {
+                            RequestHeaders headers =
+                                ServiceRequestContext.current().request().headers();
+                            assertThat(headers.get("cat")).isEqualTo("meow");
+                            assertThat(headers.get("dog")).isEqualTo("bark");
+                          } catch (AssertionError e) {
+                            responseObserver.onError(e);
+                          }
                           if (request.getResourceMetricsCount() > 0) {
                             otlpMetricsRequests.add(request);
                           }
@@ -112,6 +148,34 @@ public class FullConfigTest {
     System.setProperty("otel.exporter.zipkin.endpoint", "http://" + endpoint + "/api/v2/spans");
 
     OpenTelemetry openTelemetry = OpenTelemetrySdkAutoConfiguration.initialize();
+
+    Map<String, String> headers = new HashMap<>();
+    openTelemetry
+        .getPropagators()
+        .getTextMapPropagator()
+        .inject(
+            Context.root()
+                .with(
+                    Span.wrap(
+                        SpanContext.create(
+                            TraceId.fromLongs(1, 1),
+                            SpanId.fromLong(2),
+                            TraceFlags.getDefault(),
+                            TraceState.builder().set("cat", "meow").build())))
+                .with(Baggage.builder().put("airplane", "luggage").build()),
+            headers,
+            Map::put);
+    List<String> keys = new ArrayList<>();
+    keys.addAll(W3CTraceContextPropagator.getInstance().fields());
+    keys.addAll(W3CBaggagePropagator.getInstance().fields());
+    keys.addAll(B3Propagator.getInstance().fields());
+    keys.addAll(JaegerPropagator.getInstance().fields());
+    // Legacy baggage format.
+    keys.add("uberctx-airplane");
+    keys.addAll(OtTracerPropagator.getInstance().fields());
+    keys.addAll(AwsXrayPropagator.getInstance().fields());
+    assertThat(headers).containsOnlyKeys(keys);
+
     openTelemetry.getTracer("test").spanBuilder("test").startSpan().end();
 
     await()
@@ -122,8 +186,7 @@ public class FullConfigTest {
               assertThat(zipkinJsonRequests).hasSize(1);
 
               // Not well defined how many metric exports would have happened by now, check that any
-              // did.
-              // The metrics will be BatchSpanProcessor metrics.
+              // did. The metrics will be BatchSpanProcessor metrics.
               // TODO(anuraaga): WIP
               // assertThat(otlpMetricsRequests).isNotEmpty();
             });
