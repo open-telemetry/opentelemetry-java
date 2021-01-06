f508b3d0d8196386d7fdb8de8c8fa62de9544a93
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-01-06 14:37:06 +0900

    Remove unused

--- sdk/trace/src/main/java/io/opentelemetry/sdk/trace/samplers/ImmutableSamplingResult.java
+++ sdk/trace/src/main/java/io/opentelemetry/sdk/trace/samplers/ImmutableSamplingResult.java
@@ -5,10 +5,7 @@
 
 package io.opentelemetry.sdk.trace.samplers;
 
-import static io.opentelemetry.api.common.AttributeKey.doubleKey;
-
 import com.google.auto.value.AutoValue;
-import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import javax.annotation.concurrent.Immutable;
 
@@ -24,17 +21,6 @@ abstract class ImmutableSamplingResult implements SamplingResult {
 
   static final SamplingResult EMPTY_RECORDED_SAMPLING_RESULT =
       ImmutableSamplingResult.createWithoutAttributes(SamplingResult.Decision.RECORD_ONLY);
-
-  /**
-   * Probability value used by a probability-based Span sampling strategy.
-   *
-   * <p>Note: This will need to be updated if a specification for this value is merged which changes
-   * this proposed value. Also, once it's in the spec, we should move it somewhere more visible.
-   *
-   * <p>See https://github.com/open-telemetry/opentelemetry-specification/pull/570
-   */
-  // Visible for tests.
-  static final AttributeKey<Double> SAMPLING_PROBABILITY = doubleKey("sampling.probability");
 
   static SamplingResult createSamplingResult(Decision decision, Attributes attributes) {
     return new AutoValue_ImmutableSamplingResult(decision, attributes);
