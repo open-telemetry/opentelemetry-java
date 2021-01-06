2f698980adb0c5d7356a8700d3e19b4df5882eb1
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-01-06 15:59:31 +0900

    logunit

--- api/all/src/testDoesNotLogIfSdkFoundAndSuppressed/java/io/opentelemetry/api/GlobalOpenTelemetryTest.java
+++ api/all/src/testDoesNotLogIfSdkFoundAndSuppressed/java/io/opentelemetry/api/GlobalOpenTelemetryTest.java
@@ -5,36 +5,20 @@
 
 package io.opentelemetry.api;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
+import io.github.netmikey.logunit.api.LogCapturer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 public class GlobalOpenTelemetryTest {
 
+  @RegisterExtension
+  LogCapturer logs = LogCapturer.create().captureForType(GlobalOpenTelemetry.class);
+
   @Test
   void logIsSuppressed() {
-    Logger logger = GlobalOpenTelemetry.logger;
-    AtomicReference<LogRecord> logged = new AtomicReference<>();
-    Handler handler =
-        new Handler() {
-          @Override
-          public void publish(LogRecord record) {
-            logged.set(record);
-          }
-
-          @Override
-          public void flush() {}
-
-          @Override
-          public void close() {}
-        };
-    logger.addHandler(handler);
-    logger.setUseParentHandlers(false);
     GlobalOpenTelemetry.get();
-    assertThat(logged).hasValue(null);
+    logs.assertDoesNotContain(
+        "Attempt to access GlobalOpenTelemetry.get before OpenTelemetrySdk has been "
+            + "initialized.");
   }
 }
--- api/all/src/testLogsIfSdkFound/java/io/opentelemetry/api/GlobalOpenTelemetryTest.java
+++ api/all/src/testLogsIfSdkFound/java/io/opentelemetry/api/GlobalOpenTelemetryTest.java
@@ -7,43 +7,24 @@ package io.opentelemetry.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
+import io.github.netmikey.logunit.api.LogCapturer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.slf4j.event.Level;
+import org.slf4j.event.LoggingEvent;
 
 class GlobalOpenTelemetryTest {
 
+  @RegisterExtension
+  LogCapturer logs = LogCapturer.create().captureForType(GlobalOpenTelemetry.class);
+
   @Test
   void logsWarningOnAccessWithoutSdk() {
-    Logger logger = GlobalOpenTelemetry.logger;
-    AtomicReference<LogRecord> logged = new AtomicReference<>();
-    Handler handler =
-        new Handler() {
-          @Override
-          public void publish(LogRecord record) {
-            logged.set(record);
-          }
-
-          @Override
-          public void flush() {}
-
-          @Override
-          public void close() {}
-        };
-    logger.addHandler(handler);
-    logger.setUseParentHandlers(false);
     GlobalOpenTelemetry.get();
-    assertThat(logged)
-        .hasValueSatisfying(
-            record -> {
-              assertThat(record.getLevel()).isEqualTo(Level.SEVERE);
-              assertThat(record.getMessage())
-                  .contains(
-                      "Attempt to access GlobalOpenTelemetry.get before OpenTelemetrySdk has been "
-                          + "initialized.");
-            });
+    LoggingEvent log =
+        logs.assertContains(
+            "Attempt to access GlobalOpenTelemetry.get before OpenTelemetrySdk has been "
+                + "initialized.");
+    assertThat(log.getLevel()).isEqualTo(Level.ERROR);
   }
 }
