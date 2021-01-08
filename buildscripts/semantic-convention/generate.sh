73a59e0e152d3e950e2e6ebcab4bbde3cb99f56c
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-01-08 11:06:09 +0900

    Update to latest semantic conventions. (#2452)
    * Update to latest semantic conventions.
    
    * Update buildscripts/semantic-convention/generate.sh
    
    Co-authored-by: John Watson <jkwatson@gmail.com>
    
    Co-authored-by: Bogdan Drutu <lazy@splunk.com>
    Co-authored-by: John Watson <jkwatson@gmail.com>

--- buildscripts/semantic-convention/generate.sh
+++ buildscripts/semantic-convention/generate.sh
@@ -3,9 +3,9 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="${SCRIPT_DIR}/../../"
 # freeze the spec version to make SemanticAttributes generation reproducible
-# this hash was obtained by calling `git rev-parse master` on 14th Dec 2020
+# this hash was obtained by calling `git rev-parse master` on 2021/01/07
 # we can't use a version tag here because otel-spec releases are very rare
-SPEC_VERSION=7fb1f01e6a4d9a6b8b7f8780385809136179fd45
+SPEC_VERSION=d01d110b41b384e3d90169ec8d66d07f8b5963cd
 
 cd ${SCRIPT_DIR}
 
