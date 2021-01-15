8e0d7fbf076145a7f0c084ac185fd9d890618dd2
John Watson <jkwatson@gmail.com>
2021-01-15 08:44:59 -0800

    Update the http example to show better SDK/API separation. (#2532)

--- examples/http/src/main/java/io/opentelemetry/example/http/HttpClient.java
+++ examples/http/src/main/java/io/opentelemetry/example/http/HttpClient.java
@@ -9,16 +9,10 @@ import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.api.trace.attributes.SemanticAttributes;
-import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
-import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.context.propagation.TextMapPropagator;
-import io.opentelemetry.exporter.logging.LoggingSpanExporter;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -29,28 +23,19 @@ import java.nio.charset.Charset;
 
 public class HttpClient {
 
-  // OTel API
-  private static final LoggingSpanExporter loggingExporter = new LoggingSpanExporter();
-  private static final OpenTelemetry openTelemetry = initOpenTelemetry(loggingExporter);
+  // it's important to initialize the OpenTelemetry SDK as early in your applications lifecycle as
+  // possible.
+  private static final OpenTelemetry openTelemetry = ExampleConfiguration.initOpenTelemetry();
+
   private static final Tracer tracer =
       openTelemetry.getTracer("io.opentelemetry.example.http.HttpClient");
+  private static final TextMapPropagator textMapPropagator =
+      openTelemetry.getPropagators().getTextMapPropagator();
+
   // Export traces to log
   // Inject the span context into the request
   private static final TextMapPropagator.Setter<HttpURLConnection> setter =
       URLConnection::setRequestProperty;
-
-  private static OpenTelemetry initOpenTelemetry(LoggingSpanExporter loggingExporter) {
-    // install the W3C Trace Context propagator
-    // Get the tracer management instance
-    SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder().build();
-    // Set to process the the spans by the LogExporter
-    sdkTracerProvider.addSpanProcessor(SimpleSpanProcessor.builder(loggingExporter).build());
-
-    return OpenTelemetrySdk.builder()
-        .setTracerProvider(sdkTracerProvider)
-        .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-        .build();
-  }
 
   private void makeRequest() throws IOException {
     int port = 8080;
@@ -76,7 +61,7 @@ public class HttpClient {
       span.setAttribute(SemanticAttributes.HTTP_URL, url.toString());
 
       // Inject the request with the current Context/Span.
-      openTelemetry.getPropagators().getTextMapPropagator().inject(Context.current(), con, setter);
+      textMapPropagator.inject(Context.current(), con, setter);
 
       try {
         // Process the request
