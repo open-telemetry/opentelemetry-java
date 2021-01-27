15fcadd8d7e7b139f30260bd575b112b827c2734
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-01-27 13:32:49 +0900

    Move Sampler to SdkTracerProvider (#2585)
    * Move Sampler to SdkTracerProvider
    
    * Better zpages
    
    * Test

--- sdk-extensions/otproto/src/main/java/io/opentelemetry/sdk/extension/otproto/TraceProtoUtils.java
+++ sdk-extensions/otproto/src/main/java/io/opentelemetry/sdk/extension/otproto/TraceProtoUtils.java
@@ -8,9 +8,6 @@ package io.opentelemetry.sdk.extension.otproto;
 import com.google.protobuf.ByteString;
 import io.opentelemetry.api.trace.SpanId;
 import io.opentelemetry.api.trace.TraceId;
-import io.opentelemetry.proto.trace.v1.ConstantSampler;
-import io.opentelemetry.sdk.trace.config.TraceConfig;
-import io.opentelemetry.sdk.trace.samplers.Sampler;
 
 /** Utilities for converting various objects to protobuf representations. */
 final class TraceProtoUtils {
@@ -22,7 +19,7 @@ final class TraceProtoUtils {
    * @param spanId the spanId to convert.
    * @return a ByteString representation.
    */
-  public static ByteString toProtoSpanId(String spanId) {
+  static ByteString toProtoSpanId(String spanId) {
     return ByteString.copyFrom(SpanId.bytesFromHex(spanId, 0));
   }
 
@@ -32,50 +29,7 @@ final class TraceProtoUtils {
    * @param traceId the traceId to convert.
    * @return a ByteString representation.
    */
-  public static ByteString toProtoTraceId(String traceId) {
+  static ByteString toProtoTraceId(String traceId) {
     return ByteString.copyFrom(TraceId.bytesFromHex(traceId, 0));
-  }
-
-  /**
-   * Returns a {@code TraceConfig} from the given proto.
-   *
-   * @param traceConfigProto proto format {@code TraceConfig}.
-   * @return a {@code TraceConfig}.
-   */
-  public static TraceConfig traceConfigFromProto(
-      io.opentelemetry.proto.trace.v1.TraceConfig traceConfigProto) {
-    return TraceConfig.builder()
-        .setSampler(fromProtoSampler(traceConfigProto))
-        .setMaxNumberOfAttributes((int) traceConfigProto.getMaxNumberOfAttributes())
-        .setMaxNumberOfEvents((int) traceConfigProto.getMaxNumberOfTimedEvents())
-        .setMaxNumberOfLinks((int) traceConfigProto.getMaxNumberOfLinks())
-        .setMaxNumberOfAttributesPerEvent(
-            (int) traceConfigProto.getMaxNumberOfAttributesPerTimedEvent())
-        .setMaxNumberOfAttributesPerLink((int) traceConfigProto.getMaxNumberOfAttributesPerLink())
-        .build();
-  }
-
-  private static Sampler fromProtoSampler(
-      io.opentelemetry.proto.trace.v1.TraceConfig traceConfigProto) {
-    if (traceConfigProto.hasConstantSampler()) {
-      ConstantSampler constantSampler = traceConfigProto.getConstantSampler();
-      switch (constantSampler.getDecision()) {
-        case ALWAYS_ON:
-          return Sampler.alwaysOn();
-        case ALWAYS_OFF:
-          return Sampler.alwaysOff();
-        case ALWAYS_PARENT:
-          // TODO: add support.
-        case UNRECOGNIZED:
-          throw new IllegalArgumentException("unrecognized constant sampling samplingResult");
-      }
-    }
-    if (traceConfigProto.hasTraceIdRatioBased()) {
-      return Sampler.traceIdRatioBased(traceConfigProto.getTraceIdRatioBased().getSamplingRatio());
-    }
-    if (traceConfigProto.hasRateLimitingSampler()) {
-      // TODO: add support for RateLimiting Sampler
-    }
-    throw new IllegalArgumentException("unknown sampler in the trace config proto");
   }
 }
--- sdk/trace/src/test/java/io/opentelemetry/sdk/trace/config/TraceConfigTest.java
+++ sdk/trace/src/test/java/io/opentelemetry/sdk/trace/config/TraceConfigTest.java
@@ -6,17 +6,13 @@
 package io.opentelemetry.sdk.trace.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import io.opentelemetry.sdk.trace.samplers.Sampler;
 import org.junit.jupiter.api.Test;
 
 class TraceConfigTest {
 
   @Test
   void defaultTraceConfig() {
-    assertThat(TraceConfig.getDefault().getSampler().getDescription())
-        .isEqualTo(Sampler.parentBased(Sampler.alwaysOn()).getDescription());
     assertThat(TraceConfig.getDefault().getMaxNumberOfAttributes()).isEqualTo(1000);
     assertThat(TraceConfig.getDefault().getMaxNumberOfEvents()).isEqualTo(1000);
     assertThat(TraceConfig.getDefault().getMaxNumberOfLinks()).isEqualTo(1000);
@@ -25,79 +21,15 @@ class TraceConfigTest {
   }
 
   @Test
-  void updateTraceConfig_NullSampler() {
-    assertThatThrownBy(() -> TraceConfig.builder().setSampler(null))
-        .isInstanceOf(NullPointerException.class);
-  }
-
-  @Test
-  void updateTraceConfig_NonPositiveMaxNumberOfAttributes() {
-    assertThatThrownBy(() -> TraceConfig.builder().setMaxNumberOfAttributes(0))
-        .isInstanceOf(IllegalArgumentException.class);
-  }
-
-  @Test
-  void updateTraceConfig_NonPositiveMaxNumberOfEvents() {
-    assertThatThrownBy(() -> TraceConfig.builder().setMaxNumberOfEvents(0))
-        .isInstanceOf(IllegalArgumentException.class);
-  }
-
-  @Test
-  void updateTraceConfig_NonPositiveMaxNumberOfLinks() {
-    assertThatThrownBy(() -> TraceConfig.builder().setMaxNumberOfLinks(0))
-        .isInstanceOf(IllegalArgumentException.class);
-  }
-
-  @Test
-  void updateTraceConfig_NonPositiveMaxNumberOfAttributesPerEvent() {
-    assertThatThrownBy(() -> TraceConfig.builder().setMaxNumberOfAttributesPerEvent(0))
-        .isInstanceOf(IllegalArgumentException.class);
-  }
-
-  @Test
-  void updateTraceConfig_NonPositiveMaxNumberOfAttributesPerLink() {
-    assertThatThrownBy(() -> TraceConfig.builder().setMaxNumberOfAttributesPerLink(0))
-        .isInstanceOf(IllegalArgumentException.class);
-  }
-
-  @Test
-  void updateTraceConfig_InvalidTraceIdRatioBased() {
-    assertThatThrownBy(() -> TraceConfig.builder().setTraceIdRatioBased(2))
-        .isInstanceOf(IllegalArgumentException.class);
-  }
-
-  @Test
-  void updateTraceConfig_NegativeTraceIdRatioBased() {
-    assertThatThrownBy(() -> TraceConfig.builder().setTraceIdRatioBased(-1))
-        .isInstanceOf(IllegalArgumentException.class);
-  }
-
-  @Test
-  void updateTraceConfig_OffTraceIdRatioBased() {
-    TraceConfig traceConfig = TraceConfig.builder().setTraceIdRatioBased(0).build();
-    assertThat(traceConfig.getSampler()).isSameAs(Sampler.alwaysOff());
-  }
-
-  @Test
-  void updateTraceConfig_OnTraceIdRatioBased() {
-    TraceConfig traceConfig = TraceConfig.builder().setTraceIdRatioBased(1).build();
-
-    Sampler sampler = traceConfig.getSampler();
-    assertThat(sampler).isEqualTo(Sampler.parentBased(Sampler.alwaysOn()));
-  }
-
-  @Test
   void updateTraceConfig_All() {
     TraceConfig traceConfig =
         TraceConfig.builder()
-            .setSampler(Sampler.alwaysOff())
             .setMaxNumberOfAttributes(8)
             .setMaxNumberOfEvents(10)
             .setMaxNumberOfLinks(11)
             .setMaxNumberOfAttributesPerEvent(1)
             .setMaxNumberOfAttributesPerLink(2)
             .build();
-    assertThat(traceConfig.getSampler()).isEqualTo(Sampler.alwaysOff());
     assertThat(traceConfig.getMaxNumberOfAttributes()).isEqualTo(8);
     assertThat(traceConfig.getMaxNumberOfEvents()).isEqualTo(10);
     assertThat(traceConfig.getMaxNumberOfLinks()).isEqualTo(11);
