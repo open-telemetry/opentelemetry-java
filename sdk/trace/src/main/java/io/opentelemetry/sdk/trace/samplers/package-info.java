389f00f5f50d80bcc46a91092c637a6cce227940
jkwatson <jkwatson@gmail.com>
2021-02-03 10:53:07 -0800

    Add javadoc to the package-info.java for Sampling and update some javadoc for parent-based Samplers.

--- sdk/trace/src/main/java/io/opentelemetry/sdk/trace/samplers/package-info.java
+++ sdk/trace/src/main/java/io/opentelemetry/sdk/trace/samplers/package-info.java
@@ -3,7 +3,31 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/** Samplers for selecting traces that are recorded and exported. */
+/**
+ * This package contains {@link io.opentelemetry.sdk.trace.samplers.Sampler}s for selecting traces
+ * that are recorded and exported. <br>
+ *
+ * <p>Sampling is a mechanism to control the noise and overhead introduced by OpenTelemetry by
+ * reducing the number of samples of traces collected and sent to the backend. <br>
+ *
+ * <p>The following sampling strategies are provided here:
+ *
+ * <p>{@link io.opentelemetry.sdk.trace.samplers.Sampler#alwaysOff()} : This strategy will ensure
+ * that no Spans are ever sent to the export pipeline.
+ *
+ * <p>{@link io.opentelemetry.sdk.trace.samplers.Sampler#alwaysOn()} : This strategy will ensure
+ * that every Span will be sent to the export pipeline.
+ *
+ * <p>{@link io.opentelemetry.sdk.trace.samplers.Sampler#traceIdRatioBased(double)} : This strategy
+ * will sample the provided fraction of Spans, deterministically based on the TraceId of the Spans.
+ * This means that all spans from the a given trace will have the same sampling result.
+ *
+ * <p>{@code Sampler#parentBased(Sampler)} : This strategy will always use the sampled state of the
+ * parent span when deciding whether to sample a Span or not. If the the Span has no parent, the
+ * provided "root" Sampler will be used for that decision. The parent-based strategy is highly
+ * configurable, using the {@link io.opentelemetry.sdk.trace.samplers.ParentBasedSamplerBuilder}
+ * which can be acquired from the {@code Sampler#parentBasedBuilder(Sampler)} method.
+ */
 @ParametersAreNonnullByDefault
 package io.opentelemetry.sdk.trace.samplers;
 
