57dd6c5c7018fc6034ef3f1ab6802c2325e7f2a2
Bogdan Drutu <bogdandrutu@gmail.com>
2021-02-08 11:58:26 -0800

    Partially reverts #2750
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

--- exporters/otlp/trace/src/jmh/java/io/opentelemetry/exporter/otlp/trace/RequestMarshalState.java
+++ exporters/otlp/trace/src/jmh/java/io/opentelemetry/exporter/otlp/trace/RequestMarshalState.java
@@ -8,10 +8,8 @@ package io.opentelemetry.exporter.otlp.trace;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.SpanContext;
-import io.opentelemetry.api.trace.SpanId;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.TraceFlags;
-import io.opentelemetry.api.trace.TraceId;
 import io.opentelemetry.api.trace.TraceState;
 import io.opentelemetry.sdk.common.InstrumentationLibraryInfo;
 import io.opentelemetry.sdk.resources.Resource;
@@ -47,12 +45,8 @@ public class RequestMarshalState {
 
   private static final InstrumentationLibraryInfo INSTRUMENTATION_LIBRARY_INFO =
       InstrumentationLibraryInfo.create("name", null);
-  private static final byte[] TRACE_ID_BYTES =
-      new byte[] {123, 46, 23, 78, 12, 5, (byte) 180, (byte) 223, 45, 89, 71, 61, 62, 29, 34, 54};
-  private static final String TRACE_ID = TraceId.fromBytes(TRACE_ID_BYTES);
-  private static final byte[] SPAN_ID_BYTES =
-      new byte[] {(byte) 198, (byte) 245, (byte) 213, (byte) 156, 46, 31, 29, 101};
-  private static final String SPAN_ID = SpanId.fromBytes(SPAN_ID_BYTES);
+  private static final String TRACE_ID = "7b2e170db4df2d593ddb4ddf2ddf2d59";
+  private static final String SPAN_ID = "170d3ddb4d23e81f";
   private static final SpanContext SPAN_CONTEXT =
       SpanContext.create(TRACE_ID, SPAN_ID, TraceFlags.getSampled(), TraceState.getDefault());
 
