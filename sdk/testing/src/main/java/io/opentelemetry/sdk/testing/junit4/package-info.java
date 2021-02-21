013643c7074795e72548623d626d1d5fc237147f
Bogdan Drutu <bogdandrutu@gmail.com>
2021-02-21 10:44:00 -0800

    Add package-info for all packages (#2902)
    Signed-off-by: Bogdan Drutu <bogdandrutu@gmail.com>

---
+++ sdk/testing/src/main/java/io/opentelemetry/sdk/testing/junit4/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+@ParametersAreNonnullByDefault
+package io.opentelemetry.sdk.testing.junit4;
+
+import javax.annotation.ParametersAreNonnullByDefault;
