c099814ebc84b3d888b8a308d7cc6bc77baf9804
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-03-05 16:34:48 +0900

    Have SimpleSpanProcessor keep track of pending exports and wait for them when flushed.

--- sdk/trace/src/main/java/io/opentelemetry/sdk/trace/export/SimpleSpanProcessor.java
+++ sdk/trace/src/main/java/io/opentelemetry/sdk/trace/export/SimpleSpanProcessor.java
@@ -15,6 +15,8 @@ import io.opentelemetry.sdk.trace.SpanProcessor;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -31,6 +33,7 @@ public final class SimpleSpanProcessor implements SpanProcessor {
 
   private final SpanExporter spanExporter;
   private final boolean sampled;
+  private final Map<CompletableResultCode, Boolean> pendingExports = new ConcurrentHashMap<>();
   private final AtomicBoolean isShutdown = new AtomicBoolean(false);
 
   /**
@@ -65,8 +68,10 @@ public final class SimpleSpanProcessor implements SpanProcessor {
     try {
       List<SpanData> spans = Collections.singletonList(span.toSpanData());
       final CompletableResultCode result = spanExporter.export(spans);
+      pendingExports.put(result, true);
       result.whenComplete(
           () -> {
+            pendingExports.remove(result);
             if (!result.isSuccess()) {
               logger.log(Level.FINE, "Exporter failed");
             }
@@ -86,6 +91,27 @@ public final class SimpleSpanProcessor implements SpanProcessor {
     if (isShutdown.getAndSet(true)) {
       return CompletableResultCode.ofSuccess();
     }
-    return spanExporter.shutdown();
+    final CompletableResultCode result = new CompletableResultCode();
+
+    final CompletableResultCode flushResult = forceFlush();
+    flushResult.whenComplete(
+        () -> {
+          final CompletableResultCode shutdownResult = spanExporter.shutdown();
+          shutdownResult.whenComplete(
+              () -> {
+                if (!flushResult.isSuccess() || !shutdownResult.isSuccess()) {
+                  result.fail();
+                } else {
+                  result.succeed();
+                }
+              });
+        });
+
+    return result;
+  }
+
+  @Override
+  public CompletableResultCode forceFlush() {
+    return CompletableResultCode.ofAll(pendingExports.keySet());
   }
 }
