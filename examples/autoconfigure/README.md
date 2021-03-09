5cb97db06bc83d684ab1f9e29909cfed2cf2138b
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-03-09 18:07:06 +0100

    Remove metrics

--- examples/autoconfigure/README.md
+++ examples/autoconfigure/README.md
@@ -16,10 +16,10 @@ First build this example application:
 ../gradlew shadowJar
 ```
 
-Then start the example application with the logging exporters configured:
+Then start the example application with the logging exporter configured:
 
 ```shell
-java -Dotel.traces.exporter=logging -Dotel.metrics.exporter=logging \
+java -Dotel.traces.exporter=logging \
   -cp build/libs/opentelemetry-examples-autoconfigure-0.1.0-SNAPSHOT-all.jar \
   io.opentelemetry.example.autoconfigure.AutoConfigExample
 ```
@@ -28,7 +28,6 @@ Alternatively, instead of system properties you can use environment variables:
 
 ```shell
 export OTEL_TRACES_EXPORTER=logging
-export OTEL_METRICS_EXPORTER=logging
 
 java -cp build/libs/opentelemetry-examples-autoconfigure-0.1.0-SNAPSHOT-all.jar \
   io.opentelemetry.example.autoconfigure.AutoConfigExample
@@ -37,14 +36,12 @@ java -cp build/libs/opentelemetry-examples-autoconfigure-0.1.0-SNAPSHOT-all.jar 
 Full documentation of all supported properties can be found in
 the [OpenTelemetry SDK Autoconfigure README](https://github.com/open-telemetry/opentelemetry-java/tree/main/sdk-extensions/autoconfigure).
 
-After running the app you should see the trace and metrics data printed out in the console:
+After running the app you should see the trace printed out in the console:
 
 ```
 ...
 INFO: 'important work' : ca3938a5793f6f9aba5c757f536a50cb b5e826c981112198 INTERNAL [tracer: io.opentelemetry.example.autoconfigure.AutoConfigExample:] AttributesMap{data={foo=42, bar=a string!}, capacity=128, totalAddedValues=2}
 ...
-INFO: metric: MetricData{resource=Resource{attributes={service.name="unknown_service:java", telemetry.sdk.language="java", telemetry.sdk.name="opentelemetry", telemetry.sdk.version="1.0.0"}}, instrumentationLibraryInfo=InstrumentationLibraryInfo{name=io.opentelemetry.example.autoconfigure.AutoConfigExample, version=null}, name=count, description=, unit=1, type=LONG_SUM, data=LongSumData{points=[LongPointData{startEpochNanos=1615292303106000000, epochNanos=1615292304712000000, labels={}, value=1}], monotonic=true, aggregationTemporality=CUMULATIVE}}
-...
 ```
 
-Congratulations! You are now collecting traces and metrics using OpenTelemetry.
+Congratulations! You are now collecting traces using OpenTelemetry.
--- examples/autoconfigure/src/main/java/io/opentelemetry/example/autoconfigure/AutoConfigExample.java
+++ examples/autoconfigure/src/main/java/io/opentelemetry/example/autoconfigure/AutoConfigExample.java
@@ -1,24 +1,20 @@
 package io.opentelemetry.example.autoconfigure;
 
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.metrics.GlobalMetricsProvider;
-import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.sdk.autoconfigure.OpenTelemetrySdkAutoConfiguration;
 
 /**
  * An example of using {@link io.opentelemetry.sdk.autoconfigure.OpenTelemetrySdkAutoConfiguration}
- * and logging exporters: {@link io.opentelemetry.exporter.logging.LoggingSpanExporter} and {@link
- * io.opentelemetry.exporter.logging.LoggingMetricExporter}.
+ * and logging exporter: {@link io.opentelemetry.exporter.logging.LoggingSpanExporter}.
  */
 public final class AutoConfigExample {
   private static final String INSTRUMENTATION_NAME = AutoConfigExample.class.getName();
 
   public static void main(String[] args) throws InterruptedException {
-    // Export metrics every 800ms to avoid long Thread.sleep() at the end
-    // You probably won't need to export metrics so often in a real life scenario
-    System.setProperty("otel.imr.export.interval", "800");
+    // Turn off metrics for this example
+    System.setProperty("otel.metrics.exporter", "none");
 
     // Let the SDK configure itself using environment variables and system properties
     OpenTelemetry openTelemetry = OpenTelemetrySdkAutoConfiguration.initialize();
@@ -26,18 +22,12 @@ public final class AutoConfigExample {
     AutoConfigExample example = new AutoConfigExample(openTelemetry);
     // Do some real work that'll emit telemetry
     example.doWork();
-
-    // Flush out the metrics that have not yet been exported
-    Thread.sleep(1000);
   }
 
   private final Tracer tracer;
-  private final LongCounter counter;
 
   public AutoConfigExample(OpenTelemetry openTelemetry) {
     this.tracer = openTelemetry.getTracer(INSTRUMENTATION_NAME);
-    this.counter =
-        GlobalMetricsProvider.getMeter(INSTRUMENTATION_NAME).longCounterBuilder("count").build();
   }
 
   public void doWork() throws InterruptedException {
@@ -49,7 +39,6 @@ public final class AutoConfigExample {
             .startSpan();
     try {
       Thread.sleep(1000);
-      counter.add(1);
     } finally {
       span.end();
     }
