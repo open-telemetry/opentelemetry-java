eb40364f45473185f8e34810c7c8839409c2e704
Sergei Malafeev <sergei@malafeev.org>
2021-03-09 16:29:23 +0800

    add test
    Signed-off-by: Sergei Malafeev <sergei@malafeev.org>

---
+++ sdk-extensions/autoconfigure/src/testFullConfig/java/io/opentelemetry/sdk/autoconfigure/EndpointConfigurationTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.sdk.autoconfigure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.grpc.GrpcService;
+import com.linecorp.armeria.testing.junit5.server.ServerExtension;
+import io.grpc.stub.StreamObserver;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest;
+import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceResponse;
+import io.opentelemetry.proto.collector.metrics.v1.MetricsServiceGrpc;
+import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
+import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceResponse;
+import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+@SuppressWarnings("InterruptedExceptionSwallowed")
+class EndpointConfigurationTest {
+
+  private static final BlockingQueue<ExportTraceServiceRequest> otlpTraceRequests =
+      new LinkedBlockingDeque<>();
+  private static final BlockingQueue<ExportMetricsServiceRequest> otlpMetricsRequests =
+      new LinkedBlockingDeque<>();
+
+  @RegisterExtension
+  public static final ServerExtension server =
+      new ServerExtension() {
+        @Override
+        protected void configure(ServerBuilder sb) {
+          sb.service(
+              GrpcService.builder()
+                  // OTLP spans
+                  .addService(
+                      new TraceServiceGrpc.TraceServiceImplBase() {
+                        @Override
+                        public void export(
+                            ExportTraceServiceRequest request,
+                            StreamObserver<ExportTraceServiceResponse> responseObserver) {
+                          otlpTraceRequests.add(request);
+                          responseObserver.onNext(ExportTraceServiceResponse.getDefaultInstance());
+                          responseObserver.onCompleted();
+                        }
+                      })
+                  // OTLP metrics
+                  .addService(
+                      new MetricsServiceGrpc.MetricsServiceImplBase() {
+                        @Override
+                        public void export(
+                            ExportMetricsServiceRequest request,
+                            StreamObserver<ExportMetricsServiceResponse> responseObserver) {
+                          if (request.getResourceMetricsCount() > 0) {
+                            otlpMetricsRequests.add(request);
+                          }
+                          responseObserver.onNext(
+                              ExportMetricsServiceResponse.getDefaultInstance());
+                          responseObserver.onCompleted();
+                        }
+                      })
+                  .useBlockingTaskExecutor(true)
+                  .build());
+        }
+      };
+
+  @BeforeEach
+  void setUp() {
+    otlpTraceRequests.clear();
+    otlpMetricsRequests.clear();
+  }
+
+  @Test
+  void configures() {
+    // Point "otel.exporter.otlp.endpoint" to wrong endpoint
+    System.setProperty("otel.exporter.otlp.endpoint", "http://localhost/wrong");
+
+    // Point "otel.exporter.otlp.traces.endpoint" to correct endpoint
+    System.setProperty(
+        "otel.exporter.otlp.traces.endpoint", "http://localhost:" + server.httpPort());
+
+    // Point "otel.exporter.otlp.metrics.endpoint" to correct endpoint
+    System.setProperty(
+        "otel.exporter.otlp.metrics.endpoint", "http://localhost:" + server.httpPort());
+
+    System.setProperty("otel.exporter.otlp.timeout", "10000");
+
+    GlobalOpenTelemetry.get().getTracer("test").spanBuilder("test").startSpan().end();
+
+    await()
+        .untilAsserted(
+            () -> {
+              assertThat(otlpTraceRequests).hasSize(1);
+
+              // Not well defined how many metric exports would have happened by now, check that
+              // any
+              // did. The metrics will be BatchSpanProcessor metrics.
+              assertThat(otlpMetricsRequests).isNotEmpty();
+            });
+  }
+}
