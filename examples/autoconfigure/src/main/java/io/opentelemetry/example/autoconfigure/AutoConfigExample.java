2eb040990854ba212dcdf151e9f42fc1d62ea216
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2021-03-10 17:13:51 +0100

    Add an example that uses auto-configuration (#3003)
    * Add an example that uses auto-configuration
    
    * Don't use GlobalOpenTelemetry
    
    * Remove metrics
    
    * Remove no longer necessary metrics dependency
    
    * Add a TODO for 1.1.0 release

---
+++ examples/autoconfigure/src/main/java/io/opentelemetry/example/autoconfigure/AutoConfigExample.java
@@ -0,0 +1,47 @@
+package io.opentelemetry.example.autoconfigure;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.sdk.autoconfigure.OpenTelemetrySdkAutoConfiguration;
+
+/**
+ * An example of using {@link io.opentelemetry.sdk.autoconfigure.OpenTelemetrySdkAutoConfiguration}
+ * and logging exporter: {@link io.opentelemetry.exporter.logging.LoggingSpanExporter}.
+ */
+public final class AutoConfigExample {
+  private static final String INSTRUMENTATION_NAME = AutoConfigExample.class.getName();
+
+  public static void main(String[] args) throws InterruptedException {
+    // Turn off metrics for this example
+    // TODO: this won't be needed after 1.1.0 release
+    System.setProperty("otel.metrics.exporter", "none");
+
+    // Let the SDK configure itself using environment variables and system properties
+    OpenTelemetry openTelemetry = OpenTelemetrySdkAutoConfiguration.initialize();
+
+    AutoConfigExample example = new AutoConfigExample(openTelemetry);
+    // Do some real work that'll emit telemetry
+    example.doWork();
+  }
+
+  private final Tracer tracer;
+
+  public AutoConfigExample(OpenTelemetry openTelemetry) {
+    this.tracer = openTelemetry.getTracer(INSTRUMENTATION_NAME);
+  }
+
+  public void doWork() throws InterruptedException {
+    Span span =
+        tracer
+            .spanBuilder("important work")
+            .setAttribute("foo", 42)
+            .setAttribute("bar", "a string!")
+            .startSpan();
+    try {
+      Thread.sleep(1000);
+    } finally {
+      span.end();
+    }
+  }
+}
