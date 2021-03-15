10dc61165b236250e7ae608c03c49bc2dad638e8
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-03-15 14:45:33 +0900

    Add ConfigurableSamplerProvider to allow custom samplers in agent, etc

--- sdk-extensions/autoconfigure/src/main/java/io/opentelemetry/sdk/autoconfigure/spi/ConfigurableSpanExporterProvider.java
+++ sdk-extensions/autoconfigure/src/main/java/io/opentelemetry/sdk/autoconfigure/spi/ConfigurableSpanExporterProvider.java
@@ -10,8 +10,8 @@ import io.opentelemetry.sdk.trace.export.SpanExporter;
 
 /**
  * A service provider interface (SPI) for providing additional exporters that can be used with the
- * autoconfigured SDK. If the {@code otel.trace.exporter} property contains a value equal to what is
- * returned by {@link #getName()}, the exporter returned by {@link
+ * autoconfigured SDK. If the {@code otel.traces.exporter} property contains a value equal to what
+ * is returned by {@link #getName()}, the exporter returned by {@link
  * #createExporter(ConfigProperties)} will be enabled and added to the SDK.
  */
 public interface ConfigurableSpanExporterProvider {
@@ -23,7 +23,7 @@ public interface ConfigurableSpanExporterProvider {
   SpanExporter createExporter(ConfigProperties config);
 
   /**
-   * Returns the name of this exporter, which can be specified with the {@code otel.trace.exporter}
+   * Returns the name of this exporter, which can be specified with the {@code otel.traces.exporter}
    * property to enable it. The name returned should NOT be the same as any other exporter name. If
    * the name does conflict with another exporter name, the resulting behavior is undefined and it
    * is explicitly unspecified which exporter will actually be used.
