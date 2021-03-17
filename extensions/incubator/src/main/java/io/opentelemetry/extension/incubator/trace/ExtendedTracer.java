feabf440ad42008e030c6fcb8696b60e4a9e7047
jkwatson <jwatson@splunk.com>
2021-03-17 08:20:59 -0700

    repackage to match the module

--- extensions/incubator/src/main/java/io/opentelemetry/extension/incubator/trace/ExtendedTracer.java
+++ extensions/incubator/src/main/java/io/opentelemetry/extension/incubator/trace/ExtendedTracer.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.extension.trace;
+package io.opentelemetry.extension.incubator.trace;
 
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanBuilder;
--- extensions/incubator/src/test/java/io/opentelemetry/extension/incubator/trace/ExtendedTracerTest.java
+++ extensions/incubator/src/test/java/io/opentelemetry/extension/incubator/trace/ExtendedTracerTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.extension.trace;
+package io.opentelemetry.extension.incubator.trace;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
