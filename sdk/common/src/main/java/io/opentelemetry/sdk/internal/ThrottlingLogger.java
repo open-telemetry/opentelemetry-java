5f945a4677291458d294f725fff3338b3b2a8906
jkwatson <jwatson@splunk.com>
2021-03-30 10:11:26 -0700

    try the ratelimiter and see if it can work

--- sdk/common/src/main/java/io/opentelemetry/sdk/internal/ThrottlingLogger.java
+++ sdk/common/src/main/java/io/opentelemetry/sdk/internal/ThrottlingLogger.java
@@ -13,16 +13,16 @@ import javax.annotation.Nullable;
 
 /** Will limit the number of log messages emitted, so as not to spam when problems are happening. */
 public class ThrottlingLogger {
-//  private static final int RATE_LIMIT = 5;
-//  private static final int THROTTLED_RATE_LIMIT = 1;
-//  private static final TimeUnit rateTimeUnit = MINUTES;
+  //  private static final int RATE_LIMIT = 5;
+  //  private static final int THROTTLED_RATE_LIMIT = 1;
+  //  private static final TimeUnit rateTimeUnit = MINUTES;
 
   private final Logger delegate;
-//  private final Clock clock;
+  //  private final Clock clock;
 
   // this window tracks the number of logs messages per time unit. It holds the times of `log`
   // calls.
-//  private final Deque<Long> window = new ConcurrentLinkedDeque<>();
+  //  private final Deque<Long> window = new ConcurrentLinkedDeque<>();
   private final AtomicBoolean throttled = new AtomicBoolean(false);
   private final RateLimiter rateLimiter;
 
@@ -34,11 +34,11 @@ public class ThrottlingLogger {
   // visible for testing
   ThrottlingLogger(Logger delegate, Clock clock) {
     this.delegate = delegate;
-//    this.clock = clock;
-    this.rateLimiter = new RateLimiter(1d/60d,4d, clock);
+    //    this.clock = clock;
+    this.rateLimiter = new RateLimiter(1d / 60d, 4d, clock);
 
     // pre-populate to make the logic easier below
-//    IntStream.range(0, RATE_LIMIT).forEach(i -> window.push(0L));
+    //    IntStream.range(0, RATE_LIMIT).forEach(i -> window.push(0L));
   }
 
   /** Log a message at a level. */
@@ -59,27 +59,28 @@ public class ThrottlingLogger {
       throttled.set(false);
       doLog(level, message, throwable);
     }
-//    long now = clock.now();
-//    window.addFirst(now);
-//    long eldestTimestamp = window.removeLast();
-//
-//    long windowDurationMinutes = minutesBetween(now, eldestTimestamp);
-//
-//    if (windowDurationMinutes >= 1) {
-//      if (throwable != null) {
-//        delegate.log(level, message, throwable);
-//      } else {
-//        delegate.log(level, message);
-//      }
-//      return;
-//    }
-//
-//    if (throttled.compareAndSet(false, true)) {
-//      // shrink the queue down to only the throttled number of items
-//      IntStream.range(0, RATE_LIMIT - THROTTLED_RATE_LIMIT).forEach(i -> window.removeLast());
-//      delegate.log(
-//          level, "Too many log messages detected. Will only log once per minute from now on.");
-//    }
+    //    long now = clock.now();
+    //    window.addFirst(now);
+    //    long eldestTimestamp = window.removeLast();
+    //
+    //    long windowDurationMinutes = minutesBetween(now, eldestTimestamp);
+    //
+    //    if (windowDurationMinutes >= 1) {
+    //      if (throwable != null) {
+    //        delegate.log(level, message, throwable);
+    //      } else {
+    //        delegate.log(level, message);
+    //      }
+    //      return;
+    //    }
+    //
+    //    if (throttled.compareAndSet(false, true)) {
+    //      // shrink the queue down to only the throttled number of items
+    //      IntStream.range(0, RATE_LIMIT - THROTTLED_RATE_LIMIT).forEach(i -> window.removeLast());
+    //      delegate.log(
+    //          level, "Too many log messages detected. Will only log once per minute from now
+    // on.");
+    //    }
   }
 
   private void doLog(Level level, String message, Throwable throwable) {
@@ -94,7 +95,7 @@ public class ThrottlingLogger {
     return delegate.isLoggable(level);
   }
 
-//  private static long minutesBetween(long now, long then) {
-//    return rateTimeUnit.convert(now - then, NANOSECONDS);
-//  }
+  //  private static long minutesBetween(long now, long then) {
+  //    return rateTimeUnit.convert(now - then, NANOSECONDS);
+  //  }
 }
