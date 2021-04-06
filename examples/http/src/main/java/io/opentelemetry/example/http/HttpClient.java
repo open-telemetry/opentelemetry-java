826e3fbfc6f5433263f9c28c3a318b9490fddce7
Hangzhi <1454678938@qq.com>
2021-04-06 21:42:42 +0800

    test original example

--- examples/http/src/main/java/io/opentelemetry/example/http/HttpClient.java
+++ examples/http/src/main/java/io/opentelemetry/example/http/HttpClient.java
@@ -60,12 +60,7 @@ public final class HttpClient {
          - http.scheme, peer.ip, peer.port, http.target
       */
 
-      if (url.getUserInfo() == null || url.getUserInfo().isEmpty()) {
-        span.setAttribute(SemanticAttributes.HTTP_URL, url.toString());
-      } else {
-        span.setAttribute(
-            SemanticAttributes.HTTP_URL, url.toString().replace(url.getUserInfo() + '@', ""));
-      }
+      span.setAttribute(SemanticAttributes.HTTP_URL, url.toString());
 
       // Inject the request with the current Context/Span.
       textMapPropagator.inject(Context.current(), con, setter);
