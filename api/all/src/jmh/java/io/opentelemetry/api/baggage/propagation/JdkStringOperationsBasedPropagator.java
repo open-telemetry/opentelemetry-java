56f1a96cdfefce8e8a975e6e72bcd63e3fb159ea
Jakub Wach <jwach@splunk.com>
2021-04-28 09:24:35 +0200

    added JMH benchmark for W3C baggage propagator

---
+++ api/all/src/jmh/java/io/opentelemetry/api/baggage/propagation/JdkStringOperationsBasedPropagator.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.api.baggage.propagation;
+
+import static java.util.Collections.singletonList;
+
+import io.opentelemetry.api.baggage.Baggage;
+import io.opentelemetry.api.baggage.BaggageBuilder;
+import io.opentelemetry.api.baggage.BaggageEntryMetadata;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.context.propagation.TextMapSetter;
+import java.util.Collection;
+import java.util.List;
+import javax.annotation.Nullable;
+
+/** JDK string-ops based W3C baggage propagator. For comparison purposes. */
+class JdkStringOperationsBasedPropagator implements TextMapPropagator {
+
+  private static final String FIELD = "baggage";
+  private static final List<String> FIELDS = singletonList(FIELD);
+  private static final JdkStringOperationsBasedPropagator INSTANCE =
+      new JdkStringOperationsBasedPropagator();
+
+  static JdkStringOperationsBasedPropagator getInstance() {
+    return INSTANCE;
+  }
+
+  @Override
+  public Collection<String> fields() {
+    return FIELDS;
+  }
+
+  @Override
+  public <C> void inject(Context context, C carrier, TextMapSetter<C> setter) {
+    if (context == null || setter == null) {
+      return;
+    }
+    Baggage baggage = Baggage.fromContext(context);
+    if (baggage.isEmpty()) {
+      return;
+    }
+    StringBuilder headerContent = new StringBuilder();
+    baggage.forEach(
+        (key, baggageEntry) -> {
+          headerContent.append(key).append("=").append(baggageEntry.getValue());
+          String metadataValue = baggageEntry.getMetadata().getValue();
+          if (metadataValue != null && !metadataValue.isEmpty()) {
+            headerContent.append(";").append(metadataValue);
+          }
+          headerContent.append(",");
+        });
+    if (headerContent.length() > 0) {
+      headerContent.setLength(headerContent.length() - 1);
+      setter.set(carrier, FIELD, headerContent.toString());
+    }
+  }
+
+  @Override
+  public <C> Context extract(Context context, @Nullable C carrier, TextMapGetter<C> getter) {
+    if (context == null) {
+      return Context.root();
+    }
+    if (getter == null) {
+      return context;
+    }
+
+    String baggageHeader = getter.get(carrier, FIELD);
+    if (baggageHeader == null) {
+      return context;
+    }
+    if (baggageHeader.isEmpty()) {
+      return context.with(Baggage.empty());
+    }
+
+    BaggageBuilder baggageBuilder = Baggage.builder();
+    try {
+      extractEntries(baggageHeader, baggageBuilder);
+    } catch (RuntimeException e) {
+      return context.with(Baggage.empty());
+    }
+    return context.with(baggageBuilder.build());
+  }
+
+  private static void extractEntries(String baggageHeader, BaggageBuilder baggageBuilder) {
+    // todo: optimize this implementation; it can probably done with a single pass through the
+    // string.
+    String[] entries = baggageHeader.split(",");
+    for (String entry : entries) {
+      String metadata = "";
+      int beginningOfMetadata = entry.indexOf(";");
+      if (beginningOfMetadata > 0) {
+        metadata = entry.substring(beginningOfMetadata + 1);
+        entry = entry.substring(0, beginningOfMetadata);
+      }
+      String[] keyAndValue = entry.split("=");
+      for (int i = 0; i < keyAndValue.length; i += 2) {
+        String key = keyAndValue[i].trim();
+        String value = keyAndValue[i + 1].trim();
+        baggageBuilder.put(key, value, BaggageEntryMetadata.create(metadata.trim()));
+      }
+    }
+  }
+}
