ae3860e0698db35399d9b25ced972706df6fdb6d
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2021-05-27 21:20:41 +0300

    Add benchmark for recording exception on a span (#3262)
    * Add benchmark for recording exception on a span
    
    * Spotless

---
+++ sdk/trace/src/jmh/java/io/opentelemetry/sdk/trace/ExceptionBenchmark.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.sdk.trace;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+
+@State(Scope.Benchmark)
+public class ExceptionBenchmark {
+  private static SpanBuilder spanBuilder;
+
+  @Setup(Level.Trial)
+  public final void setup() {
+    SdkTracerProvider tracerProvider =
+        SdkTracerProvider.builder().setSampler(Sampler.alwaysOn()).build();
+
+    Tracer tracer = tracerProvider.get("benchmarkTracer");
+    spanBuilder = tracer.spanBuilder("benchmarkSpanBuilder");
+  }
+
+  @Benchmark
+  @Threads(value = 1)
+  @Fork(1)
+  @Warmup(iterations = 5, time = 1)
+  @Measurement(iterations = 10, time = 1)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  @BenchmarkMode(Mode.AverageTime)
+  public Span createSpan() {
+    Span span = spanBuilder.startSpan();
+    span.end();
+    return span;
+  }
+
+  @Benchmark
+  @Threads(value = 1)
+  @Fork(1)
+  @Warmup(iterations = 5, time = 1)
+  @Measurement(iterations = 10, time = 1)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  @BenchmarkMode(Mode.AverageTime)
+  public Span createSpanAndRecordException() {
+    Span span = spanBuilder.startSpan();
+    span.recordException(new RuntimeException());
+    span.end();
+    return span;
+  }
+
+  @Benchmark
+  @Threads(value = 1)
+  @Fork(1)
+  @Warmup(iterations = 5, time = 1)
+  @Measurement(iterations = 10, time = 1)
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  @BenchmarkMode(Mode.AverageTime)
+  public RuntimeException createException() {
+    return new RuntimeException();
+  }
+}
