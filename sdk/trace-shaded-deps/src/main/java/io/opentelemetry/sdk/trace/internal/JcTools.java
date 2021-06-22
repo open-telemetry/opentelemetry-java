99b40d4f1a0ff04f709822f3d4963b35f0d0f606
Bruno Medeiros <bruno.medeiros@vgw.co>
2021-06-22 18:06:36 +0800

    falls back to ArrayBlockingQueue if `jdk.unsupported` is not available

--- sdk/trace-shaded-deps/src/main/java/io/opentelemetry/sdk/trace/internal/JcTools.java
+++ sdk/trace-shaded-deps/src/main/java/io/opentelemetry/sdk/trace/internal/JcTools.java
@@ -6,6 +6,7 @@
 package io.opentelemetry.sdk.trace.internal;
 
 import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 import org.jctools.queues.MessagePassingQueue;
 import org.jctools.queues.MpscArrayQueue;
 
@@ -16,15 +17,24 @@ public final class JcTools {
    * Returns a new {@link Queue} appropriate for use with multiple producers and a single consumer.
    */
   public static <T> Queue<T> newFixedSizeQueue(int capacity) {
-    return new MpscArrayQueue<>(capacity);
+    try {
+      return new MpscArrayQueue<>(capacity);
+    } catch (java.lang.NoClassDefFoundError e) {
+      // Happens when modules such as jdk.unsupported are disabled in a custom JRE distribution
+      return new ArrayBlockingQueue<>(capacity);
+    }
   }
 
   /**
-   * Returns the capacity of the {@link Queue}, which must be a JcTools queue. We cast to the
-   * implementation so callers do not need to use the shaded classes.
+   * Returns the capacity of the {@link Queue}. We cast to the implementation so callers do not need
+   * to use the shaded classes.
    */
   public static long capacity(Queue<?> queue) {
-    return ((MessagePassingQueue<?>) queue).capacity();
+    if (queue instanceof MessagePassingQueue) {
+      return ((MessagePassingQueue<?>) queue).capacity();
+    } else {
+      return (long) ((ArrayBlockingQueue<?>) queue).remainingCapacity() + queue.size();
+    }
   }
 
   private JcTools() {}
