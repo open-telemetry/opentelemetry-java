51a09806e0d6c966c12f507afed0f52e3f51c5c1
John Watson <jkwatson@gmail.com>
2021-07-20 08:02:07 -0700

    Update sdk/trace/src/main/java/io/opentelemetry/sdk/trace/samplers/SamplingResult.java

--- sdk/trace/src/main/java/io/opentelemetry/sdk/trace/samplers/SamplingResult.java
+++ sdk/trace/src/main/java/io/opentelemetry/sdk/trace/samplers/SamplingResult.java
@@ -74,7 +74,7 @@ public interface SamplingResult {
    * <p>This is meant for use by custom {@link Sampler} implementations and is equivalent to calling
    * {@code SamplingResult.create(SamplingDecision.RECORD_AND_SAMPLE)}.
    *
-   * @return A {@link SamplingResult} with empty attributes and the provided {@code decision}.
+   * @return A "record and sample" {@link SamplingResult} with empty attributes.
    */
   static SamplingResult recordAndSample() {
     return ImmutableSamplingResult.EMPTY_RECORDED_AND_SAMPLED_SAMPLING_RESULT;
