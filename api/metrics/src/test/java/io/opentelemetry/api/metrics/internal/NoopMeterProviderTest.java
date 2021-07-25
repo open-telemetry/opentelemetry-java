2cbad26922530fdd1310187416ef726f2ad38486
Josh Suereth <Joshua.Suereth@gmail.com>
2021-07-25 12:25:18 -0400

    Add noop meter provider tests for code coverage.

---
+++ api/metrics/src/test/java/io/opentelemetry/api/metrics/internal/NoopMeterProviderTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.api.metrics.internal;
+
+import io.opentelemetry.api.metrics.MeterProvider;
+import org.junit.jupiter.api.Test;
+
+public class NoopMeterProviderTest {
+  @Test
+  void noopMeterProvider_getDoesNotThrow() {
+    MeterProvider provider = MeterProvider.noop();
+    provider.get("user-instrumentation");
+    provider.get("advanced-instrumetnation", "1.0");
+    provider.get("schema-instrumentation", "1.0", "myschema://url");
+  }
+
+  @Test
+  void noopMeterProvider_builderDoesNotThrow() {
+    MeterProvider provider = MeterProvider.noop();
+    provider.meterBuilder("user-instrumentation").build();
+    provider.meterBuilder("advanced-instrumetnation").setInstrumentationVersion("1.0").build();
+    provider.meterBuilder("schema-instrumentation").setSchemaUrl("myschema://url").build();
+    provider
+        .meterBuilder("schema-instrumentation")
+        .setInstrumentationVersion("1.0")
+        .setSchemaUrl("myschema://url")
+        .build();
+  }
+}
