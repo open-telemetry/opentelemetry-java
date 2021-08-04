956912dd5f021ec45a8a54953563e89b4fb5785e
jack-berg <34418638+jack-berg@users.noreply.github.com>
2021-08-04 09:59:15 -0500

    OTLP HTTP trace exporter (#3418)
    * Draft of OTLP HTTP trace exporter
    
    * Cleanup some TODOs, shade okhttp dependency
    
    * Get all tests working
    
    * Drop support for otlp/http json format
    
    * Split OtlpHttpSpanExporter to its own module.
    
    * drop @NotNull annotations
    
    * Switch to armeria mock server, respond to PR feedback.
    
    * Remove :exporter:otlp-http:trace shadowJar and other PR feedback.
    
    * Extract constant for application/x-protobuf media type

---
+++ exporters/otlp-http/trace/src/main/java/io/opentelemetry/exporter/otlp/http/trace/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** OpenTelemetry exporter which sends span data to OpenTelemetry collector via OTLP HTTP. */
+@ParametersAreNonnullByDefault
+package io.opentelemetry.exporter.otlp.http.trace;
+
+import javax.annotation.ParametersAreNonnullByDefault;
