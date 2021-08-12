b8b4d4909a02817351311facd703821c91394ff9
ran8080 <ran.gilor@logz.io>
2021-08-12 15:16:17 +0300

    Continue the implementation of LogAdapter.

--- exporters/otlp/common/src/main/java/io/opentelemetry/exporter/otlp/internal/LogAdapter.java
+++ exporters/otlp/common/src/main/java/io/opentelemetry/exporter/otlp/internal/LogAdapter.java
@@ -5,51 +5,70 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import io.opentelemetry.proto.logs.v1.InstrumentationLibraryLogs;
 import io.opentelemetry.proto.logs.v1.ResourceLogs;
-import io.opentelemetry.proto.trace.v1.Span;
 import io.opentelemetry.sdk.common.InstrumentationLibraryInfo;
+import io.opentelemetry.sdk.logging.data.LogRecord;
 import io.opentelemetry.sdk.resources.Resource;
-import jdk.internal.net.http.common.Log;
-import sun.net.www.http.HttpCaptureOutputStream;
 
 public class LogAdapter {
 
-
   public static List<ResourceLogs> toProtoResourceLogs(Collection<LogRecord> logRecordList) {
-    // Instrumentation
-    Map<Resource, Map<InstrumentationLibraryInfo, List<>>> resourceAndLibraryMap =
-
+    Map<Resource, Map<InstrumentationLibraryInfo, List<io.opentelemetry.proto.logs.v1.LogRecord>>> resourceAndLibraryMap =
         groupByResourceAndLibrary(logRecordList);
     List<ResourceLogs> resourceLogs = new ArrayList<>(resourceAndLibraryMap.size());
-
     resourceAndLibraryMap.forEach(
-        (resource, libraryLogs) -> {
+        (resource, libraryInfoListMap) -> {
           ResourceLogs.Builder resourceLogsBuilder =
               ResourceLogs.newBuilder().setResource(ResourceAdapter.toProtoResource(resource));
-        }
-    );
-
+          if (resource.getSchemaUrl() != null) {
+            resourceLogsBuilder.setSchemaUrl(resource.getSchemaUrl());
+          }
+          libraryInfoListMap.forEach(
+              (instrumentationLibraryInfo, logRecords) -> {
+                resourceLogsBuilder.addInstrumentationLibraryLogs(
+                    buildInstrumentationLibraryLogs(instrumentationLibraryInfo, logRecords));
+              });
+          resourceLogs.add(resourceLogsBuilder.build());
+        });
+    return resourceLogs;
   }
 
-  private static Map<Resource, Map<InstrumentationLibraryInfo, List<LogRecord>>>
+  private static InstrumentationLibraryLogs buildInstrumentationLibraryLogs(
+      InstrumentationLibraryInfo library, List<io.opentelemetry.proto.logs.v1.LogRecord> logs) {
+    InstrumentationLibraryLogs.Builder logsBuilder =
+        InstrumentationLibraryLogs.newBuilder()
+          .setInstrumentationLibrary(CommonAdapter.toProtoInstrumentationLibrary(library))
+          .addAllLogs(logs);
+    if (library.getSchemaUrl() != null) {
+      logsBuilder.setSchemaUrl(library.getSchemaUrl());
+    }
+    return logsBuilder.build();
+  }
+
+  private static Map<Resource, Map<InstrumentationLibraryInfo, List<io.opentelemetry.proto.logs.v1.LogRecord>>>
       groupByResourceAndLibrary(Collection<LogRecord> logRecordList) {
-
-    Map<Resource, Map<InstrumentationLibraryInfo, List<LogRecord>>> result = new HashMap<>();
-
+    Map<Resource, Map<InstrumentationLibraryInfo, List<io.opentelemetry.proto.logs.v1.LogRecord>>> result =
+        new HashMap<>();
     for (LogRecord logRecord : logRecordList) {
 
-      Map<InstrumentationLibraryInfo, List<LogRecord>> libraryInfoListMap =
+      Map<InstrumentationLibraryInfo, List<io.opentelemetry.proto.logs.v1.LogRecord>> libraryInfoListMap =
         result.computeIfAbsent(logRecord.getResource(), unused -> new HashMap<>());
-
-      List<LogRecord> logList =
+      List<io.opentelemetry.proto.logs.v1.LogRecord> logList =
           libraryInfoListMap.computeIfAbsent(
               logRecord.getInstrumentationLibraryInfo(), unused -> new ArrayList<>());
-
-      // TODO convert to log record
-      logList.add(toProtoLog(logRecord));
+      logList.add(toProtoLogRecord(logRecord));
     }
-
     return result;
+  }
+
+  static io.opentelemetry.proto.logs.v1.LogRecord toProtoLogRecord(LogRecord logRecord) {
+    io.opentelemetry.proto.logs.v1.LogRecord.Builder builder =
+        io.opentelemetry.proto.logs.v1.LogRecord.newBuilder()
+          .setName(logRecord.getName());
+
+    // TODO implement this, here get the needed data to create the proper LogRecord model
+    return builder.build();
   }
 
 }
