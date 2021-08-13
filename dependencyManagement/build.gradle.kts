f2530c3bf4dd302c5b90c7111e99608e1fec3273
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-08-13 15:53:40 +0900

    Update dependencies

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -12,21 +12,22 @@ val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
 val DEPENDENCY_BOMS = listOf(
-  "com.linecorp.armeria:armeria-bom:1.8.0",
-  "io.grpc:grpc-bom:1.38.0",
-  "io.zipkin.brave:brave-bom:5.13.3",
+  "com.fasterxml.jackson:jackson-bom:2.12.4",
   "com.google.guava:guava-bom:30.1.1-jre",
-  "com.google.protobuf:protobuf-bom:3.17.2",
-  "com.fasterxml.jackson:jackson-bom:2.12.3",
-  "org.junit:junit-bom:5.7.2",
+  "com.google.protobuf:protobuf-bom:3.17.3",
+  "com.linecorp.armeria:armeria-bom:1.9.2",
+  "com.squareup.okhttp3:okhttp-bom:4.9.1",
+  "io.grpc:grpc-bom:1.39.0",
+  "io.zipkin.brave:brave-bom:5.13.3",
   "io.zipkin.reporter2:zipkin-reporter-bom:2.16.3",
-  "com.squareup.okhttp3:okhttp-bom:4.9.0"
+  "org.junit:junit-bom:5.7.2",
+  "org.testcontainers:testcontainers-bom:1.16.0"
 )
 
 val DEPENDENCY_SETS = listOf(
   DependencySet(
     "com.google.auto.value",
-    "1.8.1",
+    "1.8.2",
     listOf("auto-value", "auto-value-annotations")
   ),
   DependencySet(
@@ -56,18 +57,13 @@ val DEPENDENCY_SETS = listOf(
   ),
   DependencySet(
     "org.openjdk.jmh",
-    "1.32",
+    "1.33",
     listOf("jmh-core", "jmh-generator-bytecode")
   ),
   DependencySet(
     "org.mockito",
-    "3.10.0",
+    "3.11.2",
     listOf("mockito-core", "mockito-junit-jupiter")
-  ),
-  DependencySet(
-    "org.testcontainers",
-    "1.15.3",
-    listOf("testcontainers", "junit-jupiter")
   )
 )
 
@@ -79,7 +75,7 @@ val DEPENDENCIES = listOf(
   "com.lmax:disruptor:3.4.4",
   "com.squareup.okhttp3:okhttp:4.9.1",
   "com.sun.net.httpserver:http:20070405",
-  "com.tngtech.archunit:archunit-junit4:0.19.0",
+  "com.tngtech.archunit:archunit-junit4:0.20.1",
   "com.uber.nullaway:nullaway:0.9.2",
   "edu.berkeley.cs.jqf:jqf-fuzz:1.7",
   "eu.rekawek.toxiproxy:toxiproxy-java:2.1.4",
@@ -88,8 +84,8 @@ val DEPENDENCIES = listOf(
   "io.opentracing:opentracing-api:0.33.0",
   "io.zipkin.zipkin2:zipkin-junit:2.23.2",
   "junit:junit:4.13.2",
-  "nl.jqno.equalsverifier:equalsverifier:3.6.1",
-  "org.assertj:assertj-core:3.19.0",
+  "nl.jqno.equalsverifier:equalsverifier:3.7.1",
+  "org.assertj:assertj-core:3.20.2",
   "org.awaitility:awaitility:4.1.0",
   "org.bouncycastle:bcpkix-jdk15on:1.69",
   "org.codehaus.mojo:animal-sniffer-annotations:1.20",
@@ -97,7 +93,7 @@ val DEPENDENCIES = listOf(
   "org.jctools:jctools-core:3.3.0",
   "org.junit-pioneer:junit-pioneer:1.4.2",
   "org.skyscreamer:jsonassert:1.5.0",
-  "org.slf4j:slf4j-simple:1.7.30"
+  "org.slf4j:slf4j-simple:1.7.32"
 )
 
 javaPlatform {
