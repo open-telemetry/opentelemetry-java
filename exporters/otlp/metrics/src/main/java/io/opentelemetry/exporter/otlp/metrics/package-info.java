4f725688178e630e892318b5c1d6bd6c6e71e119
jack-berg <jberg@newrelic.com>
2021-08-20 11:51:37 -0500

    Remove outdated docs in otlp package-info.java files

--- exporters/otlp/metrics/src/main/java/io/opentelemetry/exporter/otlp/metrics/package-info.java
+++ exporters/otlp/metrics/src/main/java/io/opentelemetry/exporter/otlp/metrics/package-info.java
@@ -4,37 +4,7 @@
  */
 
 /**
- * OpenTelemetry exporter which sends metric data to OpenTelemetry collector via gRPC.
- *
- * <p>Configuration options for {@link
- * io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter} can be read from system
- * properties, environment variables, or {@link java.util.Properties} objects.
- *
- * <p>For system properties and {@link java.util.Properties} objects, {@link
- * io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter} will look for the following names:
- *
- * <ul>
- *   <li>{@code otel.exporter.otlp.metric.timeout}: to set the max waiting time allowed to send each
- *       metric batch.
- *   <li>{@code otel.exporter.otlp.metric.endpoint}: to set the endpoint to connect to.
- *   <li>{@code otel.exporter.otlp.metric.insecure}: whether to enable client transport security for
- *       the connection.
- *   <li>{@code otel.exporter.otlp.metric.headers}: the headers associated with the requests.
- * </ul>
- *
- * <p>For environment variables, {@link
- * io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter} will look for the following names:
- *
- * <ul>
- *   <li>{@code OTEL_EXPORTER_OTLP_METRIC_TIMEOUT}: to set the max waiting time allowed to send each
- *       * span batch. *
- *   <li>{@code OTEL_EXPORTER_OTLP_METRIC_ENDPOINT}: to set the endpoint to connect to. *
- *   <li>{@code OTEL_EXPORTER_OTLP_METRIC_INSECURE}: whether to enable client transport security for
- *       * the connection. *
- *   <li>{@code OTEL_EXPORTER_OTLP_METRIC_HEADERS}: the headers associated with the requests. *
- * </ul>
- *
- * In both cases, if a property is missing, the name without "metric" is used to resolve the value.
+ * OpenTelemetry exporter which sends metric data to OpenTelemetry collector via OTLP gRPC.
  */
 @ParametersAreNonnullByDefault
 package io.opentelemetry.exporter.otlp.metrics;
