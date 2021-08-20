fdad18d2ff4c9e19e7052876949cffb2d0f77248
jack-berg <jberg@newrelic.com>
2021-08-20 12:12:16 -0500

    Fix formatting

--- exporters/otlp/trace/src/main/java/io/opentelemetry/exporter/otlp/trace/package-info.java
+++ exporters/otlp/trace/src/main/java/io/opentelemetry/exporter/otlp/trace/package-info.java
@@ -3,9 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/**
- * OpenTelemetry exporter which sends span data to OpenTelemetry collector via OTLP gRPC.
- */
+/** OpenTelemetry exporter which sends span data to OpenTelemetry collector via OTLP gRPC. */
 @ParametersAreNonnullByDefault
 package io.opentelemetry.exporter.otlp.trace;
 
