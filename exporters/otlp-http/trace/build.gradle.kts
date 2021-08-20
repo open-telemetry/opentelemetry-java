5e78fa51e9425e583674cbcb01b94df489b4ae53
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-08-20 17:38:17 +0900

    Make proto dependency in otlp-common compileOnly.

--- exporters/otlp-http/trace/build.gradle.kts
+++ exporters/otlp-http/trace/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
   api(project(":sdk:trace"))
 
   implementation(project(":exporters:otlp:common"))
+  implementation(project(":proto"))
 
   implementation("com.squareup.okhttp3:okhttp")
   implementation("com.squareup.okhttp3:okhttp-tls")
