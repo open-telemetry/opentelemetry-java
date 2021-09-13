554ed4881680397f642ff392225ed0f7d4740b7a
jack-berg <jberg@newrelic.com>
2021-09-13 13:41:10 -0500

    Add OTLP integration test for gRPC and HTTP Trace and Metric exporters

--- integration-tests/build.gradle.kts
+++ integration-tests/build.gradle.kts
@@ -6,18 +6,24 @@ description = "OpenTelemetry Integration Tests"
 otelJava.moduleName.set("io.opentelemetry.integration.tests")
 
 dependencies {
-  implementation(project(":sdk:all"))
-  implementation(project(":exporters:jaeger"))
-  implementation(project(":semconv"))
-
-  implementation("io.grpc:grpc-protobuf")
-  implementation("com.google.protobuf:protobuf-java")
-  implementation("io.grpc:grpc-netty-shaded")
-
-  testImplementation(project(":extensions:trace-propagators"))
+  testImplementation(project(":sdk:all"))
   testImplementation(project(":sdk:testing"))
+  testImplementation(project(":extensions:trace-propagators"))
+  testImplementation(project(":exporters:jaeger"))
+  testImplementation(project(":exporters:otlp:trace"))
+  testImplementation(project(":exporters:otlp:metrics"))
+  testImplementation(project(":exporters:otlp-http:trace"))
+  testImplementation(project(":exporters:otlp-http:metrics"))
+  testImplementation(project(":semconv"))
+  testImplementation(project(":proto"))
+
+  testImplementation("io.grpc:grpc-protobuf")
+  testImplementation("com.google.protobuf:protobuf-java")
+  testImplementation("io.grpc:grpc-netty-shaded")
+
   testImplementation("org.junit.jupiter:junit-jupiter-params")
   testImplementation("com.fasterxml.jackson.core:jackson-databind")
+  testImplementation("com.linecorp.armeria:armeria-grpc")
   testImplementation("com.linecorp.armeria:armeria-junit5")
   testImplementation("org.testcontainers:junit-jupiter")
   testImplementation("com.squareup.okhttp3:okhttp")
