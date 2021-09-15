f159bacde5926fed62a75caec4b13a782095da14
John Watson <jkwatson@gmail.com>
2021-09-15 10:54:23 -0700

    Apply suggestions from code review
    Thanks Jason!
    
    Co-authored-by: jason plumb <75337021+breedx-splk@users.noreply.github.com>

--- sdk-extensions/logging/src/main/java/io/opentelemetry/sdk/logging/data/Body.java
+++ sdk-extensions/logging/src/main/java/io/opentelemetry/sdk/logging/data/Body.java
@@ -10,24 +10,24 @@ import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 
 /**
- * A class that represents all the possible values for a log message body. An {@code AnyValue} can
- * currently only have 1 types of values: {@code String}, represented through {@code AnyValue.Type}.
+ * A class that represents all the possible values for a log message body. A {@code Body} can
+ * currently only have 1 types of values: {@code String}, represented through {@code Body.Type}.
  * This class will likely be extended in the future to include additional body types supported by
  * the OpenTelemetry log data model.
  */
 @Immutable
 public abstract class Body {
 
-  /** An enum that represents all the possible value types for an {@code AnyValue}. */
+  /** An enum that represents all the possible value types for an {@code Body}. */
   public enum Type {
     STRING
   }
 
   /**
-   * Returns an {@code AnyValue} with a string value.
+   * Returns an {@code Body} with a string value.
    *
    * @param stringValue The new value.
-   * @return an {@code AnyValue} with a string value.
+   * @return a {@code Body} with a string value.
    */
   public static Body stringBody(String stringValue) {
     return StringBody.create(stringValue);
