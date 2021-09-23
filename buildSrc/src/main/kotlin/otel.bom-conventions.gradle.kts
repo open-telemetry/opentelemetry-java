346f23764ef3c6493c1fa9d67d82c1849fcd9cb5
Christian Neumüller <christian.neumueller@dynatrace.com>
2021-09-23 17:52:45 +0200

    Add generateBuildSubstitutions task to bom projects.
    This is a convenience task that can be used to
    generate a code snippet that can be copy-pasted
    for use in composite builds.

---
+++ buildSrc/src/main/kotlin/otel.bom-conventions.gradle.kts
@@ -0,0 +1,60 @@
+import io.opentelemetry.gradle.OtelBomExtension
+import org.gradle.kotlin.dsl.create
+
+plugins {
+  id("otel.publish-conventions")
+  id("java-platform")
+}
+
+if (!project.name.startsWith("bom")) {
+  throw IllegalStateException("Name of BOM projects must start with 'bom'.")
+}
+
+rootProject.subprojects.forEach { subproject ->
+  if (!subproject.name.startsWith("bom")) {
+    evaluationDependsOn(subproject.path)
+  }
+}
+val otelBom = extensions.create<OtelBomExtension>("otelBom")
+
+val generateBuildSubstitutions by tasks.registering {
+  group = "publishing"
+  description = "Generate a code snippet that can be copy-pasted for use in composite builds."
+}
+
+tasks.named("publish") {
+  dependsOn(generateBuildSubstitutions)
+}
+
+afterEvaluate {
+  otelBom.projectFilter.finalizeValue()
+  val stableProjects = rootProject.subprojects
+    .sortedBy { it.findProperty("archivesBaseName") as String? }
+    .filter { !it.name.startsWith("bom") }
+    .filter(otelBom.projectFilter.get()::test)
+    .filter { it.plugins.hasPlugin("maven-publish") }
+
+  generateBuildSubstitutions {
+    val outputFile = File(buildDir, "substitutions.gradle.kts")
+    outputs.file(outputFile)
+    val substitutionSnippet = stableProjects.joinToString(
+      separator = "\n",
+      prefix = "dependencySubstitution {\n",
+      postfix = "\n}\n"
+    ) { project ->
+      val publication = project.publishing.publications.getByName("mavenPublication") as MavenPublication
+      "  substitute(module(\"${publication.groupId}:${publication.artifactId}\")).using(project(\"${project.path}\"))"
+    }
+    inputs.property("projectPathsAndArtifactCoordinates", substitutionSnippet)
+    doFirst {
+      outputFile.writeText(substitutionSnippet)
+    }
+  }
+  stableProjects.forEach { project ->
+    dependencies {
+      constraints {
+        api(project)
+      }
+    }
+  }
+}
