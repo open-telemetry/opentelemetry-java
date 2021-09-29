f4dd3995f106cff57004be2d5c726877c6afb5f8
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-09-29 11:29:36 +0900

    Update Gradle plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,11 @@ pluginManagement {
   plugins {
     id("com.github.ben-manes.versions") version "0.39.0"
     id("com.github.johnrengelman.shadow") version "7.0.0"
-    id("com.gradle.enterprise") version "3.6"
-    id("de.undercouch.download") version "4.1.1"
+    id("com.gradle.enterprise") version "3.7"
+    id("de.undercouch.download") version "4.1.2"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     id("nebula.release") version "16.0.0"
-    id("org.checkerframework") version "0.5.20"
-    id("org.jetbrains.kotlin.jvm") version "1.5.30"
+    id("org.jetbrains.kotlin.jvm") version "1.5.31"
     id("org.unbroken-dome.test-sets") version "4.0.0"
   }
 }
