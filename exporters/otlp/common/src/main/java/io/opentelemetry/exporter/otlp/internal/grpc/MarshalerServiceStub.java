3640eb0129d5611ddbaea86212e27ba240ac8ef6
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-10-05 16:53:25 +0900

    Clean

--- exporters/otlp/common/src/main/java/io/opentelemetry/exporter/otlp/internal/grpc/MarshalerServiceStub.java
+++ exporters/otlp/common/src/main/java/io/opentelemetry/exporter/otlp/internal/grpc/MarshalerServiceStub.java
@@ -8,9 +8,18 @@ package io.opentelemetry.exporter.otlp.internal.grpc;
 import com.google.common.util.concurrent.ListenableFuture;
 import io.grpc.CallOptions;
 import io.grpc.Channel;
+import io.grpc.stub.AbstractFutureStub;
+import io.opentelemetry.exporter.otlp.internal.Marshaler;
 
-public abstract class MarshalerServiceStub<T, U, S extends MarshalerServiceStub<T, U, S>>
-    extends io.grpc.stub.AbstractFutureStub<S> {
+/**
+ * A gRPC stub that uses a {@link Marshaler}.
+ *
+ * <p>This class is internal and is hence not for public use. Its APIs are unstable and can change
+ * at any time.
+ */
+public abstract class MarshalerServiceStub<
+        T extends Marshaler, U, S extends MarshalerServiceStub<T, U, S>>
+    extends AbstractFutureStub<S> {
   protected MarshalerServiceStub(Channel channel, CallOptions callOptions) {
     super(channel, callOptions);
   }
