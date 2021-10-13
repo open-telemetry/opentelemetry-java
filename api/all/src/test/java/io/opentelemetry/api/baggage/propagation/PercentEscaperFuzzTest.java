bfe4a73895982642e31a83403e14dc562fde3bf3
jkwatson <jwatson@splunk.com>
2021-10-13 13:53:01 -0700

    Add additional fuzz iterations, manually.

--- api/all/src/test/java/io/opentelemetry/api/baggage/propagation/PercentEscaperFuzzTest.java
+++ api/all/src/test/java/io/opentelemetry/api/baggage/propagation/PercentEscaperFuzzTest.java
@@ -9,12 +9,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import edu.berkeley.cs.jqf.fuzz.Fuzz;
 import edu.berkeley.cs.jqf.fuzz.JQF;
+import edu.berkeley.cs.jqf.fuzz.junit.GuidedFuzzing;
+import edu.berkeley.cs.jqf.fuzz.random.NoGuidance;
 import java.net.URLDecoder;
+import org.junit.Test;
+import org.junit.runner.Result;
 import org.junit.runner.RunWith;
 
 @RunWith(JQF.class)
 public class PercentEscaperFuzzTest {
-
   private final PercentEscaper percentEscaper = new PercentEscaper();
 
   @Fuzz
@@ -22,5 +25,21 @@ public class PercentEscaperFuzzTest {
     String escaped = percentEscaper.escape(value);
     String decoded = URLDecoder.decode(escaped, "UTF-8");
     assertThat(decoded).isEqualTo(value);
+  }
+
+  /**
+   * There doesn't seem to be any way to make the fuzzing run more than the default 100 times
+   * without using custom guidance, but this does seem to do it.
+   */
+  @Test
+  @SuppressWarnings("SystemOut")
+  public void lotsOfFuzz() {
+    Result result =
+        GuidedFuzzing.run(
+            PercentEscaperFuzzTest.class,
+            "roundTripWithUrlDecoder",
+            new NoGuidance(10000, System.out),
+            System.out);
+    assertThat(result.wasSuccessful()).isTrue();
   }
 }
