6316a7bc8c1289952ff8e7797a51bd986fb77f22
jack-berg <jberg@newrelic.com>
2021-10-14 16:20:24 -0500

    Add InMemoryLogExporter

---
+++ sdk/logs/src/main/java/io/opentelemetry/sdk/logs/export/InMemoryLogExporter.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.sdk.logs.export;
+
+import io.opentelemetry.sdk.common.CompletableResultCode;
+import io.opentelemetry.sdk.logs.data.LogData;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/** A {@link LogExporter} implementation that can be used to test OpenTelemetry integration. */
+public final class InMemoryLogExporter implements LogExporter {
+
+  // using LinkedBlockingQueue to avoid manual locks for thread-safe operations
+  private final Queue<LogData> finishedLogItems = new LinkedBlockingQueue<>();
+  private boolean isStopped = false;
+
+  private InMemoryLogExporter() {}
+
+  /**
+   * Returns a new instance of the {@code InMemoryLogExporter}.
+   *
+   * @return a new instance of the {@code InMemoryLogExporter}.
+   */
+  public static InMemoryLogExporter create() {
+    return new InMemoryLogExporter();
+  }
+
+  /**
+   * Returns a {@code List} of the finished {@code Log}s, represented by {@code LogRecord}.
+   *
+   * @return a {@code List} of the finished {@code Log}s.
+   */
+  public List<LogData> getFinishedLogItems() {
+    return Collections.unmodifiableList(new ArrayList<>(finishedLogItems));
+  }
+
+  /**
+   * Clears the internal {@code List} of finished {@code Log}s.
+   *
+   * <p>Does not reset the state of this exporter if already shutdown.
+   */
+  public void reset() {
+    finishedLogItems.clear();
+  }
+
+  /**
+   * Exports the collection of {@code Log}s into the inmemory queue.
+   *
+   * <p>If this is called after {@code shutdown}, this will return {@code ResultCode.FAILURE}.
+   */
+  @Override
+  public CompletableResultCode export(Collection<LogData> logs) {
+    if (isStopped) {
+      return CompletableResultCode.ofFailure();
+    }
+    finishedLogItems.addAll(logs);
+    return CompletableResultCode.ofSuccess();
+  }
+
+  /**
+   * Clears the internal {@code List} of finished {@code Log}s.
+   *
+   * <p>Any subsequent call to export() function on this LogExporter, will return {@code
+   * CompletableResultCode.ofFailure()}
+   */
+  @Override
+  public CompletableResultCode shutdown() {
+    isStopped = true;
+    finishedLogItems.clear();
+    return CompletableResultCode.ofSuccess();
+  }
+}
---
+++ sdk/logs/src/test/java/io/opentelemetry/sdk/logs/export/InMemoryLogExporterTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.sdk.logs.export;
+
+import static io.opentelemetry.sdk.logs.data.Severity.DEBUG;
+import static io.opentelemetry.sdk.logs.util.TestUtil.createLog;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.opentelemetry.sdk.logs.LogSink;
+import io.opentelemetry.sdk.logs.LogSinkSdkProvider;
+import io.opentelemetry.sdk.logs.data.LogData;
+import io.opentelemetry.sdk.logs.data.LogRecord;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/** Unit tests for {@link InMemoryLogExporter}. */
+class InMemoryLogExporterTest {
+  private final InMemoryLogExporter exporter = InMemoryLogExporter.create();
+
+  private LogSinkSdkProvider logSinkSdkProvider;
+  private LogSink logSink;
+
+  @BeforeEach
+  void setup() {
+    logSinkSdkProvider = LogSinkSdkProvider.builder().build();
+    logSinkSdkProvider.addLogProcessor(SimpleLogProcessor.create(exporter));
+    logSink = logSinkSdkProvider.get(null, null);
+  }
+
+  @AfterEach
+  void tearDown() {
+    logSinkSdkProvider.shutdown();
+  }
+
+  @Test
+  void getFinishedLogItems() {
+    logSink.offer(createLog(DEBUG, "message 1"));
+    logSink.offer(createLog(DEBUG, "message 2"));
+    logSink.offer(createLog(DEBUG, "message 3"));
+
+    List<LogData> logItems = exporter.getFinishedLogItems();
+    assertThat(logItems).isNotNull();
+    assertThat(logItems.size()).isEqualTo(3);
+    assertThat(logItems.get(0).getBody().asString()).isEqualTo("message 1");
+    assertThat(logItems.get(1).getBody().asString()).isEqualTo("message 2");
+    assertThat(logItems.get(2).getBody().asString()).isEqualTo("message 3");
+  }
+
+  @Test
+  void reset() {
+    logSink.offer(createLog(DEBUG, "message 1"));
+    logSink.offer(createLog(DEBUG, "message 2"));
+    logSink.offer(createLog(DEBUG, "message 3"));
+    List<LogData> logItems = exporter.getFinishedLogItems();
+    assertThat(logItems).isNotNull();
+    assertThat(logItems.size()).isEqualTo(3);
+    // Reset then expect no items in memory.
+    exporter.reset();
+    assertThat(exporter.getFinishedLogItems()).isEmpty();
+  }
+
+  @Test
+  void shutdown() {
+    logSink.offer(createLog(DEBUG, "message 1"));
+    logSink.offer(createLog(DEBUG, "message 2"));
+    logSink.offer(createLog(DEBUG, "message 3"));
+    List<LogData> logItems = exporter.getFinishedLogItems();
+    assertThat(logItems).isNotNull();
+    assertThat(logItems.size()).isEqualTo(3);
+    // Shutdown then expect no items in memory.
+    exporter.shutdown();
+    assertThat(exporter.getFinishedLogItems()).isEmpty();
+    // Cannot add new elements after the shutdown.
+    logSink.offer(createLog(DEBUG, "message 1"));
+    assertThat(exporter.getFinishedLogItems()).isEmpty();
+  }
+
+  @Test
+  void export_ReturnCode() {
+    LogRecord logRecord = createLog(DEBUG, "message 1");
+    assertThat(exporter.export(Collections.singletonList(logRecord)).isSuccess()).isTrue();
+    exporter.shutdown();
+    // After shutdown no more export.
+    assertThat(exporter.export(Collections.singletonList(logRecord)).isSuccess()).isFalse();
+    exporter.reset();
+    // Reset does not do anything if already shutdown.
+    assertThat(exporter.export(Collections.singletonList(logRecord)).isSuccess()).isFalse();
+  }
+}
