b3059253aff0d308a00376710f8d8202681802a9
jkwatson <jwatson@splunk.com>
2021-10-15 08:44:17 -0700

    renames and formatting tweaks from review

--- exporters/logging/src/main/java/io/opentelemetry/exporter/logging/SystemOutLogExporter.java
+++ exporters/logging/src/main/java/io/opentelemetry/exporter/logging/SystemOutLogExporter.java
@@ -5,18 +5,27 @@
 
 package io.opentelemetry.exporter.logging;
 
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.common.InstrumentationLibraryInfo;
 import io.opentelemetry.sdk.logs.data.LogData;
 import io.opentelemetry.sdk.logs.export.LogExporter;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 
 /**
  * A {@link LogExporter} implementation that outputs log records to standard out. The output is not
  * intended to be comprehensive, but just usable for debugging.
+ *
+ * <p>Note: this doesn't use a {@code java.util.logging Logger}, as that could result in
+ * logging-loops if an OTel appender is configured for {@code java.util.logging}.
  */
 @SuppressWarnings("SystemOut")
-public class StdOutLogExporter implements LogExporter {
+public class SystemOutLogExporter implements LogExporter {
+  private static final DateTimeFormatter ISO_FORMAT = DateTimeFormatter.ISO_DATE_TIME;
 
   @Override
   public CompletableResultCode export(Collection<LogData> logs) {
@@ -34,7 +43,10 @@ public class StdOutLogExporter implements LogExporter {
   static void formatLog(StringBuilder stringBuilder, LogData log) {
     InstrumentationLibraryInfo instrumentationLibraryInfo = log.getInstrumentationLibraryInfo();
     stringBuilder
-        .append(log.getEpochNanos())
+        .append(
+            ISO_FORMAT.format(
+                Instant.ofEpochMilli(NANOSECONDS.toMillis(log.getEpochNanos()))
+                    .atZone(ZoneOffset.UTC)))
         .append(" ")
         .append(log.getSeverity())
         .append(" '")
--- exporters/logging/src/test/java/io/opentelemetry/exporter/logging/SystemOutLogExporterTest.java
+++ exporters/logging/src/test/java/io/opentelemetry/exporter/logging/SystemOutLogExporterTest.java
@@ -20,35 +20,41 @@ import io.opentelemetry.sdk.logs.data.LogData;
 import io.opentelemetry.sdk.logs.data.LogRecord;
 import io.opentelemetry.sdk.logs.data.Severity;
 import io.opentelemetry.sdk.resources.Resource;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneOffset;
 import org.junit.jupiter.api.Test;
 
-class StdOutLogExporterTest {
+class SystemOutLogExporterTest {
 
   @Test
   void returnCodes() {
-    StdOutLogExporter exporter = new StdOutLogExporter();
-    CompletableResultCode resultCode = exporter.export(singletonList(sampleLog()));
+    SystemOutLogExporter exporter = new SystemOutLogExporter();
+    CompletableResultCode resultCode =
+        exporter.export(singletonList(sampleLog(System.currentTimeMillis())));
     assertThat(resultCode).isSameAs(CompletableResultCode.ofSuccess());
     assertThat(exporter.shutdown()).isSameAs(CompletableResultCode.ofSuccess());
   }
 
   @Test
   void format() {
-    LogData log = sampleLog();
+    long timestamp =
+        LocalDateTime.of(1970, Month.AUGUST, 7, 10, 0).toInstant(ZoneOffset.UTC).toEpochMilli();
+    LogData log = sampleLog(timestamp);
     StringBuilder output = new StringBuilder();
-    StdOutLogExporter.formatLog(output, log);
+    SystemOutLogExporter.formatLog(output, log);
     assertThat(output.toString())
         .isEqualTo(
-            "1000000000000 ERROR3 'message' : 00000000000000010000000000000002 0000000000000003 "
+            "1970-08-07T10:00:00Z ERROR3 'message' : 00000000000000010000000000000002 0000000000000003 "
                 + "[libraryInfo: logTest:1.0] {amount=1, cheese=\"cheddar\"}");
   }
 
-  private static LogData sampleLog() {
+  private static LogData sampleLog(long timestamp) {
     return LogRecord.builder(Resource.empty(), InstrumentationLibraryInfo.create("logTest", "1.0"))
         .setAttributes(Attributes.of(stringKey("cheese"), "cheddar", longKey("amount"), 1L))
         .setBody(Body.stringBody("message"))
         .setSeverity(Severity.ERROR3)
-        .setEpochMillis(1_000_000)
+        .setEpochMillis(timestamp)
         .setTraceId(TraceId.fromLongs(1, 2))
         .setSpanId(SpanId.fromLong(3))
         .build();
