86260d082d002ea1dd23eeca1ce2910e6d262ca1
Jason Plumb <jplumb@splunk.com>
2021-11-09 09:06:34 -0800

    code review comments

--- sdk/metrics/src/test/java/io/opentelemetry/sdk/metrics/internal/state/MetricStorageUtilsTest.java
+++ sdk/metrics/src/test/java/io/opentelemetry/sdk/metrics/internal/state/MetricStorageUtilsTest.java
@@ -6,19 +6,16 @@
 package io.opentelemetry.sdk.metrics.internal.state;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.entry;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
 
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.sdk.common.InstrumentationLibraryInfo;
-import io.opentelemetry.sdk.metrics.data.AggregationTemporality;
-import io.opentelemetry.sdk.metrics.data.MetricData;
 import io.opentelemetry.sdk.metrics.internal.aggregator.Aggregator;
-import io.opentelemetry.sdk.metrics.internal.aggregator.AggregatorHandle;
-import io.opentelemetry.sdk.metrics.internal.descriptor.MetricDescriptor;
-import io.opentelemetry.sdk.resources.Resource;
 import java.util.HashMap;
 import java.util.Map;
-import javax.annotation.Nullable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -46,55 +43,32 @@ class MetricStorageUtilsTest {
 
   @Test
   void mergeInPlace() {
-    Aggregator<String> agg = new TestAggregator();
+    Aggregator<String> agg = buildConcatAggregator();
     MetricStorageUtils.mergeInPlace(result, toMerge, agg);
 
-    assertThat(result.keySet()).containsExactlyInAnyOrder(a, b, c, d);
-    assertThat(result.get(a)).isEqualTo("A");
-    assertThat(result.get(b)).isEqualTo("BB'");
-    assertThat(result.get(c)).isEqualTo("C");
+    assertThat(result).containsOnly(entry(a, "A"), entry(b, "BB'"), entry(c, "C"), entry(d, null));
     assertThat(result.get(d)).isNull();
   }
 
   @Test
   void diffInPlace() {
-    Aggregator<String> agg = new TestAggregator();
+    Aggregator<String> agg = buildConcatAggregator();
     MetricStorageUtils.diffInPlace(result, toMerge, agg);
 
-    assertThat(result.keySet()).containsExactlyInAnyOrder(a, c, d);
-    assertThat(result.get(a)).isEqualTo("A");
-    assertThat(result.get(c)).isEqualTo("C");
-    assertThat(result.get(d)).isNull();
+    assertThat(result).containsOnly(entry(a, "A"), entry(c, "C"), entry(d, null));
   }
 
-  private static class TestAggregator implements Aggregator<String> {
-    @Override
-    public String merge(String previousCumulative, String delta) {
-      return previousCumulative + delta;
-    }
-
-    @Override
-    public String diff(String previousCumulative, String currentCumulative) {
-      return null;
-    }
-
-    @Override
-    public AggregatorHandle<String> createHandle() {
-      return null;
-    }
-
-    @Nullable
-    @Override
-    public MetricData toMetricData(
-        Resource resource,
-        InstrumentationLibraryInfo instrumentationLibrary,
-        MetricDescriptor metricDescriptor,
-        Map<Attributes, String> accumulationByLabels,
-        AggregationTemporality temporality,
-        long startEpochNanos,
-        long lastCollectionEpoch,
-        long epochNanos) {
-      return null;
-    }
+  @SuppressWarnings("unchecked")
+  private Aggregator<String> buildConcatAggregator() {
+    Aggregator<String> agg = mock(Aggregator.class);
+    doAnswer(
+            invocation -> {
+              String previousCumulative = invocation.getArgument(0);
+              String delta = invocation.getArgument(1);
+              return previousCumulative + delta;
+            })
+        .when(agg)
+        .merge(anyString(), anyString());
+    return agg;
   }
 }
