7124d260c8e968739554b87720286521188632a6
reugn <reugpro@gmail.com>
2021-11-17 10:08:19 +0200

    annotate the register method as Nullable

--- sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/internal/state/MeterSharedState.java
+++ sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/internal/state/MeterSharedState.java
@@ -20,6 +20,7 @@ import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 
 /**
@@ -130,6 +131,7 @@ public abstract class MeterSharedState {
         .forEach(this::register);
   }
 
+  @Nullable
   private <S extends MetricStorage> S register(S storage) {
     try {
       return getMetricStorageRegistry().register(storage);
