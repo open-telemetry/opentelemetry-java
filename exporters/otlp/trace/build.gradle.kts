42e72fe47a80aaf00ab30444cad7b1edfa602f7a
Anuraag Agrawal <anuraaga@gmail.com>
2021-11-29 11:33:13 +0900

    Move gRPC TLS exporter tests into base class and use in grpc-classpath test sets. (#3935)

--- exporters/otlp/trace/build.gradle.kts
+++ exporters/otlp/trace/build.gradle.kts
@@ -15,7 +15,6 @@ testSets {
   create("testGrpcNetty")
   create("testGrpcNettyShaded")
   create("testGrpcOkhttp")
-  create("testOkhttpOnly")
 
   // Mainly to conveniently profile through IDEA. Don't add to check task, it's for
   // manual invocation.
@@ -33,25 +32,14 @@ dependencies {
   testImplementation(project(":exporters:otlp:testing-internal"))
   testImplementation(project(":sdk:testing"))
 
-  add("testGrpcNettyImplementation", "com.linecorp.armeria:armeria-grpc")
-  add("testGrpcNettyImplementation", "com.linecorp.armeria:armeria-junit5")
   add("testGrpcNettyRuntimeOnly", "io.grpc:grpc-netty")
-  add("testGrpcNettyRuntimeOnly", "org.bouncycastle:bcpkix-jdk15on")
+  add("testGrpcNettyRuntimeOnly", "io.grpc:grpc-stub")
 
-  add("testGrpcNettyShadedImplementation", "com.linecorp.armeria:armeria-grpc")
-  add("testGrpcNettyShadedImplementation", "com.linecorp.armeria:armeria-junit5")
   add("testGrpcNettyShadedRuntimeOnly", "io.grpc:grpc-netty-shaded")
-  add("testGrpcNettyShadedRuntimeOnly", "org.bouncycastle:bcpkix-jdk15on")
+  add("testGrpcNettyShadedRuntimeOnly", "io.grpc:grpc-stub")
 
-  add("testGrpcOkhttpImplementation", "com.linecorp.armeria:armeria-grpc")
-  add("testGrpcOkhttpImplementation", "com.linecorp.armeria:armeria-junit5")
   add("testGrpcOkhttpRuntimeOnly", "io.grpc:grpc-okhttp")
-  add("testGrpcOkhttpRuntimeOnly", "org.bouncycastle:bcpkix-jdk15on")
-
-  add("testOkhttpOnlyImplementation", "com.linecorp.armeria:armeria-grpc-protocol")
-  add("testOkhttpOnlyImplementation", "com.linecorp.armeria:armeria-junit5")
-  add("testOkhttpOnlyImplementation", "com.squareup.okhttp3:okhttp-tls")
-  add("testOkhttpOnlyRuntimeOnly", "org.bouncycastle:bcpkix-jdk15on")
+  add("testGrpcOkhttpRuntimeOnly", "io.grpc:grpc-stub")
 
   add("testSpanPipelineImplementation", "io.grpc:grpc-protobuf")
   add("testSpanPipelineImplementation", "io.grpc:grpc-testing")
@@ -67,6 +55,6 @@ dependencies {
 
 tasks {
   check {
-    dependsOn("testGrpcNetty", "testGrpcNettyShaded", "testGrpcOkhttp", "testOkhttpOnly")
+    dependsOn("testGrpcNetty", "testGrpcNettyShaded", "testGrpcOkhttp")
   }
 }
