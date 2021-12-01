3aac6c47d4a48ccef9d3e088f76fa40679074b3b
jack-berg <34418638+jack-berg@users.noreply.github.com>
2021-11-30 20:55:54 -0600

    Fix metric descriptor compatibility check (#3941)

--- sdk/metrics/src/debugEnabledTest/java/io/opentelemetry/testing/TestSourceInfo.java
+++ sdk/metrics/src/debugEnabledTest/java/io/opentelemetry/testing/TestSourceInfo.java
@@ -44,17 +44,16 @@ class TestSourceInfo {
         MetricDescriptor.create(
             View.builder().build(),
             InstrumentDescriptor.create(
-                "name",
-                "description2",
-                "unit2",
-                InstrumentType.HISTOGRAM,
-                InstrumentValueType.DOUBLE));
+                "name", "description2", "unit2", InstrumentType.COUNTER, InstrumentValueType.LONG));
     assertThat(DebugUtils.duplicateMetricErrorMessage(simple, simpleWithNewDescription))
         .contains("Found duplicate metric definition: name")
         .contains("- Unit [unit2] does not match [unit]")
         .contains("- Description [description2] does not match [description]")
+        .contains("- InstrumentType [COUNTER] does not match [HISTOGRAM]")
+        .contains("- InstrumentValueType [LONG] does not match [DOUBLE]")
         .contains(simple.getSourceInstrument().getSourceInfo().multiLineDebugString())
-        .contains("Original instrument registered with same name but different description or unit")
+        .contains(
+            "Original instrument registered with same name but different description, unit, instrument type, or instrument value type.")
         .contains(
             simpleWithNewDescription.getSourceInstrument().getSourceInfo().multiLineDebugString());
   }
@@ -119,7 +118,8 @@ class TestSourceInfo {
         .contains("FROM instrument name2")
         .contains(simple.getSourceInstrument().getSourceInfo().multiLineDebugString())
         .contains("- Unit [unit] does not match [unit2]")
-        .contains("Original instrument registered with same name but different description or unit")
+        .contains(
+            "Original instrument registered with same name but different description, unit, instrument type, or instrument value type.")
         .contains(
             simpleWithNewDescription.getSourceInstrument().getSourceInfo().multiLineDebugString());
   }
