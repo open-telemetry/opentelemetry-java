ed7f82f873fb30e44e01702f551cdcb68239897f
Anuraag Agrawal <anuraaga@gmail.com>
2021-12-09 23:27:50 +0900

    Remove dependency on api:metrics which was deprecated where possible. (#3975)

--- sdk/logs/build.gradle.kts
+++ sdk/logs/build.gradle.kts
@@ -11,8 +11,6 @@ otelJava.moduleName.set("io.opentelemetry.sdk.extension.logging")
 dependencies {
   api(project(":sdk:common"))
 
-  implementation(project(":api:metrics"))
-
   testImplementation("org.awaitility:awaitility")
 
   annotationProcessor("com.google.auto.value:auto-value")
