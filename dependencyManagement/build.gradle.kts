fd2508479119e5d96d0dd143ec7c6a930eaa3b29
Anuraag Agrawal <anuraaga@gmail.com>
2022-01-05 04:45:33 +0000

    Update dependencies

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -12,13 +12,13 @@ val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
 val DEPENDENCY_BOMS = listOf(
-  "com.fasterxml.jackson:jackson-bom:2.13.0",
+  "com.fasterxml.jackson:jackson-bom:2.13.1",
   "com.google.guava:guava-bom:31.0.1-jre",
-  "com.google.protobuf:protobuf-bom:3.18.1",
+  "com.google.protobuf:protobuf-bom:3.19.1",
   "com.linecorp.armeria:armeria-bom:1.13.4",
   "com.squareup.okhttp3:okhttp-bom:4.9.3",
-  "io.grpc:grpc-bom:1.42.1",
-  "io.zipkin.brave:brave-bom:5.13.3",
+  "io.grpc:grpc-bom:1.43.1",
+  "io.zipkin.brave:brave-bom:5.13.7",
   "io.zipkin.reporter2:zipkin-reporter-bom:2.16.3",
   "org.junit:junit-bom:5.8.2",
   "org.testcontainers:testcontainers-bom:1.16.2"
@@ -27,7 +27,7 @@ val DEPENDENCY_BOMS = listOf(
 val DEPENDENCY_SETS = listOf(
   DependencySet(
     "com.google.auto.value",
-    "1.8.2",
+    "1.9",
     listOf("auto-value", "auto-value-annotations")
   ),
   DependencySet(
@@ -37,7 +37,7 @@ val DEPENDENCY_SETS = listOf(
   ),
   DependencySet(
     "io.opencensus",
-    "0.28.3",
+    "0.30.0",
     listOf(
       "opencensus-api",
       "opencensus-impl-core",
@@ -48,7 +48,7 @@ val DEPENDENCY_SETS = listOf(
   ),
   DependencySet(
     "io.prometheus",
-    "0.12.0",
+    "0.14.1",
     listOf("simpleclient", "simpleclient_common", "simpleclient_httpserver")
   ),
   DependencySet(
@@ -58,34 +58,34 @@ val DEPENDENCY_SETS = listOf(
   ),
   DependencySet(
     "org.openjdk.jmh",
-    "1.33",
+    "1.34",
     listOf("jmh-core", "jmh-generator-bytecode")
   ),
   DependencySet(
     "org.mockito",
-    "4.1.0",
+    "4.2.0",
     listOf("mockito-core", "mockito-junit-jupiter")
   )
 )
 
 val DEPENDENCIES = listOf(
   "com.github.stefanbirkner:system-rules:1.19.0",
-  "com.google.api.grpc:proto-google-common-protos:2.5.1",
+  "com.google.api.grpc:proto-google-common-protos:2.7.0",
   "com.google.code.findbugs:jsr305:3.0.2",
   "com.google.guava:guava-beta-checker:1.0",
   "com.lmax:disruptor:3.4.4",
   "com.sun.net.httpserver:http:20070405",
   "com.tngtech.archunit:archunit-junit5:0.22.0",
   "com.uber.nullaway:nullaway:0.9.2",
-  "edu.berkeley.cs.jqf:jqf-fuzz:1.7",
+  "edu.berkeley.cs.jqf:jqf-fuzz:1.8",
   "eu.rekawek.toxiproxy:toxiproxy-java:2.1.5",
-  "io.github.netmikey.logunit:logunit-jul:1.1.0",
-  "io.jaegertracing:jaeger-client:1.6.0",
+  "io.github.netmikey.logunit:logunit-jul:1.1.2",
+  "io.jaegertracing:jaeger-client:1.7.0",
   "io.opentelemetry.proto:opentelemetry-proto:0.9.0-alpha",
   "io.opentracing:opentracing-api:0.33.0",
   "junit:junit:4.13.2",
-  "nl.jqno.equalsverifier:equalsverifier:3.8",
-  "org.assertj:assertj-core:3.21.0",
+  "nl.jqno.equalsverifier:equalsverifier:3.8.2",
+  "org.assertj:assertj-core:3.22.0",
   "org.awaitility:awaitility:4.1.1",
   "org.bouncycastle:bcpkix-jdk15on:1.70",
   "org.codehaus.mojo:animal-sniffer-annotations:1.20",
