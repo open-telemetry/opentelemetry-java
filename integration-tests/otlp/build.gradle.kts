996e3b38024106f40dfe2e0c4bf04afa56e0055b
Anuraag Agrawal <anuraaga@gmail.com>
2022-01-21 05:36:05 +0000

    Migrate last test sets to test suites.

---
+++ integration-tests/otlp/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+  id("otel.java-conventions")
+}
+
+description = "OTLP Exporter Integration Tests"
+otelJava.moduleName.set("io.opentelemetry.integration.tests.otlp")
+
+dependencies {
+  api("org.testcontainers:junit-jupiter")
+
+  implementation(project(":exporters:otlp:trace"))
+  implementation(project(":exporters:otlp:metrics"))
+  implementation(project(":exporters:otlp:logs"))
+  implementation(project(":exporters:otlp-http:logs"))
+  implementation(project(":exporters:otlp-http:metrics"))
+  implementation(project(":exporters:otlp-http:trace"))
+  implementation(project(":semconv"))
+
+  implementation("com.linecorp.armeria:armeria-grpc-protocol")
+  implementation("com.linecorp.armeria:armeria-junit5")
+  implementation("io.opentelemetry.proto:opentelemetry-proto")
+  implementation("org.assertj:assertj-core")
+  implementation("org.awaitility:awaitility")
+  implementation("org.junit.jupiter:junit-jupiter-params")
+}
+
+testing {
+  suites {
+    val testGrpcJava by registering(JvmTestSuite::class) {
+      dependencies {
+        runtimeOnly("io.grpc:grpc-netty-shaded")
+        runtimeOnly("io.grpc:grpc-stub")
+      }
+    }
+  }
+}
+
+tasks {
+  check {
+    dependsOn(testing.suites)
+  }
+}
