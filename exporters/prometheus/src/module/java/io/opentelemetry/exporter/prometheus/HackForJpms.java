fb384fe853c5b2872ca6a4bf6fda3a3f1f1b1163
Anuraag Agrawal <anuraaga@gmail.com>
2022-02-21 06:14:20 +0000

    Add module-info to prometheus exporter

---
+++ exporters/prometheus/src/module/java/io/opentelemetry/exporter/prometheus/HackForJpms.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.exporter.prometheus;
+
+// Empty class to allow module-info to compile, not included in published JAR.
+class HackForJpms {}
