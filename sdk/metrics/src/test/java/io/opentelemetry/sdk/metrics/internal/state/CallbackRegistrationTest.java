7a58709da4e0144f592553311d981a60aa931e0e
jack-berg <jberg@newrelic.com>
2022-03-31 16:38:26 -0500

    Ensure callbacks are not called concurrently

--- sdk/metrics/src/test/java/io/opentelemetry/sdk/metrics/internal/state/CallbackRegistrationTest.java
+++ sdk/metrics/src/test/java/io/opentelemetry/sdk/metrics/internal/state/CallbackRegistrationTest.java
@@ -9,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.google.common.util.concurrent.AtomicDouble;
@@ -19,8 +20,17 @@ import io.opentelemetry.internal.testing.slf4j.SuppressLogger;
 import io.opentelemetry.sdk.metrics.InstrumentType;
 import io.opentelemetry.sdk.metrics.InstrumentValueType;
 import io.opentelemetry.sdk.metrics.internal.descriptor.InstrumentDescriptor;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 import org.junit.jupiter.api.Test;
@@ -120,5 +130,37 @@ class CallbackRegistrationTest {
     verify(storage1, never()).recordDouble(anyDouble(), any());
     verify(storage2, never()).recordDouble(anyDouble(), any());
     logs.assertContains("An exception occurred invoking callback for instrument name");
+  }
+
+  @Test
+  void invokeCallback_NoConcurrentCalls()
+      throws ExecutionException, InterruptedException, TimeoutException {
+    AtomicBoolean inProgress = new AtomicBoolean(false);
+    // Callback records if not called concurrently
+    Consumer<ObservableDoubleMeasurement> callback =
+        measurement -> {
+          if (inProgress.compareAndSet(false, true)) {
+            measurement.record(1.1);
+          }
+          try {
+            Thread.sleep(100);
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+          }
+          inProgress.set(false);
+        };
+    CallbackRegistration<?> callbackRegistration =
+        CallbackRegistration.createDouble(
+            DOUBLE_INSTRUMENT, callback, Collections.singletonList(storage1));
+
+    List<Future<?>> futures = new ArrayList<>();
+    ExecutorService executorService = Executors.newFixedThreadPool(2);
+    futures.add(executorService.submit(callbackRegistration::invokeCallback));
+    futures.add(executorService.submit(callbackRegistration::invokeCallback));
+    for (Future<?> future : futures) {
+      future.get(10, TimeUnit.SECONDS);
+    }
+
+    verify(storage1, times(2)).recordDouble(1.1, Attributes.empty());
   }
 }
