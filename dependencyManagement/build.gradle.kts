0f3bc931ae58807e6a2b75ee559724984d52da5e
Anuraag Agrawal <anuraaga@gmail.com>
2022-04-08 08:12:21 +0900

    Update dependencies (#4345)

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -12,14 +12,14 @@ val dependencyVersions = hashMapOf<String, String>()
 rootProject.extra["versions"] = dependencyVersions
 
 val DEPENDENCY_BOMS = listOf(
-  "com.fasterxml.jackson:jackson-bom:2.13.1",
-  "com.google.guava:guava-bom:31.0.1-jre",
-  "com.google.protobuf:protobuf-bom:3.19.4",
-  "com.linecorp.armeria:armeria-bom:1.14.0",
+  "com.fasterxml.jackson:jackson-bom:2.13.2.20220328",
+  "com.google.guava:guava-bom:31.1-jre",
+  "com.google.protobuf:protobuf-bom:3.20.0",
+  "com.linecorp.armeria:armeria-bom:1.15.0",
   "com.squareup.okhttp3:okhttp-bom:4.9.3",
-  "io.grpc:grpc-bom:1.44.0",
-  "io.micrometer:micrometer-bom:1.8.3",
-  "io.zipkin.brave:brave-bom:5.13.7",
+  "io.grpc:grpc-bom:1.45.1",
+  "io.micrometer:micrometer-bom:1.8.4",
+  "io.zipkin.brave:brave-bom:5.13.8",
   "io.zipkin.reporter2:zipkin-reporter-bom:2.16.3",
   "org.junit:junit-bom:5.8.2",
   "org.testcontainers:testcontainers-bom:1.16.3",
@@ -50,7 +50,7 @@ val DEPENDENCY_SETS = listOf(
   ),
   DependencySet(
     "io.prometheus",
-    "0.14.1",
+    "0.15.0",
     listOf("simpleclient", "simpleclient_common", "simpleclient_httpserver")
   ),
   DependencySet(
@@ -60,30 +60,30 @@ val DEPENDENCY_SETS = listOf(
   ),
   DependencySet(
     "org.openjdk.jmh",
-    "1.34",
+    "1.35",
     listOf("jmh-core", "jmh-generator-bytecode")
   ),
   DependencySet(
     "org.mockito",
-    "4.3.1",
+    "4.4.0",
     listOf("mockito-core", "mockito-junit-jupiter")
   ),
   DependencySet(
     "org.slf4j",
-    "1.7.35",
+    "1.7.36",
     listOf("jul-to-slf4j", "slf4j-simple")
   ),
 )
 
 val DEPENDENCIES = listOf(
   "com.github.stefanbirkner:system-rules:1.19.0",
-  "com.google.api.grpc:proto-google-common-protos:2.7.2",
+  "com.google.api.grpc:proto-google-common-protos:2.8.0",
   "com.google.code.findbugs:jsr305:3.0.2",
   "com.google.guava:guava-beta-checker:1.0",
   "com.lmax:disruptor:3.4.4",
   "com.sun.net.httpserver:http:20070405",
-  "com.tngtech.archunit:archunit-junit5:0.22.0",
-  "com.uber.nullaway:nullaway:0.9.5",
+  "com.tngtech.archunit:archunit-junit5:0.23.1",
+  "com.uber.nullaway:nullaway:0.9.6",
   // TODO(anuraaga): Skip 1.8 because of https://github.com/rohanpadhye/JQF/issues/172
   "edu.berkeley.cs.jqf:jqf-fuzz:1.7",
   "eu.rekawek.toxiproxy:toxiproxy-java:2.1.5",
@@ -92,13 +92,13 @@ val DEPENDENCIES = listOf(
   "io.opentelemetry.proto:opentelemetry-proto:0.16.0-alpha",
   "io.opentracing:opentracing-api:0.33.0",
   "junit:junit:4.13.2",
-  "nl.jqno.equalsverifier:equalsverifier:3.9",
+  "nl.jqno.equalsverifier:equalsverifier:3.10",
   "org.assertj:assertj-core:3.22.0",
-  "org.awaitility:awaitility:4.1.1",
+  "org.awaitility:awaitility:4.2.0",
   "org.bouncycastle:bcpkix-jdk15on:1.70",
   "org.codehaus.mojo:animal-sniffer-annotations:1.21",
   "org.jctools:jctools-core:3.3.0",
-  "org.junit-pioneer:junit-pioneer:1.5.0",
+  "org.junit-pioneer:junit-pioneer:1.6.2",
   "org.skyscreamer:jsonassert:1.5.0",
 )
 
