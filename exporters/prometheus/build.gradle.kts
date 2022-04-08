d0357cbad3a4cd5629a1d34be35d86783e01afd4
Anuraag Agrawal <anuraaga@gmail.com>
2022-04-08 21:54:29 +0900

    Remove unused transitive prometheus deps (#4352)

--- exporters/prometheus/build.gradle.kts
+++ exporters/prometheus/build.gradle.kts
@@ -11,9 +11,6 @@ otelJava.moduleName.set("io.opentelemetry.exporter.prometheus")
 dependencies {
   api(project(":sdk:metrics"))
 
-  api("io.prometheus:simpleclient")
-  implementation("io.prometheus:simpleclient_common")
-
   compileOnly("com.sun.net.httpserver:http")
 
   testImplementation("com.google.guava:guava")
