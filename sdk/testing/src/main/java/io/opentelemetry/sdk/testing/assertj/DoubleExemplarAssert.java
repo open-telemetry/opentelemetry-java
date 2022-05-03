1cabb6ca8c9efb146cef4192c24914489c7052a0
Anuraag Agrawal <anuraaga@gmail.com>
2022-05-03 06:04:50 +0000

    Split exemplar assertion into double / long

--- sdk/testing/src/main/java/io/opentelemetry/sdk/testing/assertj/DoubleExemplarAssert.java
+++ sdk/testing/src/main/java/io/opentelemetry/sdk/testing/assertj/DoubleExemplarAssert.java
@@ -11,59 +11,54 @@ import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.sdk.metrics.data.DoubleExemplarData;
-import io.opentelemetry.sdk.metrics.data.ExemplarData;
-import io.opentelemetry.sdk.metrics.data.LongExemplarData;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nullable;
 import org.assertj.core.api.AbstractAssert;
 
-/** Assertions for an exported {@link ExemplarData}. */
-public final class ExemplarDataAssert extends AbstractAssert<ExemplarDataAssert, ExemplarData> {
-  ExemplarDataAssert(@Nullable ExemplarData actual) {
-    super(actual, ExemplarDataAssert.class);
+/** Assertions for an exported {@link DoubleExemplarData}. */
+public final class DoubleExemplarAssert
+    extends AbstractAssert<DoubleExemplarAssert, DoubleExemplarData> {
+  DoubleExemplarAssert(@Nullable DoubleExemplarData actual) {
+    super(actual, DoubleExemplarAssert.class);
   }
 
   /** Asserts the exemplar has the given epoch timestamp, in nanos. */
-  public ExemplarDataAssert hasEpochNanos(long expected) {
+  public DoubleExemplarAssert hasEpochNanos(long expected) {
     isNotNull();
     assertThat(actual.getEpochNanos()).as("epochNanos").isEqualTo(expected);
     return this;
   }
 
   /** Asserts the exemplar has the given span ID. */
-  public ExemplarDataAssert hasSpanId(String expected) {
+  public DoubleExemplarAssert hasSpanId(String expected) {
     isNotNull();
     assertThat(actual.getSpanContext().getSpanId()).as("spanId").isEqualTo(expected);
     return this;
   }
 
   /** Asserts the exemplar has the given trace ID. */
-  public ExemplarDataAssert hasTraceId(String expected) {
+  public DoubleExemplarAssert hasTraceId(String expected) {
     isNotNull();
     assertThat(actual.getSpanContext().getTraceId()).as("traceId").isEqualTo(expected);
     return this;
   }
 
   /** Asserts the exemplar has the given value. */
-  public ExemplarDataAssert hasValue(double expected) {
+  public DoubleExemplarAssert hasValue(double expected) {
     isNotNull();
-    double value =
-        actual instanceof DoubleExemplarData
-            ? ((DoubleExemplarData) actual).getValue()
-            : ((LongExemplarData) actual).getValue();
-    assertThat(value).as("value").isEqualTo(expected);
+    assertThat(actual.getValue()).as("value").isEqualTo(expected);
     return this;
   }
 
   /** Asserts the exemplar has the given filtered attribute. */
-  public <T> ExemplarDataAssert hasFilteredAttribute(AttributeKey<T> key, T value) {
+  public <T> DoubleExemplarAssert hasFilteredAttribute(AttributeKey<T> key, T value) {
     return hasFilteredAttribute(OpenTelemetryAssertions.equalTo(key, value));
   }
 
   /** Asserts the exemplar has the given filtered attribute. */
-  public ExemplarDataAssert hasFilteredAttribute(AttributeAssertion attributeAssertion) {
+  public DoubleExemplarAssert hasFilteredAttribute(AttributeAssertion attributeAssertion) {
     isNotNull();
 
     Set<AttributeKey<?>> actualKeys = actual.getFilteredAttributes().asMap().keySet();
@@ -79,7 +74,7 @@ public final class ExemplarDataAssert extends AbstractAssert<ExemplarDataAssert,
   }
 
   /** Asserts the exemplar has the given filtered attributes. */
-  public ExemplarDataAssert hasFilteredAttributes(Attributes expected) {
+  public DoubleExemplarAssert hasFilteredAttributes(Attributes expected) {
     isNotNull();
     assertThat(actual.getFilteredAttributes()).as("filtered_attributes").isEqualTo(expected);
     return this;
@@ -88,7 +83,7 @@ public final class ExemplarDataAssert extends AbstractAssert<ExemplarDataAssert,
   /** Asserts the exemplar has the given filtered attributes. */
   @SuppressWarnings({"rawtypes", "unchecked"})
   @SafeVarargs
-  public final ExemplarDataAssert hasFilteredAttributes(
+  public final DoubleExemplarAssert hasFilteredAttributes(
       Map.Entry<? extends AttributeKey<?>, ?>... entries) {
     AttributesBuilder attributesBuilder = Attributes.builder();
     for (Map.Entry<? extends AttributeKey<?>, ?> attr : entries) {
@@ -99,7 +94,7 @@ public final class ExemplarDataAssert extends AbstractAssert<ExemplarDataAssert,
   }
 
   /** Asserts the exemplar has filtered attributes matching all {@code assertions} and no more. */
-  public ExemplarDataAssert hasFilteredAttributesSatisfying(AttributeAssertion... assertions) {
+  public DoubleExemplarAssert hasFilteredAttributesSatisfying(AttributeAssertion... assertions) {
     return hasFilteredAttributesSatisfying(Arrays.asList(assertions));
   }
 
@@ -108,7 +103,7 @@ public final class ExemplarDataAssert extends AbstractAssert<ExemplarDataAssert,
    * Assertions can be created using methods like {@link
    * OpenTelemetryAssertions#satisfies(AttributeKey, OpenTelemetryAssertions.LongAssertConsumer)}.
    */
-  public ExemplarDataAssert hasFilteredAttributesSatisfying(
+  public DoubleExemplarAssert hasFilteredAttributesSatisfying(
       Iterable<AttributeAssertion> assertions) {
     AssertUtil.assertAttributes(actual.getFilteredAttributes(), assertions);
     return myself;
