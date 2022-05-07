89c6323fff79992d6104e7aecb4e0ab6abf56910
jack-berg <34418638+jack-berg@users.noreply.github.com>
2022-05-06 20:00:11 -0500

    Move InMemoryMetricReader and InMemoryMetricExporter to :sdk:testing (#4442)
    * Move InMemoryMetricReader and InMemoryMetricExporter to :sdk:testing
    
    * Move tests

--- sdk/metrics-testing/src/main/java/io/opentelemetry/sdk/testing/exporter/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright The OpenTelemetry Authors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-@ParametersAreNonnullByDefault
-package io.opentelemetry.sdk.testing.exporter;
-
-import javax.annotation.ParametersAreNonnullByDefault;
