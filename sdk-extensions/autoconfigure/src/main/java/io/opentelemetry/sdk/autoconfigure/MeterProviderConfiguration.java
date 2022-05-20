3ed74b6bbc3f59cea1b3c14d1197376812f0c5ed
wallezhang <doraemon.zh@gmail.com>
2022-05-21 04:01:05 +0800

    feat: `otel.metrics.exporter` setting support multiple values (#4466)
    * feat: `otel.metrics.exporter` setting support multiple values
    
    * refactor: Refactor metric reader configuration function

--- sdk-extensions/autoconfigure/src/main/java/io/opentelemetry/sdk/autoconfigure/MeterProviderConfiguration.java
+++ sdk-extensions/autoconfigure/src/main/java/io/opentelemetry/sdk/autoconfigure/MeterProviderConfiguration.java
@@ -6,11 +6,17 @@
 package io.opentelemetry.sdk.autoconfigure;
 
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigurationException;
 import io.opentelemetry.sdk.metrics.SdkMeterProviderBuilder;
 import io.opentelemetry.sdk.metrics.export.MetricExporter;
+import io.opentelemetry.sdk.metrics.export.MetricReader;
 import io.opentelemetry.sdk.metrics.internal.SdkMeterProviderUtil;
 import io.opentelemetry.sdk.metrics.internal.exemplar.ExemplarFilter;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 final class MeterProviderConfiguration {
 
@@ -40,14 +46,33 @@ final class MeterProviderConfiguration {
         break;
     }
 
-    String exporterName = config.getString("otel.metrics.exporter");
-    if (exporterName == null) {
-      exporterName = "otlp";
+    configureMetricReaders(config, serviceClassLoader, metricExporterCustomizer)
+        .forEach(meterProviderBuilder::registerMetricReader);
+  }
+
+  static List<MetricReader> configureMetricReaders(
+      ConfigProperties config,
+      ClassLoader serviceClassLoader,
+      BiFunction<? super MetricExporter, ConfigProperties, ? extends MetricExporter>
+          metricExporterCustomizer) {
+    Set<String> exporterNames = DefaultConfigProperties.getSet(config, "otel.metrics.exporter");
+    if (exporterNames.contains("none")) {
+      if (exporterNames.size() > 1) {
+        throw new ConfigurationException(
+            "otel.metrics.exporter contains none along with other exporters");
+      }
+      return Collections.emptyList();
     }
-    if (!exporterName.equals("none")) {
-      MetricExporterConfiguration.configureExporter(
-          exporterName, config, serviceClassLoader, meterProviderBuilder, metricExporterCustomizer);
+
+    if (exporterNames.isEmpty()) {
+      exporterNames = Collections.singleton("otlp");
     }
+    return exporterNames.stream()
+        .map(
+            exporterName ->
+                MetricExporterConfiguration.configureExporter(
+                    exporterName, config, serviceClassLoader, metricExporterCustomizer))
+        .collect(Collectors.toList());
   }
 
   private MeterProviderConfiguration() {}
