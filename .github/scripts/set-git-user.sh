587adde7697dd7f26ee3a8877c34609d4a78e809
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-07-08 15:55:43 -0700

    Switch to opentelemetrybot account

--- .github/scripts/set-git-user.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-git config user.name opentelemetry-java-bot
-git config user.email 97938252+opentelemetry-java-bot@users.noreply.github.com
---
+++ .github/scripts/use-cla-approved-github-bot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+git config user.name opentelemetrybot
+git config user.email 107717825+opentelemetrybot@users.noreply.github.com
