2e314f1f6d9917db83bf808863fc2018cb82f5ab
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-07-25 08:35:25 +0200

    comments

--- sdk-extensions/autoconfigure-spi/src/main/java/io/opentelemetry/sdk/autoconfigure/spi/Ordered.java
+++ sdk-extensions/autoconfigure-spi/src/main/java/io/opentelemetry/sdk/autoconfigure/spi/Ordered.java
@@ -5,12 +5,17 @@
 
 package io.opentelemetry.sdk.autoconfigure.spi;
 
-/** Interface to be extended by SPIs that need to guarantee ordering during loading. */
+/**
+ * Interface to be extended by SPIs that need to guarantee ordering during loading.
+ *
+ * @since 1.17.0
+ */
 public interface Ordered {
 
   /**
-   * Returns the order of applying the SPI implementing this interface. Higher values are added
-   * later, for example: an SPI with order=1 will run after an SPI with order=0.
+   * Returns the order of applying the SPI implementing this interface. Higher values are applied
+   * later, for example: an SPI with order=1 will run after an SPI with order=0. SPI implementations
+   * with equal values will be run in a non-deterministic order.
    */
   default int order() {
     return 0;
