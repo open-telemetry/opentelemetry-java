77be2e0136a82907c24d55e16fa46dfd7a7921ac
jack-berg <34418638+jack-berg@users.noreply.github.com>
2022-08-08 08:28:24 -0500

    Break out :exporters:common module (#4575)
    * Break out :exporters:common module
    
    * Comment out readme links
    
    * spot

---
+++ exporters/common/src/main/java/io/opentelemetry/exporter/internal/auth/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** Utilities for exporter authentication. */
+@ParametersAreNonnullByDefault
+package io.opentelemetry.exporter.internal.auth;
+
+import javax.annotation.ParametersAreNonnullByDefault;
