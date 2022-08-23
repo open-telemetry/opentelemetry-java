5a5fbad82cf09f87299c2d7bcf1757392f9203af
Jack Berg <jberg@newrelic.com>
2022-08-23 12:54:03 -0500

    Update javadoc to reflect lower exlusive boundaries

--- sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/internal/data/exponentialhistogram/ExponentialHistogramBuckets.java
+++ sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/internal/data/exponentialhistogram/ExponentialHistogramBuckets.java
@@ -12,7 +12,7 @@ import javax.annotation.concurrent.Immutable;
  * ExponentialHistogramBuckets represents either the positive or negative measurements taken for a
  * {@link ExponentialHistogramPointData}.
  *
- * <p>The bucket boundaries are lower-bound inclusive, and are calculated using the {@link
+ * <p>The bucket boundaries are lower-bound exclusive, and are calculated using the {@link
  * ExponentialHistogramPointData#getScale()} and the {@link #getOffset()}.
  *
  * <p>For example, assume {@link ExponentialHistogramPointData#getScale()} is 0, the base is 2.0.
@@ -35,7 +35,7 @@ public interface ExponentialHistogramBuckets {
   int getOffset();
 
   /**
-   * The bucket counts is a of counts representing number of measurements that fall into each
+   * The bucket counts is a list of counts representing number of measurements that fall into each
    * bucket.
    *
    * @return the bucket counts.
