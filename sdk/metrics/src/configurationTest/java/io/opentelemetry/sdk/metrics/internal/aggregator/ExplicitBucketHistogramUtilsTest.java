9efdb5743320d0f6104dd44ee6494b8d47e34c80
Jack Berg <jberg@newrelic.com>
2022-08-24 13:05:34 -0500

    Add property for configuring legacy explicit bucket histogram bucket boundaries

---
+++ sdk/metrics/src/configurationTest/java/io/opentelemetry/sdk/metrics/internal/aggregator/ExplicitBucketHistogramUtilsTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.sdk.metrics.internal.aggregator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class ExplicitBucketHistogramUtilsTest {
+
+  /**
+   * {@link ExplicitBucketHistogramUtils#getDefaultBucketBoundaries()} returns different values when
+   * {@code otel.java.histogram.legacy.buckets.enabled=true}.
+   */
+  @Test
+  void defaultBucketBoundaries() {
+    assertThat(ExplicitBucketHistogramUtils.getDefaultBucketBoundaries())
+        .isEqualTo(
+            Arrays.asList(
+                5d, 10d, 25d, 50d, 75d, 100d, 250d, 500d, 750d, 1_000d, 2_500d, 5_000d, 7_500d,
+                10_000d));
+  }
+}
