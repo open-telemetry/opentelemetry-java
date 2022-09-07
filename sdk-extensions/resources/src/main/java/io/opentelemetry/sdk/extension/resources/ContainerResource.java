1a48fcd8bcb7c538c4ff849fe359e1d5cf7b0c33
wuzhaoyin <1449259282@qq.com>
2022-09-07 11:00:39 +0800

    Annotation adjustment
    Signed-off-by: wuzhaoyin <1449259282@qq.com>

--- sdk-extensions/resources/src/main/java/io/opentelemetry/sdk/extension/resources/ContainerResource.java
+++ sdk-extensions/resources/src/main/java/io/opentelemetry/sdk/extension/resources/ContainerResource.java
@@ -92,10 +92,8 @@ public final class ContainerResource {
     int colonIdx = lastSection.lastIndexOf(':');
 
     if (colonIdx != -1) {
-      // since k8s v1.24+, containerId is divided by the last colon when container is containerd and
-      // cgroupDriver is systemd
-      // case:
-      // 'kubepods-pod87a18a64_b74a_454a_b10b_a4a36059d0a3.slice:cri-containerd:05c48c82caff3be3d7f1e896981dd410e81487538936914f32b624d168de9db0'
+      // since containerd v1.5.0+, containerId is divided by the last colon when the cgroupDriver is systemd:
+      // https://github.com/containerd/containerd/blob/release/1.5/pkg/cri/server/helpers_linux.go#L64
       containerId = lastSection.substring(colonIdx + 1);
     } else {
       int startIdx = lastSection.lastIndexOf('-');
