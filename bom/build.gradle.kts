ede7474c3df00aa7b52f12b37d242364bbcaf51c
Jack Berg <jberg@newrelic.com>
2022-09-09 14:51:29 -0500

    Stop publishing opentelemetry-extension-annotations artifact

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -10,3 +10,4 @@ otelBom.projectFilter.set { !it.hasProperty("otel.release") }
 
 // Artifacts that were previously published and included in the BOM for backwards compatibility
 otelBom.addFallback("opentelemetry-exporter-jaeger-proto", "1.17.0")
+otelBom.addFallback("opentelemetry-extension-annotations", "1.18.0")
