92e0379e80857ce816080c2cc9888b241620fc08
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-10-11 18:07:46 +0200

    Added Graal VM Github Action build

---
+++ integration-tests/graal/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+  id("otel.java-conventions")
+  id("java-library")
+  id("org.graalvm.buildtools.native") version "0.9.7.1"
+}
+
+description = "OpenTelemetry Graal Integration Tests"
+otelJava.moduleName.set("io.opentelemetry.graal.integration.tests")
+
+sourceSets {
+  main {
+    val traceShadedDeps = project(":sdk:trace-shaded-deps")
+    output.dir(traceShadedDeps.file("build/extracted/shadow"), "builtBy" to ":sdk:trace-shaded-deps:extractShadowJar")
+  }
+}
+
+dependencies {
+  implementation(project(path = ":sdk:trace-shaded-deps"))
+
+  testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+graalvmNative {
+  binaries {
+    named("main") {
+      verbose.set(true)
+    }
+  }
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
