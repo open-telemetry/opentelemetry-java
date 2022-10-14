cc22880bb8b77a91f9050108f14be47526275fe0
jack-berg <34418638+jack-berg@users.noreply.github.com>
2022-10-14 07:51:17 -0500

    Stop publishing opentelemetry-sdk-extension-aws artifact (#4830)

--- all/src/test/java/io/opentelemetry/all/FallbackArtifactsTest.java
+++ all/src/test/java/io/opentelemetry/all/FallbackArtifactsTest.java
@@ -8,6 +8,10 @@ package io.opentelemetry.all;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * This test asserts that artifacts which are no longer published continue to be referenced in
+ * {@code opentelemetry-bom}.
+ */
 class FallbackArtifactsTest {
 
   @Test
@@ -21,6 +25,21 @@ class FallbackArtifactsTest {
   void extensionAnnotations() {
     classAvailable("io.opentelemetry.extension.annotations.WithSpan");
     classAvailable("io.opentelemetry.extension.annotations.SpanAttribute");
+  }
+
+  @Test
+  void sdkExtensionAws() {
+    classAvailable("io.opentelemetry.sdk.extension.aws.resource.BeanstalkResource");
+    classAvailable("io.opentelemetry.sdk.extension.aws.resource.BeanstalkResourceProvider");
+    classAvailable("io.opentelemetry.sdk.extension.aws.resource.Ec2Resource");
+    classAvailable("io.opentelemetry.sdk.extension.aws.resource.Ec2ResourceProvider");
+    classAvailable("io.opentelemetry.sdk.extension.aws.resource.EcsResource");
+    classAvailable("io.opentelemetry.sdk.extension.aws.resource.EcsResourceProvider");
+    classAvailable("io.opentelemetry.sdk.extension.aws.resource.EksResource");
+    classAvailable("io.opentelemetry.sdk.extension.aws.resource.EksResourceProvider");
+    classAvailable("io.opentelemetry.sdk.extension.aws.resource.LambdaResource");
+    classAvailable("io.opentelemetry.sdk.extension.aws.resource.LambdaResourceProvider");
+    classAvailable("io.opentelemetry.sdk.extension.aws.trace.AwsXrayIdGenerator");
   }
 
   private static void classAvailable(String fqcn) {
