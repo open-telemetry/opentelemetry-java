52c37d56da8ef5fff522010dc07f5ed743622a64
jack-berg <34418638+jack-berg@users.noreply.github.com>
2022-10-31 12:25:36 -0500

    Stop publishing opentelemetry-sdk-extension-resources artifact (#4828)

--- sdk-extensions/autoconfigure/src/testFullConfig/java/io/opentelemetry/sdk/autoconfigure/TestColorResourceProvider.java
+++ sdk-extensions/autoconfigure/src/testFullConfig/java/io/opentelemetry/sdk/autoconfigure/TestColorResourceProvider.java
@@ -11,9 +11,9 @@ import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
 import io.opentelemetry.sdk.resources.Resource;
 
-public class ResourceProviderCustomizer implements ResourceProvider {
+public class TestColorResourceProvider implements ResourceProvider {
   @Override
   public Resource createResource(ConfigProperties config) {
-    return Resource.create(Attributes.of(AttributeKey.stringKey("animal"), "cat"));
+    return Resource.create(Attributes.of(AttributeKey.stringKey("color"), "blue"));
   }
 }
