1f74d5c84b131de3ae63553eb5a45b83fa200e26
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2022-11-01 14:13:23 +0100

    Full multiple parent support.
    This includes:
    
    * The Baggage union of ALL parents is used.
    * All parents are added as Links, in order to preserve
      the OpenTracing reference type as an attribute
      (either CHILD_OF or FOLLOWS_FROM).

--- opentracing-shim/src/test/java/io/opentelemetry/opentracingshim/SpanBuilderShimTest.java
+++ opentracing-shim/src/test/java/io/opentelemetry/opentracingshim/SpanBuilderShimTest.java
@@ -20,6 +20,8 @@ import io.opentelemetry.sdk.trace.data.LinkData;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import io.opentelemetry.sdk.trace.samplers.Sampler;
 import io.opentelemetry.sdk.trace.samplers.SamplingResult;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import io.opentracing.References;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,6 +38,142 @@ class SpanBuilderShimTest {
   @BeforeEach
   void setUp() {
     GlobalOpenTelemetry.resetForTest();
+  }
+
+  @Test
+  void parent_single() {
+    SpanShim parentSpan = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+    try {
+      SpanShim childSpan =
+          (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).asChildOf(parentSpan).start();
+
+      try {
+        SpanData spanData = ((ReadableSpan) childSpan.getSpan()).toSpanData();
+        assertThat(parentSpan.context().toSpanId()).isEqualTo(spanData.getParentSpanId());
+      } finally {
+        childSpan.finish();
+      }
+    } finally {
+      parentSpan.finish();
+    }
+  }
+
+  @Test
+  void parent_multipleFollowsFrom() {
+    SpanShim parentSpan1 = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+    SpanShim parentSpan2 = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+
+    try {
+      SpanShim childSpan =
+          (SpanShim)
+              new SpanBuilderShim(telemetryInfo, SPAN_NAME)
+                  .addReference(References.FOLLOWS_FROM, parentSpan1.context())
+                  .addReference(References.FOLLOWS_FROM, parentSpan2.context())
+                  .start();
+
+      try {
+        // If no parent of CHILD_OF type exists, use the first value as main parent.
+        SpanData spanData = ((ReadableSpan) childSpan.getSpan()).toSpanData();
+        assertThat(parentSpan1.context().toSpanId()).isEqualTo(spanData.getParentSpanId());
+      } finally {
+        childSpan.finish();
+      }
+    } finally {
+      parentSpan1.finish();
+      parentSpan2.finish();
+    }
+  }
+
+  @Test
+  void parent_multipleDifferentRefType() {
+    SpanShim parentSpan1 = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+    SpanShim parentSpan2 = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+    SpanShim parentSpan3 = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+
+    try {
+      SpanShim childSpan =
+          (SpanShim)
+              new SpanBuilderShim(telemetryInfo, SPAN_NAME)
+                  .addReference(References.FOLLOWS_FROM, parentSpan1.context())
+                  .addReference(References.CHILD_OF, parentSpan2.context())
+                  .addReference(References.CHILD_OF, parentSpan3.context())
+                  .start();
+
+      try {
+        // The first parent with CHILD_OF becomes the direct parent (if any).
+        SpanData spanData = ((ReadableSpan) childSpan.getSpan()).toSpanData();
+        assertThat(parentSpan2.context().toSpanId()).isEqualTo(spanData.getParentSpanId());
+      } finally {
+        childSpan.finish();
+      }
+    } finally {
+      parentSpan1.finish();
+      parentSpan2.finish();
+      parentSpan3.finish();
+    }
+  }
+
+  @Test
+  void parent_multipleLinks() {
+    SpanShim parentSpan1 = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+    SpanShim parentSpan2 = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+    SpanShim parentSpan3 = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+
+    try {
+      SpanShim childSpan =
+          (SpanShim)
+              new SpanBuilderShim(telemetryInfo, SPAN_NAME)
+                  .addReference(References.FOLLOWS_FROM, parentSpan1.context())
+                  .addReference(References.CHILD_OF, parentSpan2.context())
+                  .addReference(References.FOLLOWS_FROM, parentSpan3.context())
+                  .start();
+
+      try {
+        SpanData spanData = ((ReadableSpan) childSpan.getSpan()).toSpanData();
+        List<LinkData> links = spanData.getLinks();
+        assertThat(links).hasSize(3);
+
+        assertThat(links.get(0).getSpanContext()).isEqualTo(parentSpan1.getSpan().getSpanContext());
+        assertThat(links.get(1).getSpanContext()).isEqualTo(parentSpan2.getSpan().getSpanContext());
+        assertThat(links.get(2).getSpanContext()).isEqualTo(parentSpan3.getSpan().getSpanContext());
+        assertThat(links.get(0).getAttributes().get(SemanticAttributes.OPENTRACING_REF_TYPE))
+            .isEqualTo(SemanticAttributes.OpentracingRefTypeValues.FOLLOWS_FROM);
+        assertThat(links.get(1).getAttributes().get(SemanticAttributes.OPENTRACING_REF_TYPE))
+            .isEqualTo(SemanticAttributes.OpentracingRefTypeValues.CHILD_OF);
+        assertThat(links.get(2).getAttributes().get(SemanticAttributes.OPENTRACING_REF_TYPE))
+            .isEqualTo(SemanticAttributes.OpentracingRefTypeValues.FOLLOWS_FROM);
+
+      } finally {
+        childSpan.finish();
+      }
+    } finally {
+      parentSpan1.finish();
+      parentSpan2.finish();
+      parentSpan3.finish();
+    }
+  }
+
+  @Test
+  void parent_wrongRefType() {
+    SpanShim parentSpan = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+    try {
+      SpanShim childSpan =
+          (SpanShim)
+              new SpanBuilderShim(telemetryInfo, SPAN_NAME)
+                  .addReference("wrongreftype-value", parentSpan.context())
+                  .start();
+
+      try {
+        // Incorrect typeref values get discarded.
+        SpanData spanData = ((ReadableSpan) childSpan.getSpan()).toSpanData();
+        assertThat(spanData.getParentSpanContext())
+            .isEqualTo(io.opentelemetry.api.trace.SpanContext.getInvalid());
+      } finally {
+        childSpan.finish();
+      }
+    } finally {
+      parentSpan.finish();
+    }
   }
 
   @Test
@@ -76,6 +214,32 @@ class SpanBuilderShimTest {
       }
     } finally {
       parentSpan.finish();
+    }
+  }
+
+  @Test
+  void baggage_multipleParents() {
+    SpanShim parentSpan1 = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+    SpanShim parentSpan2 = (SpanShim) new SpanBuilderShim(telemetryInfo, SPAN_NAME).start();
+    try {
+      parentSpan1.setBaggageItem("key1", "value1");
+      parentSpan2.setBaggageItem("key2", "value2");
+
+      SpanShim childSpan =
+          (SpanShim)
+              new SpanBuilderShim(telemetryInfo, SPAN_NAME)
+                  .addReference(References.FOLLOWS_FROM, parentSpan1.context())
+                  .addReference(References.CHILD_OF, parentSpan2.context())
+                  .start();
+      try {
+        assertThat(childSpan.getBaggageItem("key1")).isEqualTo("value1");
+        assertThat(childSpan.getBaggageItem("key2")).isEqualTo("value2");
+      } finally {
+        childSpan.finish();
+      }
+    } finally {
+      parentSpan1.finish();
+      parentSpan2.finish();
     }
   }
 
