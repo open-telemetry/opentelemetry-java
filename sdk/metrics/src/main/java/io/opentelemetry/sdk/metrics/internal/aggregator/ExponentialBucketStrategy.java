e73232898f43225b77a224723815582dc4b3a7da
jack-berg <34418638+jack-berg@users.noreply.github.com>
2022-12-01 09:20:19 -0600

    Add ExponentialHistogramIndexerBenchmark (#4989)

--- sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/internal/aggregator/ExponentialBucketStrategy.java
+++ sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/internal/aggregator/ExponentialBucketStrategy.java
@@ -10,26 +10,35 @@ import io.opentelemetry.sdk.metrics.internal.state.ExponentialCounterFactory;
 /** The configuration for how to create exponential histogram buckets. */
 final class ExponentialBucketStrategy {
 
-  private static final int STARTING_SCALE = 20;
+  private static final int DEFAULT_STARTING_SCALE = 20;
 
+  private final int startingScale;
   /** The maximum number of buckets that will be used for positive or negative recordings. */
   private final int maxBuckets;
   /** The mechanism of constructing and copying buckets. */
   private final ExponentialCounterFactory counterFactory;
 
-  private ExponentialBucketStrategy(int maxBuckets, ExponentialCounterFactory counterFactory) {
+  private ExponentialBucketStrategy(
+      int startingScale, int maxBuckets, ExponentialCounterFactory counterFactory) {
+    this.startingScale = startingScale;
     this.maxBuckets = maxBuckets;
     this.counterFactory = counterFactory;
   }
 
   /** Constructs fresh new buckets with default settings. */
   DoubleExponentialHistogramBuckets newBuckets() {
-    return new DoubleExponentialHistogramBuckets(STARTING_SCALE, maxBuckets, counterFactory);
+    return new DoubleExponentialHistogramBuckets(startingScale, maxBuckets, counterFactory);
   }
 
   /** Create a new strategy for generating Exponential Buckets. */
   static ExponentialBucketStrategy newStrategy(
       int maxBuckets, ExponentialCounterFactory counterFactory) {
-    return new ExponentialBucketStrategy(maxBuckets, counterFactory);
+    return new ExponentialBucketStrategy(DEFAULT_STARTING_SCALE, maxBuckets, counterFactory);
+  }
+
+  /** Create a new strategy for generating Exponential Buckets. */
+  static ExponentialBucketStrategy newStrategy(
+      int maxBuckets, ExponentialCounterFactory counterFactory, int startingScale) {
+    return new ExponentialBucketStrategy(startingScale, maxBuckets, counterFactory);
   }
 }
