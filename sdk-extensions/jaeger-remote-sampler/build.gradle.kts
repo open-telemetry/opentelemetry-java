e261421c4695fa725aa558d21175772dd26f9caf
Jack Berg <jberg@newrelic.com>
2023-01-18 14:35:24 -0600

    Make implicit gradle dependencies explicit

--- sdk-extensions/jaeger-remote-sampler/build.gradle.kts
+++ sdk-extensions/jaeger-remote-sampler/build.gradle.kts
@@ -69,6 +69,9 @@ wire {
   }
 }
 
+// Declare sourcesJar dependency on proto generation so gradle doesn't complain about implicit dependency
+tasks.getByName("sourcesJar").dependsOn("generateMainProtos")
+
 sourceSets {
   main {
     java.srcDir("$buildDir/generated/source/wire")
