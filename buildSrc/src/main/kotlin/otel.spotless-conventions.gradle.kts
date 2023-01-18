a65f6da6a4ec9a8e1d12d4bf3e7f06fcc24d0503
Jack Berg <jberg@newrelic.com>
2023-01-17 19:02:35 -0600

    Add to final newlin editorconfigoverride

--- buildSrc/src/main/kotlin/otel.spotless-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/otel.spotless-conventions.gradle.kts
@@ -26,6 +26,7 @@ spotless {
         "indent_size" to "2",
         "continuation_indent_size" to "2",
         "max_line_length" to "160",
+        "insert_final_newline" to "true",
         "ktlint_standard_no-wildcard-imports" to "disabled",
         // ktlint does not break up long lines, it just fails on them
         "ktlint_standard_max-line-length" to "disabled",
@@ -42,6 +43,7 @@ spotless {
       "indent_size" to "2",
       "continuation_indent_size" to "2",
       "max_line_length" to "160",
+      "insert_final_newline" to "true",
       "ktlint_standard_no-wildcard-imports" to "disabled",
       // ktlint does not break up long lines, it just fails on them
       "ktlint_standard_max-line-length" to "disabled",
