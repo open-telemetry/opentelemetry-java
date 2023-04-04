74aa07415eedbdf2eaed6038f9f5411349996deb
Jack Berg <jberg@newrelic.com>
2023-04-04 14:36:31 -0500

    Move kotlin plugin dependency to buildsrc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ pluginManagement {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("com.gradle.enterprise") version "3.12.6"
     id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
-    id("org.jetbrains.kotlin.jvm") version "1.8.20"
     id("org.graalvm.buildtools.native") version "0.9.20"
   }
 }
