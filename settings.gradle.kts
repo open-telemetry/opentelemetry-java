401b30418c1d77a544260cd041e0a4ba890e3923
jack-berg <34418638+jack-berg@users.noreply.github.com>
2023-06-09 12:07:48 -0500

    Publish build scans to ge.opentelemetry.io (#5510)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -62,15 +62,42 @@ include(":sdk-extensions:incubator")
 include(":sdk-extensions:jaeger-remote-sampler")
 include(":testing-internal")
 
+val gradleEnterpriseServer = "https://ge.opentelemetry.io"
 val isCI = System.getenv("CI") != null
-gradleEnterprise {
-  buildScan {
-    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-    termsOfServiceAgree = "yes"
+val geAccessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY") ?: ""
 
-    if (isCI) {
+// if GE access key is not given and we are in CI, then we publish to scans.gradle.com
+val useScansGradleCom = isCI && geAccessKey.isEmpty()
+
+if (useScansGradleCom) {
+  gradleEnterprise {
+    buildScan {
+      termsOfServiceUrl = "https://gradle.com/terms-of-service"
+      termsOfServiceAgree = "yes"
+      isUploadInBackground = !isCI
       publishAlways()
-      tag("CI")
+
+      capture {
+        isTaskInputFiles = true
+      }
+    }
+  }
+} else {
+  gradleEnterprise {
+    server = gradleEnterpriseServer
+    buildScan {
+      isUploadInBackground = !isCI
+
+      this as com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
+      publishIfAuthenticated()
+      publishAlways()
+
+      capture {
+        isTaskInputFiles = true
+      }
+
+      gradle.startParameter.projectProperties["testJavaVersion"]?.let { tag(it) }
+      gradle.startParameter.projectProperties["testJavaVM"]?.let { tag(it) }
     }
   }
 }
