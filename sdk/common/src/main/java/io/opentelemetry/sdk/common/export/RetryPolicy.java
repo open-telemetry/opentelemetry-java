bedeed8005280851f380492745823283fcd7e76f
Jack Berg <jberg@newrelic.com>
2023-06-15 13:39:39 -0500

    PR feedback

--- sdk/common/src/main/java/io/opentelemetry/sdk/common/export/RetryPolicy.java
+++ sdk/common/src/main/java/io/opentelemetry/sdk/common/export/RetryPolicy.java
@@ -5,6 +5,8 @@
 
 package io.opentelemetry.sdk.common.export;
 
+import static io.opentelemetry.api.internal.Utils.checkArgument;
+
 import com.google.auto.value.AutoValue;
 import java.time.Duration;
 
@@ -12,7 +14,17 @@ import java.time.Duration;
 @AutoValue
 public abstract class RetryPolicy {
 
-  private static final RetryPolicy DEFAULT = new RetryPolicyBuilder().build();
+  private static final int DEFAULT_MAX_ATTEMPTS = 5;
+
+  @SuppressWarnings("StronglyTypeTime")
+  private static final int DEFAULT_INITIAL_BACKOFF_SECONDS = 1;
+
+  @SuppressWarnings("StronglyTypeTime")
+  private static final int DEFAULT_MAX_BACKOFF_SECONDS = 5;
+
+  private static final double DEFAULT_BACKOFF_MULTIPLIER = 1.5;
+
+  private static final RetryPolicy DEFAULT = RetryPolicy.builder().build();
 
   RetryPolicy() {}
 
@@ -23,7 +35,11 @@ public abstract class RetryPolicy {
 
   /** Returns a new {@link RetryPolicyBuilder} to construct a {@link RetryPolicy}. */
   public static RetryPolicyBuilder builder() {
-    return new RetryPolicyBuilder();
+    return new AutoValue_RetryPolicy.Builder()
+        .setMaxAttempts(DEFAULT_MAX_ATTEMPTS)
+        .setInitialBackoff(Duration.ofSeconds(DEFAULT_INITIAL_BACKOFF_SECONDS))
+        .setMaxBackoff(Duration.ofSeconds(DEFAULT_MAX_BACKOFF_SECONDS))
+        .setBackoffMultiplier(DEFAULT_BACKOFF_MULTIPLIER);
   }
 
   /** Returns the max number of attempts, including the original request. */
@@ -38,8 +54,51 @@ public abstract class RetryPolicy {
   /** Returns the backoff multiplier. */
   public abstract double getBackoffMultiplier();
 
-  static RetryPolicy create(
-      int maxAttempts, Duration initialBackoff, Duration maxBackoff, double backoffMultiplier) {
-    return new AutoValue_RetryPolicy(maxAttempts, initialBackoff, maxBackoff, backoffMultiplier);
+  /** Builder for {@link RetryPolicy}. */
+  @AutoValue.Builder
+  public abstract static class RetryPolicyBuilder {
+
+    RetryPolicyBuilder() {}
+
+    /**
+     * Set the maximum number of attempts, including the original request. Must be greater than 1
+     * and less than 6. Defaults to {@value DEFAULT_MAX_ATTEMPTS}.
+     */
+    public abstract RetryPolicyBuilder setMaxAttempts(int maxAttempts);
+
+    /**
+     * Set the initial backoff. Must be greater than 0. Defaults to {@value
+     * DEFAULT_INITIAL_BACKOFF_SECONDS} seconds.
+     */
+    public abstract RetryPolicyBuilder setInitialBackoff(Duration initialBackoff);
+
+    /**
+     * Set the maximum backoff. Must be greater than 0. Defaults to {@value
+     * DEFAULT_MAX_BACKOFF_SECONDS} seconds.
+     */
+    public abstract RetryPolicyBuilder setMaxBackoff(Duration maxBackoff);
+
+    /**
+     * Set the backoff multiplier. Must be greater than 0.0. Defaults to {@value
+     * DEFAULT_BACKOFF_MULTIPLIER} seconds.
+     */
+    public abstract RetryPolicyBuilder setBackoffMultiplier(double backoffMultiplier);
+
+    abstract RetryPolicy autoBuild();
+
+    /** Build and return a {@link RetryPolicy} with the values of this builder. */
+    public RetryPolicy build() {
+      RetryPolicy retryPolicy = autoBuild();
+      checkArgument(
+          retryPolicy.getMaxAttempts() > 1 && retryPolicy.getMaxAttempts() < 6,
+          "maxAttempts must be greater than 1 and less than 6");
+      checkArgument(
+          retryPolicy.getInitialBackoff().toNanos() > 0, "initialBackoff must be greater than 0");
+      checkArgument(retryPolicy.getMaxBackoff().toNanos() > 0, "maxBackoff must be greater than 0");
+      checkArgument(
+          retryPolicy.getBackoffMultiplier() > 0, "backoffMultiplier must be greater than 0");
+
+      return retryPolicy;
+    }
   }
 }
