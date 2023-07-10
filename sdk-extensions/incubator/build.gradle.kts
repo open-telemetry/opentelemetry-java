9ecc6f08d8de95e3229d55804b62b02f7877b5d8
jack-berg <34418638+jack-berg@users.noreply.github.com>
2023-07-10 17:32:16 -0500

    Delete zpages (#5611)

--- sdk-extensions/incubator/build.gradle.kts
+++ sdk-extensions/incubator/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
   id("otel.java-conventions")
   id("otel.publish-conventions")
 
-  id("otel.jmh-conventions")
   id("otel.animalsniffer-conventions")
 }
 
@@ -14,17 +13,11 @@ otelJava.moduleName.set("io.opentelemetry.sdk.extension.incubator")
 dependencies {
   api(project(":sdk:all"))
 
-  compileOnly(project(":sdk:trace-shaded-deps"))
-
   annotationProcessor("com.google.auto.value:auto-value")
 
   // io.opentelemetry.sdk.extension.incubator.metric.viewconfig
   implementation(project(":sdk-extensions:autoconfigure-spi"))
   implementation("org.snakeyaml:snakeyaml-engine")
-
-  // io.opentelemetry.sdk.extension.trace.zpages
-  implementation(project(":semconv"))
-  compileOnly("com.sun.net.httpserver:http")
 
   testImplementation(project(":sdk:testing"))
   testImplementation(project(":sdk-extensions:autoconfigure"))
