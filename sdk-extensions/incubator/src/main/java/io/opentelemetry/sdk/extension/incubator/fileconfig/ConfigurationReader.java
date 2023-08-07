d08812068d51272614a43f2663ef23a9c04cf52a
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-08-07 16:36:51 -0700

    Switch from snakeyaml to snakeyaml engine

--- sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/ConfigurationReader.java
+++ sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/ConfigurationReader.java
@@ -8,18 +8,20 @@ package io.opentelemetry.sdk.extension.incubator.fileconfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.OpenTelemetryConfiguration;
 import java.io.InputStream;
-import org.yaml.snakeyaml.Yaml;
+import org.snakeyaml.engine.v2.api.Load;
+import org.snakeyaml.engine.v2.api.LoadSettings;
 
 class ConfigurationReader {
 
   private static final ObjectMapper MAPPER = new ObjectMapper();
-  private static final Yaml YAML = new Yaml();
 
   private ConfigurationReader() {}
 
   /** Parse the {@code configuration} YAML and return the {@link OpenTelemetryConfiguration}. */
   static OpenTelemetryConfiguration parse(InputStream configuration) {
-    Object yamlObj = YAML.load(configuration);
+    LoadSettings settings = LoadSettings.builder().build();
+    Load yaml = new Load(settings);
+    Object yamlObj = yaml.loadFromInputStream(configuration);
     return MAPPER.convertValue(yamlObj, OpenTelemetryConfiguration.class);
   }
 }
