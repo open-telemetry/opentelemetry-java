854b537c97d7e547bfedd886cfe69f0acc5325ff
Jack Berg <jberg@newrelic.com>
2023-08-09 15:17:08 -0500

    Initialize file configuration model interpreting

---
+++ sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/LogRecordExporterFactory.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.sdk.extension.incubator.fileconfig;
+
+import static java.util.stream.Collectors.joining;
+
+import io.opentelemetry.sdk.autoconfigure.internal.NamedSpiManager;
+import io.opentelemetry.sdk.autoconfigure.internal.SpiHelper;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigurationException;
+import io.opentelemetry.sdk.autoconfigure.spi.internal.DefaultConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.logs.ConfigurableLogRecordExporterProvider;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.Otlp;
+import io.opentelemetry.sdk.logs.export.LogRecordExporter;
+import java.io.Closeable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Nullable;
+
+final class LogRecordExporterFactory
+    implements Factory<
+        io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.LogRecordExporter,
+        LogRecordExporter> {
+
+  private static final LogRecordExporterFactory INSTANCE = new LogRecordExporterFactory();
+
+  private LogRecordExporterFactory() {}
+
+  static LogRecordExporterFactory getInstance() {
+    return INSTANCE;
+  }
+
+  @Override
+  public LogRecordExporter create(
+      @Nullable
+          io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.LogRecordExporter
+              model,
+      SpiHelper spiHelper,
+      List<Closeable> closeables) {
+    if (model == null) {
+      return LogRecordExporter.composite();
+    }
+
+    if (model.getOtlp() != null) {
+      Otlp otlp = model.getOtlp();
+
+      Map<String, String> properties = new HashMap<>();
+      if (otlp.getProtocol() != null) {
+        properties.put("otel.exporter.otlp.logs.protocol", otlp.getProtocol());
+      }
+      if (otlp.getEndpoint() != null) {
+        properties.put("otel.exporter.otlp.logs.endpoint", otlp.getEndpoint());
+      }
+      if (otlp.getHeaders() != null) {
+        properties.put(
+            "otel.exporter.otlp.logs.headers",
+            otlp.getHeaders().getAdditionalProperties().entrySet().stream()
+                .map(entry -> entry.getKey() + "=" + entry.getValue())
+                .collect(joining(",")));
+      }
+      if (otlp.getCompression() != null) {
+        properties.put("otel.exporter.otlp.logs.compression", otlp.getCompression());
+      }
+      if (otlp.getTimeout() != null) {
+        properties.put("otel.exporter.otlp.logs.timeout", Integer.toString(otlp.getTimeout()));
+      }
+      if (otlp.getCertificate() != null) {
+        properties.put("otel.exporter.otlp.logs.certificate", otlp.getCertificate());
+      }
+      if (otlp.getClientKey() != null) {
+        properties.put("otel.exporter.otlp.logs.client.key", otlp.getClientKey());
+      }
+      if (otlp.getClientCertificate() != null) {
+        properties.put("otel.exporter.otlp.logs.client.certificate", otlp.getClientCertificate());
+      }
+
+      // TODO(jack-berg): add method for creating from map
+      ConfigProperties configProperties = DefaultConfigProperties.createForTest(properties);
+
+      return FileConfigUtil.addAndReturn(
+          closeables,
+          FileConfigUtil.assertNotNull(
+              logRecordExporterSpiManager(configProperties, spiHelper).getByName("otlp"),
+              "otlp exporter"));
+    }
+
+    // TODO(jack-berg): add support for generic SPI exporters
+    if (!model.getAdditionalProperties().isEmpty()) {
+      throw new ConfigurationException(
+          "Unrecognized log record exporter(s): "
+              + model.getAdditionalProperties().keySet().stream().collect(joining(",", "[", "]")));
+    }
+
+    return LogRecordExporter.composite();
+  }
+
+  private static NamedSpiManager<LogRecordExporter> logRecordExporterSpiManager(
+      ConfigProperties config, SpiHelper spiHelper) {
+    return spiHelper.loadConfigurable(
+        ConfigurableLogRecordExporterProvider.class,
+        ConfigurableLogRecordExporterProvider::getName,
+        ConfigurableLogRecordExporterProvider::createExporter,
+        config);
+  }
+}
---
+++ sdk-extensions/incubator/src/test/java/io/opentelemetry/sdk/extension/incubator/fileconfig/OpenTelemetryConfigurationFactoryTest.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.sdk.extension.incubator.fileconfig;
+
+import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
+import io.opentelemetry.internal.testing.CleanupExtension;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.internal.SpiHelper;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigurationException;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.LogRecordExporter;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.LogRecordLimits;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.LogRecordProcessor;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.LoggerProvider;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.OpenTelemetryConfiguration;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.Otlp;
+import io.opentelemetry.sdk.logs.LogLimits;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import java.io.Closeable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+class OpenTelemetryConfigurationFactoryTest {
+
+  @RegisterExtension CleanupExtension cleanup = new CleanupExtension();
+
+  private final SpiHelper spiHelper =
+      SpiHelper.create(OpenTelemetryConfigurationFactoryTest.class.getClassLoader());
+
+  @Test
+  void create_Null() {
+    List<Closeable> closeables = new ArrayList<>();
+    OpenTelemetrySdk expectedSdk = OpenTelemetrySdk.builder().build();
+    cleanup.addCloseable(expectedSdk);
+
+    OpenTelemetrySdk sdk =
+        OpenTelemetryConfigurationFactory.getInstance().create(null, spiHelper, closeables);
+    cleanup.addCloseable(sdk);
+    cleanup.addCloseables(closeables);
+
+    assertThat(sdk.toString()).isEqualTo(expectedSdk.toString());
+  }
+
+  @Test
+  void create_InvalidFileFormat() {
+    List<Closeable> closeables = new ArrayList<>();
+    assertThatThrownBy(
+            () ->
+                OpenTelemetryConfigurationFactory.getInstance()
+                    .create(new OpenTelemetryConfiguration(), spiHelper, closeables))
+        .isInstanceOf(ConfigurationException.class)
+        .hasMessage("Unsupported file format. Supported formats include: 0.1");
+    cleanup.addCloseables(closeables);
+
+    assertThatThrownBy(
+            () ->
+                OpenTelemetryConfigurationFactory.getInstance()
+                    .create(
+                        new OpenTelemetryConfiguration().withFileFormat("1"),
+                        spiHelper,
+                        closeables))
+        .isInstanceOf(ConfigurationException.class)
+        .hasMessage("Unsupported file format. Supported formats include: 0.1");
+    cleanup.addCloseables(closeables);
+  }
+
+  @Test
+  void create_Defaults() {
+    List<Closeable> closeables = new ArrayList<>();
+    OpenTelemetrySdk expectedSdk = OpenTelemetrySdk.builder().build();
+    cleanup.addCloseable(expectedSdk);
+
+    OpenTelemetrySdk sdk =
+        OpenTelemetryConfigurationFactory.getInstance()
+            .create(new OpenTelemetryConfiguration().withFileFormat("0.1"), spiHelper, closeables);
+    cleanup.addCloseable(sdk);
+    cleanup.addCloseables(closeables);
+
+    assertThat(sdk.toString()).isEqualTo(expectedSdk.toString());
+  }
+
+  @Test
+  void create_Configured() {
+    List<Closeable> closeables = new ArrayList<>();
+    OpenTelemetrySdk expectedSdk =
+        OpenTelemetrySdk.builder()
+            .setLoggerProvider(
+                SdkLoggerProvider.builder()
+                    .setLogLimits(
+                        () ->
+                            LogLimits.builder()
+                                .setMaxAttributeValueLength(1)
+                                .setMaxNumberOfAttributes(2)
+                                .build())
+                    .addLogRecordProcessor(
+                        io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor.builder(
+                                OtlpGrpcLogRecordExporter.getDefault())
+                            .build())
+                    .build())
+            .build();
+    cleanup.addCloseable(expectedSdk);
+
+    OpenTelemetrySdk sdk =
+        OpenTelemetryConfigurationFactory.getInstance()
+            .create(
+                new OpenTelemetryConfiguration()
+                    .withFileFormat("0.1")
+                    .withLoggerProvider(
+                        new LoggerProvider()
+                            .withLimits(
+                                new LogRecordLimits()
+                                    .withAttributeValueLengthLimit(1)
+                                    .withAttributeCountLimit(2))
+                            .withProcessors(
+                                Collections.singletonList(
+                                    new LogRecordProcessor()
+                                        .withBatch(
+                                            new BatchLogRecordProcessor()
+                                                .withExporter(
+                                                    new LogRecordExporter()
+                                                        .withOtlp(new Otlp())))))),
+                spiHelper,
+                closeables);
+    cleanup.addCloseable(sdk);
+    cleanup.addCloseables(closeables);
+
+    assertThat(sdk.toString()).isEqualTo(expectedSdk.toString());
+  }
+}
