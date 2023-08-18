d6faa49f15fe1e0376285f9c53c1de42770cce1f
Trask Stalnaker <trask.stalnaker@gmail.com>
2023-08-18 11:10:57 -0700

    Use squareup wire bom

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -42,14 +42,15 @@ repositories {
 }
 
 dependencies {
+  implementation(enforcedPlatform("com.squareup.wire:wire-bom:4.8.0"))
   implementation("com.google.auto.value:auto-value-annotations:1.10.3")
   // When updating, update above in plugins too
   implementation("com.diffplug.spotless:spotless-plugin-gradle:6.20.0")
   // Needed for japicmp but not automatically brought in for some reason.
   implementation("com.google.guava:guava:32.1.2-jre")
   implementation("com.squareup:javapoet:1.13.0")
-  implementation("com.squareup.wire:wire-compiler:4.8.0")
-  implementation("com.squareup.wire:wire-gradle-plugin:4.8.0")
+  implementation("com.squareup.wire:wire-compiler")
+  implementation("com.squareup.wire:wire-gradle-plugin")
   implementation("gradle.plugin.com.google.protobuf:protobuf-gradle-plugin:0.8.18")
   implementation("gradle.plugin.io.morethan.jmhreport:gradle-jmh-report:0.9.0")
   implementation("me.champeau.gradle:japicmp-gradle-plugin:0.4.2")
