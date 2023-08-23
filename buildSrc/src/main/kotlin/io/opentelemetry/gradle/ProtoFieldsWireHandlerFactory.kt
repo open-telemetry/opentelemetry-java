8e677708c13f6f5beb3da6afe5d27b832ce0b65f
Jack Berg <jberg@newrelic.com>
2023-08-23 17:48:02 -0500

    Fix gradle build deprecation warnings

--- buildSrc/src/main/kotlin/io/opentelemetry/gradle/ProtoFieldsWireHandlerFactory.kt
+++ buildSrc/src/main/kotlin/io/opentelemetry/gradle/ProtoFieldsWireHandlerFactory.kt
@@ -3,7 +3,20 @@ package io.opentelemetry.gradle
 import com.squareup.wire.schema.SchemaHandler
 
 class ProtoFieldsWireHandlerFactory : SchemaHandler.Factory{
+  @Deprecated("deprecated in parent")
   override fun create(): SchemaHandler {
     return ProtoFieldsWireHandler()
   }
+
+  override fun create(
+    includes: List<String>,
+    excludes: List<String>,
+    exclusive: Boolean,
+    outDirectory: String,
+    options: Map<String, String>
+  ): SchemaHandler {
+    @Suppress("DEPRECATION")
+    return create()
+  }
+
 }
