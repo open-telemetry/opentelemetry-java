7b33d16f718f0e7edaba9acd171253f24358011c
Jack Berg <jberg@newrelic.com>
2023-09-05 11:30:06 -0500

    Get bom semconv version from dependencyManagement

--- bom-alpha/build.gradle.kts
+++ bom-alpha/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.function.Consumer
+
 plugins {
   id("otel.bom-conventions")
 }
@@ -15,5 +17,10 @@ dependencies {
   // Add dependency on opentelemetry-bom to ensure synchronization between alpha and stable artifacts
   api(platform(project(":bom")))
 
-  otelBom.addExtra("io.opentelemetry.semconv", "opentelemetry-semconv", "1.21.0-alpha")
+  // Get the semconv version from :dependencyManagement
+  val semconvConstraint = project(":dependencyManagement").dependencyProject.configurations["api"].allDependencyConstraints
+    .find { it.group.equals("io.opentelemetry.semconv")
+        && it.name.equals("opentelemetry-semconv") }
+    ?: throw Exception("semconv constraint not found")
+  otelBom.addExtra(semconvConstraint.group, semconvConstraint.name, semconvConstraint.version ?: throw Exception("missing version"))
 }
