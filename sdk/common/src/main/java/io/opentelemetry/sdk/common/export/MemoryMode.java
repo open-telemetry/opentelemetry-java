8cf00ba3ef8bdabd7189a1288c5751b013e2d6ea
Asaf Mesika <asaf.mesika@gmail.com>
2023-09-26 15:10:18 +0300

    style check fixes

--- sdk/common/src/main/java/io/opentelemetry/sdk/common/export/MemoryMode.java
+++ sdk/common/src/main/java/io/opentelemetry/sdk/common/export/MemoryMode.java
@@ -11,16 +11,16 @@ public enum MemoryMode {
   /**
    * Reuses objects to reduce allocations.
    *
-   * <p>In this mode, the SDK reuses objects to reduce allocations, at the expense of disallowing concurrent collections / exports.
-   *
+   * <p>In this mode, the SDK reuses objects to reduce allocations, at the expense of disallowing
+   * concurrent collections / exports.
    */
   REUSABLE_DATA,
 
   /**
    * Uses immutable data structures.
    *
-   * <p>In this mode, the SDK passes immutable objects to exporters / readers, increasing allocations but ensuring safe concurrent exports.
-   *
+   * <p>In this mode, the SDK passes immutable objects to exporters / readers, increasing
+   * allocations but ensuring safe concurrent exports.
    */
   IMMUTABLE_DATA
 }
