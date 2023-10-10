dfe84bafbe2154c5e84c455566ebfe5d84373159
Jack Berg <jberg@newrelic.com>
2023-10-10 14:33:20 -0500

    Update to opentelemetry-configuration v0.1.0

--- sdk-extensions/incubator/src/test/java/io/opentelemetry/sdk/extension/incubator/fileconfig/TextMapPropagatorFactoryTest.java
+++ sdk-extensions/incubator/src/test/java/io/opentelemetry/sdk/extension/incubator/fileconfig/TextMapPropagatorFactoryTest.java
@@ -10,7 +10,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.extension.trace.propagation.B3Propagator;
 import io.opentelemetry.extension.trace.propagation.JaegerPropagator;
@@ -26,50 +25,47 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class PropagatorsFactoryTest {
+class TextMapPropagatorFactoryTest {
 
   private final SpiHelper spiHelper =
-      SpiHelper.create(PropagatorsFactoryTest.class.getClassLoader());
+      SpiHelper.create(TextMapPropagatorFactoryTest.class.getClassLoader());
 
   @ParameterizedTest
   @MethodSource("createArguments")
-  void create(List<String> model, ContextPropagators expectedPropagators) {
-    ContextPropagators propagators =
-        PropagatorsFactory.getInstance().create(model, spiHelper, Collections.emptyList());
+  void create(List<String> model, TextMapPropagator expectedPropagator) {
+    TextMapPropagator propagator =
+        TextMapPropagatorFactory.getInstance().create(model, spiHelper, Collections.emptyList());
 
-    assertThat(propagators.toString()).isEqualTo(expectedPropagators.toString());
+    assertThat(propagator.toString()).isEqualTo(expectedPropagator.toString());
   }
 
   private static Stream<Arguments> createArguments() {
     return Stream.of(
         Arguments.of(
             null,
-            ContextPropagators.create(
-                TextMapPropagator.composite(
-                    W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance()))),
+            TextMapPropagator.composite(
+                W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance())),
         Arguments.of(
             Collections.emptyList(),
-            ContextPropagators.create(
-                TextMapPropagator.composite(
-                    W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance()))),
-        Arguments.of(Collections.singletonList("none"), ContextPropagators.noop()),
+            TextMapPropagator.composite(
+                W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance())),
+        Arguments.of(Collections.singletonList("none"), TextMapPropagator.noop()),
         Arguments.of(
             Arrays.asList("tracecontext", "baggage", "ottrace", "b3multi", "b3", "jaeger"),
-            ContextPropagators.create(
-                TextMapPropagator.composite(
-                    W3CTraceContextPropagator.getInstance(),
-                    W3CBaggagePropagator.getInstance(),
-                    OtTracePropagator.getInstance(),
-                    B3Propagator.injectingMultiHeaders(),
-                    B3Propagator.injectingSingleHeader(),
-                    JaegerPropagator.getInstance()))));
+            TextMapPropagator.composite(
+                W3CTraceContextPropagator.getInstance(),
+                W3CBaggagePropagator.getInstance(),
+                OtTracePropagator.getInstance(),
+                B3Propagator.injectingMultiHeaders(),
+                B3Propagator.injectingSingleHeader(),
+                JaegerPropagator.getInstance())));
   }
 
   @Test
   void create_NoneAndOther() {
     assertThatThrownBy(
             () ->
-                PropagatorsFactory.getInstance()
+                TextMapPropagatorFactory.getInstance()
                     .create(Arrays.asList("none", "foo"), spiHelper, Collections.emptyList()))
         .isInstanceOf(ConfigurationException.class)
         .hasMessage("propagators contains \"none\" along with other propagators");
@@ -79,9 +75,9 @@ class PropagatorsFactoryTest {
   void create_UnknownSpiPropagator() {
     assertThatThrownBy(
             () ->
-                PropagatorsFactory.getInstance()
+                TextMapPropagatorFactory.getInstance()
                     .create(Collections.singletonList("foo"), spiHelper, Collections.emptyList()))
         .isInstanceOf(ConfigurationException.class)
-        .hasMessage("Unrecognized value for otel.propagators: foo");
+        .hasMessage("Unrecognized propagator: foo");
   }
 }
