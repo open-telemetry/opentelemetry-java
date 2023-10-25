2bb40e56fc7a9326794416641e36994ab24abac8
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-10-25 13:47:34 +0200

    Spotless

--- exporters/sender/okhttp/src/test/java/io/opentelemetry/exporter/sender/okhttp/internal/OkHttpGrpcSenderTest.java
+++ exporters/sender/okhttp/src/test/java/io/opentelemetry/exporter/sender/okhttp/internal/OkHttpGrpcSenderTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.exporter.sender.okhttp.internal;
 
 import io.opentelemetry.exporter.internal.grpc.GrpcSender;
