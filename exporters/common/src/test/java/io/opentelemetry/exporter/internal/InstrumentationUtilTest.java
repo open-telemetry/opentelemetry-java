5fe0c06cbfd8cf2c98e0c36fa91798e95763e4a4
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-11-06 14:09:35 +0100

    Updating InstrumentationUtilTest

--- exporters/common/src/test/java/io/opentelemetry/exporter/internal/InstrumentationUtilTest.java
+++ exporters/common/src/test/java/io/opentelemetry/exporter/internal/InstrumentationUtilTest.java
@@ -8,19 +8,20 @@ package io.opentelemetry.exporter.internal;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import io.opentelemetry.context.Context;
 import org.junit.jupiter.api.Test;
 
 class InstrumentationUtilTest {
   @Test
   void verifySuppressInstrumentation() {
     // Should be false by default.
-    assertFalse(InstrumentationUtil.shouldSuppressInstrumentation());
+    assertFalse(InstrumentationUtil.shouldSuppressInstrumentation(Context.current()));
 
     // Should be true inside the Runnable passed to InstrumentationUtil.suppressInstrumentation.
     InstrumentationUtil.suppressInstrumentation(
-        () -> assertTrue(InstrumentationUtil.shouldSuppressInstrumentation()));
+        () -> assertTrue(InstrumentationUtil.shouldSuppressInstrumentation(Context.current())));
 
     // Should be false after the runnable finishes.
-    assertFalse(InstrumentationUtil.shouldSuppressInstrumentation());
+    assertFalse(InstrumentationUtil.shouldSuppressInstrumentation(Context.current()));
   }
 }
