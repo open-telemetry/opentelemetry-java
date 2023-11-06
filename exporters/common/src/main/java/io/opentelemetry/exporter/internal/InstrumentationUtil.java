adff2950d5285ed83592d4a8f738861294816bbd
César <56847527+LikeTheSalad@users.noreply.github.com>
2023-11-06 13:26:54 +0100

    Update exporters/common/src/main/java/io/opentelemetry/exporter/internal/InstrumentationUtil.java
    Co-authored-by: jack-berg <34418638+jack-berg@users.noreply.github.com>

--- exporters/common/src/main/java/io/opentelemetry/exporter/internal/InstrumentationUtil.java
+++ exporters/common/src/main/java/io/opentelemetry/exporter/internal/InstrumentationUtil.java
@@ -13,7 +13,7 @@ import java.util.Objects;
  * This class is internal and is hence not for public use. Its APIs are unstable and can change at
  * any time.
  */
-public class InstrumentationUtil {
+public final class InstrumentationUtil {
   private static final ContextKey<Boolean> SUPPRESS_INSTRUMENTATION_KEY =
       ContextKey.named("suppress_instrumentation");
 
