de228534bce6e70777fc2d693c9947e9a2f435d4
Peter Findeisen <pfindeis@cisco.com>
2023-12-04 15:41:46 -0800

    Updating the javadocs.

--- api/all/src/main/java/io/opentelemetry/api/trace/Span.java
+++ api/all/src/main/java/io/opentelemetry/api/trace/Span.java
@@ -74,7 +74,7 @@ public interface Span extends ImplicitContextKeyed {
   /**
    * Returns a non-recording {@link Span} that holds the provided {@link SpanContext} but has no
    * functionality. It will not be exported and all tracing operations are no-op, but it can be used
-   * to propagate a valid {@link SpanContext} downstream.
+   * to propagate a {@link SpanContext} downstream.
    */
   static Span wrap(SpanContext spanContext) {
     if (spanContext == null) {
