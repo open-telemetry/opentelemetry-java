0526ca7fa87df26792b3c396cdce551d4f73e86a
Jack Berg <jberg@newrelic.com>
2024-01-02 17:22:56 -0600

    Read normalized otel.config.file property

--- sdk-extensions/autoconfigure/src/main/java/io/opentelemetry/sdk/autoconfigure/AutoConfiguredOpenTelemetrySdkBuilder.java
+++ sdk-extensions/autoconfigure/src/main/java/io/opentelemetry/sdk/autoconfigure/AutoConfiguredOpenTelemetrySdkBuilder.java
@@ -479,10 +479,11 @@ public final class AutoConfiguredOpenTelemetrySdkBuilder implements AutoConfigur
 
   @Nullable
   private static AutoConfiguredOpenTelemetrySdk maybeConfigureFromFile(ConfigProperties config) {
-    String configurationFile = config.getString("OTEL_CONFIG_FILE");
+    String configurationFile = config.getString("otel.config.file");
     if (configurationFile == null || configurationFile.isEmpty()) {
       return null;
     }
+    logger.info("Autoconfiguring from configuration file: " + configurationFile);
     FileInputStream fis;
     try {
       fis = new FileInputStream(configurationFile);
