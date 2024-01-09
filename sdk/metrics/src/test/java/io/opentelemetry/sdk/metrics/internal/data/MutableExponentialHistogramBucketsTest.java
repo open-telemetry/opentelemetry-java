95690f1729fbbf6452a872d27473057ff790c098
Asaf Mesika <asaf.mesika@gmail.com>
2024-01-09 15:42:26 +0200

    Added sanity unit testing 2 data classes just to pass code coverage

---
+++ sdk/metrics/src/test/java/io/opentelemetry/sdk/metrics/internal/data/MutableExponentialHistogramBucketsTest.java
@@ -0,0 +1,35 @@
+package io.opentelemetry.sdk.metrics.internal.data;
+
+import io.opentelemetry.sdk.internal.DynamicPrimitiveLongList;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MutableExponentialHistogramBucketsTest {
+
+  @Test
+  void testSanity() {
+    MutableExponentialHistogramBuckets buckets = new MutableExponentialHistogramBuckets();
+    assertThat(buckets.getScale()).isEqualTo(0);
+    assertThat(buckets.getOffset()).isEqualTo(0);
+    assertThat(buckets.getTotalCount()).isEqualTo(0);
+    assertThat(buckets.getBucketCounts()).isEmpty();
+    assertThat(buckets.getReusableBucketCountsList()).isEmpty();
+
+    DynamicPrimitiveLongList bucketCounts = DynamicPrimitiveLongList.of(1,2,3);
+    buckets.set(1, 2, 3, bucketCounts);
+
+    assertThat(buckets.getScale()).isEqualTo(1);
+    assertThat(buckets.getOffset()).isEqualTo(2);
+    assertThat(buckets.getTotalCount()).isEqualTo(3);
+    assertThat(buckets.getBucketCounts()).containsExactly(1L, 2L, 3L);
+    assertThat(buckets.getReusableBucketCountsList()).containsExactly(1L, 2L, 3L);
+
+    MutableExponentialHistogramBuckets sameBuckets = new MutableExponentialHistogramBuckets();
+    sameBuckets.set(1, 2, 3, DynamicPrimitiveLongList.of(1,2,3));
+    assertThat(sameBuckets).isEqualTo(buckets);
+    assertThat(sameBuckets.hashCode()).isEqualTo(buckets.hashCode());
+
+    assertThat(buckets.toString()).isEqualTo("MutableExponentialHistogramBuckets{scale=1, offset=2, bucketCounts=[1, 2, 3], totalCount=3}");
+  }
+}
