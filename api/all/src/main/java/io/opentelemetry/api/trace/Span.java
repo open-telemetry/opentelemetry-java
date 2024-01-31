820ecc506b7f1891b2e473f8496e9f58f4699f75
jack-berg <34418638+jack-berg@users.noreply.github.com>
2024-01-31 12:22:26 -0600

    Apply suggestions from code review
    Co-authored-by: John Watson <jkwatson@gmail.com>

--- api/all/src/main/java/io/opentelemetry/api/trace/Span.java
+++ api/all/src/main/java/io/opentelemetry/api/trace/Span.java
@@ -360,7 +360,7 @@ public interface Span extends ImplicitContextKeyed {
   Span updateName(String name);
 
   /**
-   * Adds a link to the newly created {@code Span}.
+   * Adds a link to this {@code Span}.
    *
    * <p>Links are used to link {@link Span}s in different traces. Used (for example) in batching
    * operations, where a single batch handler processes multiple requests from different traces or
@@ -380,7 +380,7 @@ public interface Span extends ImplicitContextKeyed {
   }
 
   /**
-   * Adds a link to the newly created {@code Span}.
+   * Adds a link to this {@code Span}.
    *
    * <p>Links are used to link {@link Span}s in different traces. Used (for example) in batching
    * operations, where a single batch handler processes multiple requests from different traces or
