6d62b6b0531754a042dda7747f81852788abc30d
Austin Pederson <austin.pederson@smartsheet.com>
2024-02-07 15:07:50 -0800

    fix issue with setting StatusCode.OK

--- sdk/trace/src/main/java/io/opentelemetry/sdk/trace/SdkSpan.java
+++ sdk/trace/src/main/java/io/opentelemetry/sdk/trace/SdkSpan.java
@@ -395,7 +395,7 @@ final class SdkSpan implements ReadWriteSpan, ExtendedSpan {
         logger.log(Level.FINE, "Calling setStatus() on an ended Span.");
         return this;
       }
-      if (statusCode == StatusCode.OK) {
+      if (this.status.getStatusCode() == StatusCode.OK) {
         return this;
       }
       this.status = StatusData.create(statusCode, description);
