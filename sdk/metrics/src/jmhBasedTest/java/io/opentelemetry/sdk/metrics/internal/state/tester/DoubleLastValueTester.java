7dec3a9100491eaa4a8100b6221fb8422197b755
Asaf Mesika <asaf.mesika@gmail.com>
2024-02-08 16:14:21 +0200

    PR fixes

--- sdk/metrics/src/jmhBasedTest/java/io/opentelemetry/sdk/metrics/internal/state/tester/DoubleLastValueTester.java
+++ sdk/metrics/src/jmhBasedTest/java/io/opentelemetry/sdk/metrics/internal/state/tester/DoubleLastValueTester.java
@@ -6,7 +6,6 @@
 package io.opentelemetry.sdk.metrics.internal.state.tester;
 
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.metrics.DoubleCounter;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.sdk.metrics.Aggregation;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
@@ -15,45 +14,37 @@ import java.util.List;
 import java.util.Random;
 
 public class DoubleLastValueTester implements TestInstrumentType.InstrumentTester {
-  private static final int measurementsPerAttributeSet = 1_000;
-
-  static class DoubleLastValueState implements TestInstrumentType.TestInstrumentsState {
-    DoubleCounter doubleCounter;
-  }
 
   @Override
   public Aggregation testedAggregation() {
     return Aggregation.lastValue();
   }
 
+  @SuppressWarnings("ForLoopReplaceableByForEach") // This is for GC sensitivity testing: no streams
   @Override
   public TestInstrumentType.TestInstrumentsState buildInstruments(
       double instrumentCount,
       SdkMeterProvider sdkMeterProvider,
       List<Attributes> attributesList,
       Random random) {
-    DoubleLastValueState doubleLastValueState = new DoubleLastValueState();
-
     Meter meter = sdkMeterProvider.meterBuilder("meter").build();
-    doubleLastValueState.doubleCounter =
-        meter.counterBuilder("test.double.last.value").ofDoubles().build();
+    meter
+        .gaugeBuilder("test.double.last.value")
+        .buildWithCallback(
+            observableDoubleMeasurement -> {
+              for (int j = 0; j < attributesList.size(); j++) {
+                observableDoubleMeasurement.record(1.2f, attributesList.get(j));
+              }
+            });
 
-    return doubleLastValueState;
+    return new TestInstrumentType.EmptyInstrumentsState();
   }
 
-  @SuppressWarnings("ForLoopReplaceableByForEach") // This is for GC sensitivity testing: no streams
   @Override
   public void recordValuesInInstruments(
       TestInstrumentType.TestInstrumentsState testInstrumentsState,
       List<Attributes> attributesList,
       Random random) {
-    DoubleLastValueState state = (DoubleLastValueState) testInstrumentsState;
-
-    for (int j = 0; j < attributesList.size(); j++) {
-      Attributes attributes = attributesList.get(j);
-      for (int i = 0; i < measurementsPerAttributeSet; i++) {
-        state.doubleCounter.add(1.2f, attributes);
-      }
-    }
+    // Recording is done by the callback define above
   }
 }
--- sdk/metrics/src/jmhBasedTest/java/io/opentelemetry/sdk/metrics/internal/state/tester/LongLastValueTester.java
+++ sdk/metrics/src/jmhBasedTest/java/io/opentelemetry/sdk/metrics/internal/state/tester/LongLastValueTester.java
@@ -6,7 +6,6 @@
 package io.opentelemetry.sdk.metrics.internal.state.tester;
 
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.sdk.metrics.Aggregation;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
@@ -15,44 +14,38 @@ import java.util.List;
 import java.util.Random;
 
 public class LongLastValueTester implements TestInstrumentType.InstrumentTester {
-  private static final int measurementsPerAttributeSet = 1_000;
-
-  static class LongLastValueState implements TestInstrumentType.TestInstrumentsState {
-    LongCounter longCounter;
-  }
 
   @Override
   public Aggregation testedAggregation() {
     return Aggregation.lastValue();
   }
 
+  @SuppressWarnings({"ForLoopReplaceableByForEach", "resource"})
   @Override
   public TestInstrumentType.TestInstrumentsState buildInstruments(
       double instrumentCount,
       SdkMeterProvider sdkMeterProvider,
       List<Attributes> attributesList,
       Random random) {
-    LongLastValueState longLastValueState = new LongLastValueState();
-
     Meter meter = sdkMeterProvider.meterBuilder("meter").build();
-    longLastValueState.longCounter = meter.counterBuilder("test.long.last.value").build();
+    meter
+        .gaugeBuilder("test.long.last.value")
+        .ofLongs()
+        .buildWithCallback(
+            observableLongMeasurement -> {
+              for (int j = 0; j < attributesList.size(); j++) {
+                observableLongMeasurement.record(1, attributesList.get(j));
+              }
+            });
 
-    return longLastValueState;
+    return new TestInstrumentType.EmptyInstrumentsState();
   }
 
-  @SuppressWarnings("ForLoopReplaceableByForEach") // This is for GC sensitivity testing: no streams
   @Override
   public void recordValuesInInstruments(
       TestInstrumentType.TestInstrumentsState testInstrumentsState,
       List<Attributes> attributesList,
       Random random) {
-    LongLastValueState state = (LongLastValueState) testInstrumentsState;
-
-    for (int j = 0; j < attributesList.size(); j++) {
-      Attributes attributes = attributesList.get(j);
-      for (int i = 0; i < measurementsPerAttributeSet; i++) {
-        state.longCounter.add(1, attributes);
-      }
-    }
+    // Recording is done by the callback define above
   }
 }
