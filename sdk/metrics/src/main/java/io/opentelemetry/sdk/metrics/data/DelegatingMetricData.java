61c2aa620dbbe541a3396644e2e31948a7e127d9
Thomas Jaszczult <ttjazztt@gmail.com>
2024-02-10 22:54:19 -0800

    toString and equals

--- sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/data/DelegatingMetricData.java
+++ sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/data/DelegatingMetricData.java
@@ -7,43 +7,75 @@ import static java.util.Objects.requireNonNull;
 
 public abstract class DelegatingMetricData implements MetricData {
 
-    private final MetricData delegate;
+  private final MetricData delegate;
 
-    protected DelegatingMetricData(MetricData delegate) {
-      this.delegate = requireNonNull(delegate, "delegate");
-    }
+  protected DelegatingMetricData(MetricData delegate) {
+    this.delegate = requireNonNull(delegate, "delegate");
+  }
 
-    @Override
-    public Resource getResource() {
-      return delegate.getResource();
-    }
-    @Override
-    public InstrumentationScopeInfo getInstrumentationScopeInfo() {
-      return delegate.getInstrumentationScopeInfo();
-    }
+  @Override
+  public Resource getResource() {
+    return delegate.getResource();
+  }
+  @Override
+  public InstrumentationScopeInfo getInstrumentationScopeInfo() {
+    return delegate.getInstrumentationScopeInfo();
+  }
 
-    @Override
-    public String getName() {
-      return delegate.getName();
-    }
+  @Override
+  public String getName() {
+    return delegate.getName();
+  }
 
-    @Override
-    public String getDescription() {
-      return delegate.getDescription();
-    }
+  @Override
+  public String getDescription() {
+    return delegate.getDescription();
+  }
 
-    @Override
-    public String getUnit() {
-      return delegate.getUnit();
-    }
+  @Override
+  public String getUnit() {
+    return delegate.getUnit();
+  }
 
-    @Override
-    public MetricDataType getType() {
-      return delegate.getType();
-    }
+  @Override
+  public MetricDataType getType() {
+    return delegate.getType();
+  }
 
-    @Override
-    public Data<?> getData() {
-      return delegate.getData();
+  @Override
+  public Data<?> getData() {
+    return delegate.getData();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
     }
+    if (o instanceof DelegatingMetricData) {
+      DelegatingMetricData that = (DelegatingMetricData) o;
+      return this.delegate.equals(that.delegate) &&
+          this.getResource().equals(that.getResource()) &&
+          this.getInstrumentationScopeInfo().equals(that.getInstrumentationScopeInfo()) &&
+          this.getName().equals(that.getName()) &&
+          this.getDescription().equals(that.getDescription()) &&
+          this.getUnit().equals(that.getUnit()) &&
+          this.getType().equals(that.getType()) &&
+          this.getData().equals(that.getData());
+    }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return "DelegatingMetricData{" +
+        "resource=" + getResource() + ", " +
+        "instrumentationScopeInfo=" + getInstrumentationScopeInfo() + ", " +
+        "name=" + getName() + ", " +
+        "description=" + getDescription() + ", " +
+        "unit=" + getUnit() + ", " +
+        "type=" + getType() + ", " +
+        "data=" + getData() +
+        "}";
+  }
 }
