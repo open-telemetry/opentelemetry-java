7b112ea13e4fbe0b044a5a8bddbd89d68ff4c972
Asaf Mesika <asaf.mesika@gmail.com>
2024-02-11 16:36:32 +0200

    Fixes #6211 - Adds more leniency towards tests has less options to "mess" them and lower the allocation rate and are more prone to have variable allocation rate. - Constraints the InstrumentGarbageCollectionBenchmarkTest to run only in one specific environment out of the entire matrix

--- sdk/metrics/src/jmhBasedTest/java/io/opentelemetry/sdk/metrics/internal/state/TestInstrumentType.java
+++ sdk/metrics/src/jmhBasedTest/java/io/opentelemetry/sdk/metrics/internal/state/TestInstrumentType.java
@@ -24,29 +24,46 @@ public enum TestInstrumentType {
   ASYNC_COUNTER(AsyncCounterTester::new),
   EXPONENTIAL_HISTOGRAM(ExponentialHistogramTester::new),
   EXPLICIT_BUCKET(ExplicitBucketHistogramTester::new),
-  LONG_SUM(LongSumTester::new, /* dataAllocRateReductionPercentage= */ 97.3f),
-  DOUBLE_SUM(DoubleSumTester::new, /* dataAllocRateReductionPercentage= */ 97.3f),
-  LONG_LAST_VALUE(LongLastValueTester::new, /* dataAllocRateReductionPercentage= */ 97.3f),
-  DOUBLE_LAST_VALUE(DoubleLastValueTester::new, /* dataAllocRateReductionPercentage= */ 97.3f);
+  LONG_SUM(LongSumTester::new,
+      /* dataAllocRateReductionPercentage= */ 97.3f,
+      /* allowedPercentOffset= */ 4.0f),
+  DOUBLE_SUM(DoubleSumTester::new,
+      /* dataAllocRateReductionPercentage= */ 97.3f,
+      /* allowedPercentOffset= */ 2.0f),
+  LONG_LAST_VALUE(LongLastValueTester::new,
+      /* dataAllocRateReductionPercentage= */ 97.3f,
+      /* allowedPercentOffset= */ 4.0f),
+  DOUBLE_LAST_VALUE(DoubleLastValueTester::new,
+      /* dataAllocRateReductionPercentage= */ 97.3f,
+      /* allowedPercentOffset= */ 4.0f);
 
   private final Supplier<? extends InstrumentTester> instrumentTesterInitializer;
   private final float dataAllocRateReductionPercentage;
+  private final double allowedPercentOffset;
 
+  @SuppressWarnings("unused")
   TestInstrumentType(Supplier<? extends InstrumentTester> instrumentTesterInitializer) {
     this.dataAllocRateReductionPercentage = 99.8f; // default
     this.instrumentTesterInitializer = instrumentTesterInitializer;
+    this.allowedPercentOffset = 2.0f;
   }
 
   // Some instruments have different reduction percentage.
   TestInstrumentType(
       Supplier<? extends InstrumentTester> instrumentTesterInitializer,
-      float dataAllocRateReductionPercentage) {
+      float dataAllocRateReductionPercentage,
+      float allowedPercentOffset) {
     this.instrumentTesterInitializer = instrumentTesterInitializer;
     this.dataAllocRateReductionPercentage = dataAllocRateReductionPercentage;
+    this.allowedPercentOffset = allowedPercentOffset;
   }
 
   float getDataAllocRateReductionPercentage() {
     return dataAllocRateReductionPercentage;
+  }
+
+  public double getAllowedPercentOffset() {
+    return allowedPercentOffset;
   }
 
   InstrumentTester createInstrumentTester() {
