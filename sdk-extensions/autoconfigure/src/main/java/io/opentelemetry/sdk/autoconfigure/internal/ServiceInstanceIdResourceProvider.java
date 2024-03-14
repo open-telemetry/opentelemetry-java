35bcc8808f7dcb3f2db89a2d72351eceaa25ec5b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-03-14 08:26:13 +0100

    PR review

--- sdk-extensions/autoconfigure/src/main/java/io/opentelemetry/sdk/autoconfigure/internal/ServiceInstanceIdResourceProvider.java
+++ sdk-extensions/autoconfigure/src/main/java/io/opentelemetry/sdk/autoconfigure/internal/ServiceInstanceIdResourceProvider.java
@@ -23,8 +23,10 @@ public final class ServiceInstanceIdResourceProvider implements ConditionalResou
       AttributeKey.stringKey("service.instance.id");
 
   // multiple calls to this resource provider should return the same value
-  public static final Resource RANDOM =
+  private static final Resource RANDOM =
       Resource.create(Attributes.of(SERVICE_INSTANCE_ID, UUID.randomUUID().toString()));
+
+  static final int ORDER = Integer.MAX_VALUE;
 
   @Override
   public Resource createResource(ConfigProperties config) {
@@ -40,6 +42,6 @@ public final class ServiceInstanceIdResourceProvider implements ConditionalResou
   public int order() {
     // Run after environment resource provider - only set the service instance ID if it
     // hasn't been set by any other provider.
-    return Integer.MAX_VALUE;
+    return ORDER;
   }
 }
