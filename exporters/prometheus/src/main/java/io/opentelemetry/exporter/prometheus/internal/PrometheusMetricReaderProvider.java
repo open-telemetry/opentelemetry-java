46fdadc2a409477efbb0ec2e2594e6561991667b
Abhishek Kumar <abhishekkr3003@gmail.com>
2024-06-27 16:29:18 +0530

    adds config to enable default exponential histogram

--- exporters/prometheus/src/main/java/io/opentelemetry/exporter/prometheus/internal/PrometheusMetricReaderProvider.java
+++ exporters/prometheus/src/main/java/io/opentelemetry/exporter/prometheus/internal/PrometheusMetricReaderProvider.java
@@ -5,12 +5,19 @@
 
 package io.opentelemetry.exporter.prometheus.internal;
 
+import static io.opentelemetry.sdk.metrics.Aggregation.explicitBucketHistogram;
+
 import io.opentelemetry.exporter.internal.ExporterBuilderUtil;
 import io.opentelemetry.exporter.prometheus.PrometheusHttpServer;
 import io.opentelemetry.exporter.prometheus.PrometheusHttpServerBuilder;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigurationException;
 import io.opentelemetry.sdk.autoconfigure.spi.internal.ConfigurableMetricReaderProvider;
+import io.opentelemetry.sdk.metrics.Aggregation;
+import io.opentelemetry.sdk.metrics.InstrumentType;
+import io.opentelemetry.sdk.metrics.export.DefaultAggregationSelector;
 import io.opentelemetry.sdk.metrics.export.MetricReader;
+import io.opentelemetry.sdk.metrics.internal.aggregator.AggregationUtil;
 
 /**
  * SPI implementation for {@link PrometheusHttpServer}.
@@ -31,6 +38,20 @@ public class PrometheusMetricReaderProvider implements ConfigurableMetricReaderP
     String host = config.getString("otel.exporter.prometheus.host");
     if (host != null) {
       prometheusBuilder.setHost(host);
+    }
+    String defaultHistogramAggregation =
+        config.getString("otel.exporter.prometheus.metrics.default.histogram.aggregation");
+    if (defaultHistogramAggregation != null) {
+      if (AggregationUtil.aggregationName(Aggregation.base2ExponentialBucketHistogram())
+          .equalsIgnoreCase(defaultHistogramAggregation)) {
+        prometheusBuilder.setDefaultAggregationSelector(
+            DefaultAggregationSelector.getDefault()
+                .with(InstrumentType.HISTOGRAM, Aggregation.base2ExponentialBucketHistogram()));
+      } else if (!AggregationUtil.aggregationName(explicitBucketHistogram())
+          .equalsIgnoreCase(defaultHistogramAggregation)) {
+        throw new ConfigurationException(
+            "Unrecognized default histogram aggregation: " + defaultHistogramAggregation);
+      }
     }
 
     ExporterBuilderUtil.configureExporterMemoryMode(config, prometheusBuilder::setMemoryMode);
