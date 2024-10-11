b6badb2ca39063869ef8b7bcb219468c830ab5d7
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2024-10-11 16:33:11 +0200

    Adds Baggage.getEntry(String key) (#6765)
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    Co-authored-by: Jack Berg <jberg@newrelic.com>

--- api/all/src/test/java/io/opentelemetry/api/baggage/BaggageTest.java
+++ api/all/src/test/java/io/opentelemetry/api/baggage/BaggageTest.java
@@ -9,6 +9,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import javax.annotation.Nullable;
 import org.junit.jupiter.api.Test;
 
 class BaggageTest {
@@ -26,5 +30,46 @@ class BaggageTest {
       Baggage result = Baggage.current();
       assertThat(result.getEntryValue("foo")).isEqualTo("bar");
     }
+  }
+
+  @Test
+  void getEntryDefault() {
+    BaggageEntryMetadata metadata = BaggageEntryMetadata.create("flib");
+    Map<String, BaggageEntry> result = new HashMap<>();
+    result.put("a", ImmutableEntry.create("b", metadata));
+    // Implementation that only implements asMap() which is used by getEntry()
+    Baggage baggage =
+        new Baggage() {
+
+          @Override
+          public Map<String, BaggageEntry> asMap() {
+            return result;
+          }
+
+          @Override
+          public int size() {
+            return 0;
+          }
+
+          @Override
+          public void forEach(BiConsumer<? super String, ? super BaggageEntry> consumer) {
+            result.forEach(consumer);
+          }
+
+          @Nullable
+          @Override
+          public String getEntryValue(String entryKey) {
+            return null;
+          }
+
+          @Override
+          public BaggageBuilder toBuilder() {
+            return null;
+          }
+        };
+
+    BaggageEntry entry = baggage.getEntry("a");
+    assertThat(entry.getValue()).isEqualTo("b");
+    assertThat(entry.getMetadata().getValue()).isEqualTo("flib");
   }
 }
