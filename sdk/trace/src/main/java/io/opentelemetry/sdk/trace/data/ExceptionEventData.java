9700e0124980bdd2ed92a2f2cdfa8a074eab1553
Teja <vasant.teja@gmail.com>
2024-10-21 21:33:37 -0400

    polish: Update the comment around the class.
    Co-authored-by: jack-berg <34418638+jack-berg@users.noreply.github.com>

--- sdk/trace/src/main/java/io/opentelemetry/sdk/trace/data/ExceptionEventData.java
+++ sdk/trace/src/main/java/io/opentelemetry/sdk/trace/data/ExceptionEventData.java
@@ -8,7 +8,10 @@ package io.opentelemetry.sdk.trace.data;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.sdk.trace.SpanLimits;
 
+import javax.annotation.concurrent.Immutable;
+
 /** Data representation of an event for a recorded exception. */
+@Immutable
 public interface ExceptionEventData extends EventData {
 
   /**
