aa731c7544b80c7ee9b27a3ed6b9d8f73088d231
Victor <Zeusvictor88@gmail.com>
2024-10-25 22:47:35 +0100

    fix comments

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -22,20 +22,24 @@ fun hasLauncherForJavaVersion(version: Int): Boolean {
 }
 
 spotless {
-    kotlinGradle {
-        ktlint().editorConfigOverride(mapOf(
-            "indent_size" to "2",
-            "continuation_indent_size" to "2",
-            "max_line_length" to "160",
-            "insert_final_newline" to "true",
-            "ktlint_standard_no-wildcard-imports" to "disabled",
-            "ktlint_standard_max-line-length" to "disabled",
-            "ktlint_standard_trailing-comma-on-call-site" to "disabled",
-            "ktlint_standard_trailing-comma-on-declaration-site" to "disabled",
-            "ktlint_standard_wrapping" to "disabled"
-        ))
-        target("**/*.gradle.kts")
-    }
+kotlinGradle {
+    ktlint().editorConfigOverride(mapOf(
+      "indent_size" to "2",
+      "continuation_indent_size" to "2",
+      "max_line_length" to "160",
+      "insert_final_newline" to "true",
+      "ktlint_standard_no-wildcard-imports" to "disabled",
+      // ktlint does not break up long lines, it just fails on them
+      "ktlint_standard_max-line-length" to "disabled",
+      // ktlint makes it *very* hard to locate where this actually happened
+      "ktlint_standard_trailing-comma-on-call-site" to "disabled",
+      // depends on ktlint_standard_wrapping
+      "ktlint_standard_trailing-comma-on-declaration-site" to "disabled",
+      // also very hard to find out where this happens
+      "ktlint_standard_wrapping" to "disabled"
+    ))
+    target("**/*.gradle.kts")
+  }
 }
 
 repositories {
