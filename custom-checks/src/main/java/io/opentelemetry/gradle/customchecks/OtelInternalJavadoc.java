7ade0f6ab5e5f2b45742eadd684d45f5ee6761ce
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-11-15 18:11:13 -0700

    spotlessApply

--- custom-checks/src/main/java/io/opentelemetry/gradle/customchecks/OtelInternalJavadoc.java
+++ custom-checks/src/main/java/io/opentelemetry/gradle/customchecks/OtelInternalJavadoc.java
@@ -22,7 +22,8 @@ import javax.lang.model.element.Modifier;
 @BugPattern(
     summary =
         "This public internal class doesn't end with any of the applicable javadoc disclaimers: \""
-            + OtelInternalJavadoc.EXPECTED_INTERNAL_COMMENT_V1 + "\", or \""
+            + OtelInternalJavadoc.EXPECTED_INTERNAL_COMMENT_V1
+            + "\", or \""
             + OtelInternalJavadoc.EXPECTED_INTERNAL_COMMENT_V2
             + "\"",
     severity = WARNING)
