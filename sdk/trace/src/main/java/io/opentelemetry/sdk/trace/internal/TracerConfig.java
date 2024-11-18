26f359b3e9a0b9c3c98d7b48aea35db4b5ede90a
Ravishankar Nagendran <ravishankar.nagendran@gmail.com>
2024-11-18 13:59:28 -0700

    Add javadoc boilerplate internal comment v2 for experimental classes (#6886)
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- sdk/trace/src/main/java/io/opentelemetry/sdk/trace/internal/TracerConfig.java
+++ sdk/trace/src/main/java/io/opentelemetry/sdk/trace/internal/TracerConfig.java
@@ -17,8 +17,9 @@ import javax.annotation.concurrent.Immutable;
 /**
  * A collection of configuration options which define the behavior of a {@link Tracer}.
  *
- * <p>This class is internal and is hence not for public use. Its APIs are unstable and can change
- * at any time.
+ * <p>This class is internal and experimental. Its APIs are unstable and can change at any time. Its
+ * APIs (or a version of them) may be promoted to the public stable API in the future, but no
+ * guarantees are made.
  *
  * @see SdkTracerProviderUtil#setTracerConfigurator(SdkTracerProviderBuilder, ScopeConfigurator)
  * @see SdkTracerProviderUtil#addTracerConfiguratorCondition(SdkTracerProviderBuilder, Predicate,
