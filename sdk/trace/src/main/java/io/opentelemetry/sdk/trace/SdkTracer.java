6f68542bd47aa516436040cca8bd73aff06291ee
jackshirazi <jacks@fasterj.com>
2024-11-21 16:52:19 +0000

    add non-volatile comment

--- sdk/trace/src/main/java/io/opentelemetry/sdk/trace/SdkTracer.java
+++ sdk/trace/src/main/java/io/opentelemetry/sdk/trace/SdkTracer.java
@@ -19,6 +19,8 @@ final class SdkTracer implements ExtendedTracer {
 
   private final TracerSharedState sharedState;
   private final InstrumentationScopeInfo instrumentationScopeInfo;
+  // deliberately not volatile because of performance concerns
+  // - which means its eventually consistent
   private boolean tracerEnabled;
 
   SdkTracer(
