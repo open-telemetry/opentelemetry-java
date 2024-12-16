7ba8e32bf83e1e54cf95dd6c097341fb19516c64
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-12-15 17:41:41 -0800

    Some more convenience methods

--- api/all/src/main/java/io/opentelemetry/api/common/ValueBuilder.java
+++ api/all/src/main/java/io/opentelemetry/api/common/ValueBuilder.java
@@ -5,6 +5,12 @@
 
 package io.opentelemetry.api.common;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public interface ValueBuilder {
 
   Value<?> build();
@@ -28,6 +34,30 @@ public interface ValueBuilder {
 
   default ValueBuilder put(String key, boolean value) {
     put(key, Value.of(value));
+    return this;
+  }
+
+  default ValueBuilder put(String key, String... value) {
+    put(key, Value.of(Arrays.stream(value).map(Value::of).collect(toList())));
+    return this;
+  }
+
+  default ValueBuilder put(String key, long... value) {
+    put(key, Value.of(Arrays.stream(value).mapToObj(Value::of).collect(toList())));
+    return this;
+  }
+
+  default ValueBuilder put(String key, double... value) {
+    put(key, Value.of(Arrays.stream(value).mapToObj(Value::of).collect(toList())));
+    return this;
+  }
+
+  default ValueBuilder put(String key, boolean... value) {
+    List<Value<?>> values = new ArrayList<>(value.length);
+    for (boolean val : value) {
+      values.add(Value.of(val));
+    }
+    put(key, Value.of(values));
     return this;
   }
 }
