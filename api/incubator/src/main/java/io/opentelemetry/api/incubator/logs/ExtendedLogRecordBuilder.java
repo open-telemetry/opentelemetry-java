b1ee53b5c3a4ef4c81809806924b28ef75c9a27d
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-01-14 14:51:25 -0800

    javadoc

--- api/incubator/src/main/java/io/opentelemetry/api/incubator/logs/ExtendedLogRecordBuilder.java
+++ api/incubator/src/main/java/io/opentelemetry/api/incubator/logs/ExtendedLogRecordBuilder.java
@@ -12,5 +12,11 @@ public interface ExtendedLogRecordBuilder extends LogRecordBuilder {
 
   // keep this class even if it is empty, since experimental methods may be added in the future.
 
+  /**
+   * Sets the event name, which identifies the class / type of the Event.
+   *
+   * <p>This name should uniquely identify the event structure (both attributes and body). A log
+   * record with a non-empty event name is an Event.
+   */
   ExtendedLogRecordBuilder setEventName(String eventName);
 }
