d91412b0c86581b0ce3133ac63c5332bfeeabbc5
Yuriy Holinko <yuriy.holinko@gmail.com>
2025-01-18 02:18:15 +0100

    added check against null for default predicate

--- sdk/common/src/main/java/io/opentelemetry/sdk/common/export/RetryPolicy.java
+++ sdk/common/src/main/java/io/opentelemetry/sdk/common/export/RetryPolicy.java
@@ -46,8 +46,7 @@ public abstract class RetryPolicy {
         .setMaxAttempts(DEFAULT_MAX_ATTEMPTS)
         .setInitialBackoff(Duration.ofSeconds(DEFAULT_INITIAL_BACKOFF_SECONDS))
         .setMaxBackoff(Duration.ofSeconds(DEFAULT_MAX_BACKOFF_SECONDS))
-        .setBackoffMultiplier(DEFAULT_BACKOFF_MULTIPLIER)
-        .setRetryExceptionPredicate(null);
+        .setBackoffMultiplier(DEFAULT_BACKOFF_MULTIPLIER);
   }
 
   /**
