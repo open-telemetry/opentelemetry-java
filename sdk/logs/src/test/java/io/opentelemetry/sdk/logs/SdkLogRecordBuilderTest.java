14a85889d4c8b92c1a3af3575d6ecd00703d8cfa
Jason Plumb <jplumb@splunk.com>
2025-02-07 14:26:13 -0800

    add some helpful logging attribute methods

--- sdk/logs/src/test/java/io/opentelemetry/sdk/logs/SdkLogRecordBuilderTest.java
+++ sdk/logs/src/test/java/io/opentelemetry/sdk/logs/SdkLogRecordBuilderTest.java
@@ -5,7 +5,13 @@
 
 package io.opentelemetry.sdk.logs;
 
+import static io.opentelemetry.api.common.AttributeKey.booleanKey;
+import static io.opentelemetry.api.common.AttributeKey.doubleKey;
+import static io.opentelemetry.api.common.AttributeKey.longKey;
+import static io.opentelemetry.api.common.AttributeKey.stringArrayKey;
+import static io.opentelemetry.api.common.AttributeKey.stringKey;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat;
+import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.equalTo;
 import static org.mockito.Mockito.when;
 
 import io.opentelemetry.api.common.AttributeKey;
@@ -21,6 +27,8 @@ import io.opentelemetry.sdk.common.Clock;
 import io.opentelemetry.sdk.common.InstrumentationScopeInfo;
 import io.opentelemetry.sdk.resources.Resource;
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import org.junit.jupiter.api.BeforeEach;
@@ -77,7 +85,7 @@ class SdkLogRecordBuilderTest {
     builder.setTimestamp(timestamp);
     builder.setObservedTimestamp(456, TimeUnit.SECONDS);
     builder.setObservedTimestamp(observedTimestamp);
-    builder.setAttribute(null, null);
+    builder.setAttribute((String)null, (String)null);
     builder.setAttribute(AttributeKey.stringKey("k1"), "v1");
     builder.setAllAttributes(Attributes.builder().put("k2", "v2").put("k3", "v3").build());
     builder.setContext(Span.wrap(spanContext).storeInContext(Context.root()));
@@ -115,5 +123,26 @@ class SdkLogRecordBuilderTest {
         .hasAttributes(Attributes.empty())
         .hasSpanContext(SpanContext.getInvalid())
         .hasSeverity(Severity.UNDEFINED_SEVERITY_NUMBER);
+  }
+
+  @Test
+  void testConvenienceAttributeMethods(){
+    List<String> valueList = Arrays.asList("foo", "bar", "baz");
+    builder.setAttribute("foo", "bar")
+        .setAttribute("lk", 12L)
+        .setAttribute("dk", 12.123)
+        .setAttribute("bk", true)
+        .setAttribute("ik", 13)
+        .setAttribute("list", valueList)
+        .emit();
+    assertThat(emittedLog.get().toLogRecordData())
+        .hasAttributesSatisfyingExactly(
+            equalTo(stringKey("foo"), "bar"),
+            equalTo(longKey("lk"), 12L),
+            equalTo(doubleKey("dk"), 12.123),
+            equalTo(booleanKey("bk"), true),
+            equalTo(longKey("ik"), 13L),
+            equalTo(stringArrayKey("list"), valueList)
+        );
   }
 }
