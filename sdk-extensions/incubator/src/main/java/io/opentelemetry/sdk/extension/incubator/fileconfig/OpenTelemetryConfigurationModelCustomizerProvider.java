e1be59765ad2d5965e341c5cfd38309065d4b594
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-02-18 14:56:08 +0100

    add config model customizer

--- sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/OpenTelemetryConfigurationModelCustomizerProvider.java
+++ sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/OpenTelemetryConfigurationModelCustomizerProvider.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.sdk.extension.incubator.fileconfig;
 
 import io.opentelemetry.sdk.autoconfigure.spi.Ordered;
