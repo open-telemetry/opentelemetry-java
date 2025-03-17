0af474e5421b8e7d687eb450af0f5df2f8a67bb0
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-03-17 15:32:13 +0100

    Update sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/OpenTelemetryConfigurationModelCustomizerProvider.java
    Co-authored-by: jack-berg <34418638+jack-berg@users.noreply.github.com>

--- sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/OpenTelemetryConfigurationModelCustomizerProvider.java
+++ sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/OpenTelemetryConfigurationModelCustomizerProvider.java
@@ -8,6 +8,6 @@ package io.opentelemetry.sdk.extension.incubator.fileconfig;
 import io.opentelemetry.sdk.autoconfigure.spi.Ordered;
 import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.OpenTelemetryConfigurationModel;
 
-public interface OpenTelemetryConfigurationModelCustomizerProvider extends Ordered {
+public interface DeclarativeConfigurationCustomizerProvider extends Ordered {
   OpenTelemetryConfigurationModel customize(OpenTelemetryConfigurationModel model);
 }
