c0856997c4f8cb661ab61e6045c3f9177eaaf897
Jack Berg <jberg@newrelic.com>
2025-03-27 10:51:42 -0500

    Fix declarative config xray todo

--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -83,7 +83,7 @@ val DEPENDENCIES = listOf(
   "eu.rekawek.toxiproxy:toxiproxy-java:2.1.7",
   "io.github.netmikey.logunit:logunit-jul:2.0.0",
   "io.jaegertracing:jaeger-client:1.8.1",
-  "io.opentelemetry.contrib:opentelemetry-aws-xray-propagator:1.39.0-alpha",
+  "io.opentelemetry.contrib:opentelemetry-aws-xray-propagator:1.45.0-alpha",
   "io.opentelemetry.semconv:opentelemetry-semconv-incubating:1.30.0-alpha",
   "io.opentelemetry.proto:opentelemetry-proto:1.5.0-alpha",
   "io.opentracing:opentracing-api:0.33.0",
