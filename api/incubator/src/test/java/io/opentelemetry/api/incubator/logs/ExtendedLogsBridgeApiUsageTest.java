251ef99d0f32f198d73e90d8cd44fe629b2edbc4
Jack Berg <jberg@newrelic.com>
2025-04-11 10:42:03 -0500

    Add context and severity params to ExtendedLogger#isEnabled

--- api/incubator/src/test/java/io/opentelemetry/api/incubator/logs/ExtendedLogsBridgeApiUsageTest.java
+++ api/incubator/src/test/java/io/opentelemetry/api/incubator/logs/ExtendedLogsBridgeApiUsageTest.java
@@ -10,6 +10,7 @@ import static io.opentelemetry.sdk.logs.internal.LoggerConfig.disabled;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat;
 
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.logs.Severity;
 import io.opentelemetry.sdk.logs.SdkLoggerProvider;
 import io.opentelemetry.sdk.logs.SdkLoggerProviderBuilder;
 import io.opentelemetry.sdk.logs.export.SimpleLogRecordProcessor;
@@ -42,24 +43,26 @@ class ExtendedLogsBridgeApiUsageTest {
     ExtendedLogger loggerB = (ExtendedLogger) loggerProvider.get("loggerB");
 
     // Check if logger is enabled before emitting log and avoid unnecessary computation
-    if (loggerA.isEnabled()) {
+    if (loggerA.isEnabled(Severity.INFO)) {
       loggerA
           .logRecordBuilder()
+          .setSeverity(Severity.INFO)
           .setBody("hello world!")
           .setAllAttributes(Attributes.builder().put("result", flipCoin()).build())
           .emit();
     }
-    if (loggerB.isEnabled()) {
+    if (loggerB.isEnabled(Severity.INFO)) {
       loggerB
           .logRecordBuilder()
+          .setSeverity(Severity.INFO)
           .setBody("hello world!")
           .setAllAttributes(Attributes.builder().put("result", flipCoin()).build())
           .emit();
     }
 
     // loggerA is enabled, loggerB is disabled
-    assertThat(loggerA.isEnabled()).isTrue();
-    assertThat(loggerB.isEnabled()).isFalse();
+    assertThat(loggerA.isEnabled(Severity.INFO)).isTrue();
+    assertThat(loggerB.isEnabled(Severity.INFO)).isFalse();
 
     // Collected data only consists of logs from loggerA. Note, loggerB's logs would be
     // omitted from the results even if logs were emitted. The check if enabled simply avoids
