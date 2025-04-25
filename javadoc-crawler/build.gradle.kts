f59ac04880cd4cff2ba89e44830da002ae623257
Jay DeLuca <jaydeluca4@gmail.com>
2025-04-24 21:39:48 -0400

    Javadoc site crawler

---
+++ javadoc-crawler/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  id("otel.java-conventions")
+}
+
+dependencies {
+  implementation("com.fasterxml.jackson.core:jackson-databind")
+  testImplementation("org.assertj:assertj-core:3.27.3")
+}
+
+description = "OpenTelemetry Javadoc Crawler"
+otelJava.moduleName.set("io.opentelemetry.javadocs")
+
+tasks {
+  withType<JavaCompile>().configureEach {
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
+    options.release.set(17)
+  }
+
+  val crawl by registering(JavaExec::class) {
+    dependsOn(classes)
+
+    mainClass.set("io.opentelemetry.javadocs.JavaDocsCrawler")
+    classpath(sourceSets["main"].runtimeClasspath)
+  }
+}
