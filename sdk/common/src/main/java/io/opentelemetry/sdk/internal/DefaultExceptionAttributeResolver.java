7408c0855c55d245481b09ebf531accee5ac3a8b
Jack Berg <jberg@newrelic.com>
2025-06-05 18:02:44 -0500

    Add property to render stacktrace using jvm default

--- sdk/common/src/main/java/io/opentelemetry/sdk/internal/DefaultExceptionAttributeResolver.java
+++ sdk/common/src/main/java/io/opentelemetry/sdk/internal/DefaultExceptionAttributeResolver.java
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.sdk.internal;
 
+import io.opentelemetry.api.internal.ConfigUtil;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -15,10 +16,19 @@ import java.io.StringWriter;
  */
 public final class DefaultExceptionAttributeResolver implements ExceptionAttributeResolver {
 
-  private static final DefaultExceptionAttributeResolver INSTANCE =
-      new DefaultExceptionAttributeResolver();
+  private static final String ENABLE_JVM_STACKTRACE_PROPERTY =
+      "otel.experimental.sdk.jvm_stacktrace";
 
-  private DefaultExceptionAttributeResolver() {}
+  private static final DefaultExceptionAttributeResolver INSTANCE =
+      new DefaultExceptionAttributeResolver(
+          Boolean.parseBoolean(ConfigUtil.getString(ENABLE_JVM_STACKTRACE_PROPERTY, "false")));
+
+  private final boolean jvmStacktraceEnabled;
+
+  // Visible for testing
+  DefaultExceptionAttributeResolver(boolean jvmStacktraceEnabled) {
+    this.jvmStacktraceEnabled = jvmStacktraceEnabled;
+  }
 
   public static ExceptionAttributeResolver getInstance() {
     return INSTANCE;
@@ -37,14 +47,23 @@ public final class DefaultExceptionAttributeResolver implements ExceptionAttribu
       attributeSetter.setAttribute(ExceptionAttributeResolver.EXCEPTION_MESSAGE, exceptionMessage);
     }
 
+    String exceptionStacktrace =
+        jvmStacktraceEnabled
+            ? jvmStacktrace(throwable)
+            : limitsAwareStacktrace(throwable, maxAttributeLength);
+    attributeSetter.setAttribute(
+        ExceptionAttributeResolver.EXCEPTION_STACKTRACE, exceptionStacktrace);
+  }
+
+  private static String jvmStacktrace(Throwable throwable) {
     StringWriter stringWriter = new StringWriter();
     try (PrintWriter printWriter = new PrintWriter(stringWriter)) {
       throwable.printStackTrace(printWriter);
     }
-    String exceptionStacktrace = stringWriter.toString();
-    if (exceptionStacktrace != null) {
-      attributeSetter.setAttribute(
-          ExceptionAttributeResolver.EXCEPTION_STACKTRACE, exceptionStacktrace);
-    }
+    return stringWriter.toString();
+  }
+
+  private static String limitsAwareStacktrace(Throwable throwable, int maxAttributeLength) {
+    return new StackTraceRenderer(throwable, maxAttributeLength).render();
   }
 }
