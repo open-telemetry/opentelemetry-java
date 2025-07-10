eef3d1cc71d4f59e342233374113ff39dff3f6d6
Josh Suereth <joshuasuereth@google.com>
2025-07-10 12:44:57 -0400

    Remove methods we don't need yet.

--- api/incubator/src/main/java/io/opentelemetry/api/incubator/ExtendedOpenTelemetry.java
+++ api/incubator/src/main/java/io/opentelemetry/api/incubator/ExtendedOpenTelemetry.java
@@ -6,7 +6,6 @@
 package io.opentelemetry.api.incubator;
 
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.incubator.entities.Resource;
 import io.opentelemetry.api.incubator.entities.ResourceProvider;
 
 /** Extension to {@link OpenTelemetry} that adds {@link ResourceProvider}. */
@@ -14,10 +13,5 @@ public interface ExtendedOpenTelemetry extends OpenTelemetry {
   /** Returns the {@link ResourceProvider} for this {@link OpenTelemetry}. */
   default ResourceProvider getResourceProvider() {
     return ResourceProvider.noop();
-  }
-
-  /** Returns the {@link Resource} that telemetry from this {@link OpenTelemetry} uses. */
-  default Resource getResource() {
-    return getResourceProvider().getResource();
   }
 }
