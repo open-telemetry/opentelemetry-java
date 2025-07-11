48016b0dc8f45cb8ceeb964c4a6b6a8b468498f8
Josh Suereth <joshuasuereth@google.com>
2025-07-10 22:15:01 -0400

    Add more tests for ResourceProvider.

--- sdk-extensions/incubator/src/test/java/io/opentelemetry/sdk/extension/incubator/entities/TestResourceProvider.java
+++ sdk-extensions/incubator/src/test/java/io/opentelemetry/sdk/extension/incubator/entities/TestResourceProvider.java
@@ -8,6 +8,7 @@ package io.opentelemetry.sdk.extension.incubator.entities;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.sdk.resources.internal.EntityUtil;
 import org.junit.jupiter.api.Test;
 
@@ -28,5 +29,98 @@ class TestResourceProvider {
     assertThat(EntityUtil.getEntities(provider.getSdkResource()))
         .satisfiesExactlyInAnyOrder(
             e -> assertThat(e).hasType("service"), e -> assertThat(e).hasType("telemetry.sdk"));
+  }
+
+  @Test
+  void Resource_updatesDescription() {
+    SdkResourceProvider provider = SdkResourceProvider.builder().includeDefaults(false).build();
+
+    provider
+        .getResource()
+        .attachEntity("one")
+        .setSchemaUrl("one")
+        .withId(Attributes.builder().put("one.id", 1).build())
+        .emit();
+
+    provider
+        .getResource()
+        .attachEntity("one")
+        .setSchemaUrl("one")
+        .withId(Attributes.builder().put("one.id", 1).build())
+        .withDescription(Attributes.builder().put("one.desc", "desc").build())
+        .emit();
+
+    assertThat(provider.getSdkResource().getAttributes())
+        .hasSize(2)
+        .containsKey("one.id")
+        .containsKey("one.desc");
+  }
+
+  @Test
+  void Resource_ignoresNewIds() {
+    SdkResourceProvider provider = SdkResourceProvider.builder().includeDefaults(false).build();
+
+    provider
+        .getResource()
+        .attachEntity("one")
+        .setSchemaUrl("one")
+        .withId(Attributes.builder().put("one.id", 1).build())
+        .emit();
+
+    provider
+        .getResource()
+        .attachEntity("one")
+        .setSchemaUrl("one")
+        .withId(Attributes.builder().put("one.id", 2).build())
+        .withDescription(Attributes.builder().put("one.desc", "desc").build())
+        .emit();
+
+    assertThat(provider.getSdkResource().getAttributes()).hasSize(1).containsKey("one.id");
+  }
+
+  @Test
+  void Resource_ignoresNewSchemaUrl() {
+    SdkResourceProvider provider = SdkResourceProvider.builder().includeDefaults(false).build();
+
+    provider
+        .getResource()
+        .attachEntity("one")
+        .setSchemaUrl("one")
+        .withId(Attributes.builder().put("one.id", 1).build())
+        .emit();
+
+    provider
+        .getResource()
+        .attachEntity("one")
+        .setSchemaUrl("two")
+        .withId(Attributes.builder().put("one.id", 1).build())
+        .withDescription(Attributes.builder().put("one.desc", "desc").build())
+        .emit();
+
+    assertThat(provider.getSdkResource().getAttributes()).hasSize(1).containsKey("one.id");
+  }
+
+  @Test
+  void Resource_addsNewEntity() {
+    SdkResourceProvider provider = SdkResourceProvider.builder().includeDefaults(false).build();
+
+    provider
+        .getResource()
+        .attachEntity("one")
+        .setSchemaUrl("one")
+        .withId(Attributes.builder().put("one.id", 1).build())
+        .emit();
+
+    provider
+        .getResource()
+        .attachEntity("two")
+        .setSchemaUrl("two")
+        .withId(Attributes.builder().put("two.id", 2).build())
+        .emit();
+
+    assertThat(provider.getSdkResource().getAttributes())
+        .hasSize(2)
+        .containsKey("one.id")
+        .containsKey("two.id");
   }
 }
