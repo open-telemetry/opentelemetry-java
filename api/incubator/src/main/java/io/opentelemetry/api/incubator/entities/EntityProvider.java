634c4a37e847c52fefaf0a25d21b39d3fdd5c1fb
Josh Suereth <joshuasuereth@google.com>
2025-07-11 13:46:32 -0400

    Remove assertjs helpers, update javadoc.

--- api/incubator/src/main/java/io/opentelemetry/api/incubator/entities/EntityProvider.java
+++ api/incubator/src/main/java/io/opentelemetry/api/incubator/entities/EntityProvider.java
@@ -6,15 +6,13 @@
 package io.opentelemetry.api.incubator.entities;
 
 /**
- * A registry for interacting with {@link Resource}s. The name <i>Provider</i> is for consistency
+ * A registry for interacting with {@code Resource}s. The name <i>Provider</i> is for consistency
  * with other languages and it is <b>NOT</b> loaded using reflection.
- *
- * @see Resource
  */
 public interface EntityProvider {
   /**
-   * Returns a no-op {@link EntityProvider} which only creates no-op {@link Resource}s which do not
-   * record nor are emitted.
+   * Returns a no-op {@link EntityProvider} which only creates no-op {@link EntityBuilder}s which do
+   * not record nor are emitted.
    */
   static EntityProvider noop() {
     return NoopEntityProvider.INSTANCE;
