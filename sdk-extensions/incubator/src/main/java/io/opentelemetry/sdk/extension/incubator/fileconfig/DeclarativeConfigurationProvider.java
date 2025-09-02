593162e1191f4f230399e7a7d2cadf45d1729513
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-09-02 15:11:45 +0200

    add spi to load a declarative config model

--- sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/DeclarativeConfigurationProvider.java
+++ sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/DeclarativeConfigurationProvider.java
@@ -8,7 +8,7 @@ package io.opentelemetry.sdk.extension.incubator.fileconfig;
 import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.OpenTelemetryConfigurationModel;
 import javax.annotation.Nullable;
 
-/** A service provider interface (SPI) for providing a declarative configuration model */
+/** A service provider interface (SPI) for providing a declarative configuration model. */
 public interface DeclarativeConfigurationProvider {
   /**
    * Returns an OpenTelemetry configuration model to be used when configuring the SDK, or {@code
