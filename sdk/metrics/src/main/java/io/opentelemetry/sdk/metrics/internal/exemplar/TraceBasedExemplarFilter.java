01e218173e0daf9af0d7287096ad69b3483a2080
Jason Plumb <jplumb@splunk.com>
2025-09-03 10:05:56 -0700

    remove internal/experimental wording around exemplar stuff

--- sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/internal/exemplar/TraceBasedExemplarFilter.java
+++ sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/internal/exemplar/TraceBasedExemplarFilter.java
@@ -12,8 +12,6 @@ import io.opentelemetry.context.Context;
 /**
  * Exemplar sampler that only samples measurements with associated sampled traces.
  *
- * <p>This class is internal and is hence not for public use. Its APIs are unstable and can change
- * at any time.
  */
 public final class TraceBasedExemplarFilter implements ExemplarFilter {
 
