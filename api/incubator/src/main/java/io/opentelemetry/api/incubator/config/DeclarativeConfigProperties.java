0eacadca6fcee67dfed4a09776649b9daf778519
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-12-17 18:32:15 +0100

    Update api/incubator/src/main/java/io/opentelemetry/api/incubator/config/DeclarativeConfigProperties.java
    Co-authored-by: Jack Berg <34418638+jack-berg@users.noreply.github.com>

--- api/incubator/src/main/java/io/opentelemetry/api/incubator/config/DeclarativeConfigProperties.java
+++ api/incubator/src/main/java/io/opentelemetry/api/incubator/config/DeclarativeConfigProperties.java
@@ -202,7 +202,9 @@ public interface DeclarativeConfigProperties {
   }
 
   /**
-   * Returns a {@link DeclarativeConfigProperties} configuration property.
+   * Returns a {@link DeclarativeConfigProperties} configuration property, or {@link #empty()} if a property with {@code name} has not been configured.
+   * 
+   * <p>This is syntactic sugar for the common operation of calling {@code config.getStructured(name, empty())}. If you need to distinguish between a property being set but empty vs. not set, use {@link getStructured(String)}.
    *
    * @return a map-valued configuration property, or an empty {@link DeclarativeConfigProperties}
    *     instance if {@code name} has not been configured
