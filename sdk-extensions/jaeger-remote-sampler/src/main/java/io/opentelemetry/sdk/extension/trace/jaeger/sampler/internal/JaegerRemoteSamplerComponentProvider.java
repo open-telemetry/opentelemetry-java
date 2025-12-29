2c387e14b7e29af4fab2ff37cada678ad7ee5272
Jack Berg <johnmberg8@gmail.com>
2025-12-29 11:49:23 -0600

    Update declaratvie config jaeger remote sampler to require endpoint, initial_sampler

--- sdk-extensions/jaeger-remote-sampler/src/main/java/io/opentelemetry/sdk/extension/trace/jaeger/sampler/internal/JaegerRemoteSamplerComponentProvider.java
+++ sdk-extensions/jaeger-remote-sampler/src/main/java/io/opentelemetry/sdk/extension/trace/jaeger/sampler/internal/JaegerRemoteSamplerComponentProvider.java
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.sdk.extension.trace.jaeger.sampler.internal;
 
+import io.opentelemetry.api.incubator.config.DeclarativeConfigException;
 import io.opentelemetry.api.incubator.config.DeclarativeConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.internal.ComponentProvider;
 import io.opentelemetry.sdk.extension.incubator.fileconfig.DeclarativeConfiguration;
@@ -34,19 +35,21 @@ public class JaegerRemoteSamplerComponentProvider implements ComponentProvider {
   public Sampler create(DeclarativeConfigProperties config) {
     JaegerRemoteSamplerBuilder builder = JaegerRemoteSampler.builder();
 
-    // Optional configuration
     String endpoint = config.getString("endpoint");
-    if (endpoint != null) {
-      builder.setEndpoint(endpoint);
+    if (endpoint == null) {
+      throw new DeclarativeConfigException("jaeger remote sampler endpoint is required");
     }
+    builder.setEndpoint(endpoint);
+
+    DeclarativeConfigProperties initialSamplerModel = config.getStructured("initial_sampler");
+    if (initialSamplerModel == null) {
+      throw new DeclarativeConfigException("jaeger remote sampler initial_sampler is required");
+    }
+    builder.setInitialSampler(DeclarativeConfiguration.createSampler(initialSamplerModel));
+
     Long pollingIntervalMs = config.getLong("internal");
     if (pollingIntervalMs != null) {
       builder.setPollingInterval(Duration.ofMillis(pollingIntervalMs));
-    }
-    DeclarativeConfigProperties initialSamplerModel = config.getStructured("initial_sampler");
-    if (initialSamplerModel != null) {
-      Sampler initialSampler = DeclarativeConfiguration.createSampler(initialSamplerModel);
-      builder.setInitialSampler(initialSampler);
     }
 
     return builder.build();
