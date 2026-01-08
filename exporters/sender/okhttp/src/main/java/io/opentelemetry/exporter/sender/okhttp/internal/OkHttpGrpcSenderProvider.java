4a4757ed137631d511b5386437a638ff124f0bda
Jack Berg <johnmberg8@gmail.com>
2026-01-08 16:56:33 -0600

    Fix build

--- exporters/sender/okhttp/src/main/java/io/opentelemetry/exporter/sender/okhttp/internal/OkHttpGrpcSenderProvider.java
+++ exporters/sender/okhttp/src/main/java/io/opentelemetry/exporter/sender/okhttp/internal/OkHttpGrpcSenderProvider.java
@@ -22,7 +22,7 @@ public class OkHttpGrpcSenderProvider implements GrpcSenderProvider {
     return new OkHttpGrpcSender(
         grpcSenderConfig
             .getEndpoint()
-            .resolve(grpcSenderConfig.getServiceAndMethodName())
+            .resolve("/" + grpcSenderConfig.getServiceAndMethodName())
             .toString(),
         grpcSenderConfig.getCompressor(),
         grpcSenderConfig.getTimeoutNanos(),
