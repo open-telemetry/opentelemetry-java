e2cfc086dc81767b6a0c21807c333da2142f9ab8
Jack Berg <johnmberg8@gmail.com>
2026-01-09 11:35:00 -0600

    spot

--- exporters/sender/okhttp/src/test/java/io/opentelemetry/exporter/sender/okhttp/internal/UriResolveTest.java
+++ exporters/sender/okhttp/src/test/java/io/opentelemetry/exporter/sender/okhttp/internal/UriResolveTest.java
@@ -1,14 +1,20 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.exporter.sender.okhttp.internal;
 
-import org.junit.jupiter.api.Test;
-import java.net.URI;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.net.URI;
+import org.junit.jupiter.api.Test;
 
 class UriResolveTest {
 
   @Test
   void resolve() {
-    assertThat(URI.create("http://localhost:8080").resolve("path").toString()).isEqualTo("http://localhost:8080/path");
+    assertThat(URI.create("http://localhost:8080").resolve("path").toString())
+        .isEqualTo("http://localhost:8080/path");
   }
 }
