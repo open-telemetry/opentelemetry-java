c0e641d60b270979447f3129cfbabaad9f324b14
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2026-03-13 17:49:45 +0100

    fix for composable rule based sampler

--- sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/ComposableRuleBasedSamplerFactory.java
+++ sdk-extensions/incubator/src/main/java/io/opentelemetry/sdk/extension/incubator/fileconfig/ComposableRuleBasedSamplerFactory.java
@@ -100,10 +100,16 @@ final class ComposableRuleBasedSamplerFactory
     if (attributePatternsModel == null) {
       return null;
     }
+    List<String> included = attributePatternsModel.getIncluded();
+    // empty list is provided when field is omitted, but should be treated as null as empty include
+    // list will exclude all
+    if (included != null && included.isEmpty()) {
+      included = null;
+    }
     return new AttributeMatcher(
         requireNonNull(attributePatternsModel.getKey(), "attribute_patterns key"),
         IncludeExcludePredicate.createPatternMatching(
-            attributePatternsModel.getIncluded(), attributePatternsModel.getExcluded()));
+            included, attributePatternsModel.getExcluded()));
   }
 
   // Visible for testing
