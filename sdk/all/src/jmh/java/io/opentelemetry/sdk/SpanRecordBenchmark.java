4da3e71626367e760a01c63ba48a012c186aaf3d
Jack Berg <34418638+jack-berg@users.noreply.github.com>
2026-04-20 09:26:15 -0500

    Tune benchmark params to reduce variance

--- sdk/all/src/jmh/java/io/opentelemetry/sdk/SpanRecordBenchmark.java
+++ sdk/all/src/jmh/java/io/opentelemetry/sdk/SpanRecordBenchmark.java
@@ -123,9 +123,9 @@ public class SpanRecordBenchmark {
   @Benchmark
   @Group("threads1")
   @GroupThreads(1)
-  @Fork(1)
-  @Warmup(iterations = 5, time = 1)
-  @Measurement(iterations = 5, time = 1)
+  @Fork(3)
+  @Warmup(iterations = 3, time = 1)
+  @Measurement(iterations = 10, time = 1)
   @OperationsPerInvocation(RECORDS_PER_INVOCATION)
   public void record_SingleThread(BenchmarkState benchmarkState) {
     record(benchmarkState);
@@ -134,9 +134,9 @@ public class SpanRecordBenchmark {
   @Benchmark
   @Group("threads" + MAX_THREADS)
   @GroupThreads(MAX_THREADS)
-  @Fork(1)
-  @Warmup(iterations = 5, time = 1)
-  @Measurement(iterations = 5, time = 1)
+  @Fork(3)
+  @Warmup(iterations = 3, time = 1)
+  @Measurement(iterations = 10, time = 1)
   @OperationsPerInvocation(RECORDS_PER_INVOCATION)
   public void record_MultipleThreads(BenchmarkState benchmarkState) {
     record(benchmarkState);
