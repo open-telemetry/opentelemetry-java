1087f5fb36b56d406eee4b4d46c67bbcdc67089f
Jack Berg <34418638+jack-berg@users.noreply.github.com>
2026-05-19 15:50:10 -0500

    Add unit test codifying that view names need to conform to metric nam… (#8412)

--- exporters/otlp/all/build.gradle.kts
+++ exporters/otlp/all/build.gradle.kts
@@ -10,8 +10,8 @@ apply<io.opentelemetry.gradle.OtelVersionClassPlugin>()
 description = "OpenTelemetry Protocol (OTLP) Exporters"
 otelJava.moduleName.set("io.opentelemetry.exporter.otlp")
 otelJava.osgiOptionalPackages.set(listOf("io.opentelemetry.api.incubator.config"))
-// io.grpc is not an OSGi bundle and has no package versioning; must use unversioned optional.
-otelJava.osgiUnversionedOptionalPackages.set(listOf("io.grpc"))
+// io.grpc and org.jspecify.annotations are not OSGi bundles; must use unversioned optional.
+otelJava.osgiUnversionedOptionalPackages.set(listOf("io.grpc", "org.jspecify.annotations"))
 base.archivesName.set("opentelemetry-exporter-otlp")
 
 dependencies {
