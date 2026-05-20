4c3817adf26540580401b9bde9bc36e666851b76
kyyril <khairilrahman55@gmail.com>
2026-05-21 06:57:51 +0700

    test(common): add test cases to ensure 100% coverage on AttributesMap

--- sdk/common/src/test/java/io/opentelemetry/sdk/common/internal/AttributesMapTest.java
+++ sdk/common/src/test/java/io/opentelemetry/sdk/common/internal/AttributesMapTest.java
@@ -11,6 +11,9 @@ import static io.opentelemetry.api.common.AttributeKey.stringKey;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
 
+import io.opentelemetry.api.common.AttributeKey;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class AttributesMapTest {
@@ -32,5 +35,26 @@ class AttributesMapTest {
     attributesMap.put(booleanKey("key"), false);
 
     assertThat(attributesMap.asMap()).containsOnly(entry(booleanKey("key"), false));
+  }
+
+  @Test
+  void put_lastValueWins_sameType() {
+    AttributesMap attributesMap = AttributesMap.create(10, Integer.MAX_VALUE);
+    attributesMap.put(stringKey("key"), "value1");
+    attributesMap.put(stringKey("key"), "value2");
+
+    assertThat(attributesMap.asMap()).containsOnly(entry(stringKey("key"), "value2"));
+  }
+
+  @Test
+  void putAll() {
+    AttributesMap attributesMap = AttributesMap.create(10, Integer.MAX_VALUE);
+    Map<AttributeKey<?>, Object> newAttrs = new HashMap<>();
+    newAttrs.put(stringKey("key1"), "value1");
+    newAttrs.put(booleanKey("key2"), true);
+    attributesMap.putAll(newAttrs);
+
+    assertThat(attributesMap.asMap())
+        .containsOnly(entry(stringKey("key1"), "value1"), entry(booleanKey("key2"), true));
   }
 }
