990f3dd4bd4aae48b8e5e23748d580f8e1e674e9
seongjun.ha <seongjun.ha@rapportlabs.kr>
2026-06-23 17:01:04 +0900

    fix: safely initialize exemplar reservoir storage

--- sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/internal/exemplar/FixedSizeExemplarReservoir.java
+++ sdk/metrics/src/main/java/io/opentelemetry/sdk/metrics/internal/exemplar/FixedSizeExemplarReservoir.java
@@ -20,10 +20,11 @@ import javax.annotation.Nullable;
 /** Base for fixed-size reservoir sampling of Exemplars. */
 class FixedSizeExemplarReservoir implements DoubleExemplarReservoir, LongExemplarReservoir {
 
-  @Nullable private ReservoirCell[] storage;
+  @Nullable private volatile ReservoirCell[] storage;
   private final ReservoirCellSelector reservoirCellSelector;
   private final int size;
   private final Clock clock;
+  private final Object storageLock = new Object();
   private volatile boolean hasMeasurements = false;
 
   /** Instantiates an exemplar reservoir of fixed size. */
@@ -36,12 +37,10 @@ class FixedSizeExemplarReservoir implements DoubleExemplarReservoir, LongExempla
 
   @Override
   public void offerLongMeasurement(long value, Attributes attributes, Context context) {
-    if (storage == null) {
-      storage = initStorage();
-    }
+    ReservoirCell[] storage = getOrInitStorage();
     int bucket = reservoirCellSelector.reservoirCellIndexFor(storage, value, attributes, context);
     if (bucket != -1) {
-      this.storage[bucket].recordLongMeasurement(value, attributes, context);
+      storage[bucket].recordLongMeasurement(value, attributes, context);
       this.hasMeasurements = true;
     }
   }
@@ -58,14 +57,26 @@ class FixedSizeExemplarReservoir implements DoubleExemplarReservoir, LongExempla
 
   @Override
   public void offerDoubleMeasurement(double value, Attributes attributes, Context context) {
-    if (storage == null) {
-      storage = initStorage();
-    }
+    ReservoirCell[] storage = getOrInitStorage();
     int bucket = reservoirCellSelector.reservoirCellIndexFor(storage, value, attributes, context);
     if (bucket != -1) {
-      this.storage[bucket].recordDoubleMeasurement(value, attributes, context);
+      storage[bucket].recordDoubleMeasurement(value, attributes, context);
       this.hasMeasurements = true;
     }
+  }
+
+  private ReservoirCell[] getOrInitStorage() {
+    ReservoirCell[] storage = this.storage;
+    if (storage == null) {
+      synchronized (storageLock) {
+        storage = this.storage;
+        if (storage == null) {
+          storage = initStorage();
+          this.storage = storage;
+        }
+      }
+    }
+    return storage;
   }
 
   private ReservoirCell[] initStorage() {
