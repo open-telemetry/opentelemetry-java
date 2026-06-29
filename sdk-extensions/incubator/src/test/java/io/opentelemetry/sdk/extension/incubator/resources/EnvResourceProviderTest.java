a92a7ba8b52ec8078df37c88aa2610d239bd60f0
Josh Suereth <joshuasuereth@google.com>
2026-06-29 14:09:14 +0000

    Flesh out tests.

--- sdk-extensions/incubator/src/test/java/io/opentelemetry/sdk/extension/incubator/resources/EnvResourceProviderTest.java
+++ sdk-extensions/incubator/src/test/java/io/opentelemetry/sdk/extension/incubator/resources/EnvResourceProviderTest.java
@@ -9,6 +9,7 @@ import static io.opentelemetry.api.common.AttributeKey.stringKey;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.incubator.config.DeclarativeConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.internal.DefaultConfigProperties;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.resources.internal.Entity;
@@ -30,11 +31,17 @@ class EnvResourceProviderTest {
   }
 
   @Test
-  void createResource_Empty() {
+  void createResource_EmptyOrNull() {
     EnvResourceProvider provider = new EnvResourceProvider();
-    Resource resource =
+
+    Resource emptyResource =
         provider.createResource(DefaultConfigProperties.createFromMap(Collections.emptyMap()));
-    assertThat(EntityUtil.getEntities(resource)).isEmpty();
+    assertThat(EntityUtil.getEntities(emptyResource)).isEmpty();
+
+    Resource blankResource =
+        provider.createResource(
+            DefaultConfigProperties.createFromMap(Collections.singletonMap("otel.entities", "")));
+    assertThat(EntityUtil.getEntities(blankResource)).isEmpty();
   }
 
   @Test
@@ -65,5 +72,62 @@ class EnvResourceProviderTest {
                 e.getType().equals("host")
                     && e.getSchemaUrl() == null
                     && e.getId().equals(Attributes.of(stringKey("host.id"), "myhost")));
+  }
+
+  @Test
+  void createResource_PercentDecoding() {
+    Map<String, String> props = new HashMap<>();
+    props.put(
+        "otel.entities",
+        "service{service.name=my+app,space=hello%20world,utf8=%C3%A9,invalid=%2G,incomplete=%2,end=%}");
+
+    EnvResourceProvider provider = new EnvResourceProvider();
+    Resource resource = provider.createResource(DefaultConfigProperties.createFromMap(props));
+
+    Collection<Entity> entities = EntityUtil.getEntities(resource);
+    assertThat(entities).hasSize(1);
+
+    Entity entity = entities.iterator().next();
+    assertThat(entity.getId())
+        .containsEntry(stringKey("service.name"), "my+app")
+        .containsEntry(stringKey("space"), "hello world")
+        .containsEntry(stringKey("utf8"), "é")
+        .containsEntry(stringKey("invalid"), "%2G")
+        .containsEntry(stringKey("incomplete"), "%2")
+        .containsEntry(stringKey("end"), "%");
+  }
+
+  @Test
+  void createResource_Malformed() {
+    Map<String, String> props = new HashMap<>();
+    props.put(
+        "otel.entities",
+        "{empty.type=val};process{};process{=val};process{key;=val};host{host.id=valid}");
+
+    EnvResourceProvider provider = new EnvResourceProvider();
+    Resource resource = provider.createResource(DefaultConfigProperties.createFromMap(props));
+
+    Collection<Entity> entities = EntityUtil.getEntities(resource);
+    // Only the last valid host entity should be parsed
+    assertThat(entities).hasSize(1);
+    assertThat(entities.iterator().next().getType()).isEqualTo("host");
+  }
+
+  @Test
+  void create_ComponentProvider() {
+    System.setProperty("otel.entities", "service{service.name=my-service}");
+    try {
+      EnvResourceProvider provider = new EnvResourceProvider();
+      Resource resource = provider.create(DeclarativeConfigProperties.empty());
+      Collection<Entity> entities = EntityUtil.getEntities(resource);
+      assertThat(entities).hasSize(1);
+      assertThat(entities)
+          .anyMatch(
+              e ->
+                  e.getType().equals("service")
+                      && e.getId().equals(Attributes.of(stringKey("service.name"), "my-service")));
+    } finally {
+      System.clearProperty("otel.entities");
+    }
   }
 }
