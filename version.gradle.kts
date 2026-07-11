4c0102759c7e5a015d8ec45b5dcb32f6d43d38b2
Trask Stalnaker <trask.stalnaker@gmail.com>
2026-07-10 20:26:19 -0700

    Pin japicmp baseline with apidiff snapshots

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,4 +1,5 @@
 val snapshot = true
+val apidiffBaselineVersion = "1.63.0"
 
 allprojects {
   var ver = "1.65.0"
@@ -10,4 +11,5 @@ allprojects {
     ver += "-SNAPSHOT"
   }
   version = ver
+  extra["apidiffBaselineVersion"] = apidiffBaselineVersion
 }
