50f24af4be564e988d464c8472eb3ebe4e44e1a9
Rajkaran Yadav <yadavrajkaran854@gmail.com>
2026-08-02 17:38:29 +0530

    docs: document legacy metrics

--- exporters/logging-otlp/src/main/java/io/opentelemetry/exporter/logging/otlp/OtlpJsonLoggingMetricExporter.java
+++ exporters/logging-otlp/src/main/java/io/opentelemetry/exporter/logging/otlp/OtlpJsonLoggingMetricExporter.java
@@ -18,6 +18,9 @@ import java.util.logging.Logger;
 /**
  * A {@link MetricExporter} which writes {@linkplain MetricData metrics} to a {@link Logger} in OTLP
  * JSON format. Each log line will include a single {@code ResourceMetrics}.
+ *
+ * <p>Note: This class is superseded by {@code OtlpStdoutMetricExporter}, which allows configuring
+ * a custom logger or output stream.
  */
 public final class OtlpJsonLoggingMetricExporter implements MetricExporter {
 
