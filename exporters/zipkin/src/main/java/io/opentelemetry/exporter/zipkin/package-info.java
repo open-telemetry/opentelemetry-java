19d95abe44e18a7941767a59f9c5523000539ee2
Jack Berg <34418638+jack-berg@users.noreply.github.com>
2026-08-03 13:52:26 -0500

    Delete opentelemetry-exporter-zipkin (#8677)

--- exporters/zipkin/src/main/java/io/opentelemetry/exporter/zipkin/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright The OpenTelemetry Authors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-@ParametersAreNonnullByDefault
-package io.opentelemetry.exporter.zipkin;
-
-import javax.annotation.ParametersAreNonnullByDefault;
