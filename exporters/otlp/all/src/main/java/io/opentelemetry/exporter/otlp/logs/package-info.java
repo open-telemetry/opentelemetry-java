76bfd94ab00a803cbdd6ed06c3758e81e4a0d815
Eunbin Son <58901024+thswlsqls@users.noreply.github.com>
2026-08-20 23:13:09 +0900

    Fix signal name in OTLP gRPC logs package javadoc (#8737)

--- exporters/otlp/all/src/main/java/io/opentelemetry/exporter/otlp/logs/package-info.java
+++ exporters/otlp/all/src/main/java/io/opentelemetry/exporter/otlp/logs/package-info.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/** OpenTelemetry exporter which sends span data to OpenTelemetry collector via OTLP gRPC. */
+/** OpenTelemetry exporter which sends log data to OpenTelemetry collector via OTLP gRPC. */
 @ParametersAreNonnullByDefault
 package io.opentelemetry.exporter.otlp.logs;
 
