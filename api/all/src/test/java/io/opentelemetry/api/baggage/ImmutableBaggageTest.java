35636aec8d3dc6706bb483bad92383fdd6012af0
Naman Agrawal <144216560+itsmehotpants@users.noreply.github.com>
2026-08-22 08:02:26 +0530

    Fix BaggageBuilder.put() silently accepting empty string keys (#8660)
    Co-authored-by: itsmehotpants <itsmehotpants@users.noreply.github.com>
    Co-authored-by: Jack Berg <34418638+jack-berg@users.noreply.github.com>

--- api/all/src/test/java/io/opentelemetry/api/baggage/ImmutableBaggageTest.java
+++ api/all/src/test/java/io/opentelemetry/api/baggage/ImmutableBaggageTest.java
@@ -114,10 +114,17 @@ class ImmutableBaggageTest {
   @Test
   void put_keyEmpty() {
     BaggageBuilder builder = ONE_ENTRY.toBuilder();
+    Baggage built = builder.build();
     builder.put("", "value");
-    Baggage build = builder.build();
-    assertThat(build.getEntryValue("")).isNull();
-    assertThat(build).isEqualTo(ONE_ENTRY);
+    assertThat(builder.build()).isEqualTo(built);
+  }
+
+  @Test
+  void put_keyEmpty_withMetadata() {
+    BaggageBuilder builder = ONE_ENTRY.toBuilder();
+    Baggage built = builder.build();
+    builder.put("", "value", TMD);
+    assertThat(builder.build()).isEqualTo(built);
   }
 
   @Test
